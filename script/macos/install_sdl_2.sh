1aecb6d025d6fc05aee5522c73f236958477958b
Ihar Hubchyk <ihar.hubchyk@thalesdigital.io>
2019-09-13 10:02:53 +0800

    Add scripts and description for project compilation

---
+++ script/macos/install_sdl_2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Uninstall SDL 1.2
+brew remove sdl_image
+brew remove sdl_mixer
+brew remove sdl_ttf
+brew remove sdl
+
+# Install SDL 2
+brew install sdl2
+brew install sdl2_ttf
+brew install sdl2_mixer
+brew install sdl2_image
