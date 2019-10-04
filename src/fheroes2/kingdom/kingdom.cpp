5c827474a9c35a2aa1389eaa32bf1a5780dea310
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2019-10-04 21:33:58 +0800

    Code formatting

--- src/fheroes2/kingdom/kingdom.cpp
+++ src/fheroes2/kingdom/kingdom.cpp
@@ -809,15 +809,14 @@ StreamBase & operator<< (StreamBase & msg, const Kingdoms & obj)
 
 StreamBase & operator>> (StreamBase & msg, Kingdoms & obj)
 {
-    u32 kingdomscount;
-    msg >> kingdomscount; 
-	
-    if (kingdomscount <= KINGDOMMAX)
-    {
-        for(u32 ii = 0; ii < kingdomscount; ++ii)
-	msg >> obj.kingdoms[ii];
+    u32 kingdomscount = 0;
+    msg >> kingdomscount;
+
+    if (kingdomscount < KINGDOMMAX) {
+        for (u32 i = 0; i < kingdomscount; ++i)
+            msg >> obj.kingdoms[i];
     }
-       
+
     return msg;
 }
 
