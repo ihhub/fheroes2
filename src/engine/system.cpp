2e2d8a5643d10c32fbbb2af935f6ce3399c7c964
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2020-01-23 10:45:18 +0800

    Check existence of directory

--- src/engine/system.cpp
+++ src/engine/system.cpp
@@ -346,7 +346,7 @@ bool System::IsFile(const std::string & name, bool writable)
 bool System::IsDirectory(const std::string & name, bool writable)
 {
 #if defined(_MSC_VER)
-    return writable ? ( 0 == _access(name.c_str(), 06) ) : true;
+    return writable ? ( 0 == _access(name.c_str(), 06) ) : ( 0 == _access(name.c_str(), 00) );
 #elif defined(ANDROID)
     return writable ? 0 == access(name.c_str(), W_OK) : true;
 #else
