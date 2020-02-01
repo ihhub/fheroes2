39dbce4a85e3c780a95f8b64640532b341c1dd3e
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2020-02-01 12:11:50 +0800

    Code style fix

--- src/engine/display.cpp
+++ src/engine/display.cpp
@@ -283,7 +283,7 @@ Surface Display::GetSurface( const Rect & rt ) const
 bool Display::isMouseFocusActive() const
 {
 #if SDL_VERSION_ATLEAST( 2, 0, 0 )
-    return (SDL_GetWindowFlags(window) & SDL_WINDOW_MOUSE_FOCUS) == SDL_WINDOW_MOUSE_FOCUS;
+    return ( SDL_GetWindowFlags( window ) & SDL_WINDOW_MOUSE_FOCUS ) == SDL_WINDOW_MOUSE_FOCUS;
 #else
     return ( SDL_GetAppState() & SDL_APPMOUSEFOCUS ) == SDL_APPMOUSEFOCUS;
 #endif
