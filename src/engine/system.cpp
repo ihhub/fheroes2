3f71952adf38d9176bfa7239f8b9b3f0b1e30959
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2020-05-04 08:51:55 +0800

    Remove unneeded const modificator

--- src/engine/system.cpp
+++ src/engine/system.cpp
@@ -509,7 +509,7 @@ int System::GetRenderFlags( void )
 
 #if !( defined( _MSC_VER ) || defined( __MINGW32CE__ ) || defined( __MINGW32__ ) )
 // splitUnixPath - function for splitting strings by delimiter
-const std::vector<std::string> splitUnixPath( const std::string & path, const std::string & delimiter )
+std::vector<std::string> splitUnixPath( const std::string & path, const std::string & delimiter )
 {
     std::vector<std::string> result;
 
