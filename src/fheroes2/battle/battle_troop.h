c5390ee9ed5f4772b90e20558f7fe657372ab984
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2020-05-16 14:08:19 +0800

    Fix compilation

--- src/fheroes2/battle/battle_troop.h
+++ src/fheroes2/battle/battle_troop.h
@@ -206,10 +206,11 @@ namespace Battle
         ModesAffected affected;
         Unit * mirror;
 
-        std::map<int, Surface> contoursMain;
-        std::map<int, Surface> contoursReflect;
-        std::map<int, Surface> contoursWB; // white black, really get contour for stunned unit?
-        std::map<int, Surface> contoursWBReflect; // white black reflect, really get contour for stunned unit?
+        // These variables are mutable due to population of them of the fly as we don't want to calculate everything
+        mutable std::map<int, Surface> contoursMain;
+        mutable std::map<int, Surface> contoursReflect;
+        mutable std::map<int, Surface> contoursWB; // white black, really get contour for stunned unit?
+        mutable std::map<int, Surface> contoursWBReflect; // white black reflect, really get contour for stunned unit?
         const Surface & getContour( const int frameId, std::map<int, Surface> & contours, const bool isReflected, const bool isBlackWhite ) const;
 
         bool blindanswer;
