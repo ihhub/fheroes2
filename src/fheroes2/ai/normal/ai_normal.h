a59b8982022a1555d22cb0b6c8ccfd2981a3fa61
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2020-05-17 12:49:01 +0800

    Add ifdef guard

--- src/fheroes2/ai/normal/ai_normal.h
+++ src/fheroes2/ai/normal/ai_normal.h
@@ -18,6 +18,9 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 
+#ifndef H2AI_NORMAL_H
+#define H2AI_NORMAL_H
+
 #include "ai.h"
 
 namespace AI
@@ -28,3 +31,5 @@ namespace AI
         Normal();
     };
 }
+
+#endif
