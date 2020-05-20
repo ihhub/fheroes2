942d8f139d310c0a4c96c9590d1d9978566389ca
Ivan Shibanov <idshibanov@users.noreply.github.com>
2020-05-19 22:38:00 -0400

    Fix hero, castle and status army info (#549)
    close #203

--- src/fheroes2/army/army.h
+++ src/fheroes2/army/army.h
@@ -139,7 +139,7 @@ public:
 
     static void DrawMons32Line( const Troops &, s32, s32, u32, u32 = 0, u32 = 0 );
     static void DrawMons32LineWithScoute( const Troops &, s32, s32, u32, u32, u32, u32 );
-    static void DrawMons32LineShort( const Troops &, s32, s32, u32, u32, u32 );
+    static void DrawMonsterLines( const Troops & troops, s32 posX, s32 posY, u32 lineWidth, u32 scout, bool compact = true );
 
     Army( HeroBase * s = NULL );
     Army( const Maps::Tiles & );
