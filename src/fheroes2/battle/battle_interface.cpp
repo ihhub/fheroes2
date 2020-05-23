4a1efe2af448f95f8462c1dccb12073b9b7963c2
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2020-05-23 12:20:04 +0800

    Use shadowing for end movement cell
    relates to #357

--- src/fheroes2/battle/battle_interface.cpp
+++ src/fheroes2/battle/battle_interface.cpp
@@ -207,7 +207,7 @@ Surface DrawHexagon( const RGBA & color )
     return sf;
 }
 
-Surface DrawHexagonShadow( void )
+Surface DrawHexagonShadow( int alphaValue )
 {
     int l, w, h;
 
@@ -223,7 +223,7 @@ Surface DrawHexagonShadow( void )
     }
 
     Surface sf( Size( w, h ), 32, true );
-    RGBA shadow = RGBA( 0, 0, 0, 0x30 );
+    const RGBA shadow = RGBA( 0, 0, 0, alphaValue );
     Rect rt( 0, l - 1, w + 1, 2 * l + 3 );
     for ( int i = 0; i < w / 2; i += 2 ) {
         --rt.y;
@@ -904,8 +904,8 @@ Battle::Interface::Interface( Arena & a, s32 center )
 
     // hexagon
     sf_hexagon = DrawHexagon( ( light ? RGBA( 0x78, 0x94, 0 ) : RGBA( 0x38, 0x48, 0 ) ) );
-    sf_cursor = DrawHexagon( RGBA( 0xb0, 0x0c, 0 ) );
-    sf_shadow = DrawHexagonShadow();
+    sf_cursor = DrawHexagonShadow( 0x60 );
+    sf_shadow = DrawHexagonShadow( 0x30 );
 
     // buttons
     const Rect & area = border.GetArea();
