67b3b709f0685e9b00282d753e74843b330383b9
Pavel <53114202+shprotru@users.noreply.github.com>
2020-06-11 09:33:21 +0300

    Add F4 fullscreen hotkey support (#627)
    close #605

--- src/fheroes2/game/game_hotkeys.cpp
+++ src/fheroes2/game/game_hotkeys.cpp
@@ -342,8 +342,12 @@ void Game::KeyboardGlobalFilter( int sym, int mod )
     Display & display = Display::Get();
 
     // system hotkeys
-    if ( sym == key_events[EVENT_SYSTEM_FULLSCREEN] )
+    if ( sym == key_events[EVENT_SYSTEM_FULLSCREEN] && !( ( mod & KMOD_ALT ) || ( mod & KMOD_CTRL ) ) ) {
+        Cursor::Get().Hide();
         display.ToggleFullScreen();
+        Cursor::Get().Show();
+        display.Flip();
+    }
     else if ( sym == key_events[EVENT_SYSTEM_SCREENSHOT] ) {
         std::ostringstream stream;
         stream << System::ConcatePath( Settings::GetSaveDir(), "screenshot_" ) << std::time( 0 );
