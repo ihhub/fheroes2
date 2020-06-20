31a17e0584d77f563aeae61f32a4fb4da64440b4
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2020-06-21 00:30:40 +0800

    Fix incorrect drawings for many dialogs
    relates to #818
    partially fixes #532

--- src/fheroes2/dialog/dialog_box.cpp
+++ src/fheroes2/dialog/dialog_box.cpp
@@ -35,10 +35,10 @@
 #define BOXE_MIDDLE 45
 #define BOXE_BOTTOM 81
 #define BOXAREA_TOP 35
-#define BOXAREA_MIDDLE 10
+#define BOXAREA_MIDDLE 35
 #define BOXAREA_BOTTOM 35
 
-void BoxRedraw( s32 posx, s32 posy, u32 count );
+void BoxRedraw( s32 posx, s32 posy, u32 count, uint32_t middleHeight );
 
 Dialog::NonFixedFrameBox::NonFixedFrameBox( int height, int startYPos, bool showButtons )
 {
@@ -47,7 +47,7 @@ Dialog::NonFixedFrameBox::NonFixedFrameBox( int height, int startYPos, bool show
 
     const bool evil = Settings::Get().ExtGameEvilInterface();
     const u32 count_middle = ( height <= BOXAREA_TOP + BOXAREA_BOTTOM ? 0 : 1 + ( height - BOXAREA_TOP - BOXAREA_BOTTOM ) / BOXAREA_MIDDLE );
-    const u32 height_middle = count_middle * BOXAREA_MIDDLE;
+    const u32 height_middle = height <= BOXAREA_TOP + BOXAREA_BOTTOM ? 0 : height - BOXAREA_TOP - BOXAREA_BOTTOM;
     const u32 height_top_bottom = ( evil ? BOXE_TOP + BOXE_BOTTOM : BOX_TOP + BOX_BOTTOM );
 
     area.w = BOXAREA_WIDTH;
@@ -68,7 +68,7 @@ Dialog::NonFixedFrameBox::NonFixedFrameBox( int height, int startYPos, bool show
     area.x = posx + 36;
     area.y = posy + ( evil ? BOXE_TOP - BOXAREA_TOP : BOX_TOP - BOXAREA_TOP );
 
-    BoxRedraw( posx, posy, count_middle );
+    BoxRedraw( posx, posy, count_middle, height_middle );
 }
 
 Dialog::NonFixedFrameBox::~NonFixedFrameBox()
@@ -92,7 +92,7 @@ Dialog::FrameBox::FrameBox( int height, bool buttons )
 
 Dialog::FrameBox::~FrameBox() {}
 
-void BoxRedraw( s32 posx, s32 posy, u32 count )
+void BoxRedraw( s32 posx, s32 posy, u32 count, uint32_t middleHeight )
 {
     const int buybuild = Settings::Get().ExtGameEvilInterface() ? ICN::BUYBUILE : ICN::BUYBUILD;
 
@@ -107,6 +107,7 @@ void BoxRedraw( s32 posx, s32 posy, u32 count )
     AGG::GetICN( buybuild, 0 ).Blit( pt );
 
     pt.y += AGG::GetICN( buybuild, 4 ).h();
+    const int16_t posBeforeMiddle = pt.y;
     for ( u32 i = 0; i < count; ++i ) {
         // left middle sprite
         pt.x = posx;
@@ -121,6 +122,8 @@ void BoxRedraw( s32 posx, s32 posy, u32 count )
         sr.Blit( Rect( 0, 10, sr.w(), BOXAREA_MIDDLE ), pt );
         pt.y += BOXAREA_MIDDLE;
     }
+
+    pt.y = posBeforeMiddle + middleHeight;
 
     // right bottom sprite
     AGG::GetICN( buybuild, 2 ).Blit( pt );
