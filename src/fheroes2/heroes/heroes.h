cb696228e5e9622735b4cd823f1952105b569a99
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2020-07-10 00:28:29 +0800

    Correct spelling

--- src/fheroes2/heroes/heroes.h
+++ src/fheroes2/heroes/heroes.h
@@ -317,8 +317,8 @@ private:
     bool MoveStep( bool fast = false );
     static void MoveStep( Heroes &, s32 from, s32 to, bool newpos );
 
-    // This function is useful only in situation when AI hero moves out of fog
-    // we don't update his direction during movement under fog so ther is a situation
+    // This function is useful only in a situation when AI hero moves out of fog
+    // we don't update his direction during movement under fog so there is a situation
     // when initial hero's sprite is set incorrectly. This function fixes it
     void SetValidDirectionSprite();
 
