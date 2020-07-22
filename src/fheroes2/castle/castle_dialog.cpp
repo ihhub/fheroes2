b8eeaa222afc0875a01ff08e08c30c5c65d46b78
Ivan Shibanov <idshibanov@users.noreply.github.com>
2020-07-22 04:32:42 -0400

    Add boat building animation (#1100)
    close #984

--- src/fheroes2/castle/castle_dialog.cpp
+++ src/fheroes2/castle/castle_dialog.cpp
@@ -155,57 +155,29 @@ void RedrawIcons( const Castle & castle, const CastleHeroes & heroes, const Poin
     const Heroes * hero1 = heroes.Guard();
     const Heroes * hero2 = heroes.Guest();
 
-    if ( Settings::Get().QVGA() ) {
-        AGG::GetICN( ICN::SWAPWIN, 0 ).Blit( Rect( 36, 267, 43, 43 ), pt.x + 2, pt.y + 79 );
-        AGG::GetICN( ICN::SWAPWIN, 0 ).Blit( Rect( 36, 267, 43, 43 ), pt.x + 2, pt.y + 124 );
+    AGG::GetICN( ICN::STRIP, 0 ).Blit( pt.x, pt.y + 256 );
 
-        Surface icon1, icon2;
+    Surface icon1, icon2;
 
-        if ( hero1 )
-            icon1 = hero1->GetPortrait( PORT_MEDIUM );
-        else if ( castle.isBuild( BUILD_CAPTAIN ) )
-            icon1 = castle.GetCaptain().GetPortrait( PORT_MEDIUM );
-        else
-            icon1 = AGG::GetICN( ICN::BRCREST, Color::GetIndex( castle.GetColor() ) );
+    if ( hero1 )
+        icon1 = hero1->GetPortrait( PORT_BIG );
+    else if ( castle.isBuild( BUILD_CAPTAIN ) )
+        icon1 = castle.GetCaptain().GetPortrait( PORT_BIG );
+    else
+        icon1 = AGG::GetICN( ICN::CREST, Color::GetIndex( castle.GetColor() ) );
 
-        if ( hero2 )
-            icon2 = hero2->GetPortrait( PORT_MEDIUM );
-        else
-            icon2 = AGG::GetICN( ICN::BRCREST, Color::GetIndex( castle.GetColor() ) );
+    if ( hero2 )
+        icon2 = hero2->GetPortrait( PORT_BIG );
+    else
+        icon2 = AGG::GetICN( ICN::STRIP, 3 );
 
-        if ( icon1.isValid() )
-            icon1.Blit( Rect( ( icon1.w() - 41 ) / 2, ( icon1.h() - 41 ) / 2, 41, 41 ), pt.x + 3, pt.y + 80, display );
-        if ( icon2.isValid() )
-            icon2.Blit( Rect( ( icon2.w() - 41 ) / 2, ( icon2.h() - 41 ) / 2, 41, 41 ), pt.x + 3, pt.y + 125, display );
+    if ( icon1.isValid() )
+        icon1.Blit( pt.x + 5, pt.y + 262, display );
+    if ( icon2.isValid() )
+        icon2.Blit( pt.x + 5, pt.y + 361, display );
 
-        if ( !hero2 )
-            AGG::GetICN( ICN::STONEBAK, 0 ).Blit( Rect( 0, 0, 223, 53 ), pt.x + 47, pt.y + 124 );
-    }
-    else {
-        AGG::GetICN( ICN::STRIP, 0 ).Blit( pt.x, pt.y + 256 );
-
-        Surface icon1, icon2;
-
-        if ( hero1 )
-            icon1 = hero1->GetPortrait( PORT_BIG );
-        else if ( castle.isBuild( BUILD_CAPTAIN ) )
-            icon1 = castle.GetCaptain().GetPortrait( PORT_BIG );
-        else
-            icon1 = AGG::GetICN( ICN::CREST, Color::GetIndex( castle.GetColor() ) );
-
-        if ( hero2 )
-            icon2 = hero2->GetPortrait( PORT_BIG );
-        else
-            icon2 = AGG::GetICN( ICN::STRIP, 3 );
-
-        if ( icon1.isValid() )
-            icon1.Blit( pt.x + 5, pt.y + 262, display );
-        if ( icon2.isValid() )
-            icon2.Blit( pt.x + 5, pt.y + 361, display );
-
-        if ( !hero2 )
-            AGG::GetICN( ICN::STRIP, 11 ).Blit( pt.x + 112, pt.y + 361 );
-    }
+    if ( !hero2 )
+        AGG::GetICN( ICN::STRIP, 11 ).Blit( pt.x + 112, pt.y + 361 );
 }
 
 Surface GetMeetingSprite( void )
@@ -568,6 +540,28 @@ int Castle::OpenDialog( bool readonly, bool fade )
                         case BUILD_SHIPYARD:
                             if ( Dialog::OK == Dialog::BuyBoat( AllowBuyBoat() ) ) {
                                 BuyBoat();
+
+                                int alpha = 1;
+                                uint32_t buildFrame = 0;
+                                const int boatICN = GetICNBoat( GetRace() );
+                                while ( le.HandleEvents() && alpha < 255 ) {
+                                    if ( Game::AnimateInfrequentDelay( Game::CASTLE_BUILD_DELAY ) ) {
+                                        cursor.Hide();
+
+                                        Sprite shipyardSprite = AGG::GetICN( boatICN, 0 );
+                                        shipyardSprite.SetAlphaMod( alpha, true );
+                                        shipyardSprite.Blit( cur_pt.x + shipyardSprite.x(), cur_pt.y + shipyardSprite.y() );
+                                        Sprite boatSprite = AGG::GetICN( boatICN, 1 );
+                                        boatSprite.SetAlphaMod( alpha, true );
+                                        boatSprite.Blit( cur_pt.x + boatSprite.x(), cur_pt.y + boatSprite.y() );
+
+                                        cursor.Show();
+                                        display.Flip();
+                                        alpha += 15;
+                                    }
+                                    ++buildFrame;
+                                }
+
                                 need_redraw = true;
                             }
                             break;
