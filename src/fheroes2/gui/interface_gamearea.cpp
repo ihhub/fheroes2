bd84ff53b7d6f3633612b85e59d7b3481f2642c0
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2020-07-22 10:32:58 +0800

    ID cannot be negative

--- src/fheroes2/gui/interface_gamearea.cpp
+++ src/fheroes2/gui/interface_gamearea.cpp
@@ -184,13 +184,13 @@ void Interface::GameArea::Redraw( Surface & dst, int flag ) const
             const std::pair<int, int> monsterIndicies = Maps::Tiles::GetMonsterSpriteIndices( world.GetTiles( removalInfo.tile ), removalInfo.index );
 
             // base monster sprite
-            if ( monsterIndicies.first != -1 ) {
+            if ( monsterIndicies.first >= 0 ) {
                 Sprite sprite = AGG::GetICN( ICN::MINIMON, monsterIndicies.first );
                 sprite.SetAlphaMod( removalInfo.alpha, false );
                 BlitOnTile( dst, sprite, sprite.x() + 16, sprite.y() + TILEWIDTH, mp );
             }
             // animated monster part
-            if ( monsterIndicies.second != -1 ) {
+            if ( monsterIndicies.second >= 0 ) {
                 Sprite sprite = AGG::GetICN( ICN::MINIMON, monsterIndicies.second );
                 sprite.SetAlphaMod( removalInfo.alpha, false );
                 BlitOnTile( dst, sprite, sprite.x() + 16, sprite.y() + TILEWIDTH, mp );
