ihhub/fheroes2

commit bd38d3f61d13640062267c631f198e79cac86146

Add boat summoning animation (#1106)
close #1043

src/fheroes2/gui/interface_gamearea.cpp
@@ -174,31 +174,36 @@ void Interface::GameArea::Redraw( Surface & dst, int flag ) const
         }
     }
 
-    // remove animation
-    Game::RemoveAnimation::Info & removalInfo = Game::RemoveAnimation::Get();
-    if ( removalInfo.object != MP2::OBJ_ZERO ) {
-        const Point mp = Maps::GetPoint( removalInfo.tile );
-        const int icn = MP2::GetICNObject( removalInfo.object );
+    // object fade in/fade out animation
+    Game::ObjectFadeAnimation::Info & fadeInfo = Game::ObjectFadeAnimation::Get();
+    if ( fadeInfo.object != MP2::OBJ_ZERO ) {
+        const Point mp = Maps::GetPoint( fadeInfo.tile );
+        const int icn = MP2::GetICNObject( fadeInfo.object );
 
         if ( icn == ICN::MONS32 ) {
-            const std::pair<int, int> monsterIndicies = Maps::Tiles::GetMonsterSpriteIndices( world.GetTiles( removalInfo.tile ), removalInfo.index );
+            const std::pair<int, int> monsterIndicies = Maps::Tiles::GetMonsterSpriteIndices( world.GetTiles( fadeInfo.tile ), fadeInfo.index );
 
             // base monster sprite
             if ( monsterIndicies.first >= 0 ) {
                 Sprite sprite = AGG::GetICN( ICN::MINIMON, monsterIndicies.first );
-                sprite.SetAlphaMod( removalInfo.alpha, false );
+                sprite.SetAlphaMod( fadeInfo.alpha, false );
                 BlitOnTile( dst, sprite, sprite.x() + 16, sprite.y() + TILEWIDTH, mp );
             }
             // animated monster part
             if ( monsterIndicies.second >= 0 ) {
                 Sprite sprite = AGG::GetICN( ICN::MINIMON, monsterIndicies.second );
-                sprite.SetAlphaMod( removalInfo.alpha, false );
+                sprite.SetAlphaMod( fadeInfo.alpha, false );
                 BlitOnTile( dst, sprite, sprite.x() + 16, sprite.y() + TILEWIDTH, mp );
             }
         }
+        else if ( fadeInfo.object == MP2::OBJ_BOAT ) {
+            Sprite sprite = AGG::GetICN( ICN::BOAT32, fadeInfo.index );
+            sprite.SetAlphaMod( fadeInfo.alpha, false );
+            BlitOnTile( dst, sprite, sprite.x(), sprite.y() + TILEWIDTH, mp );
+        }
         else {
-            Sprite sprite = AGG::GetICN( icn, removalInfo.index );
-            sprite.SetAlphaMod( removalInfo.alpha, false );
+            Sprite sprite = AGG::GetICN( icn, fadeInfo.index );
+            sprite.SetAlphaMod( fadeInfo.alpha, false );
             BlitOnTile( dst, sprite, 0, 0, mp );
         }
     }

src/fheroes2/game/game.cpp
@@ -70,20 +70,22 @@ namespace Game
     int save_version = CURRENT_FORMAT_VERSION;
     std::vector<int> reserved_vols( LOOPXX_COUNT, 0 );
 
-    namespace RemoveAnimation
+    namespace ObjectFadeAnimation
     {
         Info::Info()
             : object( MP2::OBJ_ZERO )
             , index( 0 )
             , tile( 0 )
             , alpha( 255 )
+            , isFadeOut( true )
         {}
 
-        Info::Info( u8 object_, u8 index_, s32 tile_, u32 alpha_ )
+        Info::Info( u8 object_, u8 index_, s32 tile_, u32 alpha_, bool fadeOut )
             : object( object_ )
             , tile( tile_ )
             , alpha( alpha_ )
             , surfaceSize( world.GetTiles( tile_ ).GetTileSurface().GetSize() )
+            , isFadeOut( fadeOut )
         {
             index = ICN::AnimationFrame( MP2::GetICNObject( object ), index_, 0 );
             if ( 0 == index ) {
@@ -201,12 +203,12 @@ void Game::SetCurrentMusic( int mus )
     current_music = mus;
 }
 
-void Game::RemoveAnimation::Set( const Info & info )
+void Game::ObjectFadeAnimation::Set( const Info & info )
 {
     removeInfo = info;
 }
 
-Game::RemoveAnimation::Info & Game::RemoveAnimation::Get()
+Game::ObjectFadeAnimation::Info & Game::ObjectFadeAnimation::Get()
 {
     return removeInfo;
 }

src/fheroes2/game/game.h
@@ -254,18 +254,19 @@ namespace Game
     void OpenCastleDialog( Castle & );
     std::string GetEncodeString( const std::string & );
 
-    namespace RemoveAnimation
+    namespace ObjectFadeAnimation
     {
         struct Info
         {
             Info();
-            Info( u8 object_, u8 index_, s32 tile_, u32 alpha_ = 255u );
+            Info( u8 object_, u8 index_, s32 tile_, u32 alpha_ = 255u, bool fadeOut = true );
 
             u8 object;
             u8 index;
             s32 tile;
             u32 alpha;
             Size surfaceSize;
+            bool isFadeOut;
         };
 
         void Set( const Info & info );

src/fheroes2/game/game_startgame.cpp
@@ -985,13 +985,17 @@ int Interface::Basic::HumanTurn( bool isload )
         }
 
         if ( Game::AnimateInfrequentDelay( Game::HEROES_PICKUP_DELAY ) ) {
-            Game::RemoveAnimation::Info & removalInfo = Game::RemoveAnimation::Get();
-            if ( removalInfo.object != MP2::OBJ_ZERO ) {
-                if ( removalInfo.alpha < 20 ) {
-                    removalInfo.object = MP2::OBJ_ZERO;
+            Game::ObjectFadeAnimation::Info & fadeInfo = Game::ObjectFadeAnimation::Get();
+            if ( fadeInfo.object != MP2::OBJ_ZERO ) {
+                if ( fadeInfo.isFadeOut && fadeInfo.alpha < 20 ) {
+                    fadeInfo.object = MP2::OBJ_ZERO;
+                }
+                else if ( !fadeInfo.isFadeOut && fadeInfo.alpha > 235 ) {
+                    world.GetTiles( fadeInfo.tile ).SetObject( fadeInfo.object );
+                    fadeInfo.object = MP2::OBJ_ZERO;
                 }
                 else {
-                    removalInfo.alpha -= 20;
+                    fadeInfo.alpha += ( fadeInfo.isFadeOut ) ? -20 : 20;
                 }
                 gameArea.SetRedraw();
             }

src/fheroes2/heroes/heroes_action.cpp
@@ -285,7 +285,7 @@ void AnimationRemoveObject( Maps::Tiles & tile )
     if ( NULL == addon )
         return;
 
-    Game::RemoveAnimation::Set( Game::RemoveAnimation::Info( addon->object, addon->index, tile.GetIndex() ) );
+    Game::ObjectFadeAnimation::Set( Game::ObjectFadeAnimation::Info( addon->object, addon->index, tile.GetIndex() ) );
 }
 
 void RecruitMonsterFromTile( Heroes & hero, Maps::Tiles & tile, const std::string & msg, const Troop & troop, bool remove )

src/fheroes2/heroes/heroes_spell.cpp
@@ -24,6 +24,7 @@
 #include "castle.h"
 #include "cursor.h"
 #include "dialog.h"
+#include "game.h"
 #include "game_interface.h"
 #include "heroes.h"
 #include "interface_list.h"
@@ -329,7 +330,7 @@ bool ActionSpellSummonBoat( Heroes & hero )
         if ( Maps::isValidAbsIndex( boat ) ) {
             if ( Rand::Get( 1, 100 ) <= chance ) {
                 world.GetTiles( boat ).SetObject( MP2::OBJ_ZERO );
-                world.GetTiles( dst_water ).SetObject( MP2::OBJ_BOAT );
+                Game::ObjectFadeAnimation::Set( Game::ObjectFadeAnimation::Info( MP2::OBJ_BOAT, 18, dst_water, 0, false ) );
                 return true;
             }
             break;

commit ef036ea522838d7069624adabab8229f03c8bbe7

Fix regression introduced in #1106

src/fheroes2/gui/interface_gamearea.cpp
@@ -204,7 +204,7 @@ void Interface::GameArea::Redraw( Surface & dst, int flag ) const
         else {
             Sprite sprite = AGG::GetICN( icn, fadeInfo.index );
             sprite.SetAlphaMod( fadeInfo.alpha, false );
-            BlitOnTile( dst, sprite, 0, 0, mp );
+            BlitOnTile( dst, sprite, sprite.x(), sprite.y(), mp );
         }
     }
 