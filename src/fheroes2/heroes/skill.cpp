4398feb5080e8b28d1eca3d8dbf620f4ac836b2b
shprotru <shprot@mail.ua>
2020-07-31 20:56:59 +0300

    I1168 moved to switch similar to the skill wisdom

--- src/fheroes2/heroes/skill.cpp
+++ src/fheroes2/heroes/skill.cpp
@@ -439,13 +439,18 @@ std::string Skill::Secondary::GetDescription( void ) const
 
     switch ( Skill() ) {
     case PATHFINDING:
-        if ( this->Level() >= Level::EXPERT ) {
-            str = _( GetName() + std::string( " eliminates the movement penalty for rough terrain." ) );
-        }
-        else {
+        switch ( Level() ) {
+        case Level::BASIC:
+        case Level::ADVANCED: {
             const std::string tmpDescription = std::string( std::string( GetName() ) + std::string( " reduces the movement penalty for rough terrain by %{count} percent." ) );
             str = _n( tmpDescription.c_str(),
                       tmpDescription.c_str(), count );
+        } break;
+        case Level::EXPERT:
+            str = _( GetName() + std::string( " eliminates the movement penalty for rough terrain." ) );
+            break;
+        default:
+            break;
         }
         break;
     case ARCHERY:
