7b005ba53a9e534306be31f01d9409e6364c31b1
ghostBot <figey@yandex.com>
2020-08-04 16:53:57 +0000

    Fix crash if player has not select castle

--- src/fheroes2/heroes/heroes_spell.cpp
+++ src/fheroes2/heroes/heroes_spell.cpp
@@ -543,7 +543,8 @@ bool ActionSpellTownPortal( Heroes & hero )
                 display.Flip();
             }
         }
-        teleportDestination = listBox.GetCurrent();
+        if ( listBox.isSelected() )
+            teleportDestination = listBox.GetCurrent();
     }
 
     // restore settings
@@ -560,7 +561,7 @@ bool ActionSpellTownPortal( Heroes & hero )
     I.Redraw();
     display.Flip();
 
-    if ( result == Dialog::OK )
+    if ( result == Dialog::OK && teleportDestination != -1 )
         return HeroesTownGate( hero, world.GetCastle( Maps::GetPoint( teleportDestination ) ) );
 
     // restore the original focus
