685be36e9aa07c16879efb2dc9a50e2d444f6137
Ivan Shibanov <shibanov.ivan@gmail.com>
2020-08-04 18:57:58 -0400

    Move AI selection call

--- src/fheroes2/ai/ai_common.cpp
+++ src/fheroes2/ai/ai_common.cpp
@@ -20,10 +20,29 @@
 
 #include "ai.h"
 #include "castle.h"
+#include "empty/ai_empty.h"
 #include "kingdom.h"
+#include "normal/ai_normal.h"
+#include "simple/ai_simple.h"
 
 namespace AI
 {
+    // AI Selector here
+    Base & Get( AI_TYPE type )
+    {
+        static AI::Empty empty;
+        static AI::Simple simple;
+        static AI::Normal normal;
+
+        switch ( type ) {
+        case AI::SIMPLE:
+            return simple;
+        case AI::NORMAL:
+            return normal;
+        }
+        return normal;
+    }
+
     bool BuildIfAvailable( Castle & castle, int building )
     {
         if ( !castle.isBuild( building ) )
