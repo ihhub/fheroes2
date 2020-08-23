b7ab96a5faa1cac0d15e7937bb1a46488cfd0a2e
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2020-08-23 19:53:24 +0800

    Fix cursor in SDL 1

--- src/fheroes2/game/fheroes2.cpp
+++ src/fheroes2/game/fheroes2.cpp
@@ -153,7 +153,7 @@ int main( int argc, char ** argv )
             fheroes2::engine().setTitle( GetCaption() );
 
             // display.SetVideoMode( conf.VideoMode().w, conf.VideoMode().h, conf.FullScreen(), conf.KeepAspectRatio(), conf.ChangeFullscreenResolution() );
-            // display.HideCursor();
+            Display::Get().HideCursor();
             // display.SetCaption( GetCaption().c_str() );
 
             // Ensure the mouse position is updated to prevent bad initial values.
