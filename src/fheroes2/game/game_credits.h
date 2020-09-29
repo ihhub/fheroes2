df567c53febd964c4278fb04d2dc07aedc01c630
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2020-09-29 21:08:34 +0800

    Update to 0.8.2

--- src/fheroes2/game/game_credits.h
+++ src/fheroes2/game/game_credits.h
@@ -1,8 +1,6 @@
 /***************************************************************************
- *   Copyright (C) 2009 by Andrey Afletdinov <fheroes2@gmail.com>          *
- *                                                                         *
- *   Part of the Free Heroes2 Engine:                                      *
- *   http://sourceforge.net/projects/fheroes2                              *
+ *   Free Heroes of Might and Magic II: https://github.com/ihhub/fheroes2  *
+ *   Copyright (C) 2020                                                    *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
@@ -19,52 +17,10 @@
  *   Free Software Foundation, Inc.,                                       *
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
-#ifndef H2GAMEDEFS_H
-#define H2GAMEDEFS_H
 
-#include "engine.h"
+#pragma once
 
-#define MAJOR_VERSION 0
-#define MINOR_VERSION 8
-#define INTERMEDIATE_VERSION 2
-
-#ifdef WITH_XML
-#include "tinyxml.h"
-#endif
-
-#include "translations.h"
-#define _( s ) Translation::gettext( s )
-#define _n( a, b, c ) Translation::ngettext( a, b, c )
-
-// hardcore defines: kingdom
-#define KINGDOMMAX 6
-
-// hardcore defines: world
-#define MAXCASTLES 72
-#define DAYOFWEEK 7
-#define WEEKOFMONTH 4
-
-// hardcore defines: castle
-#define CASTLEMAXMONSTER 6
-
-// hardcore defines: heroes
-#define HEROESMAXARTIFACT 14
-#define HEROESMAXSKILL 8
-#define HEROESMAXCOUNT 71
-
-// hardcore defines: skill
-#define MAXPRIMARYSKILL 4
-#define MAXSECONDARYSKILL 14
-
-// hardcore defines: army
-#define ARMYMAXTROOPS 5
-
-// hardcore defines: interface
-#define RADARWIDTH 144
-#define BORDERWIDTH 16
-
-// ai/hero speed
-#define DEFAULT_SPEED_DELAY 5
-#define DEFAULT_BATTLE_SPEED 4
-
-#endif
+namespace Game
+{
+    void ShowCredits();
+}
