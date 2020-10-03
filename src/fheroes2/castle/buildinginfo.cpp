c33ca7359ccdc210c5307e48e88ff1a272453c09
shprotru <shprot@mail.ua>
2020-10-03 19:19:52 +0300

    I1850 ArmyInfo -> DwellingInfo for Towns/Castles Overview screen

--- src/fheroes2/castle/buildinginfo.cpp
+++ src/fheroes2/castle/buildinginfo.cpp
@@ -794,7 +794,7 @@ bool DwellingsBar::ActionBarSingleClick( const Point & cursor, DwellingItem & dw
 
 bool DwellingsBar::ActionBarPressRight( const Point & cursor, DwellingItem & dwl, const Rect & pos )
 {
-    Dialog::ArmyInfo( Troop( dwl.mons, castle.GetDwellingLivedCount( dwl.type ) ), 0 );
+    Dialog::DwellingInfo( dwl.mons, castle.GetDwellingLivedCount( dwl.type ) );
 
     return true;
 }
