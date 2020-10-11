9aac41cfdaf214e039a61854f599fa2d407d4b5f
Pavel <53114202+shprotru@users.noreply.github.com>
2020-10-11 16:16:05 +0300

    Battle doesn't stop, after AI kills the last creature (#1952)
    close #1948

--- src/fheroes2/battle/battle_arena.cpp
+++ src/fheroes2/battle/battle_arena.cpp
@@ -374,12 +374,14 @@ void Battle::Arena::TurnTroop( Unit * current_troop )
                 Force::UpdateOrderUnits( *army1, *army2, *armies_order );
 
             // check end battle
-            if ( !BattleValid() )
+            if ( !BattleValid() ) {
                 end_turn = true;
+                break;
+            }
 
             // good morale
-            if ( !end_turn && current_troop->isValid() && !current_troop->Modes( TR_SKIPMOVE ) && current_troop->Modes( TR_MOVED ) && current_troop->Modes( MORALE_GOOD )
-                 && BattleValid() ) {
+            if ( !end_turn && current_troop->isValid() && !current_troop->Modes( TR_SKIPMOVE ) && current_troop->Modes( TR_MOVED )
+                 && current_troop->Modes( MORALE_GOOD ) ) {
                 actions.push_back( Command( MSG_BATTLE_MORALE, current_troop->GetUID(), true ) );
                 end_turn = false;
             }
