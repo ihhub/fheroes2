e9df95fba6c4f908669add9e9cfce0580ad710e5
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2020-10-18 17:15:50 +0800

    Change main menu popup window descriptions (#2036)
    close #1925

--- src/fheroes2/dialog/dialog_file.cpp
+++ src/fheroes2/dialog/dialog_file.cpp
@@ -107,16 +107,19 @@ int Dialog::FileOptions( void )
             break;
         }
         else if ( le.MousePressRight( buttonNew.area() ) ) {
-            Dialog::Message( "", _( "Start a single or multi-player game." ), Font::BIG );
+            Dialog::Message( _( "New Game" ), _( "Start a single or multi-player game." ), Font::BIG );
         }
         else if ( le.MousePressRight( buttonLoad.area() ) ) {
-            Dialog::Message( "", _( "Load a previously saved game." ), Font::BIG );
+            Dialog::Message( _( "Load Game" ), _( "Load a previously saved game." ), Font::BIG );
         }
         else if ( le.MousePressRight( buttonSave.area() ) ) {
-            Dialog::Message( "", _( "Save the current game." ), Font::BIG );
+            Dialog::Message( _( "Save Game" ), _( "Save the current game." ), Font::BIG );
         }
         else if ( le.MousePressRight( buttonQuit.area() ) ) {
-            Dialog::Message( "", _( "Quit out of Heroes of Might and Magic II." ), Font::BIG );
+            Dialog::Message( _( "Quit" ), _( "Quit out of Free Heroes of Might and Magic II." ), Font::BIG );
+        }
+        else if ( le.MousePressRight( buttonCancel.area() ) ) {
+            Dialog::Message( _( "Cancel" ), _( "Exit this menu without doing anything." ), Font::BIG );
         }
     }
 
