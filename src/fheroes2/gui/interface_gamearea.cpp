c689fa828f4ea85e411d671cce83d4f583c51e90
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2020-10-25 17:45:23 +0800

    Small optimizations by caching some data
    a little faster

--- src/fheroes2/gui/interface_gamearea.cpp
+++ src/fheroes2/gui/interface_gamearea.cpp
@@ -142,53 +142,63 @@ void Interface::GameArea::Redraw( fheroes2::Image & dst, int flag, bool isPuzzle
 {
     const Rect tileROI = GetVisibleTileROI();
 
-    std::vector<std::pair<Point, const Heroes *> > heroList;
-
     // ground and bottom layer
+    const bool drawBottom = ( flag & LEVEL_BOTTOM ) == LEVEL_BOTTOM;
+
     for ( int16_t y = 0; y < tileROI.h; ++y ) {
         Point offset( 0, tileROI.y + y );
-        const bool isEmptyTile = offset.y < 0 || offset.y >= world.h();
-        for ( s32 x = 0; x < tileROI.w; ++x ) {
-            offset.x = tileROI.x + x;
 
-            if ( isEmptyTile || offset.x < 0 || offset.x >= world.w() ) {
+        if ( offset.y < 0 || offset.y >= world.h() ) {
+            for ( s32 x = 0; x < tileROI.w; ++x ) {
+                offset.x = tileROI.x + x;
                 Maps::Tiles::RedrawEmptyTile( dst, offset );
             }
-            else {
-                const Maps::Tiles & tile = world.GetTiles( offset.x, offset.y );
+        }
+        else {
+            for ( s32 x = 0; x < tileROI.w; ++x ) {
+                offset.x = tileROI.x + x;
 
-                tile.RedrawTile( dst );
+                if ( offset.x < 0 || offset.x >= world.w() ) {
+                    Maps::Tiles::RedrawEmptyTile( dst, offset );
+                }
+                else {
+                    const Maps::Tiles & tile = world.GetTiles( offset.x, offset.y );
 
-                // bottom
-                if ( flag & LEVEL_BOTTOM )
-                    tile.RedrawBottom( dst, isPuzzleDraw );
+                    tile.RedrawTile( dst );
 
-                // map object
-                if ( flag & LEVEL_BOTTOM )
-                    tile.RedrawObjects( dst, isPuzzleDraw );
+                    // bottom and objects
+                    if ( drawBottom ) {
+                        tile.RedrawBottom( dst, isPuzzleDraw );
+                        tile.RedrawObjects( dst, isPuzzleDraw );
+                    }
+                }
             }
         }
     }
 
     // objects
-    for ( int16_t y = 0; y < tileROI.h; ++y ) {
-        const int32_t offsetY = tileROI.y + y;
-        if ( offsetY < 0 || offsetY >= world.h() )
-            continue;
-        for ( s32 x = 0; x < tileROI.w; ++x ) {
-            const int32_t offsetX = tileROI.x + x;
-            if ( offsetX < 0 || offsetX >= world.w() )
+    const bool drawMonstersAndBoats = ( flag & LEVEL_OBJECTS ) && !isPuzzleDraw;
+    if ( drawMonstersAndBoats ) {
+        for ( int16_t y = 0; y < tileROI.h; ++y ) {
+            const int32_t offsetY = tileROI.y + y;
+            if ( offsetY < 0 || offsetY >= world.h() )
                 continue;
+            for ( s32 x = 0; x < tileROI.w; ++x ) {
+                const int32_t offsetX = tileROI.x + x;
+                if ( offsetX < 0 || offsetX >= world.w() )
+                    continue;
 
-            const Maps::Tiles & tile = world.GetTiles( offsetX, offsetY );
-
-            // map object
-            if ( flag & LEVEL_OBJECTS && !isPuzzleDraw )
+                const Maps::Tiles & tile = world.GetTiles( offsetX, offsetY );
                 tile.RedrawMonstersAndBoat( dst );
+            }
         }
     }
 
     // top layer
+    const bool drawTop = ( flag & LEVEL_TOP ) == LEVEL_TOP;
+    const bool drawHeroes = ( flag & LEVEL_HEROES ) == LEVEL_HEROES;
+    std::vector<std::pair<Point, const Heroes *> > heroList;
+
     for ( int16_t y = 0; y < tileROI.h; ++y ) {
         const int32_t offsetY = tileROI.y + y;
         if ( offsetY < 0 || offsetY >= world.h() )
@@ -201,11 +211,11 @@ void Interface::GameArea::Redraw( fheroes2::Image & dst, int flag, bool isPuzzle
             const Maps::Tiles & tile = world.GetTiles( offsetX, offsetY );
 
             // top
-            if ( flag & LEVEL_TOP )
+            if ( drawTop )
                 tile.RedrawTop( dst );
 
             // heroes will be drawn later
-            if ( tile.GetObject() == MP2::OBJ_HEROES && ( flag & LEVEL_HEROES ) ) {
+            if ( tile.GetObject() == MP2::OBJ_HEROES && drawHeroes ) {
                 const Heroes * hero = tile.GetHeroes();
                 if ( hero ) {
                     heroList.emplace_back( GetRelativeTilePosition( Point( offsetX, offsetY ) ), hero );
@@ -249,14 +259,15 @@ void Interface::GameArea::Redraw( fheroes2::Image & dst, int flag, bool isPuzzle
     }
 
     // route
-    const Heroes * hero = flag & LEVEL_HEROES ? GetFocusHeroes() : NULL;
+    const Heroes * hero = drawHeroes ? GetFocusHeroes() : NULL;
 
     if ( hero && hero->GetPath().isShow() ) {
         const Route::Path & path = hero->GetPath();
         int green = path.GetAllowedSteps();
 
         const int pathfinding = hero->GetLevelSkill( Skill::Secondary::PATHFINDING );
-        const bool skipfirst = hero->isMoveEnabled() && 45 > hero->GetSpriteIndex() && 2 < ( hero->GetSpriteIndex() % 9 );
+        const int heroSpriteIndex = hero->GetSpriteIndex();
+        const bool skipfirst = hero->isMoveEnabled() && 45 > heroSpriteIndex && 2 < ( heroSpriteIndex % 9 );
 
         Route::Path::const_iterator pathEnd = path.end();
         Route::Path::const_iterator currentStep = path.begin();
