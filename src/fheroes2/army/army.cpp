1d4415a4a20523c0356ec4d89047178aa844b449
eos428 <zkt1088@gmail.com>
2020-11-20 16:50:04 +0700

    make countText const in Troops::DrawMons32Line

--- src/fheroes2/army/army.cpp
+++ src/fheroes2/army/army.cpp
@@ -771,7 +771,7 @@ void Troops::DrawMons32Line( int32_t cx, int32_t cy, uint32_t width, uint32_t fi
 
                     fheroes2::Blit( monster, fheroes2::Display::instance(), cx - monster.width() / 2, cy + offsetY );
 
-                    std::string countText
+                    const std::string countText
                         = isScouteView ? Game::CountScoute( ( *it )->GetCount(), drawPower, compact ) : Game::CountThievesGuild( ( *it )->GetCount(), drawPower );
 
                     text.Set( countText );
