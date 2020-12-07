d0105709d0de411f4cffddcc377745c65dfa5671
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2020-12-07 20:36:18 +0800

    Correct the message

--- src/fheroes2/dialog/dialog_armyinfo.cpp
+++ src/fheroes2/dialog/dialog_armyinfo.cpp
@@ -562,9 +562,10 @@ int Dialog::ArmyJoinWithCost( const Troop & troop, u32 join, u32 gold, Heroes & 
 
     std::string message;
 
-    if ( troop.GetCount() == 1 )
-        message = _( "The creature is swayed by your diplomatic\ntongue, and makes you an offer:\n \n"
-                     "1 %{monster} will join your army for the sum of %{gold} gold.\nDo you accept?" );
+    if ( troop.GetCount() == 1 ) {
+        message = _( "The %{monster} is swayed by your diplomatic tongue, and offers to join your army for the sum of %{gold} gold.\nDo you accept?" );
+        StringReplace( message, "%{monster}", StringLower( troop.GetName( join ) ) );
+    }
     else {
         message = _( "The creatures are swayed by your diplomatic\ntongue, and make you an offer:\n \n" );
 
