8fdf14c4642c92ba58def853f73d7ef7ca0274d0
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2020-12-27 11:00:25 +0800

    We don't modify the variable

--- src/engine/system.cpp
+++ src/engine/system.cpp
@@ -100,7 +100,7 @@ std::string System::GetHomeDirectory( const std::string & prog )
     std::string res;
 
 #if SDL_VERSION_ATLEAST( 2, 0, 0 )
-    char * path = SDL_GetPrefPath( "", prog.c_str() );
+    const char * path = SDL_GetPrefPath( "", prog.c_str() );
     if ( path ) {
         res = path;
         SDL_free( path );
