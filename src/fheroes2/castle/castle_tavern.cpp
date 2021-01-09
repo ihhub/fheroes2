dbbed7b1c5b2ff113754c8dee65540043fa45886
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2021-01-09 09:40:51 +0800

    Use prefix increment

--- src/fheroes2/castle/castle_tavern.cpp
+++ src/fheroes2/castle/castle_tavern.cpp
@@ -100,7 +100,7 @@ void Castle::OpenTavern( void )
             fheroes2::Blit( tavernSprite, display, dst_pt.x, dst_pt.y );
 
             if ( const u32 index = ICN::AnimationFrame( tavwin, 0, frame ) ) {
-                frame++;
+                ++frame;
                 const fheroes2::Sprite & s22 = fheroes2::AGG::GetICN( tavwin, index );
                 fheroes2::Blit( s22, display, dst_pt.x + s22.x(), dst_pt.y + s22.y() );
             }
