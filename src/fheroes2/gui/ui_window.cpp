b7da3075af51ec98b2ddfc15813e9f4ea4780070
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2021-01-16 21:14:37 +0800

    Remove pragma once

--- src/fheroes2/gui/ui_window.cpp
+++ src/fheroes2/gui/ui_window.cpp
@@ -18,8 +18,6 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 
-#pragma once
-
 #include "ui_window.h"
 #include "agg.h"
 #include "settings.h"
