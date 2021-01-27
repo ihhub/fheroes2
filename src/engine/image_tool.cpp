c58a7d9d5306e1aa491ac81651f4c03d6aac44f1
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2021-01-27 22:48:33 +0800

    Fix missing tranform layer set for decoded images

--- src/engine/image_tool.cpp
+++ src/engine/image_tool.cpp
@@ -115,6 +115,7 @@ namespace fheroes2
     Sprite decodeICNSprite( const uint8_t * data, uint32_t sizeData, const int32_t width, const int32_t height, const int16_t offsetX, const int16_t offsetY )
     {
         Sprite sprite( width, height, offsetX, offsetY );
+        sprite.reset();
 
         uint8_t * imageData = sprite.image();
         uint8_t * imageTransform = sprite.transform();
