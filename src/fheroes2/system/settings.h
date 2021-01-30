f027389d4bd6e073b6da519c8880997164c66b43
Andrii Kurdiumov <kant2002@gmail.com>
2021-01-30 22:34:23 +0600

    Remove dependency on the dialog.h (#2665)

--- src/fheroes2/system/settings.h
+++ src/fheroes2/system/settings.h
@@ -203,6 +203,7 @@ public:
 
     const std::string & MapsCharset( void ) const;
     const std::string & ForceLang( void ) const;
+    const std::string & loadedFileLanguage() const;
     const std::string & FontsNormal( void ) const;
     const std::string & FontsSmall( void ) const;
     int FontsNormalSize( void ) const;
@@ -417,6 +418,7 @@ private:
     std::string font_normal;
     std::string font_small;
     std::string force_lang;
+    std::string _loadedFileLanguage; // not a part of save or configuration file
     std::string maps_charset;
     int size_normal;
     int size_small;
