b3de7b345ffae03315748be5a0b5fd743be5ee9c
Andrii Kurdiumov <kant2002@gmail.com>
2021-02-12 13:03:48 +0600

    Change line endings

--- script/macos/install_sdl_1.sh
+++ script/macos/install_sdl_1.sh
@@ -1,1 +1,19 @@
-#!/bin/bash# Uninstall dev toolsbrew remove gettext# Install dev toolsbrew install gettext# Uninstall SDL 2brew remove sdl2_imagebrew remove sdl2_mixerbrew remove sdl2_ttfbrew remove sdl2# Install SDL 1.2brew install sdlbrew install sdl_ttfbrew install sdl_mixerbrew install sdl_image
+#!/bin/bash
+
+# Uninstall dev tools
+brew remove gettext
+
+# Install dev tools
+brew install gettext
+
+# Uninstall SDL 2
+brew remove sdl2_image
+brew remove sdl2_mixer
+brew remove sdl2_ttf
+brew remove sdl2
+
+# Install SDL 1.2
+brew install sdl
+brew install sdl_ttf
+brew install sdl_mixer
+brew install sdl_image
--- script/macos/install_sdl_2.sh
+++ script/macos/install_sdl_2.sh
@@ -1,1 +1,19 @@
-#!/bin/bash# Uninstall dev toolsbrew remove gettext# Install dev toolsbrew install gettext# Uninstall SDL 1.2brew remove sdl_imagebrew remove sdl_mixerbrew remove sdl_ttfbrew remove sdl# Install SDL 2brew install sdl2brew install sdl2_ttfbrew install sdl2_mixerbrew install sdl2_image
+#!/bin/bash
+
+# Uninstall dev tools
+brew remove gettext
+
+# Install dev tools
+brew install gettext
+
+# Uninstall SDL 1.2
+brew remove sdl_image
+brew remove sdl_mixer
+brew remove sdl_ttf
+brew remove sdl
+
+# Install SDL 2
+brew install sdl2
+brew install sdl2_ttf
+brew install sdl2_mixer
+brew install sdl2_image
