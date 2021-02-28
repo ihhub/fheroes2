7d1f0c3324e55da567f401f4b9e5374a80b3d71e
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2021-02-28 14:04:30 +0800

    Add experimental image loading
    only RGB Bitmaps for now

--- src/engine/image_tool.cpp
+++ src/engine/image_tool.cpp
@@ -110,6 +110,40 @@ namespace fheroes2
         return SaveImage( temp, path );
     }
 
+    bool Load( const std::string & path, Image & image )
+    {
+        SDL_Surface * surface = SDL_LoadBMP( path.c_str() );
+        if ( surface == nullptr ) {
+            return false;
+        }
+
+        if ( surface->format->BytesPerPixel != 3 ) {
+            SDL_FreeSurface( surface );
+            return false;
+        }
+
+        memset( image.transform(), 0, surface->w * surface->h );
+
+        const uint8_t * inY = reinterpret_cast<uint8_t *>( surface->pixels );
+        uint8_t * outY = image.image();
+
+        const uint8_t * inYEnd = inY + surface->h * surface->pitch;
+
+        for ( ; inY != inYEnd; inY += surface->pitch, outY += surface->w ) {
+            const uint8_t * inX = inY;
+            uint8_t * outX = outY;
+            const uint8_t * inXEnd = inX + surface->w * 3;
+
+            for ( ; inX != inXEnd; inX += 3, ++outX ) {
+                *outX = GetColorId( *( inX + 2 ), *( inX + 1 ), *( inX ) );
+            }
+        }
+
+        SDL_FreeSurface( surface );
+
+        return true;
+    }
+
     Sprite decodeICNSprite( const uint8_t * data, uint32_t sizeData, const int32_t width, const int32_t height, const int16_t offsetX, const int16_t offsetY )
     {
         Sprite sprite( width, height, offsetX, offsetY );
