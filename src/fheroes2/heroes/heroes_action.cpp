2d95db622257af183170f0d5d66e4fb9624755dc
vasilenkoalexey <alex.camponotus@gmail.com>
2021-03-03 16:06:00 +0300

    Add barrier fading animation (#2884)
    close #2399

--- src/fheroes2/heroes/heroes_action.cpp
+++ src/fheroes2/heroes/heroes_action.cpp
@@ -3057,9 +3057,9 @@ void ActionToBarrier( Heroes & hero, u32 obj, s32 dst_index )
             _( "A magical barrier stands tall before you, blocking your way. Runes on the arch read,\n\"Speak the key and you may pass.\"\nAs you speak the magic word, the glowing barrier dissolves into nothingness." ),
             Font::BIG, Dialog::OK );
 
+        AnimationRemoveObject( tile );
         tile.SetObject( hero.GetMapsObject() );
         hero.SetMapsObject( MP2::OBJ_ZERO );
-        AnimationRemoveObject( tile );
         tile.RemoveObjectSprite();
         // TODO: fix pathfinding
         if ( tile.GetIndex() == hero.GetIndex() ) {
