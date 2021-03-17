d3134a038edad23f9c782b3e3679afb2b1bb8e79
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2021-03-17 16:22:04 +0800

    Fix compilation

--- src/fheroes2/agg/agg_image.cpp
+++ src/fheroes2/agg/agg_image.cpp
@@ -18,6 +18,7 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 
+#include <cstring>
 #include <map>
 #include <vector>
 
