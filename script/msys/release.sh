77755c82bdb106a0c4652d6c3ae3d992064b50ef
hgdagon <hgdagon@gmail.com>
2021-03-24 04:38:19 -0700

    MSys script update
    Don't download SDL_TTF and SDL_Mixer. Don't pack fonts and POs

--- script/msys/release.sh
+++ script/msys/release.sh
@@ -27,32 +27,38 @@ if   [ $fh2arch = x86 ]; then
     if [ $fh2sdl = SDL1 ]; then
         sdl="https://www.libsdl.org/release/SDL-1.2.15-win32.zip"
         mix="https://www.libsdl.org/projects/SDL_mixer/release/SDL_mixer-1.2.12-win32.zip"
-        ttf="https://www.libsdl.org/projects/SDL_ttf/release/SDL_ttf-2.0.11-win32.zip"
+        # ttf="https://www.libsdl.org/projects/SDL_ttf/release/SDL_ttf-2.0.11-win32.zip"
+		# img="https://www.libsdl.org/projects/SDL_image/release/SDL_image-1.2.12-win32.zip"
     else
         sdl="https://www.libsdl.org/release/SDL2-2.0.14-win32-x86.zip"
         mix="https://www.libsdl.org/projects/SDL_mixer/release/SDL2_mixer-2.0.4-win32-x86.zip"
-        ttf="https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-2.0.15-win32-x86.zip"
+        # ttf="https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-2.0.15-win32-x86.zip"
+		# img="https://www.libsdl.org/projects/SDL_image/release/SDL2_image-2.0.5-win32-x86.zip"
     fi 
 elif [ $fh2arch = x64 ]; then
     if [ $fh2sdl = SDL1 ]; then
         sdl="https://www.libsdl.org/release/SDL-1.2.15-win32-x64.zip"
         mix="https://www.libsdl.org/projects/SDL_mixer/release/SDL_mixer-1.2.12-win32-x64.zip"
-        ttf="https://www.libsdl.org/projects/SDL_ttf/release/SDL_ttf-2.0.11-win32-x64.zip"
+        # ttf="https://www.libsdl.org/projects/SDL_ttf/release/SDL_ttf-2.0.11-win32-x64.zip"
+		# img="https://www.libsdl.org/projects/SDL_image/release/SDL_image-1.2.12-win32-x64.zip"
     else
         sdl="https://www.libsdl.org/release/SDL2-2.0.14-win32-x64.zip"
         mix="https://www.libsdl.org/projects/SDL_mixer/release/SDL2_mixer-2.0.4-win32-x64.zip"
-        ttf="https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-2.0.15-win32-x64.zip"
+        # ttf="https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-2.0.15-win32-x64.zip"
+		# img="https://www.libsdl.org/projects/SDL_image/release/SDL2_image-2.0.5-win32-x64.zip"
     fi
 else
     echo "Error! You're doing something wrong!"
     exit 1
 fi
 
-echo -e "Downloading $fh2sdl $fh2arch libraries"
-wget -q -c -P package $sdl $mix $ttf
-7z e package/\*.zip -aos -bs{o,p}0  -x{\!*FLAC*,\!*mikmod*,\!*modplug*,\!*mpg123*,\!*opus*,\!*smpeg*,\!README.txt} -opackage
+echo "Downloading $fh2sdl $fh2arch libraries"
+wget -q -c -P package $sdl $mix $ttf $img
+7z e package/\*.zip -aos -bs{o,p}0  -x{\!*FLAC*,\!*mikmod*,\!*modplug*,\!*mpg123*,\!*opus*,\!*smpeg*,\!*jpeg*,\!*libpng*,\!*tiff*,\!*webp*,!README.txt} -opackage
 rm -f package/*.zip
 rm -f fheroes2_${fh2arch}_$fh2sdl-$version.zip
-echo -e "Packing fheroes2_${fh2arch}_$fh2sdl-$version.zip"
-7z a -tzip fheroes2_${fh2arch}_$fh2sdl-$version.zip -stl -mx9 -bs{o,p}0 ./package/* ./doc/README.txt changelog.txt fheroes2.key fheroes2.exe LICENSE ./script/demo/download_demo.bat
+echo "Packing fheroes2_${fh2arch}_$fh2sdl-$version.zip"
+cp LICENSE package/License.txt
+# 7z a -tzip fheroes2_${fh2arch}_$fh2sdl-$version.zip -stl -mx9 -bs{o,p}0 ./package/* ./doc/README.txt changelog.txt fheroes2.key fheroes2.exe files/fonts/* files/lang/*.po ./script/demo/download_demo.bat
+7z a -tzip fheroes2_${fh2arch}_$fh2sdl-$version.zip -stl -mx9 -bs{o,p}0 ./package/* ./doc/README.txt changelog.txt fheroes2.key fheroes2.exe ./script/demo/download_demo.bat
 rm -rf package
