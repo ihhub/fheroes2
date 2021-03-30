3732d53d251a86c141f4eaa7661fce6b9d0df026
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2021-03-30 23:44:40 +0800

    Fix compilation of tools for makefile (#3060)

--- src/tools/til2img.cpp
+++ src/tools/til2img.cpp
@@ -20,6 +20,7 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 
+#include <cstring>
 #include <iomanip>
 #include <iostream>
 #include <sstream>
