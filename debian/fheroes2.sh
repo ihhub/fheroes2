98ef7d2a92cc6b5e095bf6d989606819b7681b51
undef <csm.sub21@gmail.com>
2021-04-11 20:55:02 +0300

    move common stuff to own dir

--- debian/fheroes2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-FHEROES2_DIR=$HOME/.fheroes2
-
-test -d $FHEROES2_DIR || ( mkdir -p $FHEROES2_DIR && cp /usr/share/doc/fheroes2.key $FHEROES2_DIR )
-cd $FHEROES2_DIR
-exec /usr/lib/games/fheroes2/fheroes2 $@
