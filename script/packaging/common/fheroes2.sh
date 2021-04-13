b9d544d6cd7c07cad7060c763c6efaecf1a6a653
undef <csm.sub21@gmail.com>
2021-04-13 14:04:11 +0300

    workaround while game search resources by relative paths

--- script/packaging/common/fheroes2.sh
+++ script/packaging/common/fheroes2.sh
@@ -2,6 +2,7 @@
 
 FHEROES2_DIR=$HOME/.fheroes2
 
-test -d $FHEROES2_DIR || ( mkdir -p $FHEROES2_DIR && cp /usr/share/doc/fheroes2/fheroes2.key $FHEROES2_DIR )
+test -d $FHEROES2_DIR || mkdir $FHEROES2_DIR || exit
+test -x $FHEROES2_DIR/fheroes2 || (ln -s /usr/lib/games/fheroes2/fheroes2 $FHEROES2_DIR && cp /usr/share/doc/fheroes2/fheroes2.key $FHEROES2_DIR) || exit
 cd $FHEROES2_DIR
-exec /usr/lib/games/fheroes2/fheroes2 $@
+exec ./fheroes2 $@
