cc5bc548ccebfa96cec469ebe8ca35a36a38f9db
Oleg Derevenetz <oleg-derevenetz@yandex.ru>
2021-04-15 07:21:01 +0300

    Fix incorrect heroes for surrender conditions (#3193)
    close #3190

--- src/fheroes2/battle/battle_arena.cpp
+++ src/fheroes2/battle/battle_arena.cpp
@@ -812,9 +812,9 @@ s32 Battle::Arena::GetFreePositionNearHero( int color ) const
 
 bool Battle::Arena::CanSurrenderOpponent( int color ) const
 {
-    const HeroBase * hero1 = GetCommander( color, false ); // enemy
-    const HeroBase * hero2 = GetCommander( color, true );
-    return hero1 && hero1->isHeroes() && hero2 && world.GetKingdom( hero2->GetColor() ).GetCastles().size();
+    const HeroBase * hero1 = GetCommander( color, true ); // enemy
+    const HeroBase * hero2 = GetCommander( color, false );
+    return hero1 && hero1->isHeroes() && hero2 && hero2->isHeroes() && !world.GetKingdom( hero2->GetColor() ).GetCastles().empty();
 }
 
 bool Battle::Arena::CanRetreatOpponent( int color ) const
