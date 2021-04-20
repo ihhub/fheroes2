82ca8d8654e5fe0fc0ee2cab1e5179997c2954f9
aalawy <alawya2017@gmail.com>
2021-04-20 11:50:43 -0400

    Display correct message when capturing abandoned mine (#3219)
    close #1368

--- src/fheroes2/heroes/heroes_action.cpp
+++ src/fheroes2/heroes/heroes_action.cpp
@@ -2055,7 +2055,13 @@ void ActionToCaptureObject( Heroes & hero, u32 obj, s32 dst_index )
         body = _( "You gain control of a sawmill. It will provide you with %{count} units of wood per day." );
         break;
 
+    case MP2::OBJ_ABANDONEDMINE:
     case MP2::OBJ_MINES: {
+        if ( obj == MP2::OBJ_ABANDONEDMINE && tile.GetQuantity3() != Spell::HAUNT ) {
+            body = _( "You beat the Ghosts and are able to restore the mine to production." );
+            break;
+        }
+
         resource = tile.QuantityResourceCount().first;
         header = Maps::GetMinesName( resource );
 
@@ -2079,10 +2085,6 @@ void ActionToCaptureObject( Heroes & hero, u32 obj, s32 dst_index )
             break;
         }
     } break;
-
-    case MP2::OBJ_ABANDONEDMINE:
-        body = _( "You beat the Ghosts and are able to restore the mine to production." );
-        break;
 
     case MP2::OBJ_LIGHTHOUSE:
         header = MP2::StringObject( obj );
