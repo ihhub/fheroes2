987322aa1330834ac29b848c2d4a165502603aea
eos428 <zkt1088@gmail.com>
2021-05-13 22:11:22 +0700

    fix ambiguous constructor overload error

--- src/fheroes2/campaign/campaign_scenariodata.h
+++ src/fheroes2/campaign/campaign_scenariodata.h
@@ -88,7 +88,7 @@ namespace Campaign
                       const std::string & description, const ScenarioVictoryCondition victoryCondition = ScenarioVictoryCondition::STANDARD,
                       const ScenarioLossCondition lossCondition = ScenarioLossCondition::STANDARD );
         ScenarioData( int scenarioID, const std::vector<int> & nextMaps, const std::vector<Campaign::ScenarioBonusData> & bonuses, const std::string & fileName,
-                      const std::string & description, const ScenarioLossCondition lossCondition = ScenarioLossCondition::STANDARD );
+                      const std::string & description, const ScenarioLossCondition lossCondition );
 
         const std::vector<int> & getNextMaps() const
         {
