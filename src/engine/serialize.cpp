e01229f44d949148fbed9351a89781e6de797cae
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2021-05-13 18:16:56 +0800

    Fix compilation

--- src/engine/serialize.cpp
+++ src/engine/serialize.cpp
@@ -21,6 +21,7 @@
  ***************************************************************************/
 
 #include <algorithm>
+#include <cstring>
 #include <iomanip>
 #include <iostream>
 #include <sstream>
