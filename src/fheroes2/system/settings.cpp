a1bcbef3e732c600a3ec9307176280f6bfb53aa1
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2021-05-17 10:36:35 +0800

    Fix a crash while loading a new save
    close #3426

--- src/fheroes2/system/settings.cpp
+++ src/fheroes2/system/settings.cpp
@@ -1835,8 +1835,15 @@ StreamBase & operator>>( StreamBase & msg, Settings & conf )
     u32 opt_game = 0; // skip: settings
 
     // map file
-    msg >> conf.current_maps_file >> conf.game_difficulty >> conf.game_type >> conf.preferably_count_players >> debug >> opt_game >> conf.opt_world >> conf.opt_battle
-        >> conf.opt_addons >> conf.players;
+    msg >> conf.current_maps_file;
+
+    // TODO: once the minimum supported version will be FORMAT_VERSION_094_RELEASE remove this check.
+    if ( Game::GetLoadVersion() >= FORMAT_VERSION_094_RELEASE ) {
+        msg >> conf.current_maps_file._version;
+    }
+
+    msg >> conf.game_difficulty >> conf.game_type >> conf.preferably_count_players >> debug >> opt_game >> conf.opt_world >> conf.opt_battle >> conf.opt_addons
+        >> conf.players;
 
 #ifndef WITH_DEBUG
     conf.debug = debug;
