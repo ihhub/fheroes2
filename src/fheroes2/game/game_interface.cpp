0e4b3df50fa21422e8003de0c58078d3fe84a144
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2021-05-17 22:28:42 +0800

    Add extended cursor icons from 4+ to 7+
    relates to #3299

--- src/fheroes2/game/game_interface.cpp
+++ src/fheroes2/game/game_interface.cpp
@@ -265,7 +265,7 @@ int32_t Interface::Basic::GetDimensionDoorDestination( const int32_t from, const
                 valid = ( ( spellROI & mp ) && MP2::isClearGroundObject( tile.GetObject() ) && water == world.GetTiles( dst ).isWater() );
             }
 
-            cursor.SetThemes( valid ? ( water ? Cursor::BOAT : Cursor::MOVE ) : Cursor::WAR_NONE );
+            cursor.SetThemes( valid ? ( water ? Cursor::CURSOR_HERO_BOAT : Cursor::CURSOR_HERO_MOVE ) : Cursor::WAR_NONE );
 
             if ( dst >= 0 && le.MousePressRight() ) {
                 const Maps::Tiles & tile = world.GetTiles( dst );
