585a8ff263107a5560e15ff7f055ab506488a777
hgdagon <hgdagon@gmail.com>
2021-05-19 05:14:11 -0700

    Added merged demo download script for all (most?) unix-like OSs

---
+++ script/demo/demo_unix.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if ! [[ -x $(type -p wget) ]]; then
+	get='curl > demo.zip -L'
+else
+	get='wget -O demo.zip'
+fi
+
+
+echo 'Downloading demo...'
+eval $get https://archive.org/download/HeroesofMightandMagicIITheSuccessionWars_1020/h2demo.zip
+echo 'Extracting necessary files...'
+unzip -o -qq demo.zip DATA/* MAPS/*
+rm -f demo.zip
+echo 'All done!'
