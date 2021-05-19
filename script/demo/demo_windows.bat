2216cd66a06071bc024154fea56df84676f65b34
hgdagon <hgdagon@gmail.com>
2021-05-19 05:12:07 -0700

    Use `shell.application` to extract zip

--- script/demo/demo_windows.bat
+++ script/demo/demo_windows.bat
@@ -1,21 +1,43 @@
 @echo off
 
-powershell -Command "(New-Object Net.WebClient).DownloadFile('https://archive.org/download/HeroesofMightandMagicIITheSuccessionWars_1020/h2demo.zip','demo.zip')"
+set fh2Path=
+set dev=
 
-for /f usebackq %%g in (
-`powershell -Command $PSVersionTable.PSVersion.Major`) do (
-if %%g lss 4 (
-powershell -Command "(New-Object Net.WebClient).DownloadFile('https://www.willus.com/archive/zip64/unzip.exe','unzip.exe')"
-unzip -o -qq -L demo.zip data/* maps/*
-del unzip.exe
+if not exist "fheroes2.exe" (
+    Rem this is not fheroes2 related directory. Let's see if it's for developer
+    if exist "..\..\src" (
+        set fh2Path=..\..\
+        set dev=y
+    )
+)
+
+
+echo [1/3] downloading demo version
+
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://archive.org/download/HeroesofMightandMagicIITheSuccessionWars_1020/h2demo.zip', '%fh2Path%demo.zip')"
+
+echo [2/3] unpacking archive
+
+if not exist "%fh2Path%demo" mkdir %fh2Path%demo
+powershell -Command "$shell = new-object -com shell.application; $zip = $shell.NameSpace((Resolve-Path '%fh2Path%demo.zip').Path); foreach($item in $zip.items()) { $shell.Namespace((Resolve-Path '%fh2Path%demo').Path).copyhere($item, 0x14) }"
+
+echo [3/3] copying files
+
+if not exist "%fh2Path%data" mkdir "%fh2Path%data"
+if not exist "%fh2Path%maps" mkdir "%fh2Path%maps"
+xcopy /Y /s "%fh2Path%demo\DATA" "%fh2Path%data" >nul
+xcopy /Y /s "%fh2Path%demo\MAPS" "%fh2Path%maps" >nul
+
+if defined dev (
+    powershell -Command "(New-Object Net.WebClient).DownloadFile('https://wikidll.com/download/25503/wing32.zip', '%fh2Path%wing32.zip')"
+    powershell -Command "$shell = new-object -com shell.application; $zip = $shell.NameSpace((Resolve-Path '%fh2Path%wing32.zip').Path); foreach($item in $zip.items()) { $shell.Namespace((Resolve-Path '%fh2Path%demo').Path).copyhere($item, 0x14) }"
+    del %fh2Path%wing32.zip
 ) else (
-powershell -command "Expand-Archive -Force 'demo.zip'"
-xcopy /y /s "demo\DATA" "data\" >nul
-xcopy /y /s "demo\MAPS" "maps\" >nul
-rd /S /Q demo
+    rmdir /s /q %fh2Path%demo
 )
+del %fh2Path%demo.zip
+
+if not "%APPVEYOR_REPO_PROVIDER%" == "gitHub" (
+    echo Press any key to exit...
+    pause >nul
 )
-
-del demo.zip
-
-pause
