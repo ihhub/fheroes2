0e64bad452d2e4ca7de7609e3b1e551e89775dcf
hgdagon <hgdagon@gmail.com>
2021-05-19 05:59:46 -0700

    Use shell.application to extract zip

--- script/windows/install_packages.bat
+++ script/windows/install_packages.bat
@@ -16,30 +16,24 @@ REM echo [6/6] Downloading SDL2_Image...
 REM powershell -Command "(New-Object Net.WebClient).DownloadFile('https://www.libsdl.org/projects/SDL_image/release/SDL2_image-devel-2.0.5-VC.zip', '%pkgPath%\sdl2_image.zip')"
 
 echo Extracting archives...
-for /f usebackq %%g in (
-`powershell -Command $PSVersionTable.PSVersion.Major`) do (
-if %%g lss 4 (
-powershell -Command "(New-Object Net.WebClient).DownloadFile('https://www.willus.com/archive/zip64/unzip.exe','unzip.exe')"
-unzip -o -qq -L %pkgPath%\zlib1.2.11.zip -d %pkgPath%\zlib
-unzip -o %pkgPath%\sdl*.zip */lib/* */include/* */README*.* -d %pkgPath%
-del unzip.exe
-) else (
-powershell -command "Expand-Archive -Force  %pkgPath%\zlib1.2.11.zip -DestinationPath %pkgPath%\zlib"
-powershell -command "Get-ChildItem %pkgPath%\*.zip -exclude zlib*.zip | Expand-Archive -Force -DestinationPath %pkgPath%"
-)
-)
+if not exist %pkgPath%\zlib mkdir %pkgPath%\zlib
+powershell -Command "$shell = new-object -com shell.application; $zip = $shell.NameSpace((Resolve-Path '%pkgPath%\zlib1.2.11.zip').Path); foreach($item in $zip.items()) { $shell.Namespace((Resolve-Path '%pkgPath%\zlib').Path).copyhere($item, 0x14) }"
+powershell -command "$shell = new-object -com shell.application; Get-ChildItem %pkgPath%\*.zip -exclude zlib*.zip | %%{$zip = $shell.NameSpace($_.FullName); foreach($item in $zip.items()) { $shell.Namespace((Resolve-Path '%pkgPath%').Path).copyhere($item, 0x14) }}"
+
+echo Tidying up...
 xcopy /Y /s /Q "%pkgPath%\SDL-1.2.15\"       "%pkgPath%\sdl\" >nul
 xcopy /Y /s /Q "%pkgPath%\SDL_mixer-1.2.12\" "%pkgPath%\sdl\" >nul
 xcopy /Y /s /Q "%pkgPath%\SDL2-2.0.14\"      "%pkgPath%\sdl2\" >nul
 xcopy /Y /s /Q "%pkgPath%\SDL2_mixer-2.0.4\" "%pkgPath%\sdl2\" >nul
-xcopy /Y /s /Q "%pkgPath%\SDL2_ttf-2.0.15\"  "%pkgPath%\sdl2\" >nul
-xcopy /Y /s /Q "%pkgPath%\SDL2_image-2.0.5\" "%pkgPath%\sdl2\" >nul
+REM xcopy /Y /s /Q "%pkgPath%\SDL2_ttf-2.0.15\"  "%pkgPath%\sdl2\" >nul
+REM xcopy /Y /s /Q "%pkgPath%\SDL2_image-2.0.5\" "%pkgPath%\sdl2\" >nul
 rd /S /Q "%pkgPath%\SDL-1.2.15"
 rd /S /Q "%pkgPath%\SDL_mixer-1.2.12"
 rd /S /Q "%pkgPath%\SDL2-2.0.14"
 rd /S /Q "%pkgPath%\SDL2_mixer-2.0.4"
-rd /S /Q "%pkgPath%\SDL2_ttf-2.0.15"
-rd /S /Q "%pkgPath%\SDL2_image-2.0.5"
+REM rd /S /Q "%pkgPath%\SDL2_ttf-2.0.15"
+REM rd /S /Q "%pkgPath%\SDL2_image-2.0.5"
+del /F /Q %pkgPath%\sdl*.zip >nul
 
 if not "%APPVEYOR_REPO_PROVIDER%" == "gitHub" (
     echo All done!
