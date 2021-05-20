7533d19afd64271cebc6f0562427abdbae325cf2
Oleg Derevenetz <oleg-derevenetz@yandex.ru>
2021-05-20 21:11:08 +0300

    Use single script for Linux and MacOS, check SHA256 checksum for downloaded h2demo.zip

---
+++ script/demo/demo_unix.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -e
+
+H2DEMO_URL="https://archive.org/download/HeroesofMightandMagicIITheSuccessionWars_1020/h2demo.zip"
+H2DEMO_SHA256="12048c8b03875c81e69534a3813aaf6340975e77b762dc1b79a4ff5514240e3c"
+
+[[ ! -d demo ]] && mkdir demo
+cd demo
+
+if [[ "$(which wget 2> /dev/null)" != "" ]]; then
+    wget "$H2DEMO_URL"
+elif [[ "$(which curl 2> /dev/null)" != "" ]]; then
+    curl -O -L "$H2DEMO_URL" > h2demo.zip
+else
+    echo "wget or curl not found, aborting"
+    exit 1
+fi
+
+echo "$H2DEMO_SHA256 *h2demo.zip" > checksums
+
+if [[ "$(which shasum 2> /dev/null)" != "" ]]; then
+    shasum --check --algorithm 256 checksums
+elif [[ "$(which sha256sum 2> /dev/null)" != "" ]]; then
+    sha256sum --check --strict checksums
+else
+    echo "shasum or sha256sum not found, aborting"
+    exit 1
+fi
+
+unzip -o h2demo.zip
+
+[[ ! -d ../../../data ]] && mkdir ../../../data
+[[ ! -d ../../../maps ]] && mkdir ../../../maps
+
+cp -r DATA/* ../../../data
+cp -r MAPS/* ../../../maps
