d3c7958d2c3bd61d03b64fc74d4b786566fd52da
Oleg Derevenetz <oleg-derevenetz@yandex.ru>
2021-05-20 19:52:14 +0300

    Migrate demo_windows.bat to PowerShell

---
+++ script/demo/demo_windows.ps1
@@ -0,0 +1,94 @@
+try {
+    $h2DemoURL = "https://archive.org/download/HeroesofMightandMagicIITheSuccessionWars_1020/h2demo.zip"
+    $h2DemoSHA256 = "12048C8B03875C81E69534A3813AAF6340975E77B762DC1B79A4FF5514240E3C"
+
+    $wing32URL = "https://wikidll.com/download/25503/wing32.zip"
+    $wing32SHA256 = "0CD89F09C66F53F30782858DF5453F6AC4C8A6D482F558E4FDF24C26E0A05A49"
+
+    $shell = New-Object -com "shell.application" -ErrorAction Stop
+    $webClient = New-Object Net.WebClient -ErrorAction Stop
+
+    $fheroes2Path = ""
+
+    if (-Not (Test-Path -Path "fheroes2.exe" -PathType Leaf) -And (Test-Path -Path "..\..\src" -PathType Container)) {
+        $fheroes2Path = "..\..\"
+    }
+
+    if (-Not (Test-Path -Path "demo" -PathType Container)) {
+        [void](New-Item -Path "demo" -ItemType "directory" -ErrorAction Stop)
+    }
+
+    Write-Host "[1/4] downloading demo version"
+
+    $webClient.DownloadFile($h2DemoURL, "demo\demo.zip")
+
+    $hash = (Get-FileHash -Path "demo\demo.zip" -Algorithm SHA256).Hash
+
+    if ($hash -Ne $h2DemoSHA256) {
+        Write-Host -ForegroundColor Red (-Join("FATAL ERROR: Invalid hash for HoMM2 demo archive`r`n", `
+                                               "Expected:`t$h2DemoSHA256`r`n", `
+                                               "Got:`t`t$hash`r`n", `
+                                               "Installation aborted"))
+
+        return
+    }
+
+    Write-Host "[2/4] downloading wing32.dll library"
+
+    $webClient.DownloadFile($wing32URL, "demo\wing32.zip")
+
+    $hash = (Get-FileHash -Path "demo\wing32.zip" -Algorithm SHA256).Hash
+
+    if ($hash -Ne $wing32SHA256) {
+        Write-Host -ForegroundColor Red (-Join("FATAL ERROR: Invalid hash for wing32.dll archive`r`n", `
+                                               "Expected:`t$wing32SHA256`r`n", `
+                                               "Got:`t`t$hash`r`n", `
+                                               "Installation aborted"))
+
+        return
+    }
+
+    Write-Host "[3/4] unpacking archives"
+
+    $zip = $shell.NameSpace((Resolve-Path "demo\demo.zip").Path)
+
+    foreach ($item in $zip.items()) {
+        $shell.Namespace((Resolve-Path "demo").Path).CopyHere($item, 0x14)
+    }
+
+    $zip = $shell.NameSpace((Resolve-Path "demo\wing32.zip").Path)
+
+    foreach ($item in $zip.items()) {
+        $shell.Namespace((Resolve-Path "demo").Path).CopyHere($item, 0x14)
+    }
+
+    Write-Host "[4/4] copying files"
+
+    $dataPath = (-Join($fheroes2Path, "data"))
+    $mapsPath = (-Join($fheroes2Path, "maps"))
+
+    if (-Not (Test-Path -Path $dataPath -PathType Container)) {
+        [void](New-Item -Path $dataPath -ItemType "directory" -ErrorAction Stop)
+    }
+    if (-Not (Test-Path -Path $mapsPath -PathType Container)) {
+        [void](New-Item -Path $mapsPath -ItemType "directory" -ErrorAction Stop)
+    }
+
+    $data = $shell.NameSpace((Resolve-Path "demo\DATA").Path)
+    $maps = $shell.NameSpace((Resolve-Path "demo\MAPS").Path)
+
+    foreach ($item in $data.items()) {
+        $shell.Namespace((Resolve-Path $dataPath).Path).CopyHere($item, 0x14)
+    }
+    foreach ($item in $maps.items()) {
+        $shell.Namespace((Resolve-Path $mapsPath).Path).CopyHere($item, 0x14)
+    }
+} catch {
+    Write-Host -ForegroundColor Red (-Join("FATAL ERROR: ", $_))
+} finally {
+    if ($Env:APPVEYOR_REPO_PROVIDER -Ne "gitHub") {
+        Write-Host "Press any key to exit..."
+
+        [void][System.Console]::ReadKey($true);
+    }
+}
