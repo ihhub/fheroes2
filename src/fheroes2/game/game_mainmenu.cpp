bcc4f0b5f06527fe19d8154259e51fd8f024a823
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2021-05-24 23:16:47 +0800

    Code style fix

--- src/fheroes2/game/game_mainmenu.cpp
+++ src/fheroes2/game/game_mainmenu.cpp
@@ -140,7 +140,7 @@ void Game::mainGameLoop( bool isFirstGameRun )
             break;
         case fheroes2::GameMode::COMPLETE_CAMPAIGN_SCENARIO_FROM_LOAD_FILE:
             result = Game::CompleteCampaignScenario();
-            if ( result == fheroes2::GameMode::SELECT_CAMPAIGN_SCENARIO) {
+            if ( result == fheroes2::GameMode::SELECT_CAMPAIGN_SCENARIO ) {
                 result = Game::SelectCampaignScenario( fheroes2::GameMode::LOAD_CAMPAIN );
             }
             break;
