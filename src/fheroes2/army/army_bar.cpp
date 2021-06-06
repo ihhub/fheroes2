608fbba15ba4262d77fca9b024c8572268454332
Félix-Antoine Constantin <felix-antoine.constantin@polymtl.ca>
2021-06-05 23:31:13 -0400

    I3621 The code should now work properly

--- src/fheroes2/army/army_bar.cpp
+++ src/fheroes2/army/army_bar.cpp
@@ -59,8 +59,8 @@ void RedistributeArmy( ArmyTroop & troopFrom, ArmyTroop & troopTarget, Army * ar
         if ( isSameTroopType )
             ++freeSlots;
 
-        const uint32_t maxCount = troopFrom.GetCount();
-        uint32_t redistributeCount = isSameTroopType ? 1 : troopFrom.GetCount() / 2;
+        const uint32_t maxCount = saveLastTroop ? troopFrom.GetCount() - 1 : troopFrom.GetCount();
+        uint32_t redistributeCount = isSameTroopType - 1 ? 1 : troopFrom.GetCount() / 2;
 
         // if splitting to the same troop type, use this bool to turn off fast split option at the beginning of the dialog
         bool useFastSplit = !isSameTroopType;
