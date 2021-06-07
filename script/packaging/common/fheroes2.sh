4fb235cccd17d64268b5d79795fe64210b14175d
undef <csm.sub21@gmail.com>
2021-06-07 19:08:32 +0300

    update version
    - remove unneeded dependencies;
    - bump version

--- script/packaging/common/fheroes2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-FHEROES2_DIR=$HOME/.fheroes2
-
-test -d $FHEROES2_DIR || mkdir $FHEROES2_DIR || exit
-test -x $FHEROES2_DIR/fheroes2 || (ln -s /usr/lib/games/fheroes2/fheroes2 $FHEROES2_DIR && cp /usr/share/doc/fheroes2/fheroes2.key $FHEROES2_DIR) || exit
-cd $FHEROES2_DIR
-exec ./fheroes2 $@
