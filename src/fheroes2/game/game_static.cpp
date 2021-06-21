2059eff74b84970688a84762e822f1acc42fd465
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2021-06-21 19:22:32 +0800

    Fix compilation

--- src/fheroes2/game/game_static.cpp
+++ src/fheroes2/game/game_static.cpp
@@ -20,6 +20,7 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 
+#include "game.h"
 #include "game_static.h"
 #include "mp2.h"
 #include "race.h"
