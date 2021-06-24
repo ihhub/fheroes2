ihhub/fheroes2

commit 91b027726f3de1d1588c180075c9c8044c7310da

Fix rendering priorities of map's objects (#3744)

src/fheroes2/maps/maps_tiles.cpp
@@ -209,11 +209,6 @@ bool Maps::TilesAddon::PredicateSortRules1( const Maps::TilesAddon & ta1, const 
     return ( ( ta1.level % 4 ) > ( ta2.level % 4 ) );
 }
 
-bool Maps::TilesAddon::PredicateSortRules2( const Maps::TilesAddon & ta1, const Maps::TilesAddon & ta2 )
-{
-    return ( ( ta1.level % 4 ) < ( ta2.level % 4 ) );
-}
-
 int Maps::Tiles::GetLoyaltyObject( const uint8_t tileset, const uint8_t icnIndex )
 {
     switch ( MP2::GetICNObject( tileset ) ) {
@@ -909,7 +904,7 @@ void Maps::Tiles::Init( s32 index, const MP2::mp2tile_t & mp2 )
     else {
         objectTileset = mp2.objectName1;
         objectIndex = mp2.indexName1;
-        uniq = mp2.editorObjectLink;
+        uniq = mp2.level1ObjectUID;
     }
     AddonsPushLevel2( mp2 );
 }
@@ -1316,7 +1311,7 @@ int Maps::Tiles::GetPassable( void ) const
 void Maps::Tiles::AddonsPushLevel1( const MP2::mp2tile_t & mt )
 {
     if ( mt.objectName1 && mt.indexName1 < 0xFF )
-        AddonsPushLevel1( TilesAddon( 0, mt.editorObjectLink, mt.objectName1, mt.indexName1 ) );
+        AddonsPushLevel1( TilesAddon( mt.quantity1, mt.level1ObjectUID, mt.objectName1, mt.indexName1 ) );
 
     // MP2 "objectName" is a bitfield
     // 6 bits is ICN tileset id, 1 bit isRoad flag, 1 bit hasAnimation flag
@@ -1326,8 +1321,9 @@ void Maps::Tiles::AddonsPushLevel1( const MP2::mp2tile_t & mt )
 
 void Maps::Tiles::AddonsPushLevel1( const MP2::mp2addon_t & ma )
 {
-    if ( ma.objectNameN1 && ma.indexNameN1 < 0xFF )
-        AddonsPushLevel1( TilesAddon( ma.quantityN, ma.editorObjectLink, ma.objectNameN1, ma.indexNameN1 ) );
+    if ( ma.objectNameN1 && ma.indexNameN1 < 0xFF ) {
+        AddonsPushLevel1( TilesAddon( ma.quantityN, ma.level1ObjectUID, ma.objectNameN1, ma.indexNameN1 ) );
+    }
 }
 
 void Maps::Tiles::AddonsPushLevel1( const TilesAddon & ta )
@@ -1337,14 +1333,16 @@ void Maps::Tiles::AddonsPushLevel1( const TilesAddon & ta )
 
 void Maps::Tiles::AddonsPushLevel2( const MP2::mp2tile_t & mt )
 {
-    if ( mt.objectName2 && mt.indexName2 < 0xFF )
-        AddonsPushLevel2( TilesAddon( 0, mt.editorObjectOverlay, mt.objectName2, mt.indexName2 ) );
+    if ( mt.objectName2 && mt.indexName2 < 0xFF ) {
+        AddonsPushLevel2( TilesAddon( mt.quantity1, mt.level2ObjectUID, mt.objectName2, mt.indexName2 ) );
+    }
 }
 
 void Maps::Tiles::AddonsPushLevel2( const MP2::mp2addon_t & ma )
 {
-    if ( ma.objectNameN2 && ma.indexNameN2 < 0xFF )
-        AddonsPushLevel2( TilesAddon( ma.quantityN, ma.editorObjectOverlay, ma.objectNameN2, ma.indexNameN2 ) );
+    if ( ma.objectNameN2 && ma.indexNameN2 < 0xFF ) {
+        AddonsPushLevel2( TilesAddon( ma.quantityN, ma.level2ObjectUID, ma.objectNameN2, ma.indexNameN2 ) );
+    }
 }
 
 void Maps::Tiles::AddonsPushLevel2( const TilesAddon & ta )
@@ -1355,41 +1353,17 @@ void Maps::Tiles::AddonsPushLevel2( const TilesAddon & ta )
         addons_level2.push_back( ta );
 }
 
-void Maps::Tiles::AddonsSort( void )
+void Maps::Tiles::AddonsSort()
 {
-    Addons::iterator lastObject = addons_level1.end();
-    for ( Addons::iterator it = addons_level1.begin(); it != addons_level1.end(); ++it ) {
-        const int icn = MP2::GetICNObject( it->object );
-
-        // force rivers and roads to be lowest priority
-        if ( icn == ICN::STREAM || icn == ICN::ROAD )
-            continue;
-
-        // force monster object on top (fix old saves)
-        if ( mp2_object == MP2::OBJ_MONSTER && icn == ICN::MONS32 ) {
-            lastObject = it;
-            break;
-        }
-        // Level is actually a bitfield, but if 0 then it's an actual "full object"
-        // To compare: level 2 is shadow, level 3 is roads/river
-        else if ( it->level % 4 == 0 ) {
-            lastObject = it;
-        }
-    }
-
-    // Re-order tiles (Fixing mistakes of original maps)
-    if ( lastObject != addons_level1.end() ) {
-        if ( objectTileset != 0 && objectIndex < 255 )
-            addons_level1.push_front( TilesAddon( 0, uniq, objectTileset, objectIndex ) );
-
-        uniq = lastObject->uniq;
-        objectTileset = lastObject->object;
-        objectIndex = lastObject->index;
-        addons_level1.erase( lastObject );
+    // Push everything to the container and sort it by level.
+    bool topObjectExists = false;
+    if ( objectTileset != 0 && objectIndex < 255 ) {
+        topObjectExists = true;
+        addons_level1.emplace_front( quantity1 & 3, uniq, objectTileset, objectIndex );
     }
 
     // Some original maps have issues with identifying tiles as roads. This code fixes it. It's not an ideal solution but works fine in most of cases.
-    if ( !addons_level1.empty() && !tileIsRoad && ( MP2::OBJ_ZERO == mp2_object || MP2::OBJ_COAST == mp2_object ) ) {
+    if ( !tileIsRoad ) {
         for ( const TilesAddon & addon : addons_level1 ) {
             if ( addon.isRoad() ) {
                 tileIsRoad = true;
@@ -1398,11 +1372,19 @@ void Maps::Tiles::AddonsSort( void )
         }
     }
 
-    // FIXME: check if sort is still needed
-    // if ( !addons_level1.empty() )
-    //    addons_level1.sort( TilesAddon::PredicateSortRules1 );
-    // if ( !addons_level2.empty() )
-    //    addons_level2.sort( TilesAddon::PredicateSortRules2 );
+    addons_level1.sort( TilesAddon::PredicateSortRules1 );
+
+    if ( topObjectExists ) {
+        const TilesAddon & highestPriorityAddon = addons_level1.back();
+        uniq = highestPriorityAddon.uniq;
+        objectTileset = highestPriorityAddon.object;
+        objectIndex = highestPriorityAddon.index;
+        quantity1 = ( quantity1 & ( 0xFF - 0x03 ) ) + highestPriorityAddon.level;
+
+        addons_level1.pop_back();
+    }
+
+    // Level 2 objects don't have any rendering priorities so they should be rendered first in queue first to render.
 }
 
 int Maps::Tiles::GetGround( void ) const

src/fheroes2/maps/maps_fileinfo.cpp
@@ -334,8 +334,8 @@ bool Maps::FileInfo::ReadMP2( const std::string & filename )
         // offset first addon
         fs.getLE16();
 
-        mp2tile.editorObjectLink = fs.getLE32();
-        mp2tile.editorObjectOverlay = fs.getLE32();
+        mp2tile.level1ObjectUID = fs.getLE32();
+        mp2tile.level2ObjectUID = fs.getLE32();
 
         Maps::Tiles tile;
         tile.Init( 0, mp2tile );

src/fheroes2/maps/maps_tiles.h
@@ -98,7 +98,6 @@ namespace Maps
         static int GetActionObject( const TilesAddon & );
 
         static bool PredicateSortRules1( const TilesAddon &, const TilesAddon & );
-        static bool PredicateSortRules2( const TilesAddon &, const TilesAddon & );
 
         static bool ForceLevel1( const TilesAddon & );
 

src/fheroes2/maps/mp2.h
@@ -40,31 +40,54 @@ namespace MP2
     // origin mp2 tile
     struct mp2tile_t
     {
-        u16 tileIndex; // tile (ocean, grass, snow, swamp, lava, desert, dirt, wasteland, beach)
+        uint16_t tileIndex; // Tile index representing a type of surface: ocean, grass, snow, swamp, lava, desert, dirt, wasteland, beach.
+
         u8 objectName1; // level 1.0
         u8 indexName1; // index level 1.0 or 0xFF
         u8 quantity1; // Bitfield, first 3 bits are flags, rest is used as quantity
         u8 quantity2; // Used as a part of quantity, field size is actually 13 bits. Has most significant bits
         u8 objectName2; // level 2.0
         u8 indexName2; // index level 2.0 or 0xFF
-        u8 flags; // Bitfield: 1st bit is passability, last two is tile shape: 0 none, 1 vertical, 2 horizontal, 3 any
+
+        // First 2 bits responsible for tile shape (0 - 3). Subsequent 3 bits are still unknown. Possible values are 1 and 5. They are set only for tiles with transition
+        // between land and sea.
+        uint8_t flags;
+
         u8 mapObject; // zero or object
-        u16 indexAddon; // zero or index addons_t
-        u32 editorObjectLink; // Map editor variable: object link
-        u32 editorObjectOverlay; // Map editor variable: overlay link
+
+        uint16_t nextAddonIndex; // Next add-on index. Zero value means it's the last addon chunk.
+
+        // Ground (bottom) level object UID. An object can allocate more than 1 tile. Each tile could have multiple objects pieces.
+        // UID is used to find all pieces/addons which belong to the same object.
+        // In Editor first object will have UID as 0. Then second object placed on the map will have UID 0 + number of pieces / tiles per previous object and etc.
+        uint32_t level1ObjectUID;
+
+        // Top level object UID. An object can allocate more than 1 tile. Each tile could have multiple objects pieces.
+        // UID is used to find all pieces/addons which belong to the same object.
+        // In Editor first object will have UID as 0. Then second object placed on the map will have UID 0 + number of pieces / tiles per previous object and etc.
+        uint32_t level2ObjectUID;
     };
 
     // origin mp2 addons tile
     struct mp2addon_t
     {
-        u16 indexAddon; // zero or next addons_t
+        uint16_t nextAddonIndex; // Next add-on index. Zero value means it's the last addon chunk.
+
         u8 objectNameN1; // level 1.N. Last bit indicates if object is animated. Second-last controls overlay
         u8 indexNameN1; // level 1.N or 0xFF
         u8 quantityN; // Bitfield containing metadata
         u8 objectNameN2; // level 2.N
         u8 indexNameN2; // level 1.N or 0xFF
-        u32 editorObjectLink; // Map editor variable: object link
-        u32 editorObjectOverlay; // Map editor variable: overlay link
+
+        // Ground (bottom) level object UID. An object can allocate more than 1 tile. Each tile could have multiple objects pieces.
+        // UID is used to find all pieces/addons which belong to the same object.
+        // In Editor first object will have UID as 0. Then second object placed on the map will have UID 0 + number of pieces / tiles per previous object and etc.
+        uint32_t level1ObjectUID;
+
+        // Top level object UID. An object can allocate more than 1 tile. Each tile could have multiple objects pieces.
+        // UID is used to find all pieces/addons which belong to the same object.
+        // In Editor first object will have UID as 0. Then second object placed on the map will have UID 0 + number of pieces / tiles per previous object and etc.
+        uint32_t level2ObjectUID;
     };
 
     // origin mp2 castle

src/fheroes2/world/world.cpp
@@ -334,9 +334,9 @@ void World::NewMaps( int32_t sw, int32_t sh )
         mp2tile.indexName2 = 0xff; // index sprite level 2
         mp2tile.flags = static_cast<uint8_t>( Rand::Get( 0, 3 ) ); // shape reflect % 4, 0 none, 1 vertical, 2 horizontal, 3 any
         mp2tile.mapObject = MP2::OBJ_ZERO;
-        mp2tile.indexAddon = 0;
-        mp2tile.editorObjectLink = 0;
-        mp2tile.editorObjectOverlay = 0;
+        mp2tile.nextAddonIndex = 0;
+        mp2tile.level1ObjectUID = 0; // means that there's no object on this tile.
+        mp2tile.level2ObjectUID = 0;
 
         vec_tiles[i].Init( static_cast<int32_t>( i ), mp2tile );
     }

src/fheroes2/world/world_loadmap.cpp
@@ -139,15 +139,15 @@ bool World::LoadMapMP2( const std::string & filename )
     std::vector<MP2::mp2addon_t> vec_mp2addons( fs.getLE32() /* count mp2addon_t */ );
 
     for ( MP2::mp2addon_t & mp2addon : vec_mp2addons ) {
-        mp2addon.indexAddon = fs.getLE16();
+        mp2addon.nextAddonIndex = fs.getLE16();
         mp2addon.objectNameN1 = fs.get() * 2;
         mp2addon.indexNameN1 = fs.get();
         mp2addon.quantityN = fs.get();
         mp2addon.objectNameN2 = fs.get();
         mp2addon.indexNameN2 = fs.get();
 
-        mp2addon.editorObjectLink = fs.getLE32();
-        mp2addon.editorObjectOverlay = fs.getLE32();
+        mp2addon.level1ObjectUID = fs.getLE32();
+        mp2addon.level2ObjectUID = fs.getLE32();
     }
 
     const size_t endof_addons = fs.tell();
@@ -181,6 +181,25 @@ bool World::LoadMapMP2( const std::string & filename )
         mp2tile.indexName2 = fs.get();
         mp2tile.flags = fs.get();
         mp2tile.mapObject = fs.get();
+        mp2tile.nextAddonIndex = fs.getLE16();
+        mp2tile.level1ObjectUID = fs.getLE32();
+        mp2tile.level2ObjectUID = fs.getLE32();
+
+        tile.Init( i, mp2tile );
+
+        // Read extra information if it's present.
+        size_t addonIndex = mp2tile.nextAddonIndex;
+        while ( addonIndex > 0 ) {
+            if ( vec_mp2addons.size() <= addonIndex ) {
+                DEBUG_LOG( DBG_GAME, DBG_WARN, "index out of range" );
+                break;
+            }
+            tile.AddonsPushLevel1( vec_mp2addons[addonIndex] );
+            tile.AddonsPushLevel2( vec_mp2addons[addonIndex] );
+            addonIndex = vec_mp2addons[addonIndex].nextAddonIndex;
+        }
+
+        tile.AddonsSort();
 
         switch ( mp2tile.mapObject ) {
         case MP2::OBJ_RNDTOWN:
@@ -197,27 +216,6 @@ bool World::LoadMapMP2( const std::string & filename )
         default:
             break;
         }
-
-        // offset first addon
-        size_t offsetAddonsBlock = fs.getLE16();
-
-        mp2tile.editorObjectLink = fs.getLE32();
-        mp2tile.editorObjectOverlay = fs.getLE32();
-
-        tile.Init( i, mp2tile );
-
-        // load all addon for current tils
-        while ( offsetAddonsBlock ) {
-            if ( vec_mp2addons.size() <= offsetAddonsBlock ) {
-                DEBUG_LOG( DBG_GAME, DBG_WARN, "index out of range" );
-                break;
-            }
-            tile.AddonsPushLevel1( vec_mp2addons[offsetAddonsBlock] );
-            tile.AddonsPushLevel2( vec_mp2addons[offsetAddonsBlock] );
-            offsetAddonsBlock = vec_mp2addons[offsetAddonsBlock].indexAddon;
-        }
-
-        tile.AddonsSort();
     }
 
     DEBUG_LOG( DBG_GAME, DBG_INFO, "read all tiles, tellg: " << fs.tell() );

commit 65ab43b98dd8b6a8e14571f1ab4b3d3a6ae930f0

Fix incorrect object sorting logic
regression from #3744

src/fheroes2/maps/maps_tiles.cpp
@@ -1379,7 +1379,7 @@ void Maps::Tiles::AddonsSort()
         uniq = highestPriorityAddon.uniq;
         objectTileset = highestPriorityAddon.object;
         objectIndex = highestPriorityAddon.index;
-        quantity1 = ( quantity1 & ( 0xFF - 0x03 ) ) + highestPriorityAddon.level;
+        quantity1 = ( quantity1 & ( 0xFF - 0x03 ) ) + ( highestPriorityAddon.level & 0x03 );
 
         addons_level1.pop_back();
     }