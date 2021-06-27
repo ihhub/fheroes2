fb374397e61137f8f0808a6a5c431748f21a0c53
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2021-06-27 22:16:51 +0800

    Make the variable const

--- src/fheroes2/dialog/dialog_quickinfo.cpp
+++ src/fheroes2/dialog/dialog_quickinfo.cpp
@@ -61,7 +61,7 @@ std::string ShowGuardiansInfo( const Maps::Tiles & tile, bool isOwned, bool exte
     std::string str;
     const Troop & troop = tile.QuantityTroop();
 
-    bool isAbandonnedMine = ( tile.GetObject() == MP2::OBJN_ABANDONEDMINE || tile.GetObject() == MP2::OBJ_ABANDONEDMINE );
+    const bool isAbandonnedMine = ( tile.GetObject() == MP2::OBJN_ABANDONEDMINE || tile.GetObject() == MP2::OBJ_ABANDONEDMINE );
 
     if ( MP2::OBJ_MINES == tile.GetObject() ) {
         str = Maps::GetMinesName( tile.QuantityResourceCount().first );
