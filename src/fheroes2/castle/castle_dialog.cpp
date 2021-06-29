ac486cd68de444653f6df1987221b671218dba6d
eos428 <zkt1088@gmail.com>
2021-06-29 15:39:45 +0700

    put getPressedBuildingHotkey to anonymous namespace

--- src/fheroes2/castle/castle_dialog.cpp
+++ src/fheroes2/castle/castle_dialog.cpp
@@ -47,36 +47,39 @@
 
 void CastleRedrawTownName( const Castle & castle, const fheroes2::Point & dst );
 
-building_t getPressedBuildingHotkey()
+namespace
 {
-    if ( HotKeyPressEvent( Game::EVENT_TOWN_CREATURE_1 ) )
-        return DWELLING_MONSTER1;
-    else if ( HotKeyPressEvent( Game::EVENT_TOWN_CREATURE_2 ) )
-        return DWELLING_MONSTER2;
-    else if ( HotKeyPressEvent( Game::EVENT_TOWN_CREATURE_3 ) )
-        return DWELLING_MONSTER3;
-    else if ( HotKeyPressEvent( Game::EVENT_TOWN_CREATURE_4 ) )
-        return DWELLING_MONSTER4;
-    else if ( HotKeyPressEvent( Game::EVENT_TOWN_CREATURE_5 ) )
-        return DWELLING_MONSTER5;
-    else if ( HotKeyPressEvent( Game::EVENT_TOWN_CREATURE_6 ) )
-        return DWELLING_MONSTER6;
-    else if ( HotKeyPressEvent( Game::EVENT_TOWN_MARKETPLACE ) )
-        return BUILD_MARKETPLACE;
-    else if ( HotKeyPressEvent( Game::EVENT_TOWN_WELL ) )
-        return BUILD_WELL;
-    else if ( HotKeyPressEvent( Game::EVENT_TOWN_MAGE_GUILD ) )
-        return BUILD_MAGEGUILD;
-    else if ( HotKeyPressEvent( Game::EVENT_TOWN_SHIPYARD ) )
-        return BUILD_SHIPYARD;
-    else if ( HotKeyPressEvent( Game::EVENT_TOWN_THIEVES_GUILD ) )
-        return BUILD_THIEVESGUILD;
-    else if ( HotKeyPressEvent( Game::EVENT_TOWN_TAVERN ) )
-        return BUILD_TAVERN;
-    else if ( HotKeyPressEvent( Game::EVENT_TOWN_JUMP_TO_BUILD_SELECTION ) )
-        return BUILD_CASTLE;
+    building_t getPressedBuildingHotkey()
+    {
+        if ( HotKeyPressEvent( Game::EVENT_TOWN_CREATURE_1 ) )
+            return DWELLING_MONSTER1;
+        else if ( HotKeyPressEvent( Game::EVENT_TOWN_CREATURE_2 ) )
+            return DWELLING_MONSTER2;
+        else if ( HotKeyPressEvent( Game::EVENT_TOWN_CREATURE_3 ) )
+            return DWELLING_MONSTER3;
+        else if ( HotKeyPressEvent( Game::EVENT_TOWN_CREATURE_4 ) )
+            return DWELLING_MONSTER4;
+        else if ( HotKeyPressEvent( Game::EVENT_TOWN_CREATURE_5 ) )
+            return DWELLING_MONSTER5;
+        else if ( HotKeyPressEvent( Game::EVENT_TOWN_CREATURE_6 ) )
+            return DWELLING_MONSTER6;
+        else if ( HotKeyPressEvent( Game::EVENT_TOWN_MARKETPLACE ) )
+            return BUILD_MARKETPLACE;
+        else if ( HotKeyPressEvent( Game::EVENT_TOWN_WELL ) )
+            return BUILD_WELL;
+        else if ( HotKeyPressEvent( Game::EVENT_TOWN_MAGE_GUILD ) )
+            return BUILD_MAGEGUILD;
+        else if ( HotKeyPressEvent( Game::EVENT_TOWN_SHIPYARD ) )
+            return BUILD_SHIPYARD;
+        else if ( HotKeyPressEvent( Game::EVENT_TOWN_THIEVES_GUILD ) )
+            return BUILD_THIEVESGUILD;
+        else if ( HotKeyPressEvent( Game::EVENT_TOWN_TAVERN ) )
+            return BUILD_TAVERN;
+        else if ( HotKeyPressEvent( Game::EVENT_TOWN_JUMP_TO_BUILD_SELECTION ) )
+            return BUILD_CASTLE;
 
-    return BUILD_NOTHING;
+        return BUILD_NOTHING;
+    }
 }
 
 void RedrawIcons( const Castle & castle, const CastleHeroes & heroes, const fheroes2::Point & pt )
