db79461fa7d89d1a9011b6e22b7e13b56ffd6885
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2021-07-04 20:06:09 +0800

    Code style correction

--- src/fheroes2/ai/ai.h
+++ src/fheroes2/ai/ai.h
@@ -70,7 +70,10 @@ namespace AI
         virtual void CastleTurn( Castle & castle, bool defensive );
         virtual void BattleTurn( Battle::Arena & arena, const Battle::Unit & unit, Battle::Actions & actions );
         virtual void HeroTurn( Heroes & hero );
-        virtual bool HeroesTurn( VecHeroes & ) { return true; }
+        virtual bool HeroesTurn( VecHeroes & )
+        {
+            return true;
+        }
 
         virtual void revealFog( const Maps::Tiles & tile );
 
