ihhub/fheroes2

commit 95fe29b0b459f7f33792e9aa6d36260ea4005d72

Fix next hero choose logic (#3431)

src/fheroes2/gui/interface_events.cpp
@@ -129,7 +129,7 @@ void Interface::Basic::EventNextHero( void )
             ++it;
             if ( it == myHeroes.end() )
                 it = myHeroes.begin();
-            if ( ( *it )->MayStillMove() ) {
+            if ( ( *it )->MayStillMove( true ) ) {
                 SetFocus( *it );
                 CalculateHeroPath( *it, -1 );
                 break;
@@ -137,11 +137,10 @@ void Interface::Basic::EventNextHero( void )
         } while ( it != currentHero );
     }
     else {
-        const size_t heroesCount = myHeroes.size();
-        for ( size_t i = 0; i < heroesCount; ++i ) {
-            if ( myHeroes[i]->MayStillMove() ) {
-                SetFocus( myHeroes[i] );
-                CalculateHeroPath( myHeroes[i], -1 );
+        for ( Heroes * hero : myHeroes ) {
+            if ( hero->MayStillMove( true ) ) {
+                SetFocus( hero );
+                CalculateHeroPath( hero, -1 );
                 break;
             }
         }

src/fheroes2/heroes/heroes.h
@@ -251,7 +251,7 @@ public:
 
     u32 GetMovePoints( void ) const;
     void IncreaseMovePoints( u32 );
-    bool MayStillMove( void ) const;
+    bool MayStillMove( const bool ignorePath = true ) const;
     void ResetMovePoints( void );
     void MovePointsScaleFixed( void );
 

src/fheroes2/heroes/heroes.cpp
@@ -1409,12 +1409,16 @@ void Heroes::ResetMovePoints( void )
     move_point = 0;
 }
 
-bool Heroes::MayStillMove( void ) const
+bool Heroes::MayStillMove( const bool ignorePath ) const
 {
-    if ( Modes( SLEEPER | GUARDIAN ) || isFreeman() )
+    if ( Modes( SLEEPER | GUARDIAN ) || isFreeman() ) {
         return false;
+    }
 
-    return path.isValid() ? ( move_point >= path.getLastMovePenalty() ) : CanMove();
+    if ( path.isValid() && !ignorePath ) {
+        return move_point >= path.getLastMovePenalty();
+    }
+    return CanMove();
 }
 
 bool Heroes::MayCastAdventureSpells() const

commit 53e0ad1dc4b0bc1f635562361e64b506b3a6001b

Fix inifinte loop during AI turn
regression from #3431

src/fheroes2/ai/ai_base.cpp
@@ -114,7 +114,7 @@ namespace AI
 
     bool Base::HeroesCanMove( const Heroes & hero )
     {
-        return hero.MayStillMove() && !hero.Modes( Heroes::MOVED );
+        return hero.MayStillMove( false ) && !hero.Modes( Heroes::MOVED );
     }
 
     void Base::HeroTurn( Heroes & hero )

src/fheroes2/ai/normal/ai_normal_hero.cpp
@@ -830,7 +830,7 @@ namespace AI
                 }
             }
             hero->ResetModes( Heroes::WAITING | Heroes::MOVED | Heroes::SKIPPED_TURN );
-            if ( !hero->MayStillMove() ) {
+            if ( !hero->MayStillMove( false ) ) {
                 hero->SetModes( Heroes::MOVED );
             }
             else {
@@ -929,7 +929,7 @@ namespace AI
             }
 
             for ( size_t i = 0; i < availableHeroes.size(); ) {
-                if ( !availableHeroes[i].hero->MayStillMove() ) {
+                if ( !availableHeroes[i].hero->MayStillMove( false ) ) {
                     availableHeroes[i].hero->SetModes( Heroes::MOVED );
                     availableHeroes.erase( availableHeroes.begin() + i );
                     continue;
@@ -944,7 +944,7 @@ namespace AI
         const bool allHeroesMoved = availableHeroes.empty();
 
         for ( HeroToMove & heroInfo : availableHeroes ) {
-            if ( !heroInfo.hero->MayStillMove() ) {
+            if ( !heroInfo.hero->MayStillMove( false ) ) {
                 heroInfo.hero->SetModes( Heroes::MOVED );
             }
         }

src/fheroes2/gui/interface_buttons.cpp
@@ -202,7 +202,7 @@ fheroes2::GameMode Interface::ButtonsArea::QueueEventProcessing( void )
 void Interface::ButtonsArea::SetButtonStatus()
 {
     Heroes * currentHero = GetFocusHeroes();
-    if ( currentHero == nullptr || !currentHero->GetPath().isValid() || !currentHero->MayStillMove() )
+    if ( currentHero == nullptr || !currentHero->GetPath().isValid() || !currentHero->MayStillMove( false ) )
         buttonMovement.disable();
     else
         buttonMovement.enable();
@@ -217,7 +217,7 @@ void Interface::ButtonsArea::SetButtonStatus()
 
     bool isMovableHeroPresent = false;
     for ( size_t i = 0; i < heroes.size(); ++i ) {
-        if ( heroes[i]->MayStillMove() ) {
+        if ( heroes[i]->MayStillMove( false ) ) {
             isMovableHeroPresent = true;
             break;
         }

src/fheroes2/gui/interface_events.cpp
@@ -80,7 +80,7 @@ void Interface::Basic::ShowPathOrStartMoveHero( Heroes * hero, s32 destinationId
         CalculateHeroPath( hero, destinationIdx );
     }
     // start move
-    else if ( path.isValid() && hero->MayStillMove() ) {
+    else if ( path.isValid() && hero->MayStillMove( false ) ) {
         SetFocus( hero );
         RedrawFocus();
 

src/fheroes2/heroes/heroes.h
@@ -251,7 +251,7 @@ public:
 
     u32 GetMovePoints( void ) const;
     void IncreaseMovePoints( u32 );
-    bool MayStillMove( const bool ignorePath = true ) const;
+    bool MayStillMove( const bool ignorePath ) const;
     void ResetMovePoints( void );
     void MovePointsScaleFixed( void );
 

src/fheroes2/kingdom/kingdom.cpp
@@ -452,7 +452,7 @@ bool Kingdom::isValidKingdomObject( const Maps::Tiles & tile, int objectID ) con
 
 bool Kingdom::HeroesMayStillMove( void ) const
 {
-    return std::any_of( heroes.begin(), heroes.end(), []( const Heroes * hero ) { return hero->MayStillMove(); } );
+    return std::any_of( heroes.begin(), heroes.end(), []( const Heroes * hero ) { return hero->MayStillMove( false ); } );
 }
 
 u32 Kingdom::GetCountCapital( void ) const