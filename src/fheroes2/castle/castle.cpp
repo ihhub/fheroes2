ihhub/fheroes2

commit 1d628f562a916384202591954a70c7b5a67b8dd9

Differentiate castle entrance and castle tiles (#3866)

src/fheroes2/castle/castle.cpp
@@ -2770,7 +2770,7 @@ StreamBase & operator>>( StreamBase & msg, VecCastles & castles )
 
     for ( auto it = castles.begin(); it != castles.end(); ++it ) {
         msg >> index;
-        *it = ( index < 0 ? nullptr : world.GetCastle( Maps::GetPoint( index ) ) );
+        *it = ( index < 0 ? nullptr : world.getCastleEntrance( Maps::GetPoint( index ) ) );
     }
 
     return msg;

src/fheroes2/world/world.cpp
@@ -481,15 +481,37 @@ const Kingdom & World::GetKingdom( int color ) const
     return vec_kingdoms.GetKingdom( color );
 }
 
-/* get castle from index maps */
-Castle * World::GetCastle( const fheroes2::Point & center )
+Castle * World::getCastle( const fheroes2::Point & tilePosition )
 {
-    return vec_castles.Get( center );
+    return vec_castles.Get( tilePosition );
 }
 
-const Castle * World::GetCastle( const fheroes2::Point & center ) const
+const Castle * World::getCastle( const fheroes2::Point & tilePosition ) const
 {
-    return vec_castles.Get( center );
+    return vec_castles.Get( tilePosition );
+}
+
+const Castle * World::getCastleEntrance( const fheroes2::Point & tilePosition ) const
+{
+    if ( !isValidCastleEntrance( tilePosition ) ) {
+        return nullptr;
+    }
+
+    return vec_castles.Get( tilePosition );
+}
+
+Castle * World::getCastleEntrance( const fheroes2::Point & tilePosition )
+{
+    if ( !isValidCastleEntrance( tilePosition ) ) {
+        return nullptr;
+    }
+
+    return vec_castles.Get( tilePosition );
+}
+
+bool World::isValidCastleEntrance( const fheroes2::Point & tilePosition ) const
+{
+    return Maps::isValidAbsPoint( tilePosition.x, tilePosition.y ) && ( GetTiles( tilePosition.x, tilePosition.y ).GetObjectUID() == MP2::OBJ_CASTLE );
 }
 
 Heroes * World::GetHeroes( int id )
@@ -928,11 +950,9 @@ void World::CaptureObject( s32 index, int color )
     int obj = GetTiles( index ).GetObject( false );
     map_captureobj.Set( index, obj, color );
 
-    if ( MP2::OBJ_CASTLE == obj ) {
-        Castle * castle = GetCastle( Maps::GetPoint( index ) );
-        if ( castle && castle->GetColor() != color )
-            castle->ChangeColor( color );
-    }
+    Castle * castle = getCastleEntrance( Maps::GetPoint( index ) );
+    if ( castle && castle->GetColor() != color )
+        castle->ChangeColor( color );
 
     if ( color & ( Color::ALL | Color::UNUSED ) )
         GetTiles( index ).CaptureFlags32( obj, color );
@@ -1103,7 +1123,7 @@ bool World::KingdomIsWins( const Kingdom & kingdom, int wins ) const
         return kingdom.GetColor() == vec_kingdoms.GetNotLossColors();
 
     case GameOver::WINS_TOWN: {
-        const Castle * town = GetCastle( conf.WinsMapsPositionObject() );
+        const Castle * town = getCastleEntrance( conf.WinsMapsPositionObject() );
         // check comp also wins
         return ( kingdom.isControlHuman() || conf.WinsCompAlsoWins() ) && ( town && town->GetColor() == kingdom.GetColor() );
     }
@@ -1161,7 +1181,7 @@ bool World::KingdomIsLoss( const Kingdom & kingdom, int loss ) const
         return kingdom.isLoss();
 
     case GameOver::LOSS_TOWN: {
-        const Castle * town = GetCastle( conf.LossMapsPositionObject() );
+        const Castle * town = getCastleEntrance( conf.LossMapsPositionObject() );
         return ( town && town->GetColor() != kingdom.GetColor() );
     }
 

src/fheroes2/ai/ai_hero_action.cpp
@@ -549,10 +549,12 @@ namespace AI
     void AIToCastle( Heroes & hero, s32 dst_index )
     {
         const Settings & conf = Settings::Get();
-        Castle * castle = world.GetCastle( Maps::GetPoint( dst_index ) );
-
-        if ( !castle )
+        Castle * castle = world.getCastleEntrance( Maps::GetPoint( dst_index ) );
+        if ( castle == nullptr ) {
+            // Something is wrong while calling this function for incorrect tile.
+            assert( 0 );
             return;
+        }
 
         if ( hero.GetColor() == castle->GetColor() || ( conf.ExtUnionsAllowCastleVisiting() && Players::isFriends( hero.GetColor(), castle->GetColor() ) ) ) {
             DEBUG_LOG( DBG_AI, DBG_INFO, hero.GetName() << " goto castle " << castle->GetName() );

src/fheroes2/ai/normal/ai_normal_hero.cpp
@@ -36,7 +36,7 @@ namespace
 {
     bool AIShouldVisitCastle( const Heroes & hero, int castleIndex )
     {
-        const Castle * castle = world.GetCastle( Maps::GetPoint( castleIndex ) );
+        const Castle * castle = world.getCastleEntrance( Maps::GetPoint( castleIndex ) );
         if ( castle ) {
             if ( hero.GetColor() == castle->GetColor() ) {
                 return castle->GetHeroes().Guest() == nullptr;
@@ -499,7 +499,7 @@ namespace AI
         const int objectID = tile.GetObject();
 
         if ( objectID == MP2::OBJ_CASTLE ) {
-            const Castle * castle = world.GetCastle( Maps::GetPoint( index ) );
+            const Castle * castle = world.getCastleEntrance( Maps::GetPoint( index ) );
             if ( !castle )
                 return valueToIgnore;
 
@@ -769,7 +769,7 @@ namespace AI
                 const RegionStats & regionStats = _regions[world.GetTiles( node.first ).GetRegion()];
 
                 if ( heroStrength < regionStats.highestThreat ) {
-                    const Castle * castle = world.GetCastle( Maps::GetPoint( node.first ) );
+                    const Castle * castle = world.getCastleEntrance( Maps::GetPoint( node.first ) );
 
                     if ( castle && ( castle->GetGarrisonStrength( &hero ) <= 0 || castle->GetColor() == hero.GetColor() ) )
                         value -= dangerousTaskPenalty / 2;

src/fheroes2/ai/normal/ai_normal_kingdom.cpp
@@ -100,7 +100,7 @@ namespace AI
                     }
                 }
                 else if ( objectID == MP2::OBJ_CASTLE && tileColor != Color::NONE && !Players::isFriends( color, tileColor ) ) {
-                    const Castle * castle = world.GetCastle( Maps::GetPoint( idx ) );
+                    const Castle * castle = world.getCastleEntrance( Maps::GetPoint( idx ) );
                     if ( !castle )
                         continue;
 

src/fheroes2/battle/battle_arena.cpp
@@ -157,7 +157,7 @@ Battle::Arena::Arena( Army & a1, Army & a2, s32 index, bool local )
     , armies_order( nullptr )
     , current_color( Color::NONE )
     , preferredColor( -1 ) // be aware of unknown color
-    , castle( world.GetCastle( Maps::GetPoint( index ) ) )
+    , castle( world.getCastleEntrance( Maps::GetPoint( index ) ) )
     , _isTown( castle != nullptr )
     , catapult( nullptr )
     , bridge( nullptr )

src/fheroes2/game/game_over.cpp
@@ -50,7 +50,7 @@ namespace
 
         case GameOver::WINS_TOWN: {
             body = _( "You captured %{name}!\nYou are victorious." );
-            const Castle * town = world.GetCastle( conf.WinsMapsPositionObject() );
+            const Castle * town = world.getCastleEntrance( conf.WinsMapsPositionObject() );
             if ( town )
                 StringReplace( body, "%{name}", town->GetName() );
         } break;
@@ -102,7 +102,7 @@ namespace
         switch ( cond ) {
         case GameOver::LOSS_ENEMY_WINS_TOWN: {
             body = _( "The enemy has captured %{name}!\nThey are triumphant." );
-            const Castle * town = world.GetCastle( conf.WinsMapsPositionObject() );
+            const Castle * town = world.getCastleEntrance( conf.WinsMapsPositionObject() );
             if ( town )
                 StringReplace( body, "%{name}", town->GetName() );
             break;
@@ -135,7 +135,7 @@ namespace
 
         case GameOver::LOSS_TOWN: {
             body = _( "The enemy has captured %{name}!\nThey are triumphant." );
-            const Castle * town = world.GetCastle( conf.LossMapsPositionObject() );
+            const Castle * town = world.getCastleEntrance( conf.LossMapsPositionObject() );
             if ( town )
                 StringReplace( body, "%{name}", town->GetName() );
             break;
@@ -226,10 +226,9 @@ std::string GameOver::GetActualDescription( int cond )
     if ( WINS_ALL == cond || WINS_SIDE == cond )
         msg = GetString( WINS_ALL );
     else if ( WINS_TOWN & cond ) {
-        const Castle * town = world.GetCastle( conf.WinsMapsPositionObject() );
+        const Castle * town = world.getCastleEntrance( conf.WinsMapsPositionObject() );
         if ( town ) {
             msg = town->isCastle() ? _( "Capture the castle '%{name}'" ) : _( "Capture the town '%{name}'" );
-            ;
             StringReplace( msg, "%{name}", town->GetName() );
         }
     }
@@ -260,7 +259,7 @@ std::string GameOver::GetActualDescription( int cond )
     if ( LOSS_ALL == cond )
         msg = GetString( LOSS_ALL );
     else if ( LOSS_TOWN & cond ) {
-        const Castle * town = world.GetCastle( conf.LossMapsPositionObject() );
+        const Castle * town = world.getCastleEntrance( conf.LossMapsPositionObject() );
         if ( town ) {
             msg = town->isCastle() ? _( "Lose the castle '%{name}'." ) : _( "Lose the town '%{name}'." );
             StringReplace( msg, "%{name}", town->GetName() );

src/fheroes2/game/game_startgame.cpp
@@ -348,13 +348,12 @@ void ShowWarningLostTownsDialog()
     }
 }
 
-/* return changee cursor */
 int Interface::Basic::GetCursorFocusCastle( const Castle & from_castle, const Maps::Tiles & tile )
 {
     switch ( tile.GetObject() ) {
     case MP2::OBJN_CASTLE:
     case MP2::OBJ_CASTLE: {
-        const Castle * to_castle = world.GetCastle( tile.GetCenter() );
+        const Castle * to_castle = world.getCastleEntrance( tile.GetCenter() );
 
         if ( nullptr != to_castle )
             return to_castle->GetColor() == from_castle.GetColor() ? Cursor::CASTLE : Cursor::POINTER;
@@ -388,7 +387,7 @@ int Interface::Basic::GetCursorFocusShipmaster( const Heroes & from_hero, const 
 
     case MP2::OBJN_CASTLE:
     case MP2::OBJ_CASTLE: {
-        const Castle * castle = world.GetCastle( tile.GetCenter() );
+        const Castle * castle = world.getCastle( tile.GetCenter() );
 
         if ( castle )
             return from_hero.GetColor() == castle->GetColor() ? Cursor::CASTLE : Cursor::POINTER;
@@ -443,7 +442,7 @@ int Interface::Basic::GetCursorFocusHeroes( const Heroes & from_hero, const Maps
 
     case MP2::OBJN_CASTLE:
     case MP2::OBJ_CASTLE: {
-        const Castle * castle = world.GetCastle( tile.GetCenter() );
+        const Castle * castle = world.getCastle( tile.GetCenter() );
 
         if ( nullptr != castle ) {
             if ( tile.GetObject() == MP2::OBJN_CASTLE ) {
@@ -1150,7 +1149,7 @@ void Interface::Basic::MouseCursorAreaClickLeft( const int32_t index_maps )
         if ( MP2::OBJN_CASTLE != tileObjId && MP2::OBJ_CASTLE != tileObjId )
             break;
 
-        Castle * to_castle = world.GetCastle( tile.GetCenter() );
+        Castle * to_castle = world.getCastle( tile.GetCenter() );
         if ( to_castle == nullptr )
             break;
 
@@ -1208,7 +1207,7 @@ void Interface::Basic::MouseCursorAreaPressRight( s32 index_maps ) const
             switch ( tile.GetObject() ) {
             case MP2::OBJN_CASTLE:
             case MP2::OBJ_CASTLE: {
-                const Castle * castle = world.GetCastle( tile.GetCenter() );
+                const Castle * castle = world.getCastle( tile.GetCenter() );
                 if ( castle )
                     Dialog::QuickInfo( *castle );
                 else

commit 719992525ba8f6be207b152b26e5d489a760725a

Fix missing castle list
regression from #3866

src/fheroes2/castle/castle.cpp
@@ -2771,6 +2771,7 @@ StreamBase & operator>>( StreamBase & msg, VecCastles & castles )
     for ( auto it = castles.begin(); it != castles.end(); ++it ) {
         msg >> index;
         *it = ( index < 0 ? nullptr : world.getCastleEntrance( Maps::GetPoint( index ) ) );
+        assert( *it != nullptr );
     }
 
     return msg;

src/fheroes2/world/world.cpp
@@ -511,7 +511,7 @@ Castle * World::getCastleEntrance( const fheroes2::Point & tilePosition )
 
 bool World::isValidCastleEntrance( const fheroes2::Point & tilePosition ) const
 {
-    return Maps::isValidAbsPoint( tilePosition.x, tilePosition.y ) && ( GetTiles( tilePosition.x, tilePosition.y ).GetObjectUID() == MP2::OBJ_CASTLE );
+    return Maps::isValidAbsPoint( tilePosition.x, tilePosition.y ) && ( GetTiles( tilePosition.x, tilePosition.y ).GetObject( false ) == MP2::OBJ_CASTLE );
 }
 
 Heroes * World::GetHeroes( int id )