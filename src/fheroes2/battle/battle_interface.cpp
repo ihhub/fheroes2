ihhub/fheroes2

commit f00d0d75b53bcbd688653cd86a4ccb3f60743c89

Highlight cells from where a monster will attack (#3911)

src/fheroes2/battle/battle_interface.cpp
@@ -1529,8 +1529,9 @@ void Battle::Interface::RedrawCover()
 
     // cursor
     const Cell * cell = Board::GetCell( index_pos );
+    const int cursorType = Cursor::Get().Themes();
 
-    if ( cell && _currentUnit && conf.BattleShowMouseShadow() && Cursor::Get().Themes() != Cursor::WAR_NONE ) {
+    if ( cell && _currentUnit && conf.BattleShowMouseShadow() && cursorType != Cursor::WAR_NONE ) {
         std::set<const Cell *> highlightCells;
 
         if ( humanturn_spell.isValid() ) {
@@ -1581,7 +1582,7 @@ void Battle::Interface::RedrawCover()
             }
         }
         else if ( _currentUnit->isAbilityPresent( fheroes2::MonsterAbilityType::AREA_SHOT )
-                  && ( Cursor::Get().Themes() == Cursor::WAR_ARROW || Cursor::Get().Themes() == Cursor::WAR_BROKENARROW ) ) {
+                  && ( cursorType == Cursor::WAR_ARROW || cursorType == Cursor::WAR_BROKENARROW ) ) {
             highlightCells.emplace( cell );
             const Indexes around = Board::GetAroundIndexes( index_pos );
             for ( size_t i = 0; i < around.size(); ++i ) {
@@ -1591,7 +1592,7 @@ void Battle::Interface::RedrawCover()
                 }
             }
         }
-        else if ( _currentUnit->GetTailIndex() != -1 && ( Cursor::Get().Themes() == Cursor::WAR_MOVE || Cursor::Get().Themes() == Cursor::WAR_FLY ) ) {
+        else if ( _currentUnit->GetTailIndex() != -1 && ( cursorType == Cursor::WAR_MOVE || cursorType == Cursor::WAR_FLY ) ) {
             highlightCells.emplace( cell );
             int tailDirection = _currentUnit->isReflect() ? RIGHT : LEFT;
 
@@ -1611,6 +1612,45 @@ void Battle::Interface::RedrawCover()
                         highlightCells.emplace( tailCell );
                     }
                 }
+            }
+        }
+        else if ( cursorType == Cursor::SWORD_TOPLEFT || cursorType == Cursor::SWORD_TOPRIGHT || cursorType == Cursor::SWORD_BOTTOMLEFT
+                  || cursorType == Cursor::SWORD_BOTTOMRIGHT || cursorType == Cursor::SWORD_LEFT || cursorType == Cursor::SWORD_RIGHT ) {
+            highlightCells.emplace( cell );
+
+            int direction = 0;
+            if ( cursorType == Cursor::SWORD_TOPLEFT ) {
+                direction = BOTTOM_RIGHT;
+            }
+            else if ( cursorType == Cursor::SWORD_TOPRIGHT ) {
+                direction = BOTTOM_LEFT;
+            }
+            else if ( cursorType == Cursor::SWORD_BOTTOMLEFT ) {
+                direction = TOP_RIGHT;
+            }
+            else if ( cursorType == Cursor::SWORD_BOTTOMRIGHT ) {
+                direction = TOP_LEFT;
+            }
+            else if ( cursorType == Cursor::SWORD_LEFT ) {
+                direction = RIGHT;
+            }
+            else if ( cursorType == Cursor::SWORD_RIGHT ) {
+                direction = LEFT;
+            }
+            else {
+                assert( 0 );
+            }
+
+            const Cell * attackerCell = Board::GetCell( cell->GetIndex(), direction );
+            assert( attackerCell != nullptr );
+            highlightCells.emplace( attackerCell );
+
+            if ( _currentUnit->GetTailIndex() != -1 ) {
+                const int tailDirection = _currentUnit->isReflect() ? RIGHT : LEFT;
+                const Cell * tailAttackerCell = Board::GetCell( attackerCell->GetIndex(), tailDirection );
+                assert( tailAttackerCell != nullptr );
+
+                highlightCells.emplace( tailAttackerCell );
             }
         }
         else {

commit 782c3ccd1d3688f170b5164d7c45716815fd27a4

Fix an assertion call during battle shadow rendering
for wide monsters which are at the end of the battlefield

regression from #3911

src/fheroes2/battle/battle_interface.cpp
@@ -1646,8 +1646,13 @@ void Battle::Interface::RedrawCover()
             highlightCells.emplace( attackerCell );
 
             if ( _currentUnit->GetTailIndex() != -1 ) {
-                const int tailDirection = _currentUnit->isReflect() ? RIGHT : LEFT;
+                int tailDirection = _currentUnit->isReflect() ? RIGHT : LEFT;
                 const Cell * tailAttackerCell = Board::GetCell( attackerCell->GetIndex(), tailDirection );
+                if ( tailAttackerCell == nullptr ) {
+                    // A monster doesn't move.
+                    tailDirection = _currentUnit->isReflect() ? LEFT : RIGHT;
+                    tailAttackerCell = Board::GetCell( attackerCell->GetIndex(), tailDirection );
+                }
                 assert( tailAttackerCell != nullptr );
 
                 highlightCells.emplace( tailAttackerCell );