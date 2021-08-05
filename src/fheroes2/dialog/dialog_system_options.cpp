24c5489031feacd4f31db919034479e38c75a94f
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2021-08-05 19:54:53 +0800

    Code style fix

--- src/fheroes2/dialog/dialog_system_options.cpp
+++ src/fheroes2/dialog/dialog_system_options.cpp
@@ -407,8 +407,8 @@ namespace fheroes2
 
                 const fheroes2::Rect newRoi = gamearea.GetROI();
 
-                gamearea.SetCenterInPixels( prevCenter + fheroes2::Point( newRoi.width / 2, newRoi.height / 2 ) + fheroes2::Point( newRoi.x, newRoi.y ) -
-                                            fheroes2::Point( prevRoi.width / 2, prevRoi.height / 2 ) - fheroes2::Point( prevRoi.x, prevRoi.y ) );
+                gamearea.SetCenterInPixels( prevCenter + fheroes2::Point( newRoi.width / 2, newRoi.height / 2 ) + fheroes2::Point( newRoi.x, newRoi.y )
+                                            - fheroes2::Point( prevRoi.width / 2, prevRoi.height / 2 ) - fheroes2::Point( prevRoi.x, prevRoi.y ) );
 
                 // We need to redraw radar first due to the nature of restorers. Only then we can redraw everything.
                 basicInterface.Redraw( Interface::REDRAW_RADAR );
