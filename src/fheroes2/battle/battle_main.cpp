b5fbc642b995026ac87ce2b726d6697dff7dea07
vincent-grosbois <vincent.grosbois@gmail.com>
2021-08-26 22:43:20 +0200

    Fix bug in manual/auto battle seed selection

--- src/fheroes2/battle/battle_main.cpp
+++ src/fheroes2/battle/battle_main.cpp
@@ -101,8 +101,11 @@ Battle::Result Battle::Loader( Army & army1, Army & army2, s32 mapsindex )
 
     const size_t battleDeterministicSeed = static_cast<size_t>( mapsindex ) + static_cast<size_t>( world.GetMapSeed() );
     const size_t battlePureRandomSeed = Rand::Get( std::numeric_limits<uint32_t>::max() );
-    const size_t battleSeed = Settings::Get().ExtBattleDeterministicResult() ? battleDeterministicSeed : battlePureRandomSeed;
-    Rand::DeterministicRandomGenerator randomGenerator( battleSeed );
+
+    const size_t autoBattleSeed = battleDeterministicSeed;
+    const size_t manualBattleSeed = Settings::Get().ExtBattleDeterministicResult() ? battleDeterministicSeed : battlePureRandomSeed;
+
+    Rand::DeterministicRandomGenerator randomGenerator( showBattle ? manualBattleSeed : autoBattleSeed );
 
     std::unique_ptr<Arena> arena( new Arena( army1, army2, mapsindex, showBattle, randomGenerator ) );
 
@@ -139,8 +142,8 @@ Battle::Result Battle::Loader( Army & army1, Army & army2, s32 mapsindex )
             // Have to destroy old Arena instance first
             arena.reset();
 
-            // reset random seed to the deterministic seed
-            randomGenerator.UpdateSeed( battleDeterministicSeed );
+            // reset random seed to correspond to manual battle
+            randomGenerator.UpdateSeed( manualBattleSeed );
 
             arena = std::unique_ptr<Arena>( new Arena( army1, army2, mapsindex, true, randomGenerator ) );
 
