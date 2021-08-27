a4d4dc052017fa3689bdadbe302f7e6b909dbb99
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2021-08-27 21:56:34 +0800

    Fix Radar appearance while right clicking on hero/castle (#4087)
    close #4037
    
    also exclude one directory from SonarQube scan

--- src/fheroes2/heroes/heroes_dialog.cpp
+++ src/fheroes2/heroes/heroes_dialog.cpp
@@ -327,7 +327,7 @@ int Heroes::OpenDialog( bool readonly /* = false */, bool fade /* = false */, bo
 
         // right info
         if ( !readonly && le.MousePressRight( portPos ) )
-            Dialog::QuickInfo( *this );
+            Dialog::QuickInfo( *this, fheroes2::Rect( 0, 0, display.width(), display.height() ) );
         else if ( le.MousePressRight( rectSpreadArmyFormat ) )
             Dialog::Message( _( "Spread Formation" ), descriptionSpreadArmyFormat, Font::BIG );
         else if ( le.MousePressRight( rectGroupedArmyFormat ) )
