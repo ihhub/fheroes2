5f02d72f4dde94f783e97816dac086c3dbebbb05
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2021-09-04 14:11:01 +0800

    Fix incorrect object info when a hero stands on it
    close #4143
    
    also moved all functions under anonymous namespace

--- src/fheroes2/dialog/dialog.h
+++ src/fheroes2/dialog/dialog.h
@@ -86,7 +86,7 @@ namespace Dialog
     std::string SelectFileLoad( void );
     std::string SelectFileSave( void );
     // show info cell maps
-    void QuickInfo( const Maps::Tiles & tile );
+    void QuickInfo( const Maps::Tiles & tile, const bool ignoreHeroOnTile = false );
     void QuickInfo( const Castle & castle, const fheroes2::Rect & activeArea, const fheroes2::Point & position = fheroes2::Point() );
     void QuickInfo( const Heroes & hero, const fheroes2::Rect & activeArea, const fheroes2::Point & position = fheroes2::Point() );
     int Message( const std::string &, const std::string &, int ft, int buttons = 0 /* buttons: OK : CANCEL : OK|CANCEL : YES|NO */ );
