e85ec69529e66e6f104e3f001ffa2203c626c0c3
Oleg Derevenetz <oleg-derevenetz@yandex.ru>
2021-09-05 11:43:42 +0300

    anim_extract_windows.ps1: use Start-Process -Wait instead of "&" to run DOSBOX, don't wait for a key to be pressed upon successful completion

--- script/homm2/anim_extract_windows.ps1
+++ script/homm2/anim_extract_windows.ps1
@@ -1,6 +1,8 @@
 $ErrorActionPreference = "Stop"
 
 try {
+    $successfulExtraction = $false
+
     Write-Host -ForegroundColor Green "This script will extract animation resources from the original Heroes of Might and Magic II CD image provided by GOG`r`n"
 
     if ((Test-Path -Path "MUSIC" -PathType Container) -And
@@ -13,17 +15,22 @@ try {
 
         Write-Host -ForegroundColor Green "Running DOSBOX to extract animation resources, please wait..."
 
-        & "DOSBOX\DOSBox.exe" -c "imgmount D homm2.ins -t iso -fs iso" `
-                              -c "mount E ." `
-                              -c "copy D:\HEROES2\ANIM\*.* E:\ANIM" `
-                              -c "exit"
+        Start-Process -FilePath "DOSBOX\DOSBox.exe" -ArgumentList @("-c", "`"imgmount D homm2.ins -t iso -fs iso`"", `
+                                                                    "-c", "`"mount E .`"", `
+                                                                    "-c", "`"copy D:\HEROES2\ANIM\*.* E:\ANIM`"", `
+                                                                    "-c", "exit") `
+                      -Wait
+
+        $successfulExtraction = $true
     } else {
         Write-Host -ForegroundColor Red "FATAL ERROR: Unable to find GOG files"
     }
 } catch {
     Write-Host -ForegroundColor Red (-Join("FATAL ERROR: ", ($_ | Out-String)))
 } finally {
-    Write-Host "Press any key to exit..."
+    if (-Not $successfulExtraction) {
+        Write-Host "Press any key to exit..."
 
-    [void][System.Console]::ReadKey($true);
+        [void][System.Console]::ReadKey($true);
+    }
 }
