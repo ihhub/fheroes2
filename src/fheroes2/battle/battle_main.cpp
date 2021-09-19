e6ad05cc593c093a01737c43030be265263fbc02
Alex Shulgin <alex.shulgin@gmail.com>
2021-09-19 15:15:34 +0200

    even less duplication

--- src/fheroes2/battle/battle_main.cpp
+++ src/fheroes2/battle/battle_main.cpp
@@ -207,8 +207,7 @@ Battle::Result Battle::Loader( Army & army1, Army & army2, s32 mapsindex )
 
         if ( showBattle ) {
             // fade arena
-            const bool clearMessageLog
-                = ( result.army1 & RESULT_RETREAT ) || ( result.army2 & RESULT_RETREAT ) || ( result.army1 & RESULT_SURRENDER ) || ( result.army2 & RESULT_SURRENDER );
+            const bool clearMessageLog = ( result.army1 & ( RESULT_RETREAT | RESULT_SURRENDER ) ) || ( result.army2 & ( RESULT_RETREAT | RESULT_SURRENDER ) );
             arena->FadeArena( clearMessageLog );
         }
 
