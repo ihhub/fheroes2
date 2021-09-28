3dfc5ad6164e86daf043c40bbafa4e3f8ffe3eb4
Alex Shulgin <alex.shulgin@gmail.com>
2021-09-28 09:05:08 +0200

    Replace scrollbar in Experimental Settings dialog
    Fix #1420

--- src/fheroes2/dialog/dialog_settings.cpp
+++ src/fheroes2/dialog/dialog_settings.cpp
@@ -97,18 +97,14 @@ void SettingsListBox::RedrawBackground( const fheroes2::Point & origin )
 {
     fheroes2::Display & display = fheroes2::Display::instance();
 
-    const int window_h = 400;
-    const int ah = window_h - 54;
+    const int windowHeight = 400;
+    const int scrollbarHeight = windowHeight - 64 - 2 * 16; // up/down buttons are 16px tall
 
     _restorer.restore();
 
-    for ( int ii = 1; ii < ( window_h / 25 ); ++ii )
-        fheroes2::Blit( fheroes2::AGG::GetICN( ICN::DROPLISL, 11 ), display, origin.x + 295, origin.y + 35 + ( 19 * ii ) );
-
-    fheroes2::Blit( fheroes2::AGG::GetICN( ICN::DROPLISL, 10 ), display, origin.x + 295, origin.y + 46 );
-
-    const fheroes2::Sprite & lowerPart = fheroes2::AGG::GetICN( ICN::DROPLISL, 12 );
-    fheroes2::Blit( lowerPart, display, origin.x + 295, origin.y + ah - lowerPart.height() );
+    const fheroes2::Image & sprite = fheroes2::AGG::GetICN( ICN::ESCROLL, 1 );
+    fheroes2::Blit( sprite, 0, 0, display, origin.x + 295, origin.y + 41, 16, scrollbarHeight / 2 );
+    fheroes2::Blit( sprite, 0, sprite.height() - scrollbarHeight / 2, display, origin.x + 295, origin.y + 41 + scrollbarHeight / 2, 16, scrollbarHeight / 2 );
 }
 
 void SettingsListBox::ActionListDoubleClick( u32 & item )
@@ -192,9 +188,9 @@ void Dialog::ExtSettings( bool readonly )
     const int ah = 340;
 
     listbox.RedrawBackground( area.getPosition() );
-    listbox.SetScrollButtonUp( ICN::DROPLISL, 6, 7, fheroes2::Point( area.x + 295, area.y + 25 ) );
-    listbox.SetScrollButtonDn( ICN::DROPLISL, 8, 9, fheroes2::Point( area.x + 295, area.y + ah + 5 ) );
-    listbox.SetScrollBar( fheroes2::AGG::GetICN( ICN::DROPLISL, 13 ), fheroes2::Rect( area.x + 300, area.y + 49, 12, ah - 47 ) );
+    listbox.SetScrollButtonUp( ICN::ESCROLL, 4, 5, fheroes2::Point( area.x + 295, area.y + 25 ) );
+    listbox.SetScrollButtonDn( ICN::ESCROLL, 6, 7, fheroes2::Point( area.x + 295, area.y + ah + 5 ) );
+    listbox.SetScrollBar( fheroes2::AGG::GetICN( ICN::ESCROLL, 3 ), fheroes2::Rect( area.x + 298, area.y + 44, 10, ah - 42 ) );
     listbox.SetAreaMaxItems( ah / 40 );
     listbox.SetAreaItems( fheroes2::Rect( area.x + 10, area.y + 30, 290, ah + 5 ) );
     listbox.SetListContent( states );
