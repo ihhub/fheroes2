b3b0bfa5fe354945d1a5a8faea834fa489f9e7c9
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2021-10-30 23:02:17 +0800

    Fix interaction passability with water objects (#4522)

--- src/fheroes2/maps/mp2.cpp
+++ src/fheroes2/maps/mp2.cpp
@@ -1164,9 +1164,11 @@ bool MP2::isNeedStayFront( const MapObjectType objectType )
     case OBJ_BOAT:
     case OBJ_BARRIER:
     case OBJ_JAIL:
-    case OBJ_SHIPWRECK:
     case OBJ_BUOY:
     case OBJ_SKELETON:
+    case OBJ_DERELICTSHIP:
+    case OBJ_MERMAID:
+    case OBJ_SIRENS:
         return true;
     default:
         break;
