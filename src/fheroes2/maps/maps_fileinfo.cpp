ihhub/fheroes2

commit c5d7cdd9d0a426c14f59388d66a8940d691857b1

Fix Passability for Reefs (#4514)
relates to #4495
- also deduplicate code
- update readability of code related to map format

src/fheroes2/maps/maps_fileinfo.cpp
@@ -38,6 +38,7 @@
 #include "maps_fileinfo.h"
 #include "maps_tiles.h"
 #include "mp2.h"
+#include "mp2_helper.h"
 #include "race.h"
 #include "serialize.h"
 #include "settings.h"
@@ -340,18 +341,10 @@ bool Maps::FileInfo::ReadMP2( const std::string & filename )
     // If the color is under computer control only we have to make it as an ally for human player.
     if ( conditions_loss == LOSS_HERO && conditions_wins == VICTORY_DEFEAT_EVERYONE && Colors( allow_human_colors ).size() == 1 ) {
         // Each tile needs 16 + 8 + 8 + 8 + 8 + 8 + 8 + 8 + 8 + 16 + 32 + 32 = 160 bits or 20 bytes.
-        fs.seek( MP2OFFSETDATA + ( loss1 + loss2 * size_w ) * 20 );
+        fs.seek( MP2::MP2OFFSETDATA + ( loss1 + loss2 * size_w ) * 20 );
 
         MP2::mp2tile_t mp2tile;
-        mp2tile.tileIndex = fs.getLE16();
-        mp2tile.objectName1 = fs.get();
-        mp2tile.indexName1 = fs.get();
-        mp2tile.quantity1 = fs.get();
-        mp2tile.quantity2 = fs.get();
-        mp2tile.objectName2 = fs.get();
-        mp2tile.indexName2 = fs.get();
-        mp2tile.flags = fs.get();
-        mp2tile.mapObject = fs.get();
+        MP2::loadTile( fs, mp2tile );
 
         // offset first addon
         fs.getLE16();

src/fheroes2/maps/mp2_helper.cpp
@@ -1,8 +1,6 @@
 /***************************************************************************
- *   Copyright (C) 2009 by Andrey Afletdinov <fheroes2@gmail.com>          *
- *                                                                         *
- *   Part of the Free Heroes2 Engine:                                      *
- *   http://sourceforge.net/projects/fheroes2                              *
+ *   Free Heroes of Might and Magic II: https://github.com/ihhub/fheroes2  *
+ *   Copyright (C) 2021                                                    *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
@@ -20,36 +18,34 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 
-#ifndef H2OBJXLOC_H
-#define H2OBJXLOC_H
+#include "mp2_helper.h"
+#include "mp2.h"
+#include "serialize.h"
 
-#include "types.h"
-
-namespace ObjXlc1
+namespace MP2
 {
-    int GetPassable( const uint8_t index );
-    bool isAction( u32 index );
-    bool isShadow( const uint8_t index );
-    int GetActionObject( u32 index );
+    void loadTile( StreamBase & stream, mp2tile_t & tile )
+    {
+        tile.surfaceType = stream.getLE16();
+        tile.objectName1 = stream.get();
+        tile.level1IcnImageIndex = stream.get();
+        tile.quantity1 = stream.get();
+        tile.quantity2 = stream.get();
+        tile.objectName2 = stream.get();
+        tile.level2IcnImageIndex = stream.get();
+        tile.flags = stream.get();
+        tile.mapObjectType = stream.get();
+    }
+
+    void loadAddon( StreamBase & stream, mp2addon_t & addon )
+    {
+        addon.nextAddonIndex = stream.getLE16();
+        addon.objectNameN1 = stream.get() * 2;
+        addon.indexNameN1 = stream.get();
+        addon.quantityN = stream.get();
+        addon.objectNameN2 = stream.get();
+        addon.indexNameN2 = stream.get();
+        addon.level1ObjectUID = stream.getLE32();
+        addon.level2ObjectUID = stream.getLE32();
+    }
 }
-
-namespace ObjXlc2
-{
-    int GetPassable( const uint8_t index );
-    bool isAction( u32 index );
-    bool isShadow( const uint8_t index );
-    int GetActionObject( u32 index );
-
-    // Returns true if the index belongs to Reefs type of the object.
-    bool isReefs( const uint8_t index );
-}
-
-namespace ObjXlc3
-{
-    int GetPassable( const uint8_t index );
-    bool isAction( u32 index );
-    bool isShadow( const uint8_t index );
-    int GetActionObject( u32 index );
-}
-
-#endif

fheroes2-vs2015.vcxproj
@@ -348,6 +348,7 @@
     <ClCompile Include="src\fheroes2\maps\maps_tiles.cpp" />
     <ClCompile Include="src\fheroes2\maps\maps_tiles_quantity.cpp" />
     <ClCompile Include="src\fheroes2\maps\mp2.cpp" />
+    <ClCompile Include="src\fheroes2\maps\mp2_helper.cpp" />
     <ClCompile Include="src\fheroes2\maps\position.cpp" />
     <ClCompile Include="src\fheroes2\maps\visit.cpp" />
     <ClCompile Include="src\fheroes2\monster\monster.cpp" />
@@ -516,6 +517,7 @@
     <ClInclude Include="src\fheroes2\maps\maps_objects.h" />
     <ClInclude Include="src\fheroes2\maps\maps_tiles.h" />
     <ClInclude Include="src\fheroes2\maps\mp2.h" />
+	<ClInclude Include="src\fheroes2\maps\mp2_helper.h" />
     <ClInclude Include="src\fheroes2\maps\pairs.h" />
     <ClInclude Include="src\fheroes2\maps\position.h" />
     <ClInclude Include="src\fheroes2\maps\visit.h" />

fheroes2-vs2019.vcxproj
@@ -349,6 +349,7 @@
     <ClCompile Include="src\fheroes2\maps\maps_tiles.cpp" />
     <ClCompile Include="src\fheroes2\maps\maps_tiles_quantity.cpp" />
     <ClCompile Include="src\fheroes2\maps\mp2.cpp" />
+    <ClCompile Include="src\fheroes2\maps\mp2_helper.cpp" />
     <ClCompile Include="src\fheroes2\maps\position.cpp" />
     <ClCompile Include="src\fheroes2\maps\visit.cpp" />
     <ClCompile Include="src\fheroes2\monster\monster.cpp" />
@@ -517,6 +518,7 @@
     <ClInclude Include="src\fheroes2\maps\maps_objects.h" />
     <ClInclude Include="src\fheroes2\maps\maps_tiles.h" />
     <ClInclude Include="src\fheroes2\maps\mp2.h" />
+    <ClInclude Include="src\fheroes2\maps\mp2_helper.h" />
     <ClInclude Include="src\fheroes2\maps\pairs.h" />
     <ClInclude Include="src\fheroes2\maps\position.h" />
     <ClInclude Include="src\fheroes2\maps\visit.h" />

src/fheroes2/maps/maps_tiles.cpp
@@ -153,6 +153,11 @@ namespace
         return false;
     }
 
+    bool isValidReefsSprite( const int icn, const uint8_t icnIndex )
+    {
+        return icn == ICN::X_LOC2 && ObjXlc2::isReefs( icnIndex );
+    }
+
 #if defined( VERIFY_SHADOW_SPRITES )
     // Define VERIFY_SHADOW_SPRITES macro to be able to use these functions.
     bool isShadowImage( const fheroes2::Image & image )
@@ -503,7 +508,7 @@ MP2::MapObjectType Maps::Tiles::GetLoyaltyObject( const uint8_t tileset, const u
             return MP2::OBJ_SIRENS;
         else if ( 46 < icnIndex && icnIndex < 111 )
             return MP2::OBJN_SIRENS;
-        else if ( 110 < icnIndex && icnIndex < 136 )
+        else if ( ObjXlc2::isReefs( icnIndex ) )
             return MP2::OBJ_REEFS;
         break;
 
@@ -728,9 +733,9 @@ void Maps::Tiles::Init( s32 index, const MP2::mp2tile_t & mp2 )
     quantity3 = 0;
     fog_colors = Color::ALL;
 
-    SetTile( mp2.tileIndex, mp2.flags );
+    SetTile( mp2.surfaceType, mp2.flags );
     SetIndex( index );
-    SetObject( static_cast<MP2::MapObjectType>( mp2.mapObject ) );
+    SetObject( static_cast<MP2::MapObjectType>( mp2.mapObjectType ) );
 
     addons_level1.clear();
     addons_level2.clear();
@@ -739,12 +744,12 @@ void Maps::Tiles::Init( s32 index, const MP2::mp2tile_t & mp2 )
     tileIsRoad = ( mp2.objectName1 >> 1 ) & 1;
 
     // If an object has priority 2 (shadow) or 3 (ground) then we put it as an addon.
-    if ( mp2.mapObject == MP2::OBJ_ZERO && ( _level >> 1 ) & 1 ) {
+    if ( mp2.mapObjectType == MP2::OBJ_ZERO && ( _level >> 1 ) & 1 ) {
         AddonsPushLevel1( mp2 );
     }
     else {
         objectTileset = mp2.objectName1;
-        objectIndex = mp2.indexName1;
+        objectIndex = mp2.level1IcnImageIndex;
         uniq = mp2.level1ObjectUID;
     }
     AddonsPushLevel2( mp2 );
@@ -906,6 +911,12 @@ int Maps::Tiles::getOriginalPassability() const
     if ( ( objectTileset == 0 || objectIndex == 255 ) || ( ( _level >> 1 ) & 1 ) || isShadow() ) {
         // No object exists. Make it fully passable.
         return DIRECTION_ALL;
+    }
+
+    if ( isValidReefsSprite( MP2::GetICNObject( objectTileset ), objectIndex ) ) {
+        // If this assertion blows up we miss somewhere the conversion from Stones type to Reefs.
+        assert( objectType == MP2::OBJ_REEFS );
+        return 0;
     }
 
     // Objects have fixed passability.
@@ -1084,8 +1095,8 @@ bool Maps::Tiles::isClearGround() const
 
 void Maps::Tiles::AddonsPushLevel1( const MP2::mp2tile_t & mt )
 {
-    if ( mt.objectName1 && mt.indexName1 < 0xFF ) {
-        addons_level1.emplace_back( mt.quantity1, mt.level1ObjectUID, mt.objectName1, mt.indexName1 );
+    if ( mt.objectName1 != 0 && mt.level1IcnImageIndex != 0xFF ) {
+        addons_level1.emplace_back( mt.quantity1, mt.level1ObjectUID, mt.objectName1, mt.level1IcnImageIndex );
     }
 
     // MP2 "objectName" is a bitfield
@@ -1108,8 +1119,8 @@ void Maps::Tiles::AddonsPushLevel1( const TilesAddon & ta )
 
 void Maps::Tiles::AddonsPushLevel2( const MP2::mp2tile_t & mt )
 {
-    if ( mt.objectName2 && mt.indexName2 < 0xFF ) {
-        addons_level2.emplace_back( mt.quantity1, mt.level2ObjectUID, mt.objectName2, mt.indexName2 );
+    if ( mt.objectName2 && mt.level2IcnImageIndex != 0xFF ) {
+        addons_level2.emplace_back( mt.quantity1, mt.level2ObjectUID, mt.objectName2, mt.level2IcnImageIndex );
     }
 }
 
@@ -1817,41 +1828,72 @@ void Maps::Tiles::CorrectFlags32( const int col, const u32 index, const bool up 
         addons_level1.emplace_back( TilesAddon::UPPER, World::GetUniq(), 0x38, index );
 }
 
-void Maps::Tiles::FixedPreload( Tiles & tile )
+void Maps::Tiles::fixTileObjectType( Tiles & tile )
 {
-    // fix skeleton: left position
-    if ( MP2::GetICNObject( tile.objectTileset ) == ICN::OBJNDSRT && tile.objectIndex == 83 ) {
+    const MP2::MapObjectType originalObjectType = tile.GetObject( false );
+    const int originalICN = MP2::GetICNObject( tile.objectTileset );
+
+    // Left tile of a skeleton on Desert should be mark as non-action tile.
+    if ( originalObjectType == MP2::OBJ_SKELETON && originalICN == ICN::OBJNDSRT && tile.objectIndex == 83 ) {
         tile.SetObject( MP2::OBJN_SKELETON );
+
+        // There is no need to check the rest of things as we fixed this object.
+        return;
     }
 
-    // fix price loyalty objects.
-    if ( Settings::Get().isPriceOfLoyaltySupported() )
-        switch ( tile.GetObject() ) {
-        case MP2::OBJ_UNKNW_79:
-        case MP2::OBJ_UNKNW_7A:
-        case MP2::OBJ_UNKNW_F9:
-        case MP2::OBJ_UNKNW_FA: {
-            MP2::MapObjectType objectType = Maps::Tiles::GetLoyaltyObject( tile.objectTileset, tile.objectIndex );
-            if ( objectType == MP2::OBJ_ZERO ) {
-                // if nothing was found it means there's overlay tile on top of the object; search for it
-                for ( auto it = tile.addons_level2.begin(); it != tile.addons_level2.end(); ++it ) {
-                    objectType = Maps::Tiles::GetLoyaltyObject( it->object, it->index );
-                    if ( objectType != MP2::OBJ_ZERO )
-                        break;
-                }
-            }
+    // Original Editor marks Reefs as Stones. We're fixing this issue by changing the type of the object without changing the content of a tile.
+    // This is also required in order to properly calculate Reefs' passbility.
+    if ( originalObjectType == MP2::OBJ_STONES && isValidReefsSprite( originalICN, tile.objectIndex ) ) {
+        tile.SetObject( MP2::OBJ_REEFS );
 
-            if ( MP2::OBJ_ZERO != objectType )
-                tile.SetObject( objectType );
-            else {
-                DEBUG_LOG( DBG_GAME, DBG_WARN, "invalid expansion object at index: " << tile._index );
-            }
+        // There is no need to check the rest of things as we fixed this object.
+        return;
+    }
+
+    // Fix The Price of Loyalty objects even if the map is The Succession Wars type.
+    switch ( originalObjectType ) {
+    case MP2::OBJ_UNKNW_79:
+    case MP2::OBJ_UNKNW_7A:
+    case MP2::OBJ_UNKNW_F9:
+    case MP2::OBJ_UNKNW_FA: {
+        MP2::MapObjectType objectType = Maps::Tiles::GetLoyaltyObject( tile.objectTileset, tile.objectIndex );
+        if ( objectType != MP2::OBJ_ZERO ) {
+            tile.SetObject( objectType );
             break;
         }
 
-        default:
+        // Add-ons of level 1 shouldn't even exist if no top object. However, let's play safe and verify it as well.
+        for ( const TilesAddon & addon : tile.addons_level1 ) {
+            objectType = Maps::Tiles::GetLoyaltyObject( addon.object, addon.index );
+            if ( objectType != MP2::OBJ_ZERO )
+                break;
+        }
+
+        if ( objectType != MP2::OBJ_ZERO ) {
+            tile.SetObject( objectType );
             break;
         }
+
+        for ( const TilesAddon & addon : tile.addons_level2 ) {
+            objectType = Maps::Tiles::GetLoyaltyObject( addon.object, addon.index );
+            if ( objectType != MP2::OBJ_ZERO )
+                break;
+        }
+
+        if ( objectType != MP2::OBJ_ZERO ) {
+            tile.SetObject( objectType );
+            break;
+        }
+
+        DEBUG_LOG( DBG_GAME, DBG_WARN,
+                   "Invalid object type index " << tile._index << ": type " << MP2::StringObject( originalObjectType ) << ", icn ID "
+                                                << static_cast<int>( tile.objectIndex ) );
+        break;
+    }
+
+    default:
+        break;
+    }
 }
 
 /* true: if protection or has guardians */

src/fheroes2/maps/maps_tiles.h
@@ -303,7 +303,9 @@ namespace Maps
         static std::pair<uint32_t, uint32_t> GetMonsterSpriteIndices( const Tiles & tile, const uint32_t monsterIndex );
         static void PlaceMonsterOnTile( Tiles & tile, const Monster & mons, const uint32_t count );
         static void UpdateAbandoneMineSprite( Tiles & tile );
-        static void FixedPreload( Tiles & tile );
+
+        // Some tiles have incorrect object type. This is due to original Editor issues.
+        static void fixTileObjectType( Tiles & tile );
 
         static int32_t getIndexOfMainTile( const Maps::Tiles & tile );
 

src/fheroes2/maps/mp2.cpp
@@ -29,8 +29,7 @@
 
 #include <cassert>
 
-/* return name icn object */
-int MP2::GetICNObject( int tileset )
+int MP2::GetICNObject( const uint8_t tileset )
 {
     switch ( tileset >> 2 ) {
     // reserverd

src/fheroes2/maps/mp2.h
@@ -26,36 +26,38 @@
 
 #include "types.h"
 
-#define MP2OFFSETDATA 428
-#define SIZEOFMP2TILE 20
-#define SIZEOFMP2ADDON 15
-#define SIZEOFMP2CASTLE 0x46
-#define SIZEOFMP2HEROES 0x4c
-
-#define SIZEOFMP2SIGN 0x0a
-#define SIZEOFMP2RUMOR 0x09
-#define SIZEOFMP2EVENT 0x32
-#define SIZEOFMP2RIDDLE 0x8a
-
 namespace MP2
 {
+    enum MP2Info
+    {
+        MP2OFFSETDATA = 428,
+        SIZEOFMP2TILE = 20,
+        SIZEOFMP2ADDON = 15,
+        SIZEOFMP2CASTLE = 70,
+        SIZEOFMP2HEROES = 76,
+        SIZEOFMP2SIGN = 10,
+        SIZEOFMP2RUMOR = 9,
+        SIZEOFMP2EVENT = 50,
+        SIZEOFMP2RIDDLE = 138
+    };
+
     // origin mp2 tile
     struct mp2tile_t
     {
-        uint16_t tileIndex; // Tile index representing a type of surface: ocean, grass, snow, swamp, lava, desert, dirt, wasteland, beach.
+        uint16_t surfaceType; // Tile index representing a type of surface: ocean, grass, snow, swamp, lava, desert, dirt, wasteland, beach.
 
-        u8 objectName1; // level 1.0
-        u8 indexName1; // index level 1.0 or 0xFF
-        u8 quantity1; // Bitfield, first 3 bits are flags, rest is used as quantity
-        u8 quantity2; // Used as a part of quantity, field size is actually 13 bits. Has most significant bits
-        u8 objectName2; // level 2.0
-        u8 indexName2; // index level 2.0 or 0xFF
+        uint8_t objectName1; // Ground (bottom) level object type (first 2 bits) and object tile set (6 bits). Tile set refers to ICN ID.
+        uint8_t level1IcnImageIndex; // ICN image index (image index for corresponding ICN Id) for ground (bottom) object. 255 means it's an empty object.
+        uint8_t quantity1; // Bitfield, first 3 bits are flags, rest is used as quantity
+        uint8_t quantity2; // Used as a part of quantity, field size is actually 13 bits. Has most significant bits
+        uint8_t objectName2; // Top level object type (first 2 bits) and object tile set (6 bits). Tile set refers to ICN ID.
+        uint8_t level2IcnImageIndex; // ICN image index (image index for corresponding ICN Id) for top level object. 255 means it's an empty object.
 
         // First 2 bits responsible for tile shape (0 - 3). Subsequent 3 bits are still unknown. Possible values are 1 and 5. They are set only for tiles with transition
-        // between land and sea.
+        // between land and sea. They can be related to passabilities.
         uint8_t flags;
 
-        u8 mapObject; // zero or object
+        uint8_t mapObjectType; // Object type. Please refer to MapObjectType enumeration.
 
         uint16_t nextAddonIndex; // Next add-on index. Zero value means it's the last addon chunk.
 
@@ -75,11 +77,11 @@ namespace MP2
     {
         uint16_t nextAddonIndex; // Next add-on index. Zero value means it's the last addon chunk.
 
-        u8 objectNameN1; // level 1.N. Last bit indicates if object is animated. Second-last controls overlay
-        u8 indexNameN1; // level 1.N or 0xFF
-        u8 quantityN; // Bitfield containing metadata
-        u8 objectNameN2; // level 2.N
-        u8 indexNameN2; // level 1.N or 0xFF
+        uint8_t objectNameN1; // level 1.N. Last bit indicates if object is animated. Second-last controls overlay
+        uint8_t indexNameN1; // level 1.N or 0xFF
+        uint8_t quantityN; // Bitfield containing metadata
+        uint8_t objectNameN2; // level 2.N
+        uint8_t indexNameN2; // level 1.N or 0xFF
 
         // Ground (bottom) level object UID. An object can allocate more than 1 tile. Each tile could have multiple objects pieces.
         // UID is used to find all pieces/addons which belong to the same object.
@@ -100,31 +102,31 @@ namespace MP2
         bool customBuilding;
         u16 building;
         /*
-        0000 0000 0000 0010	Thieve's Guild
-        0000 0000 0000 0100	Tavern
-        0000 0000 0000 1000	Shipyard
-        0000 0000 0001 0000	Well
-        0000 0000 1000 0000	Statue
-        0000 0001 0000 0000	Left Turret
-        0000 0010 0000 0000	Right Turret
-        0000 0100 0000 0000	Marketplace
-        0000 1000 0000 0000	Farm, Garbage He, Crystal Gar, Waterfall, Orchard, Skull Pile
-        0001 0000 0000 0000	Moat
-        0010 0000 0000 0000	Fortification, Coliseum, Rainbow, Dungeon, Library, Storm
+        0000 0000 0000 0010 Thieve's Guild
+        0000 0000 0000 0100 Tavern
+        0000 0000 0000 1000 Shipyard
+        0000 0000 0001 0000 Well
+        0000 0000 1000 0000 Statue
+        0000 0001 0000 0000 Left Turret
+        0000 0010 0000 0000 Right Turret
+        0000 0100 0000 0000 Marketplace
+        0000 1000 0000 0000 Farm, Garbage He, Crystal Gar, Waterfall, Orchard, Skull Pile
+        0001 0000 0000 0000 Moat
+        0010 0000 0000 0000 Fortification, Coliseum, Rainbow, Dungeon, Library, Storm
         */
         u16 dwelling;
         /*
-        0000 0000 0000 1000	dweling1
-        0000 0000 0001 0000	dweling2
-        0000 0000 0010 0000	dweling3
-        0000 0000 0100 0000	dweling4
-        0000 0000 1000 0000	dweling5
-        0000 0001 0000 0000	dweling6
-        0000 0010 0000 0000	upgDweling2
-        0000 0100 0000 0000	upgDweling3
-        0000 1000 0000 0000	upgDweling4
-        0001 0000 0000 0000	upgDweling5
-        0010 0000 0000 0000	upgDweling6
+        0000 0000 0000 1000 dweling1
+        0000 0000 0001 0000 dweling2
+        0000 0000 0010 0000 dweling3
+        0000 0000 0100 0000 dweling4
+        0000 0000 1000 0000 dweling5
+        0000 0001 0000 0000 dweling6
+        0000 0010 0000 0000 upgDweling2
+        0000 0100 0000 0000 upgDweling3
+        0000 1000 0000 0000 upgDweling4
+        0001 0000 0000 0000 upgDweling5
+        0010 0000 0000 0000 upgDweling6
         */
         u8 magicTower;
         bool customTroops;
@@ -561,7 +563,9 @@ namespace MP2
         OBJ_WATERALTAR = 0xFF
     };
 
-    int GetICNObject( int tileset );
+    // Return Icn ID related to this tileset value.
+    int GetICNObject( const uint8_t tileset );
+
     const char * StringObject( const MapObjectType objectType );
 
     // This function returns plural forms only for certain object types.

commit 2daf4be558275bf4bcb319ccd260b9934c5cbbf9

Fix incorrect map loading algorithm
regression from #4514

relates to #4519

src/fheroes2/maps/maps_fileinfo.cpp
@@ -346,12 +346,6 @@ bool Maps::FileInfo::ReadMP2( const std::string & filename )
         MP2::mp2tile_t mp2tile;
         MP2::loadTile( fs, mp2tile );
 
-        // offset first addon
-        fs.getLE16();
-
-        mp2tile.level1ObjectUID = fs.getLE32();
-        mp2tile.level2ObjectUID = fs.getLE32();
-
         Maps::Tiles tile;
         tile.Init( 0, mp2tile );
 

src/fheroes2/maps/mp2_helper.cpp
@@ -35,6 +35,9 @@ namespace MP2
         tile.level2IcnImageIndex = stream.get();
         tile.flags = stream.get();
         tile.mapObjectType = stream.get();
+        tile.nextAddonIndex = stream.getLE16();
+        tile.level1ObjectUID = stream.getLE32();
+        tile.level2ObjectUID = stream.getLE32();
     }
 
     void loadAddon( StreamBase & stream, mp2addon_t & addon )