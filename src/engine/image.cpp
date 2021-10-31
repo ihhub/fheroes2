db821dc9230717022bcddf019ad69bef91d423cd
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2021-10-31 21:54:45 +0800

    Correct original Waterfall images to remove Cave
    close #2614

--- src/engine/image.cpp
+++ src/engine/image.cpp
@@ -1537,6 +1537,23 @@ namespace fheroes2
         }
     }
 
+    void FillTransform( Image & image, int32_t x, int32_t y, int32_t width, int32_t height, uint8_t tranformId )
+    {
+        if ( !Verify( image, x, y, width, height ) )
+            return;
+
+        const int32_t imageWidth = image.width();
+
+        uint8_t * imageY = image.image() + y * imageWidth + x;
+        uint8_t * transformY = image.transform() + y * imageWidth + x;
+        const uint8_t * imageYEnd = imageY + height * imageWidth;
+
+        for ( ; imageY != imageYEnd; imageY += imageWidth, transformY += imageWidth ) {
+            std::fill( imageY, imageY + width, 0 );
+            std::fill( transformY, transformY + width, tranformId );
+        }
+    }
+
     Image FilterOnePixelNoise( const Image & input )
     {
         if ( input.width() < 3 || input.height() < 3 ) {
