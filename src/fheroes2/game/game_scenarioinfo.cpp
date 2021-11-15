148c77117880f57b07a1a9607cd1f287edbfa023
zenseii <zenseii@users.noreply.github.com>
2021-11-15 01:53:52 +0100

    Fix difficulty text logic

--- src/fheroes2/game/game_scenarioinfo.cpp
+++ src/fheroes2/game/game_scenarioinfo.cpp
@@ -46,6 +46,7 @@
 #include "tools.h"
 #include "translations.h"
 #include "ui_button.h"
+#include "ui_text.h"
 #include "ui_tool.h"
 #include "world.h"
 
@@ -125,13 +126,15 @@ namespace
 
     void RedrawDifficultyInfo( const fheroes2::Point & dst )
     {
-        const uint32_t width = 65;
-        const uint32_t height = 69;
+        const uint32_t width = 77;
+        const uint32_t height = 70;
+        //std::vector<fheroes2::Rect> coordDifficulty;
 
         for ( u32 current = Difficulty::EASY; current <= Difficulty::IMPOSSIBLE; ++current ) {
-            const uint32_t offset = current * ( width + 12 );
-            Text text( Difficulty::String( current ), Font::SMALL );
-            text.Blit( dst.x + offset + ( width - text.w() ) / 2, dst.y + height );
+            const uint32_t offset = width * current;
+
+            fheroes2::Text text( Difficulty::String( current ), { fheroes2::FontSize::SMALL, fheroes2::FontColor::WHITE } );
+            text.draw( /*coordDifficulty[current].x */dst.x + 31 + offset - ( text.width() / 2 ), dst.y + height, fheroes2::Display::instance() );
         }
     }
 
