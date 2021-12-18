e502d897b0152528b152b2549d44db13b80fb689
vincent-grosbois <vincent.grosbois@gmail.com>
2021-12-19 00:00:46 +0100

    test

--- src/fheroes2/network/test.cpp
+++ src/fheroes2/network/test.cpp
@@ -1,6 +1,6 @@
 #pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wshadow"
+#pragma GCC diagnostic ignored "-Wshadow" // required because asio will trigger this warning on macOS builds
 #include "asio.hpp"
 #pragma GCC diagnostic pop
 
-int a = 2;
+asio::io_context test;
