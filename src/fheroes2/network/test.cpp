f5a33ad48da7e79ab696f9728dac1b83d018f2d4
vincent-grosbois <vincent.grosbois@gmail.com>
2021-12-19 01:23:04 +0100

    Remove asio from vita/switch build
    they don't have POSIX

--- src/fheroes2/network/test.cpp
+++ src/fheroes2/network/test.cpp
@@ -1,6 +1,10 @@
+#ifndef NO_NETWORK
+
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wshadow" // required because asio will trigger this warning on macOS builds
 #include "asio.hpp"
 #pragma GCC diagnostic pop
 
 asio::io_context test;
+
+#endif // NO_NETWORK
