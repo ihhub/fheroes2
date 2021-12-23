7b444b86c5b4b6b6ae2861362b0babd66c3b07fd
vincent-grosbois <vincent.grosbois@gmail.com>
2021-12-23 19:59:37 +0100

    fix remarks

--- src/fheroes2/network/test.cpp
+++ src/fheroes2/network/test.cpp
@@ -1,10 +1,13 @@
-#ifndef NO_NETWORK
+#if !defined( NO_NETWORK )
 
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wshadow" // required because asio will trigger this warning on macOS builds
 #include "asio.hpp"
 #pragma GCC diagnostic pop
 
-asio::io_context test;
+namespace
+{
+    asio::io_context test;
+}
 
 #endif // NO_NETWORK
