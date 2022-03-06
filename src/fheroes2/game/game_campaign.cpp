49993df0255c4b61919e8adcd7b827394a4b19dc
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2022-03-06 13:45:48 +0800

    Allow selecting options while restarting a campaign scenario (#5067)

--- src/fheroes2/game/game_campaign.cpp
+++ src/fheroes2/game/game_campaign.cpp
@@ -843,7 +843,7 @@ fheroes2::GameMode Game::SelectCampaignScenario( const fheroes2::GameMode prevMo
     const fheroes2::Point optionButtonOffset( 590, 199 );
     const int32_t optionButtonStep = 22;
 
-    const fheroes2::Sprite & pressedButton = fheroes2::AGG::GetICN( ICN::CAMPXTRG, allowToRestart ? 9 : 8 );
+    const fheroes2::Sprite & pressedButton = fheroes2::AGG::GetICN( ICN::CAMPXTRG, 8 );
     fheroes2::Sprite releaseButton( pressedButton.width(), pressedButton.height(), pressedButton.x(), pressedButton.y() );
     fheroes2::Copy( backgroundImage, optionButtonOffset.x + pressedButton.x(), optionButtonOffset.y + pressedButton.y(), releaseButton, 0, 0, releaseButton.width(),
                     releaseButton.height() );
@@ -930,15 +930,14 @@ fheroes2::GameMode Game::SelectCampaignScenario( const fheroes2::GameMode prevMo
         if ( allowToRestart ) {
             le.MousePressLeft( buttonRestart.area() ) ? buttonRestart.drawOnPress() : buttonRestart.drawOnRelease();
         }
-        else {
-            for ( uint32_t i = 0; i < bonusChoiceCount; ++i ) {
-                if ( le.MousePressLeft( choiceArea[i] ) ) {
-                    buttonChoices.button( i ).press();
-                    optionButtonGroup.draw();
-                    scenarioBonus = bonusChoices[i];
 
-                    break;
-                }
+        for ( uint32_t i = 0; i < bonusChoiceCount; ++i ) {
+            if ( le.MousePressLeft( choiceArea[i] ) ) {
+                buttonChoices.button( i ).press();
+                optionButtonGroup.draw();
+                scenarioBonus = bonusChoices[i];
+
+                break;
             }
         }
 
