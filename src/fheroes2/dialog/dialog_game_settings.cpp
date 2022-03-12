99f3546c13522b03a115fe6675f3749abd43cf6f
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2022-03-12 09:09:11 +0800

    Use Okay over OK (#5111)

--- src/fheroes2/dialog/dialog_game_settings.cpp
+++ src/fheroes2/dialog/dialog_game_settings.cpp
@@ -1,6 +1,6 @@
 /***************************************************************************
  *   Free Heroes of Might and Magic II: https://github.com/ihhub/fheroes2  *
- *   Copyright (C) 2021                                                    *
+ *   Copyright (C) 2021 - 2022                                             *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
@@ -187,7 +187,7 @@ namespace
                 fheroes2::showMessage( header, body, 0 );
             }
             else if ( le.MousePressRight( okayButton.area() ) ) {
-                fheroes2::Text header( _( "OK" ), fheroes2::FontType::normalYellow() );
+                fheroes2::Text header( _( "Okay" ), fheroes2::FontType::normalYellow() );
                 fheroes2::Text body( _( "Exit this menu." ), fheroes2::FontType::normalWhite() );
 
                 fheroes2::showMessage( header, body, 0 );
