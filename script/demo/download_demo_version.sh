48999bac32b5b50b6c020ac1d843acb6d3bcfed0
Kreeblah <kreeblah@gmail.com>
2022-03-20 13:12:35 -0700

    Revert changes to demo download script, as these were moved to a separate script

--- script/demo/download_demo_version.sh
+++ script/demo/download_demo_version.sh
@@ -34,11 +34,8 @@ elif [[ -d ../../src ]]; then
 fi
 
 if [[ -z "$DEST_PATH" || ! -d "$DEST_PATH" || ! -w "$DEST_PATH" ]]; then
-    PLATFORM_NAME="$(uname 2> /dev/null)"
-    if [[ "${PLATFORM_NAME}" == "Linux" ]]; then
+    if [[ "$(uname 2> /dev/null)" == "Linux" ]]; then
         DEST_PATH="${XDG_CONFIG_HOME:-$HOME/.local/share}/fheroes2"
-    elif [[ "${PLATFORM_NAME}" == "Darwin" ]]; then
-        DEST_PATH="${XDG_CONFIG_HOME:-$HOME/Library/Application Support}/fheroes2"
     else
         DEST_PATH="$HOME/.fheroes2"
     fi
