01446d9025b610c228cc05d3d59cb66c95b731cf
Kreeblah <kreeblah@gmail.com>
2022-03-20 16:13:56 -0700

    Fix reference to removed script function

--- script/homm2/extract_homm2_resources_for_app_bundle.sh
+++ script/homm2/extract_homm2_resources_for_app_bundle.sh
@@ -4,7 +4,7 @@ set -e
 
 PLATFORM_NAME="$(uname 2> /dev/null)"
 if [[ "${PLATFORM_NAME}" != "Darwin" ]]; then
-    echo_red "This script must be run on a macOS host"
+    echo "This script must be run on a macOS host"
     exit 1
 fi
 
