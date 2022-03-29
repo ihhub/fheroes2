8953e597fb74b2d708d74e3796c283fcce69257d
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2022-03-29 15:21:49 +0800

    Reserve the logic

--- src/engine/serialize.cpp
+++ src/engine/serialize.cpp
@@ -253,12 +253,14 @@ StreamBuf::StreamBuf( const u8 * buf, size_t bufsz )
 
 StreamBuf & StreamBuf::operator=( StreamBuf && st ) noexcept
 {
-    std::swap( itbeg, st.itbeg );
-    std::swap( itget, st.itget );
-    std::swap( itput, st.itput );
-    std::swap( itend, st.itend );
+    if ( &st != this ) {
+        StreamBase::operator=( std::move( st ) );
 
-    StreamBase::operator=( std::move( st ) );
+        std::swap( itbeg, st.itbeg );
+        std::swap( itget, st.itget );
+        std::swap( itput, st.itput );
+        std::swap( itend, st.itend );
+    }
 
     return *this;
 }
