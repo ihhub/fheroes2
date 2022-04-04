0911c512d62ee92739cd9bc5af414c3c759bbfa4
Félix-Antoine Constantin <felix-antoine.constantin@polymtl.ca>
2022-04-03 21:32:17 -0400

    Broke a prototype by mistake

--- src/fheroes2/ai/normal/ai_normal.h
+++ src/fheroes2/ai/normal/ai_normal.h
@@ -155,7 +155,7 @@ namespace AI
         void KingdomTurn( Kingdom & kingdom ) override;
         void CastleTurn( Castle & castle, bool defensive ) override;
         void BattleTurn( Battle::Arena & arena, const Battle::Unit & currentUnit, Battle::Actions & actions ) override;
-        .ool HeroesTurn( VecHeroes & heroes ) override;
+        bool HeroesTurn( VecHeroes & heroes ) override;
         uint8_t purchaseNewHeroes( VecHeroes & heroes, const std::vector<AICastle> & sortedCastleList, const std::set<int> & castlesInDanger, int32_t availableHeroCount );
 
         void revealFog( const Maps::Tiles & tile ) override;
