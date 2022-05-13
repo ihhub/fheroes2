8a7ddf5aead92db17c79e2e4b980fe8853a955ea
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2022-05-13 22:16:35 +0800

    Code cleanup

--- src/fheroes2/gui/ui_castle.h
+++ src/fheroes2/gui/ui_castle.h
@@ -17,12 +17,9 @@
  *   Free Software Foundation, Inc.,                                       *
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
-
 #pragma once
 
 #include "image.h"
-
-class Castle;
 
 class Castle;
 class Funds;
