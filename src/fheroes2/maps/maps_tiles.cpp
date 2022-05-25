ihhub/fheroes2

commit 699bbe3ff1d3f41b28e579ff639859d93032105a

Always mark the entrance to the castle/town as a road, do not automatically mark the tile south of the entrance as a road (#4756)

src/fheroes2/maps/maps_tiles.cpp
@@ -535,23 +535,18 @@ MP2::MapObjectType Maps::Tiles::GetLoyaltyObject( const uint8_t tileset, const u
 bool Maps::TilesAddon::isRoad() const
 {
     switch ( MP2::GetICNObject( object ) ) {
-    // from sprite road
+    // road sprite
     case ICN::ROAD:
         if ( 1 == index || 8 == index || 10 == index || 11 == index || 15 == index || 22 == index || 23 == index || 24 == index || 25 == index || 27 == index )
             return false;
         else
             return true;
 
-    // castle and tower (gate)
+    // castle or town gate
     case ICN::OBJNTOWN:
+    case ICN::OBJNTWRD:
         if ( 13 == index || 29 == index || 45 == index || 61 == index || 77 == index || 93 == index || 109 == index || 125 == index || 141 == index || 157 == index
              || 173 == index || 189 == index )
-            return true;
-        break;
-
-        // castle lands (gate)
-    case ICN::OBJNTWBA:
-        if ( 7 == index || 17 == index || 27 == index || 37 == index || 47 == index || 57 == index || 67 == index || 77 == index )
             return true;
         break;
 

commit 4f1b5906c659d28e132f1a2292e57e43d5b1ddad

Fix incorrect tiles being marked as road
close #5261

It is a regression from #4756

src/fheroes2/maps/maps_tiles.cpp
@@ -538,11 +538,14 @@ bool Maps::TilesAddon::isRoad() const
 
     // castle or town gate
     case ICN::OBJNTOWN:
-    case ICN::OBJNTWRD:
         if ( 13 == index || 29 == index || 45 == index || 61 == index || 77 == index || 93 == index || 109 == index || 125 == index || 141 == index || 157 == index
              || 173 == index || 189 == index )
             return true;
         break;
+
+    // Random castle or town gate.
+    case ICN::OBJNTWRD:
+        return ( index == 13 || index == 29 );
 
     default:
         break;