a485cecad79d75c297f53a9c34a9636403bf5386
Zense <zenseii@users.noreply.github.com>
2022-05-28 17:31:05 +0200

    Code style

--- src/fheroes2/army/army.cpp
+++ src/fheroes2/army/army.cpp
@@ -450,7 +450,7 @@ void Troops::MoveTroops( const Troops & from, bool moveAll )
         }
 
         // As a last resort try to merge troops to make free slots.
-        const uint32_t troopCountPreMerge = GetCount(); 
+        const uint32_t troopCountPreMerge = GetCount();
         MergeTroops( from.GetCount() );
         const uint32_t troopCountPostMerge = GetCount();
         if ( troopCountPreMerge == troopCountPostMerge ) {
