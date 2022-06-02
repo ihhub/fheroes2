781960e3e49da76ab73d387c934484b16d445ae3
Zense <zenseii@users.noreply.github.com>
2022-06-02 16:58:04 +0200

    Clang comment

--- src/fheroes2/castle/castle_dialog.cpp
+++ src/fheroes2/castle/castle_dialog.cpp
@@ -464,8 +464,8 @@ Castle::CastleDialogReturnValue Castle::OpenDialog( const bool readOnly, const b
                     redrawAfterArmyAction = true;
                 }
                 // Swap troops.
-                else if ( HotKeyPressEvent( Game::HotKeyEvent::SWAP_ARMIES ) || !heroes.Guard() && le.MouseClickLeft( rectSign1 )
-                          || heroes.Guard() && le.MouseClickLeft( buttonSwap.area() ) ) {
+                else if ( HotKeyPressEvent( Game::HotKeyEvent::SWAP_ARMIES ) || ( !heroes.Guard() && le.MouseClickLeft( rectSign1 ) )
+                          || ( heroes.Guard() && le.MouseClickLeft( buttonSwap.area() ) ) ) {
                     Army::swapArmies( GetArmy(), heroes.Guest()->GetArmy() );
                     redrawAfterArmyAction = true;
                 }
