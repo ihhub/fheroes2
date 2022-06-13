cddc2205ff94533f6eb8ed98f5604ca31087a768
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2022-06-13 23:01:41 +0800

    Fix compilation

--- src/fheroes2/game/game.cpp
+++ src/fheroes2/game/game.cpp
@@ -341,7 +341,7 @@ uint32_t & Game::MapsAnimationFrame()
 // play environment sounds from the game area in focus
 void Game::EnvironmentSoundMixer()
 {
-    int availableChannels = AudioManager::getAudioChannelCount();
+    int availableChannels = Mixer::getChannelCount();
     if ( availableChannels <= 2 ) {
         // 2 channels are left for hero's movement.
         return;
