833172474ba8011744e1dd5b35d1e5e7f8ccef47
Oleg Derevenetz <oleg-derevenetz@yandex.ru>
2022-06-23 21:03:36 +0300

    Shorten the static_assert message

--- src/fheroes2/world/world_loadmap.cpp
+++ src/fheroes2/world/world_loadmap.cpp
@@ -739,7 +739,7 @@ void World::ProcessNewMap()
     // There is a tile with a predefined Ultimate Artifact, pick a tile nearby in the radius specified in the artifact's properties
     else {
         static_assert( std::is_same_v<decltype( ultArtTileIter->GetQuantity1() ), uint8_t> && std::is_same_v<decltype( ultArtTileIter->GetQuantity2() ), uint8_t>,
-                       "The types of tile's quantities has been changed, please check the bitwise arithmetic below" );
+                       "Types of tile's quantities have been changed, check the bitwise arithmetic below" );
 
         // The radius can be in the range 0 - 127, it is represented by 2 low-order bits of quantity2 and 5 high-order bits of quantity1
         const int32_t radius = ( ( ultArtTileIter->GetQuantity2() & 0x03 ) << 5 ) + ( ultArtTileIter->GetQuantity1() >> 3 );
