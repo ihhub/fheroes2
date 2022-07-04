b71f0dbf7d950c2b4aa6f3d5a33903a9d366fce4
Ivan Shibanov <idshibanov@users.noreply.github.com>
2022-07-03 20:28:56 -0400

    Implement AI courier hero role for better logistics (#5583)

--- src/fheroes2/ai/normal/ai_normal_kingdom.cpp
+++ src/fheroes2/ai/normal/ai_normal_kingdom.cpp
@@ -35,6 +35,21 @@ namespace
 {
     const double fighterStrengthMultiplier = 3;
 
+    struct HeroValue
+    {
+        Heroes * hero = nullptr;
+        double strength = 0.0;
+        int stats = 0;
+
+        HeroValue( Heroes * inHero, double inStrength, int inStats )
+            : hero( inHero )
+            , strength( inStrength )
+            , stats( inStats )
+        {
+            // Do nothing.
+        }
+    };
+
     void setHeroRoles( KingdomHeroes & heroes )
     {
         if ( heroes.empty() ) {
@@ -58,28 +73,42 @@ namespace
         }
 
         // Set hero's roles. First calculate each hero strength and sort it in descending order.
-        std::vector<std::pair<double, Heroes *>> heroStrength;
+        std::vector<HeroValue> heroList;
         for ( Heroes * hero : heroes ) {
-            heroStrength.emplace_back( hero->GetArmy().GetStrength(), hero );
+            // AI heroes set on patrol mode can only be fighters; ignore them otherwise
+            if ( hero->Modes( Heroes::PATROL ) ) {
+                hero->setAIRole( Heroes::Role::FIGHTER );
+            }
+            else {
+                heroList.emplace_back( hero, hero->GetArmy().GetStrength(), hero->getStatsValue() );
+            }
         }
 
-        std::sort( heroStrength.begin(), heroStrength.end(),
-                   []( const std::pair<double, Heroes *> & first, const std::pair<double, Heroes *> & second ) { return first.first > second.first; } );
+        // If there's plenty of heroes we can assign special roles
+        if ( heroList.size() > 3 ) {
+            std::sort( heroList.begin(), heroList.end(), []( const HeroValue & first, const HeroValue & second ) { return first.stats > second.stats; } );
 
-        const double medianStrength = heroStrength[heroStrength.size() / 2].first;
+            // Assign the role and remove them so they aren't counted towards the median strength
+            heroList.back().hero->setAIRole( Heroes::Role::COURIER );
+            heroList.pop_back();
+        }
 
-        for ( std::pair<double, Heroes *> & hero : heroStrength ) {
+        std::sort( heroList.begin(), heroList.end(), []( const HeroValue & first, const HeroValue & second ) { return first.strength > second.strength; } );
+
+        const double medianStrength = heroList[heroList.size() / 2].strength;
+
+        for ( HeroValue & object : heroList ) {
             // TODO: a valuable hero must be marked as a champion.
-            if ( valuableHero != nullptr && hero.second == valuableHero ) {
-                hero.second->setAIRole( Heroes::Role::FIGHTER );
+            if ( valuableHero != nullptr && object.hero == valuableHero ) {
+                object.hero->setAIRole( Heroes::Role::FIGHTER );
                 continue;
             }
 
-            if ( hero.first > medianStrength * fighterStrengthMultiplier ) {
-                hero.second->setAIRole( Heroes::Role::FIGHTER );
+            if ( object.strength > medianStrength * fighterStrengthMultiplier ) {
+                object.hero->setAIRole( Heroes::Role::FIGHTER );
             }
             else {
-                hero.second->setAIRole( Heroes::Role::HUNTER );
+                object.hero->setAIRole( Heroes::Role::HUNTER );
             }
         }
     }
@@ -290,7 +319,7 @@ namespace AI
 
     std::set<int> Normal::findCastlesInDanger( const KingdomCastles & castles, const std::vector<std::pair<int, const Army *>> & enemyArmies, int myColor )
     {
-        const uint32_t threatDistanceLimit = 2500; // 25 tiles, roughly how much maxed out hero can move in a turn
+        const uint32_t threatDistanceLimit = 3000; // 30 tiles, roughly how much maxed out hero can move in a turn
         std::set<int> castlesInDanger;
 
         for ( const std::pair<int, const Army *> & enemy : enemyArmies ) {
@@ -571,23 +600,5 @@ namespace AI
 
         // target found, buy hero
         return recruitmentCastle && recruitHero( *recruitmentCastle, !slowEarlyGame, false );
-    }
-
-    double Normal::getTargetArmyStrength( const Maps::Tiles & tile, const MP2::MapObjectType objectType )
-    {
-        if ( !isMonsterStrengthCacheable( objectType ) ) {
-            return Army( tile ).GetStrength();
-        }
-
-        const int32_t tileId = tile.GetIndex();
-
-        auto iter = _neutralMonsterStrengthCache.find( tileId );
-        if ( iter != _neutralMonsterStrengthCache.end() ) {
-            // Cache hit.
-            return iter->second;
-        }
-
-        auto newEntry = _neutralMonsterStrengthCache.emplace( tileId, Army( tile ).GetStrength() );
-        return newEntry.first->second;
     }
 }
