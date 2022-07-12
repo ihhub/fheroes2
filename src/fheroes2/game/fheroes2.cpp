67b2637ad03d74b05cf663a42b21e4cd9d5ad8f5
Oleg Derevenetz <oleg-derevenetz@yandex.ru>
2022-07-12 12:23:03 +0300

    Fix build without WITH_DEBUG

--- src/fheroes2/game/fheroes2.cpp
+++ src/fheroes2/game/fheroes2.cpp
@@ -254,9 +254,11 @@ int main( int argc, char ** argv )
         const DataInitializer dataInitializer;
 
         const ListFiles midiSoundFonts = Settings::FindFiles( System::ConcatePath( "files", "soundfonts" ), ".sf2", false );
+#ifdef WITH_DEBUG
         for ( const std::string & file : midiSoundFonts ) {
             DEBUG_LOG( DBG_GAME, DBG_INFO, "MIDI sound font to load: " << file )
         }
+#endif
 
         const AudioManager::AudioInitializer audioInitializer( dataInitializer.getOriginalAGGFilePath(), dataInitializer.getExpansionAGGFilePath(), midiSoundFonts );
 
