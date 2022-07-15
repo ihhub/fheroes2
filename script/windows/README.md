5a199aaf277ad97425f9bc74db4341a139e7da76
Oleg Derevenetz <oleg-derevenetz@yandex.ru>
2022-07-15 23:50:48 +0300

    Update docs

--- script/windows/README.md
+++ script/windows/README.md
@@ -2,4 +2,7 @@ Package installation for Visual Studio
 ======
 
 Run "install_packages.bat" file and it would try to install everything in one shot.
-In case of some failure please follow instructions.
+
+Packages related to SDL1 will be downloaded in binary form from the libsdl.org website.
+Packages related to SDL2 will be installed using vcpkg. It is assumed that vcpkg is
+installed in C:\vcpkg.
--- script/windows/install_packages.bat
+++ script/windows/install_packages.bat
@@ -4,9 +4,8 @@ echo Setting up SDL1... && ^
 call %~dp0\setup_sdl1.bat %~dp0\..\..\VisualStudio/packages %~dp0\..\..\VisualStudio/packages/installed/sdl1 && ^
 echo Setting up SDL2... && ^
 call %~dp0\setup_sdl2.bat C:\vcpkg x86 %~dp0\..\..\VisualStudio/packages/installed/sdl2 && ^
-call %~dp0\setup_sdl2.bat C:\vcpkg x64 %~dp0\..\..\VisualStudio/packages/installed/sdl2 && ( ^
-    echo "SUCCESS! Installation is completed"
-)
+call %~dp0\setup_sdl2.bat C:\vcpkg x64 %~dp0\..\..\VisualStudio/packages/installed/sdl2 && ^
+echo "SUCCESS! Installation is completed"
 
 echo Press any key to exit...
 pause >nul
