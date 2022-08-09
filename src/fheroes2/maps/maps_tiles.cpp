977246e981bc20e7da47c6ee2e088e0edb4e5c67
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2022-08-09 17:55:50 +0800

    Fix missing shadows from path arrows when they are on the edge of screen (#5729)

--- src/fheroes2/maps/maps_tiles.cpp
+++ src/fheroes2/maps/maps_tiles.cpp
@@ -1239,22 +1239,8 @@ int Maps::Tiles::GetGround() const
     return Maps::Ground::BEACH;
 }
 
-void Maps::Tiles::RedrawTile( fheroes2::Image & dst, const fheroes2::Rect & visibleTileROI, const Interface::GameArea & area ) const
+void Maps::Tiles::RedrawEmptyTile( fheroes2::Image & dst, const fheroes2::Point & mp, const Interface::GameArea & area )
 {
-    const fheroes2::Point & mp = Maps::GetPoint( _index );
-
-    if ( !( visibleTileROI & mp ) )
-        return;
-
-    area.DrawTile( dst, GetTileSurface(), mp );
-}
-
-void Maps::Tiles::RedrawEmptyTile( fheroes2::Image & dst, const fheroes2::Point & mp, const fheroes2::Rect & visibleTileROI, const Interface::GameArea & area )
-{
-    if ( !( visibleTileROI & mp ) ) {
-        return;
-    }
-
     if ( mp.y == -1 && mp.x >= 0 && mp.x < world.w() ) { // top first row
         area.DrawTile( dst, fheroes2::AGG::GetTIL( TIL::STON, 20 + ( mp.x % 4 ), 0 ), mp );
     }
@@ -1272,30 +1258,23 @@ void Maps::Tiles::RedrawEmptyTile( fheroes2::Image & dst, const fheroes2::Point 
     }
 }
 
-void Maps::Tiles::RedrawPassable( fheroes2::Image & dst, const fheroes2::Rect & visibleTileROI, const Interface::GameArea & area ) const
+void Maps::Tiles::RedrawPassable( fheroes2::Image & dst, const Interface::GameArea & area ) const
 {
 #ifdef WITH_DEBUG
-    const fheroes2::Point & mp = Maps::GetPoint( _index );
-
-    if ( ( visibleTileROI & mp ) && ( 0 == tilePassable || DIRECTION_ALL != tilePassable ) ) {
-        area.BlitOnTile( dst, PassableViewSurface( tilePassable ), 0, 0, mp, false, 255 );
+    if ( 0 == tilePassable || DIRECTION_ALL != tilePassable ) {
+        area.BlitOnTile( dst, PassableViewSurface( tilePassable ), 0, 0, Maps::GetPoint( _index ), false, 255 );
     }
 #else
     (void)dst;
-    (void)visibleTileROI;
     (void)area;
 #endif
 }
 
-void Maps::Tiles::redrawBottomLayerObjects( fheroes2::Image & dst, const fheroes2::Rect & visibleTileROI, bool isPuzzleDraw, const Interface::GameArea & area,
-                                            const uint8_t level ) const
+void Maps::Tiles::redrawBottomLayerObjects( fheroes2::Image & dst, bool isPuzzleDraw, const Interface::GameArea & area, const uint8_t level ) const
 {
     assert( level <= 0x03 );
 
     const fheroes2::Point & mp = Maps::GetPoint( _index );
-    if ( !( visibleTileROI & mp ) ) {
-        return;
-    }
 
     // Since the original game stores information about objects in a very weird way and this is how it is implemented for us we need to do the following procedure:
     // - run through all bottom objects first which are stored in the addon stack
@@ -1510,12 +1489,9 @@ std::vector<std::pair<fheroes2::Point, fheroes2::Sprite>> Maps::Tiles::getBoatSh
     return output;
 }
 
-void Maps::Tiles::redrawTopLayerObjects( fheroes2::Image & dst, const fheroes2::Rect & visibleTileROI, const bool isPuzzleDraw, const Interface::GameArea & area ) const
+void Maps::Tiles::redrawTopLayerObjects( fheroes2::Image & dst, const bool isPuzzleDraw, const Interface::GameArea & area ) const
 {
     const fheroes2::Point & mp = Maps::GetPoint( _index );
-
-    if ( !( visibleTileROI & mp ) )
-        return;
 
     // Ghost animation is unique and can be rendered in multiple cases.
     bool renderFlyingGhosts = false;
