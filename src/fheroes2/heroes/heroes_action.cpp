9d340ff2747a9763558ead384709defcda940698
shprotru <shprot@mail.ua>
2022-08-30 10:14:23 +0300

    code improvements

--- src/fheroes2/heroes/heroes_action.cpp
+++ src/fheroes2/heroes/heroes_action.cpp
@@ -724,20 +724,25 @@ void ActionToHeroes( Heroes & hero, int32_t dst_index )
     if ( hero.GetColor() == other_hero->GetColor() ) {
         DEBUG_LOG( DBG_GAME, DBG_INFO, hero.GetName() << " meeting " << other_hero->GetName() )
         hero.MeetingDialog( *other_hero );
+        return;
     }
-    else if ( hero.isFriends( other_hero->GetColor() )
-              && fheroes2::showMessage( fheroes2::Text{ _ ( "Are you sure you want to attack your teammate?" ), fheroes2::FontType::normalWhite() },
-                 fheroes2::Text{ "", fheroes2::FontType::normalWhite() }, Dialog::YES | Dialog::NO )
-                     == Dialog::YES ) {
-        DEBUG_LOG( DBG_GAME, DBG_INFO, hero.GetName() << " attack ally hero " << other_hero->GetName() )
+    else if ( hero.isFriends( other_hero->GetColor() ) ) {
+        if ( fheroes2::showMessage( fheroes2::Text{ _( "Are you sure you want to attack your ally?" ), fheroes2::FontType::normalWhite() },
+                                    fheroes2::Text{ "", fheroes2::FontType::normalWhite() }, Dialog::YES | Dialog::NO )
+                                        == Dialog::NO ) {
+            DEBUG_LOG( DBG_GAME, DBG_INFO, hero.GetName() << " declined attack ally hero " << other_hero->GetName() )
+            return;
+        }
 
-        auto p1 = Players::Get( hero.GetColor() );
-        auto p2 = Players::Get( other_hero->GetColor() );
+        DEBUG_LOG( DBG_GAME, DBG_INFO, hero.GetName() << " confirmed attack ally hero " << other_hero->GetName() )
+
+        auto *p1 = Players::Get( hero.GetColor() );
+        auto *p2 = Players::Get( other_hero->GetColor() );
 
         at_exit = std::shared_ptr<void>( nullptr,
                                          std::bind(
-                                             []( Player *p1, Player *p2 ) {
-                                                 fheroes2::showMessage( fheroes2::Text{ _ ( "You've made yourself another enemy" ), fheroes2::FontType::normalWhite() },
+                                             []( Player * p1, Player * p2 ) {
+                                                 fheroes2::showMessage( fheroes2::Text{ _( "You've made yourself another enemy" ), fheroes2::FontType::normalWhite() },
                                                                         fheroes2::Text{ "", fheroes2::FontType::normalWhite() }, Dialog::OK );
 
                                                  p1->SetFriends( p2->GetColor() );
@@ -745,40 +750,37 @@ void ActionToHeroes( Heroes & hero, int32_t dst_index )
                                              },
                                              p1, p2 ) );
 
-        goto lbattle;
     }
-    else {
-    lbattle:
-        const Castle * other_hero_castle = other_hero->inCastle();
-        if ( other_hero_castle && other_hero == other_hero_castle->GetHeroes().GuardFirst() ) {
-            ActionToCastle( hero, dst_index );
-            return;
-        }
 
-        DEBUG_LOG( DBG_GAME, DBG_INFO, hero.GetName() << " attack enemy hero " << other_hero->GetName() )
+    const Castle * other_hero_castle = other_hero->inCastle();
+    if ( other_hero_castle && other_hero == other_hero_castle->GetHeroes().GuardFirst() ) {
+        ActionToCastle( hero, dst_index );
+        return;
+    }
 
-        // new battle
-        Battle::Result res = Battle::Loader( hero.GetArmy(), other_hero->GetArmy(), dst_index );
+    DEBUG_LOG( DBG_GAME, DBG_INFO, hero.GetName() << " attack enemy hero " << other_hero->GetName() )
 
-        // TODO: make fading animation of both heroes together.
+    // new battle
+    Battle::Result res = Battle::Loader( hero.GetArmy(), other_hero->GetArmy(), dst_index );
 
-        // loss defender
-        if ( !res.DefenderWins() )
-            BattleLose( *other_hero, res, false );
+    // TODO: make fading animation of both heroes together.
 
-        // loss attacker
-        if ( !res.AttackerWins() )
-            BattleLose( hero, res, true );
+    // loss defender
+    if ( !res.DefenderWins() )
+        BattleLose( *other_hero, res, false );
 
-        // wins attacker
-        if ( res.AttackerWins() ) {
-            hero.IncreaseExperience( res.GetExperienceAttacker() );
-        }
-        else
-            // wins defender
-            if ( res.DefenderWins() ) {
-            other_hero->IncreaseExperience( res.GetExperienceDefender() );
-        }
+    // loss attacker
+    if ( !res.AttackerWins() )
+        BattleLose( hero, res, true );
+
+    // wins attacker
+    if ( res.AttackerWins() ) {
+        hero.IncreaseExperience( res.GetExperienceAttacker() );
+    }
+    else
+        // wins defender
+        if ( res.DefenderWins() ) {
+        other_hero->IncreaseExperience( res.GetExperienceDefender() );
     }
 }
 
