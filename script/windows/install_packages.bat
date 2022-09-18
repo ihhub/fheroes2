65742b3fbb91ee30249fce814885e38169bfd127
Oleg Derevenetz <oleg-derevenetz@yandex.ru>
2022-09-19 02:25:43 +0300

    Update the dependency package hash for Windows (#5855)

--- script/windows/install_packages.bat
+++ script/windows/install_packages.bat
@@ -3,7 +3,7 @@
 set DST_DIR=%~dp0\..\..\VisualStudio\packages
 
 set PKG_FILE=windows.zip
-set PKG_FILE_SHA256=4E1A622183E839727245782BDBF3CFA7BD4206FBC183FF764E1B7E9C79669D29
+set PKG_FILE_SHA256=290F84E793A120E8B555CF7A98D32232BC6E8B4C648EB05A50AD22C329662D0B
 set PKG_URL=https://github.com/fheroes2/fheroes2-prebuilt-deps/releases/download/windows-deps/%PKG_FILE%
 set PKG_TLS=[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
