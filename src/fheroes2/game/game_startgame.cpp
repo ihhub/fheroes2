431ff2e21a419f60c172a2e3a57a8ccd993b672f
Oleg Derevenetz <oleg-derevenetz@yandex.ru>
2022-10-10 12:57:46 +0300

    Recalculate the hero's path when the hero was selected with the mouse

--- src/fheroes2/game/game_startgame.cpp
+++ src/fheroes2/game/game_startgame.cpp
@@ -1163,6 +1163,7 @@ void Interface::Basic::MouseCursorAreaClickLeft( const int32_t index_maps )
         if ( nullptr != to_hero ) {
             if ( !from_hero || from_hero != to_hero ) {
                 SetFocus( to_hero );
+                CalculateHeroPath( to_hero, -1 );
                 RedrawFocus();
             }
             else {
