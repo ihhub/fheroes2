eb79625b46a56d06c5e18455648407401269de3d
Oleg Derevenetz <oleg-derevenetz@yandex.ru>
2022-10-19 21:53:48 +0300

    Do not build for 32-bit archs

--- android/app/jni/Application.mk
+++ android/app/jni/Application.mk
@@ -20,7 +20,5 @@
 
 # Build app with shared C++ STL
 APP_STL := c++_shared
-# Generate code for the following ABIs
-APP_ABI := armeabi-v7a arm64-v8a x86 x86_64
 # Minimum supported Android API level
 APP_PLATFORM=android-19
