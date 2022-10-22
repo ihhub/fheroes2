e1feeb3812c130a68ff4218c1e3a83d899fd51a3
Oleg Derevenetz <oleg-derevenetz@yandex.ru>
2022-10-22 18:37:16 +0300

    Android: create separate JNI modules for libengine, fheroes2 and libsmacker, build with WITH_DEBUG only in debug mode, enable warnings (#6019)

--- android/app/jni/engine/Android.mk
+++ android/app/jni/engine/Android.mk
@@ -20,25 +20,23 @@
 
 LOCAL_PATH := $(call my-dir)
 
-# Common for both C and C++ compilers
-FHEROES2_C_WARN_OPTIONS := \
-    -pedantic \
-    -Wall \
-    -Wextra \
-    -Wcast-align \
-    -Wextra-semi \
-    -Wfloat-conversion \
-    -Wfloat-equal \
-    -Winit-self \
-    -Wredundant-decls \
-    -Wshadow \
-    -Wundef \
-    -Wuninitialized \
-    -Wunused
+include $(CLEAR_VARS)
 
-# C++ compilers only
-FHEROES2_CPP_WARN_OPTIONS := \
-    -Wctor-dtor-privacy \
-    -Woverloaded-virtual
+# Mitigate the issue with Windows command line size limit
+LOCAL_SHORT_COMMANDS := true
 
-include $(call all-subdir-makefiles)
+ENGINE_SRC_DIR := $(LOCAL_PATH)/../../../../src/engine
+
+LOCAL_MODULE := engine
+LOCAL_SRC_FILES := $(wildcard $(ENGINE_SRC_DIR)/*.cpp)
+LOCAL_SHARED_LIBRARIES := SDL2 SDL2_mixer
+LOCAL_STATIC_LIBRARIES := smacker
+LOCAL_CFLAGS := $(FHEROES2_C_WARN_OPTIONS)
+LOCAL_CPP_FEATURES := exceptions rtti
+LOCAL_CPPFLAGS := \
+    -std=c++17 \
+    $(FHEROES2_CPP_WARN_OPTIONS)
+LOCAL_EXPORT_C_INCLUDES := $(ENGINE_SRC_DIR)
+LOCAL_EXPORT_LDLIBS := -llog -lz
+
+include $(BUILD_STATIC_LIBRARY)
