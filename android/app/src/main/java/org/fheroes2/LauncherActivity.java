5735f6832a963171521e69f52497b5f1d26e2ebf
Oleg Derevenetz <oleg-derevenetz@yandex.ru>
2022-10-23 23:55:24 +0300

    Style nits

--- android/app/src/main/java/org/fheroes2/LauncherActivity.java
+++ android/app/src/main/java/org/fheroes2/LauncherActivity.java
@@ -10,14 +10,14 @@ import android.os.Environment;
 import android.provider.Settings;
 
 public class LauncherActivity extends Activity {
-    private static final int REQUEST_CODE_WRITE_EXTERNAL_STORAGE = 1001;
+    private static final int REQUEST_CODE_EXTERNAL_STORAGE_ACCESS = 1001;
 
     @Override
     protected void onResume() {
         super.onResume();
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            // On Android 11+ we need to request the MANAGE_EXTERNAL_STORAGE permission
+            // On Android 11+ we need to request MANAGE_EXTERNAL_STORAGE permission
             if (!Environment.isExternalStorageManager()) {
                 Intent intent = new Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
                 intent.setData(Uri.fromParts("package", getPackageName(), null));
@@ -27,11 +27,11 @@ public class LauncherActivity extends Activity {
                 return;
             }
         } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            // On Android 6 to 10 we need to request the WRITE_EXTERNAL_STORAGE permission,
-            // READ_EXTERNAL_STORAGE permission will be granted automatically
+            // On Android 6 to 10 we need to request READ_EXTERNAL_STORAGE and WRITE_EXTERNAL_STORAGE permissions
             if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED ||
                     checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CODE_WRITE_EXTERNAL_STORAGE);
+                requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CODE_EXTERNAL_STORAGE_ACCESS);
 
                 return;
             }
@@ -44,7 +44,7 @@ public class LauncherActivity extends Activity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (requestCode == REQUEST_CODE_WRITE_EXTERNAL_STORAGE) {
+        if (requestCode == REQUEST_CODE_EXTERNAL_STORAGE_ACCESS) {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                 if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED &&
                         checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
