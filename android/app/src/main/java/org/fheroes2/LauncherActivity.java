938b3132d0bf44139e0e5eb5c55069266ef01871
Oleg Derevenetz <oleg-derevenetz@yandex.ru>
2022-10-24 09:03:13 +0300

    Maybe we don't need the MANAGE_EXTERNAL_STORAGE at all

--- android/app/src/main/java/org/fheroes2/LauncherActivity.java
+++ android/app/src/main/java/org/fheroes2/LauncherActivity.java
@@ -24,10 +24,7 @@ import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.net.Uri;
 import android.os.Build;
-import android.os.Environment;
-import android.provider.Settings;
 
 public class LauncherActivity extends Activity
 {
@@ -38,19 +35,8 @@ public class LauncherActivity extends Activity
     {
         super.onResume();
 
-        // On Android 11+ we need to request MANAGE_EXTERNAL_STORAGE permission
-        if ( Build.VERSION.SDK_INT >= Build.VERSION_CODES.R ) {
-            if ( !Environment.isExternalStorageManager() ) {
-                Intent intent = new Intent( Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION );
-                intent.setData( Uri.fromParts( "package", getPackageName(), null ) );
-
-                startActivity( intent );
-
-                return;
-            }
-        }
-        // On Android 6 to 10 we need to request READ_EXTERNAL_STORAGE and WRITE_EXTERNAL_STORAGE permissions
-        else if ( Build.VERSION.SDK_INT >= Build.VERSION_CODES.M ) {
+        // On Android 6 to 9 we need to request READ_EXTERNAL_STORAGE and WRITE_EXTERNAL_STORAGE permissions
+        if ( Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && Build.VERSION.SDK_INT < Build.VERSION_CODES.Q ) {
             if ( checkSelfPermission( Manifest.permission.READ_EXTERNAL_STORAGE ) != PackageManager.PERMISSION_GRANTED
                  || checkSelfPermission( Manifest.permission.WRITE_EXTERNAL_STORAGE ) != PackageManager.PERMISSION_GRANTED ) {
                 requestPermissions( new String[] { Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE },
