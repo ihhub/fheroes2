a07d2e56b1167341e5d30820ecf4bffb4132cb80
Oleg Derevenetz <oleg-derevenetz@yandex.ru>
2022-10-28 21:03:56 +0300

    Android: remove the version info from the AndroidManifest.xml, set proper versionName in the build.gradle, update hash of the dependency package (#6054)

--- script/android/install_packages.sh
+++ script/android/install_packages.sh
@@ -3,7 +3,7 @@
 set -e
 
 PKG_FILE="android.zip"
-PKG_FILE_SHA256="b26ec4f5fc5871fb7cc090f8679794d7764f24cc622ebfa68b9778f850dc8ac5"
+PKG_FILE_SHA256="f33350acc50d23d4c7a7d8c34ed57fb0198f27861e1f7fcc70542868a92eee6f"
 PKG_URL="https://github.com/fheroes2/fheroes2-prebuilt-deps/releases/download/android-deps/$PKG_FILE"
 
 TMP_DIR="$(mktemp -d)"
