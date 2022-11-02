64e23dcf702c51ab5b15c9e6c888341470661dfa
morgane-du-tweize <morgane.rt@wanadoo.fr>
2022-11-02 15:18:32 +0100

    corrected unadapted sentence - rise(s) from the dead - 1701 issue

--- src/fheroes2/battle/battle_troop.cpp
+++ src/fheroes2/battle/battle_troop.cpp
@@ -1514,7 +1514,11 @@ void Battle::Unit::SpellRestoreAction( const Spell & spell, uint32_t spoint, con
         SetPosition( GetPosition() );
 
         if ( Arena::GetInterface() ) {
-            std::string str( _( "%{count} %{name} rise(s) from the dead!" ) );
+            std::string str( _( "%{count} %{name} rise from the dead!" ) );
+            if (count > 1)
+            {
+                StringReplace( str, "rise", "rises") ;
+            }
             StringReplace( str, "%{count}", resurrect );
             StringReplace( str, "%{name}", GetName() );
             Arena::GetInterface()->SetStatus( str, true );
