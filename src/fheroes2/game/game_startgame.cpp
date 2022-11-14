70c7c4014dfde1f66059f8725530b01dea822ea8
Michael <13953785+Laserlicht@users.noreply.github.com>
2022-11-14 20:55:38 +0100

    codestyle

--- src/fheroes2/game/game_startgame.cpp
+++ src/fheroes2/game/game_startgame.cpp
@@ -930,7 +930,8 @@ fheroes2::GameMode Interface::Basic::HumanTurn( bool isload )
             break;
         }
 
-        if ( fheroes2::cursor().isFocusActive() && !gameArea.isDragScroll() && !radar.isDragRadar() && ( le.MouseInsideRenderArea() || le.GetCurrentInputMode() != LocalEvent::INPUTMODE_TOUCH ) ) {
+        if ( fheroes2::cursor().isFocusActive() && !gameArea.isDragScroll() && !radar.isDragRadar()
+             && ( le.MouseInsideRenderArea() || le.GetCurrentInputMode() != LocalEvent::INPUTMODE_TOUCH ) ) {
             int scrollPosition = SCROLL_NONE;
 
             if ( isScrollLeft( le.GetMouseCursor() ) )
