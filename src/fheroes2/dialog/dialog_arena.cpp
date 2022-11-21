b1dd4885bf0d5e450ce59bfc283be14f9f3779cf
Oleg Derevenetz <oleg-derevenetz@yandex.ru>
2022-11-21 17:22:23 +0300

    Fix headers

--- src/fheroes2/dialog/dialog_arena.cpp
+++ src/fheroes2/dialog/dialog_arena.cpp
@@ -21,7 +21,6 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 
-#include <cstdint>
 #include <memory>
 
 #include "agg_image.h"
