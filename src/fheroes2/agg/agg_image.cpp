fcb86d4b9602577526ddb6820da574f9ec80b814
Zenseii <zenseii@users.noreply.github.com>
2022-11-25 17:42:41 +0100

    Fit standard game

--- src/fheroes2/agg/agg_image.cpp
+++ src/fheroes2/agg/agg_image.cpp
@@ -510,7 +510,7 @@ namespace fheroes2
                     Fill( out, 13, 11, 113, 31, getButtonFillingColor( i == 0 ) );
                 }
 
-                renderTextOnButton( _icnVsSprite[id][0], _icnVsSprite[id][1], gettext_noop( "STANDARD\nGAME" ), { 12, 5 }, { 11, 6 }, { 117, 47 },
+                renderTextOnButton( _icnVsSprite[id][0], _icnVsSprite[id][1], gettext_noop( "STANDARD\nGAME" ), { 12, 5 }, { 11, 6 }, { 120, 47 },
                                     fheroes2::FontColor::WHITE );
 
                 break;
