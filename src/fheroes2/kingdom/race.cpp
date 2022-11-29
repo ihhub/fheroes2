1072322d1225c109adef506a3384960d6c988a0d
Zenseii <zenseii@users.noreply.github.com>
2022-11-29 01:35:37 +0100

    include cassert instead..

--- src/fheroes2/kingdom/race.cpp
+++ src/fheroes2/kingdom/race.cpp
@@ -20,7 +20,7 @@
  *   Free Software Foundation, Inc.,                                       *
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
-#include <assert.h>
+#include <cassert>
 
 #include "race.h"
 #include "rand.h"
