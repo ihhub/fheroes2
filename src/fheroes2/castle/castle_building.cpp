ddbb8ccd971461e34d97bca0633f58ed94685176
undef <csm.sub21@gmail.com>
2022-11-30 09:17:00 +0300

    fix position of left barbarian's captain quarters part

--- src/fheroes2/castle/castle_building.cpp
+++ src/fheroes2/castle/castle_building.cpp
@@ -410,8 +410,8 @@ void CastleDialog::CastleRedrawBuildingExtended( const Castle & castle, const fh
         const fheroes2::Sprite & rightCaptainQuarters = fheroes2::AGG::GetICN( ICN::TWNBCAPT, 0 );
         const fheroes2::Sprite & leftCaptainQuarters = fheroes2::AGG::GetICN( ICN::BARBARIAN_CASTLE_CAPTAIN_QUARTERS_LEFT_SIDE, 0 );
         fheroes2::drawCastleDialogBuilding( ICN::BARBARIAN_CASTLE_CAPTAIN_QUARTERS_LEFT_SIDE, 0, castle,
-                                            { dst_pt.x + rightCaptainQuarters.x() - leftCaptainQuarters.width() + leftCaptainQuarters.x(),
-                                              dst_pt.y + rightCaptainQuarters.y() + leftCaptainQuarters.y() },
+                                            { dst_pt.x + rightCaptainQuarters.x() - leftCaptainQuarters.width(),
+                                              dst_pt.y + rightCaptainQuarters.y() },
                                             max, alpha );
     }
 }
