ihhub/fheroes2

commit 960298fa92dd9f9dea1ab5003fc85c78114a55a5

Fix rendering and mouse behavior for SDL2 (#2238)
close #900
close #1091

src/engine/screen.cpp
@@ -23,6 +23,7 @@
 
 #include <SDL_version.h>
 #if SDL_VERSION_ATLEAST( 2, 0, 0 )
+#include <SDL_hints.h>
 #include <SDL_mouse.h>
 #include <SDL_render.h>
 #include <SDL_video.h>
@@ -433,7 +434,7 @@ namespace
             else {
                 SDL_UpdateTexture( _texture, NULL, _surface->pixels, _surface->pitch );
                 if ( SDL_SetRenderTarget( _renderer, NULL ) == 0 ) {
-                    if ( SDL_RenderCopy( _renderer, _texture, NULL, NULL ) == 0 ) {
+                    if ( SDL_RenderClear( _renderer ) == 0 && SDL_RenderCopy( _renderer, _texture, NULL, NULL ) == 0 ) {
                         SDL_RenderPresent( _renderer );
                     }
                 }
@@ -486,7 +487,11 @@ namespace
             }
 
             _createPalette();
-
+            SDL_SetHint( SDL_HINT_RENDER_SCALE_QUALITY, "linear" );
+            if ( SDL_RenderSetLogicalSize( _renderer, width_, height_ ) ) {
+                clear();
+                return false;
+            }
             _texture = SDL_CreateTextureFromSurface( _renderer, _surface );
             if ( _texture == NULL ) {
                 clear();

src/engine/localevent.cpp
@@ -924,27 +924,6 @@ bool LocalEvent::MouseCursor( const Rect & rt ) const
     return rt & mouse_cu;
 }
 
-const Point & LocalEvent::GetMouseCursor( void )
-{
-#ifdef WITHOUT_MOUSE
-    if ( !emulate_mouse )
-#endif
-    {
-        int x, y;
-
-        SDL_PumpEvents();
-        SDL_GetMouseState( &x, &y );
-
-        mouse_cu.x = x;
-        mouse_cu.y = y;
-    }
-
-    if ( modes & MOUSE_OFFSET )
-        mouse_cu += mouse_st;
-
-    return mouse_cu;
-}
-
 int LocalEvent::KeyMod( void ) const
 {
     return SDL_GetModState();

src/engine/localevent.h
@@ -186,7 +186,11 @@ public:
     bool MouseMotion( void ) const;
     bool MouseMotion( const Rect & rt ) const;
 
-    const Point & GetMouseCursor( void );
+    const Point & GetMouseCursor( void )
+    {
+        return mouse_cu;
+    }
+
     const Point & GetMousePressLeft( void ) const;
     const Point & GetMousePressMiddle( void ) const;
     const Point & GetMousePressRight( void ) const;

commit 51c62f19af3d74768db3caf994922397cfcaa59e

Make an option to toggle scaling
Regression from #2238

With introduction of "fake" full screen mode for Windows it is visible that the picture is blurred with linear mode. To cover needs of everyone we are adding a new configuration option to choose pixelated image or blurred. By default it is pixelated.

src/engine/screen.cpp
@@ -658,7 +658,8 @@ namespace
 
             if ( width_ != VITA_FULLSCREEN_WIDTH || height_ != VITA_FULLSCREEN_HEIGHT ) {
                 if ( isFullScreen ) {
-                    vita2d_texture_set_filters( _texBuffer, SCE_GXM_TEXTURE_FILTER_LINEAR, SCE_GXM_TEXTURE_FILTER_LINEAR );
+                    vita2d_texture_set_filters( _texBuffer, isLinearScaling() ? SCE_GXM_TEXTURE_FILTER_LINEAR : SCE_GXM_TEXTURE_FILTER_POINT,
+                                                isLinearScaling() ? SCE_GXM_TEXTURE_FILTER_LINEAR : SCE_GXM_TEXTURE_FILTER_POINT );
                     if ( ( static_cast<float>( VITA_FULLSCREEN_WIDTH ) / VITA_FULLSCREEN_HEIGHT ) >= ( static_cast<float>( width_ ) / height_ ) ) {
                         const float scale = static_cast<float>( VITA_FULLSCREEN_HEIGHT ) / height_;
                         _destRect.width = static_cast<int32_t>( static_cast<float>( width_ ) * scale );
@@ -1198,7 +1199,7 @@ namespace
                 return false;
             }
 
-            if ( SDL_SetHint( SDL_HINT_RENDER_SCALE_QUALITY, "linear" ) == SDL_FALSE ) {
+            if ( SDL_SetHint( SDL_HINT_RENDER_SCALE_QUALITY, ( isLinearScaling() ? "linear" : " nearest" ) ) == SDL_FALSE ) {
                 ERROR_LOG( "Failed to set a linear scale hint for rendering." )
             }
 

src/engine/screen.h
@@ -81,6 +81,16 @@ namespace fheroes2
             // Do nothing.
         }
 
+        void setLinearScaling( const bool enable )
+        {
+            _linearScaling = enable;
+        }
+
+        bool isLinearScaling() const
+        {
+            return _linearScaling;
+        }
+
     protected:
         BaseRenderEngine()
             : _isFullScreen( false )
@@ -118,6 +128,8 @@ namespace fheroes2
 
     private:
         bool _isFullScreen;
+
+        bool _linearScaling;
     };
 
     class Display : public Image

src/fheroes2/system/settings.cpp
@@ -78,7 +78,8 @@ namespace
         GLOBAL_BATTLE_SHOW_MOVE_SHADOW = 0x02000000,
         GLOBAL_BATTLE_AUTO_RESOLVE = 0x04000000,
         GLOBAL_BATTLE_AUTO_SPELLCAST = 0x08000000,
-        GLOBAL_AUTO_SAVE_AT_BEGINNING_OF_TURN = 0x10000000
+        GLOBAL_AUTO_SAVE_AT_BEGINNING_OF_TURN = 0x10000000,
+        GLOBAL_SCREEN_LINEAR_SCALING = 0x20000000
     };
 }
 
@@ -335,6 +336,10 @@ bool Settings::Read( const std::string & filePath )
         }
     }
 
+    if ( config.Exists( "screen scaling type" ) ) {
+        setAutoSaveAtBeginningOfTurn( config.StrParams( "screen scaling type" ) == "linear" );
+    }
+
     return true;
 }
 
@@ -476,6 +481,9 @@ std::string Settings::String() const
 
     os << std::endl << "# enable cursor software rendering" << std::endl;
     os << "cursor soft rendering = " << ( _optGlobal.Modes( GLOBAL_CURSOR_SOFT_EMULATION ) ? "on" : "off" ) << std::endl;
+
+    os << std::endl << "# scaling type: nearest (default) or linear" << std::endl;
+    os << "screen scaling type = " << ( _optGlobal.Modes( GLOBAL_SCREEN_LINEAR_SCALING ) ? "linear" : "nearest" ) << std::endl;
 
     return os.str();
 }
@@ -776,6 +784,18 @@ void Settings::setEvilInterface( const bool enable )
     }
     else {
         _optGlobal.ResetModes( GLOBAL_EVIL_INTERFACE );
+    }
+}
+
+void Settings::setScreenLinearScaling( const bool enable )
+{
+    if ( enable ) {
+        _optGlobal.SetModes( GLOBAL_SCREEN_LINEAR_SCALING );
+        fheroes2::engine().setLinearScaling( true );
+    }
+    else {
+        _optGlobal.ResetModes( GLOBAL_SCREEN_LINEAR_SCALING );
+        fheroes2::engine().setLinearScaling( false );
     }
 }
 

src/fheroes2/system/settings.h
@@ -245,6 +245,7 @@ public:
     void setBattleDamageInfo( const bool enable );
     void setHideInterface( const bool enable );
     void setEvilInterface( const bool enable );
+    void setScreenLinearScaling( const bool enable );
 
     void SetSoundVolume( int v );
     void SetMusicVolume( int v );