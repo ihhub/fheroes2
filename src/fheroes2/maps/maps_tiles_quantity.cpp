5d86c001e5318d3c80e7727d6d2e3c4265d06bd9
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2022-12-18 21:42:56 +0800

    Fix incorrect resource placements on certain maps
    close #6393

--- src/fheroes2/maps/maps_tiles_quantity.cpp
+++ src/fheroes2/maps/maps_tiles_quantity.cpp
@@ -554,10 +554,32 @@ void Maps::Tiles::QuantityUpdate( bool isFirstLoad )
     }
 
     case MP2::OBJ_RESOURCE: {
-        const int res = Resource::FromIndexSprite( objectIndex );
+        int resourceType = Resource::UNKNOWN;
+        // TODO: add a function opposite to MP2::GetICNObject() to return tileset ID.
+        const int resourceTileSet = 46;
+
+        if ( ( objectTileset >> 2 ) == resourceTileSet ) {
+            // The resource is located at the top.
+            resourceType = Resource::FromIndexSprite( objectIndex );
+        }
+        else {
+            for ( TilesAddon & addon : addons_level1 ) {
+                if ( ( addon.object >> 2 ) == resourceTileSet ) {
+                    resourceType = Resource::FromIndexSprite( addon.index );
+                    // If this happens we are in trouble. It looks like that map maker put the resource under an object which is impossible to do.
+                    // Let's swap the addon and main tile objects
+                    std::swap( addon.object, objectTileset );
+                    std::swap( addon.index, objectIndex );
+                    std::swap( addon.uniq, uniq );
+                    std::swap( addon.level, _level );
+
+                    break;
+                }
+            }
+        }
         uint32_t count = 0;
 
-        switch ( res ) {
+        switch ( resourceType ) {
         case Resource::GOLD:
             count = 100 * Rand::Get( 5, 10 );
             break;
@@ -565,12 +587,20 @@ void Maps::Tiles::QuantityUpdate( bool isFirstLoad )
         case Resource::ORE:
             count = Rand::Get( 5, 10 );
             break;
-        default:
+        case Resource::MERCURY:
+        case Resource::SULFUR:
+        case Resource::CRYSTAL:
+        case Resource::GEMS:
             count = Rand::Get( 3, 6 );
+            break;
+        default:
+            // Some maps have broken resources being put which ideally we need to correct. Let's make them 0 Wood.
+            resourceType = Resource::WOOD;
+            count = 0;
             break;
         }
 
-        QuantitySetResource( res, count );
+        QuantitySetResource( resourceType, count );
         break;
     }
 
