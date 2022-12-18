6226914f5de77c41a07ea73bb9fed0bbed11c2ad
Zenseii <zenseii@users.noreply.github.com>
2022-12-18 13:45:20 +0100

    Create wider Trade button

--- src/fheroes2/agg/agg_image.cpp
+++ src/fheroes2/agg/agg_image.cpp
@@ -826,11 +826,30 @@ namespace fheroes2
 
                 for ( int32_t i = 0; i < static_cast<int32_t>( _icnVsSprite[id].size() ); ++i ) {
                     Sprite & out = _icnVsSprite[id][i];
-                    out = GetICN( baseIcnID, 11 + i );
+                    const Sprite & originalButton = GetICN( baseIcnID, 11 + i );
+                    out.resize( originalButton.width() + 30, originalButton.height() );
+                    out.reset();
+
+                    const int widthEndPart = 4 + 3 * i;
+                    const int widthFirstPart = originalButton.width() - widthEndPart;
+                    const int widthMiddlePart = 16 + i;
+                    const int offsetXEndPart = widthFirstPart + widthMiddlePart;
+                    const int startOffsetXMiddlePart = 36;
+
+                    // Copy left main body of button.
+                    fheroes2::Copy( originalButton, 0, 0, out, 0, 0, widthFirstPart, originalButton.height() );
+
+                    // Copy middle body of button.
+                    fheroes2::Copy( originalButton, originalButton.width() - startOffsetXMiddlePart, 0, out, widthFirstPart, 0, widthMiddlePart,
+                                    originalButton.height() );
+
+                    // Copy terminating right margin of the button.
+                    fheroes2::Copy( originalButton, originalButton.width() - widthEndPart, 0, out, offsetXEndPart, 0, widthEndPart,
+                                    originalButton.height() );
                 }
 
                 const fheroes2::FontColor buttonFontColor = isEvilInterface ? fheroes2::FontColor::GRAY : fheroes2::FontColor::WHITE;
-                renderTextOnButton( _icnVsSprite[id][0], _icnVsSprite[id][1], gettext_noop( "TRADE" ), { 6, 5 }, { 4, 6 }, { 86, 16 }, buttonFontColor );
+                renderTextOnButton( _icnVsSprite[id][0], _icnVsSprite[id][1], gettext_noop( "TRADE" ), { 7, 5 }, { 5, 6 }, { 102, 16 }, buttonFontColor );
 
                 break;
             }
