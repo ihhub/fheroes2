0f0b091210c361419a5c88e271ca70c3d20b6316
Michael <13953785+Laserlicht@users.noreply.github.com>
2022-12-31 21:10:01 +0100

    Create changelog_update_appdata.py

---
+++ script/tools/changelog_update_appdata.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+import argparse
+import re
+import datetime
+import html
+
+#
+# Appstream file could be validated with 'appstream-util validate io.github.ihhub.Fheroes2.appdata.xml'
+#
+
+class CustomArgumentParser(argparse.ArgumentParser):
+    def error(self, message):
+        self.print_usage(sys.stderr)
+        self.exit(1, "%(prog)s: error: %(message)s\n" %
+                  {"prog": self.prog, "message": message})
+
+def parse_arguments():
+    parser = CustomArgumentParser()
+    parser.add_argument("changelog_file", help="changelog.txt file")
+    parser.add_argument("appdata_file", help="*.appdata.xml file")
+    return parser.parse_args()
+
+def main():
+    args = parse_arguments()
+    
+    changelog = open(args.changelog_file, encoding='utf-8').read()
+    appdata = open(args.appdata_file, encoding='utf-8').read()
+    
+    tpl = '''
+    <release date="{}" version="v{}">
+      <url>https://github.com/ihhub/fheroes2/releases/tag/{}</url>
+      <description>
+        <p>Changes in v{} ({}):</p>
+        <ul>
+{}
+        </ul>
+      </description>
+    </release>\n'''[1:]
+    tmp = ''
+    
+    regex = r"version ([\d.]+) \(([\w ]+)\)\n(.*?)[\n]{2}"
+    for match in re.findall(regex, changelog, re.MULTILINE | re.DOTALL):
+        tmp += tpl.format(
+            datetime.datetime.strptime(match[1], '%d %B %Y').strftime('%Y-%m-%d'),
+            match[0],
+            match[0],
+            match[0],
+            match[1],
+            re.sub(r'- (.*)', r'          <li>\1</li>', html.escape(match[2]))
+        )
+        
+    new_appdata = re.sub(r'<releases>(.*?)</releases>', r'<releases>\n' + tmp + '  </releases>',
+        appdata,
+        flags=re.MULTILINE | re.DOTALL
+    )
+    
+    open(args.appdata_file, 'w', encoding='utf-8').write(new_appdata)
+
+if __name__ == "__main__":
+    main()
