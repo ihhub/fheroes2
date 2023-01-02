9266114bf6adb08f70efa36b10e78caad1a1e94c
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2023-01-02 19:01:06 +0800

    Update copyright year

--- src/fheroes2/agg/mus.cpp
+++ src/fheroes2/agg/mus.cpp
@@ -1,6 +1,6 @@
 /***************************************************************************
  *   fheroes2: https://github.com/ihhub/fheroes2                           *
- *   Copyright (C) 2019 - 2022                                             *
+ *   Copyright (C) 2019 - 2023                                             *
  *                                                                         *
  *   Free Heroes2 Engine: http://sourceforge.net/projects/fheroes2         *
  *   Copyright (C) 2008 by Josh Matthews <josh@joshmatthews.net>           *
