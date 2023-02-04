ihhub/fheroes2

commit 19d433787e88131cb083eee6a678efa2c488f969

Optimize rendering in multiple places (#6375)

src/fheroes2/gui/statusbar.cpp
@@ -36,7 +36,7 @@ void StatusBar::ShowMessage( const std::string & msg )
         SetText( msg );
         SetPos( center.x - w() / 2, center.y - h() / 2 );
         Show();
-        fheroes2::Display::instance().render();
+        fheroes2::Display::instance().render( GetRect() );
         prev = msg;
     }
 }

src/fheroes2/castle/castle_mageguild.cpp
@@ -144,7 +144,6 @@ bool RowSpells::QueueEventProcessing()
 
         if ( spell != Spell::NONE ) {
             fheroes2::SpellDialogElement( spell, nullptr ).showPopup( le.MousePressRight() ? Dialog::ZERO : Dialog::OK );
-            fheroes2::Display::instance().render();
         }
     }
 

src/fheroes2/dialog/dialog_box.cpp
@@ -175,7 +175,7 @@ Dialog::NonFixedFrameBox::~NonFixedFrameBox()
 {
     _restorer->restore();
 
-    fheroes2::Display::instance().render();
+    fheroes2::Display::instance().render( _restorer->rect() );
 }
 
 Dialog::FrameBox::FrameBox( int height, bool buttons )

src/fheroes2/gui/interface_gamearea.cpp
@@ -1044,11 +1044,13 @@ void Interface::GameArea::runSingleObjectAnimation( const std::shared_ptr<BaseOb
     addObjectAnimationInfo( info );
 
     LocalEvent & le = LocalEvent::Get();
+    fheroes2::Display & display = fheroes2::Display::instance();
+    Interface::Basic & basicInterface = Interface::Basic::Get();
 
     while ( le.HandleEvents() && !info->isAnimationCompleted() ) {
         if ( Game::validateAnimationDelay( Game::HEROES_PICKUP_DELAY ) ) {
-            Interface::Basic::Get().Redraw( Interface::REDRAW_GAMEAREA );
-            fheroes2::Display::instance().render();
+            basicInterface.Redraw( Interface::REDRAW_GAMEAREA );
+            display.render();
         }
     }
 }

src/fheroes2/heroes/heroes_action.cpp
@@ -3150,13 +3150,15 @@ void ActionToHutMagi( Heroes & hero, const MP2::MapObjectType objectType, int32_
         if ( !vec_eyes.empty() ) {
             Interface::Basic & I = Interface::Basic::Get();
 
+            fheroes2::Display & display = fheroes2::Display::instance();
+
             for ( const int32_t eyeIndex : vec_eyes ) {
                 Maps::ClearFog( eyeIndex, GameStatic::getFogDiscoveryDistance( GameStatic::FogDiscoveryType::MAGI_EYES ), hero.GetColor() );
 
                 I.GetGameArea().SetCenter( Maps::GetPoint( eyeIndex ) );
                 I.Redraw( Interface::REDRAW_GAMEAREA | Interface::REDRAW_RADAR );
 
-                fheroes2::Display::instance().render();
+                display.render();
 
                 LocalEvent & le = LocalEvent::Get();
                 int delay = 0;
@@ -3166,12 +3168,16 @@ void ActionToHutMagi( Heroes & hero, const MP2::MapObjectType objectType, int32_
                         ++delay;
                         Game::updateAdventureMapAnimationIndex();
                         I.Redraw( Interface::REDRAW_GAMEAREA | Interface::REDRAW_RADAR );
+
+                        display.render();
                     }
                 }
             }
 
             I.GetGameArea().SetCenter( hero.GetCenter() );
             I.SetRedraw( Interface::REDRAW_GAMEAREA | Interface::REDRAW_RADAR );
+
+            display.render();
         }
     }
 

commit 72f6ab81149666656818d3e5886e65c100b6e068

Fix incorrect status bar update call
regression from #6375

src/fheroes2/gui/statusbar.cpp
@@ -23,6 +23,7 @@
 
 #include "statusbar.h"
 #include "screen.h"
+#include "tools.h"
 
 void StatusBar::SetCenter( int32_t cx, int32_t cy )
 {
@@ -33,10 +34,12 @@ void StatusBar::SetCenter( int32_t cx, int32_t cy )
 void StatusBar::ShowMessage( const std::string & msg )
 {
     if ( msg != prev ) {
+        const fheroes2::Rect prevRoi = GetRect();
         SetText( msg );
         SetPos( center.x - w() / 2, center.y - h() / 2 );
         Show();
-        fheroes2::Display::instance().render( GetRect() );
+
+        fheroes2::Display::instance().render( fheroes2::getBoundaryRect( prevRoi, GetRect() ) );
         prev = msg;
     }
 }