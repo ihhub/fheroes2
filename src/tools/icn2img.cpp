0d38958fe61961a34458cc2d46b40cd4010f772e
Oleg Derevenetz <oleg-derevenetz@yandex.ru>
2023-02-05 03:46:44 +0300

    icn2img tool: handle multiple ICN files

--- src/tools/icn2img.cpp
+++ src/tools/icn2img.cpp
@@ -21,13 +21,16 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 
+#include <cassert>
 #include <cstdint>
 #include <cstdlib>
+#include <filesystem>
 #include <iomanip>
 #include <iostream>
 #include <memory>
 #include <sstream> // IWYU pragma: keep
 #include <string>
+#include <string_view>
 #include <vector>
 
 #include "agg_file.h"
@@ -35,89 +38,185 @@
 #include "image_palette.h"
 #include "image_tool.h"
 #include "serialize.h"
-#include "system.h"
+
+namespace
+{
+#if defined( _WIN32 )
+    // On Windows there is no support for shell-level globbing, so we need to resolve wildcard patterns ourselves
+    constexpr bool ignoreGlob = false;
+#else
+    // On Unix-like platforms, POSIX-compatible shells perform globbing themselves, we only see the resulting filenames
+    constexpr bool ignoreGlob = true;
+#endif
+
+    bool globMatch( const std::string_view string, const std::string_view wildcard )
+    {
+        size_t stringIdx = 0;
+        size_t wildcardIdx = 0;
+
+        size_t fallbackStringIdx = std::string_view::npos;
+        size_t fallbackWildcardIdx = std::string_view::npos;
+
+        while ( stringIdx < string.length() ) {
+            const bool isWildcardNotEnded = ( wildcardIdx < wildcard.length() );
+
+            if ( isWildcardNotEnded && wildcard[wildcardIdx] == '*' ) {
+                ++wildcardIdx;
+
+                fallbackStringIdx = stringIdx;
+                fallbackWildcardIdx = wildcardIdx;
+            }
+            else if ( isWildcardNotEnded && ( wildcard[wildcardIdx] == '?' || wildcard[wildcardIdx] == string[stringIdx] ) ) {
+                ++stringIdx;
+                ++wildcardIdx;
+            }
+            else {
+                if ( fallbackWildcardIdx == std::string_view::npos ) {
+                    return false;
+                }
+
+                assert( fallbackStringIdx != std::string_view::npos );
+
+                ++fallbackStringIdx;
+
+                stringIdx = fallbackStringIdx;
+                wildcardIdx = fallbackWildcardIdx;
+            }
+        }
+
+        for ( ; wildcardIdx < wildcard.length(); ++wildcardIdx ) {
+            if ( wildcard[wildcardIdx] != '*' ) {
+                break;
+            }
+        }
+
+        return wildcardIdx == wildcard.length();
+    }
+
+    void globFiles( const std::string_view arg, std::vector<std::string> & fileNames )
+    {
+        const std::filesystem::path argPath( arg );
+
+        const std::filesystem::path dir = [&argPath]() -> std::filesystem::path {
+            const std::filesystem::path parentPath = argPath.parent_path();
+            if ( !parentPath.empty() ) {
+                return parentPath;
+            }
+
+            return { "." };
+        }();
+
+        std::error_code ec;
+        // Using the non-throwing overload
+        if ( !std::filesystem::is_directory( dir, ec ) ) {
+            return;
+        }
+
+        const std::string pattern = argPath.filename().string();
+
+        if ( ignoreGlob || ( pattern.find( '*' ) == std::string_view::npos && pattern.find( '?' ) == std::string_view::npos ) ) {
+            fileNames.push_back( std::string{ arg } );
+            return;
+        }
+
+        for ( const std::filesystem::directory_entry & entry : std::filesystem::directory_iterator( dir ) ) {
+            const std::filesystem::path & entryPath = entry.path();
+
+            if ( globMatch( entryPath.filename().string(), pattern ) ) {
+                fileNames.push_back( entryPath.string() );
+            }
+        }
+    }
+}
 
 int main( int argc, char ** argv )
 {
-    if ( argc != 4 ) {
-        std::cout << argv[0] << " inputFile.icn destinationDirectory paletteFileName.pal" << std::endl;
+    if ( argc < 4 ) {
+        std::cerr << argv[0] << " destinationDirectory paletteFileName.pal inputFile.icn ..." << std::endl;
         return EXIT_FAILURE;
     }
 
-    std::string inputFileName( argv[1] );
-    std::string prefix( argv[2] );
-
-    std::string paletteFileName( argv[3] );
+    const char * dstDir = argv[1];
+    const char * paletteFileName = argv[2];
 
     StreamFile paletteFile;
     if ( !paletteFile.open( paletteFileName, "rb" ) ) {
-        std::cout << "Cannot open " << paletteFileName << std::endl;
+        std::cerr << "Cannot open " << paletteFileName << std::endl;
         return EXIT_FAILURE;
     }
 
-    std::vector<uint8_t> palette = paletteFile.getRaw();
+    const std::vector<uint8_t> palette = paletteFile.getRaw();
     if ( palette.size() != 768 ) {
-        std::cout << "Invalid palette size of " << palette.size() << " instead of 768." << std::endl;
+        std::cerr << "Invalid palette size of " << palette.size() << " instead of 768." << std::endl;
         return EXIT_FAILURE;
     }
 
     fheroes2::setGamePalette( palette );
 
-    StreamFile sf;
-
-    if ( !sf.open( inputFileName, "rb" ) ) {
-        std::cout << "Cannot open " << inputFileName << std::endl;
-        return EXIT_FAILURE;
+    std::vector<std::string> inputFileNames;
+    for ( int i = 3; i < argc; ++i ) {
+        globFiles( argv[i], inputFileNames );
     }
 
-    int count_sprite = sf.getLE16();
-    int total_size = sf.getLE32();
+    for ( const std::string & inputFileName : inputFileNames ) {
+        StreamFile sf;
 
-    inputFileName.replace( inputFileName.find( ".icn" ), 4, "" );
-    prefix = System::concatPath( prefix, inputFileName );
+        if ( !sf.open( inputFileName, "rb" ) ) {
+            std::cerr << "Cannot open " << inputFileName << std::endl;
+            return EXIT_FAILURE;
+        }
 
-    if ( 0 != System::MakeDirectory( prefix ) ) {
-        std::cout << "error mkdir: " << prefix << std::endl;
-        return EXIT_SUCCESS;
-    }
+        const std::filesystem::path prefix = std::filesystem::path( dstDir ) / std::filesystem::path( inputFileName ).stem();
 
-    size_t save_pos = sf.tell();
+        if ( !std::filesystem::exists( prefix ) && !std::filesystem::create_directories( prefix ) ) {
+            std::cerr << "Cannot create directory " << prefix << std::endl;
+            return EXIT_FAILURE;
+        }
 
-    std::vector<fheroes2::ICNHeader> headers( count_sprite );
-    for ( int ii = 0; ii < count_sprite; ++ii )
-        sf >> headers[ii];
+        std::cout << std::endl << "Processing " << inputFileName << "..." << std::endl << std::endl;
 
-    for ( int ii = 0; ii < count_sprite; ++ii ) {
-        const fheroes2::ICNHeader & head = headers[ii];
+        const int spritesCount = sf.getLE16();
+        const int totalSize = sf.getLE32();
 
-        uint32_t data_size = ( ii + 1 != count_sprite ? headers[ii + 1].offsetData - head.offsetData : total_size - head.offsetData );
-        sf.seek( save_pos + head.offsetData );
-        std::cerr << data_size << std::endl;
-        std::vector<uint8_t> buf = sf.getRaw( data_size );
+        const size_t beginPos = sf.tell();
 
-        if ( !buf.empty() ) {
-            const fheroes2::Sprite image = fheroes2::decodeICNSprite( &buf[0], data_size, head.width, head.height, head.offsetX, head.offsetY );
+        std::vector<fheroes2::ICNHeader> headers( spritesCount );
+        for ( fheroes2::ICNHeader & header : headers ) {
+            sf >> header;
+        }
+
+        for ( int spriteIdx = 0; spriteIdx < spritesCount; ++spriteIdx ) {
+            const fheroes2::ICNHeader & header = headers[spriteIdx];
+
+            sf.seek( beginPos + header.offsetData );
+
+            const uint32_t dataSize = ( spriteIdx + 1 != spritesCount ? headers[spriteIdx + 1].offsetData - header.offsetData : totalSize - header.offsetData );
+
+            const std::vector<uint8_t> buf = sf.getRaw( dataSize );
+            if ( buf.empty() ) {
+                continue;
+            }
+
+            const fheroes2::Sprite image = fheroes2::decodeICNSprite( &buf[0], dataSize, header.width, header.height, header.offsetX, header.offsetY );
 
             std::ostringstream os;
-            os << std::setw( 3 ) << std::setfill( '0' ) << ii;
+            os << std::setw( 3 ) << std::setfill( '0' ) << spriteIdx;
 
-            std::string dstfile = System::concatPath( prefix, os.str() );
+            std::string dstFileName = ( prefix / os.str() ).string();
 
             if ( fheroes2::isPNGFormatSupported() ) {
-                dstfile += ".png";
+                dstFileName += ".png";
             }
             else {
-                dstfile += ".bmp";
+                dstFileName += ".bmp";
             }
 
-            std::cout << "Image " << ii + 1 << " has offset of [" << static_cast<int32_t>( head.offsetX ) << ", " << static_cast<int32_t>( head.offsetY ) << "]"
-                      << std::endl;
+            std::cout << "Image " << spriteIdx + 1 << " has offset of [" << static_cast<int32_t>( header.offsetX ) << ", " << static_cast<int32_t>( header.offsetY )
+                      << "]" << std::endl;
 
-            fheroes2::Save( image, dstfile, 23 );
+            fheroes2::Save( image, dstFileName, 23 );
         }
     }
-
-    sf.close();
 
     return EXIT_SUCCESS;
 }
