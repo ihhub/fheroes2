275ccfd0b4b7fc6dbf9c7498df2388fa5938541a
Oleg Derevenetz <oleg-derevenetz@yandex.ru>
2023-02-07 15:54:51 +0000

    icn2img tool: handle multiple ICN files (#6564)

--- src/tools/icn2img.cpp
+++ src/tools/icn2img.cpp
@@ -1,6 +1,6 @@
 /***************************************************************************
  *   fheroes2: https://github.com/ihhub/fheroes2                           *
- *   Copyright (C) 2022                                                    *
+ *   Copyright (C) 2022 - 2023                                             *
  *                                                                         *
  *   Free Heroes2 Engine: http://sourceforge.net/projects/fheroes2         *
  *   Copyright (C) 2009 by Andrey Afletdinov <fheroes2@gmail.com>          *
@@ -23,11 +23,14 @@
 
 #include <cstdint>
 #include <cstdlib>
+#include <filesystem>
 #include <iomanip>
 #include <iostream>
 #include <memory>
 #include <sstream> // IWYU pragma: keep
 #include <string>
+#include <system_error>
+#include <type_traits>
 #include <vector>
 
 #include "agg_file.h"
@@ -39,85 +42,103 @@
 
 int main( int argc, char ** argv )
 {
-    if ( argc != 4 ) {
-        std::cout << argv[0] << " inputFile.icn destinationDirectory paletteFileName.pal" << std::endl;
+    if ( argc < 4 ) {
+        std::cerr << argv[0] << " destinationDirectory paletteFileName.pal inputFile.icn ..." << std::endl;
         return EXIT_FAILURE;
     }
 
-    std::string inputFileName( argv[1] );
-    std::string prefix( argv[2] );
-
-    std::string paletteFileName( argv[3] );
+    const char * dstDir = argv[1];
+    const char * paletteFileName = argv[2];
 
     StreamFile paletteFile;
     if ( !paletteFile.open( paletteFileName, "rb" ) ) {
-        std::cout << "Cannot open " << paletteFileName << std::endl;
+        std::cerr << "Cannot open " << paletteFileName << std::endl;
         return EXIT_FAILURE;
     }
 
-    std::vector<uint8_t> palette = paletteFile.getRaw();
+    const std::vector<uint8_t> palette = paletteFile.getRaw();
     if ( palette.size() != 768 ) {
-        std::cout << "Invalid palette size of " << palette.size() << " instead of 768." << std::endl;
+        std::cerr << "Invalid palette size of " << palette.size() << " instead of 768." << std::endl;
         return EXIT_FAILURE;
     }
 
     fheroes2::setGamePalette( palette );
 
-    StreamFile sf;
-
-    if ( !sf.open( inputFileName, "rb" ) ) {
-        std::cout << "Cannot open " << inputFileName << std::endl;
-        return EXIT_FAILURE;
-    }
-
-    int count_sprite = sf.getLE16();
-    int total_size = sf.getLE32();
-
-    inputFileName.replace( inputFileName.find( ".icn" ), 4, "" );
-    prefix = System::concatPath( prefix, inputFileName );
-
-    if ( 0 != System::MakeDirectory( prefix ) ) {
-        std::cout << "error mkdir: " << prefix << std::endl;
-        return EXIT_SUCCESS;
-    }
-
-    size_t save_pos = sf.tell();
-
-    std::vector<fheroes2::ICNHeader> headers( count_sprite );
-    for ( int ii = 0; ii < count_sprite; ++ii )
-        sf >> headers[ii];
-
-    for ( int ii = 0; ii < count_sprite; ++ii ) {
-        const fheroes2::ICNHeader & head = headers[ii];
-
-        uint32_t data_size = ( ii + 1 != count_sprite ? headers[ii + 1].offsetData - head.offsetData : total_size - head.offsetData );
-        sf.seek( save_pos + head.offsetData );
-        std::cerr << data_size << std::endl;
-        std::vector<uint8_t> buf = sf.getRaw( data_size );
-
-        if ( !buf.empty() ) {
-            const fheroes2::Sprite image = fheroes2::decodeICNSprite( &buf[0], data_size, head.width, head.height, head.offsetX, head.offsetY );
-
-            std::ostringstream os;
-            os << std::setw( 3 ) << std::setfill( '0' ) << ii;
-
-            std::string dstfile = System::concatPath( prefix, os.str() );
-
-            if ( fheroes2::isPNGFormatSupported() ) {
-                dstfile += ".png";
-            }
-            else {
-                dstfile += ".bmp";
-            }
-
-            std::cout << "Image " << ii + 1 << " has offset of [" << static_cast<int32_t>( head.offsetX ) << ", " << static_cast<int32_t>( head.offsetY ) << "]"
-                      << std::endl;
-
-            fheroes2::Save( image, dstfile, 23 );
+    std::vector<std::string> inputFileNames;
+    for ( int i = 3; i < argc; ++i ) {
+        if ( System::isShellLevelGlobbingSupported() ) {
+            inputFileNames.emplace_back( argv[i] );
+        }
+        else {
+            System::globFiles( argv[i], inputFileNames );
         }
     }
 
-    sf.close();
+    for ( const std::string & inputFileName : inputFileNames ) {
+        StreamFile sf;
+
+        if ( !sf.open( inputFileName, "rb" ) ) {
+            std::cerr << "Cannot open " << inputFileName << std::endl;
+            return EXIT_FAILURE;
+        }
+
+        const std::filesystem::path prefix = std::filesystem::path( dstDir ) / std::filesystem::path( inputFileName ).stem();
+
+        std::error_code ec;
+
+        // Using the non-throwing overloads
+        if ( !std::filesystem::exists( prefix, ec ) && !std::filesystem::create_directories( prefix, ec ) ) {
+            std::cerr << "Cannot create directory " << prefix << std::endl;
+            return EXIT_FAILURE;
+        }
+
+        std::cout << std::endl << "Processing " << inputFileName << "..." << std::endl << std::endl;
+
+        const uint16_t spritesCount = sf.getLE16();
+        const uint32_t totalSize = sf.getLE32();
+
+        const size_t beginPos = sf.tell();
+
+        std::vector<fheroes2::ICNHeader> headers( spritesCount );
+        for ( fheroes2::ICNHeader & header : headers ) {
+            sf >> header;
+        }
+
+        for ( uint16_t spriteIdx = 0; spriteIdx < spritesCount; ++spriteIdx ) {
+            const fheroes2::ICNHeader & header = headers[spriteIdx];
+
+            sf.seek( beginPos + header.offsetData );
+
+            const uint32_t dataSize = ( spriteIdx + 1 < spritesCount ? headers[spriteIdx + 1].offsetData - header.offsetData : totalSize - header.offsetData );
+
+            const std::vector<uint8_t> buf = sf.getRaw( dataSize );
+            if ( buf.empty() ) {
+                continue;
+            }
+
+            const fheroes2::Sprite image = fheroes2::decodeICNSprite( buf.data(), dataSize, header.width, header.height, header.offsetX, header.offsetY );
+
+            std::ostringstream os;
+            os << std::setw( 3 ) << std::setfill( '0' ) << spriteIdx;
+
+            std::string dstFileName = ( prefix / os.str() ).string();
+
+            if ( fheroes2::isPNGFormatSupported() ) {
+                dstFileName += ".png";
+            }
+            else {
+                dstFileName += ".bmp";
+            }
+
+            static_assert( std::is_same_v<decltype( header.offsetX ), uint16_t> && std::is_same_v<decltype( header.offsetY ), uint16_t>,
+                           "Offset types have been changed, check the casts below" );
+
+            std::cout << "Image " << spriteIdx + 1 << " has offset of [" << static_cast<int16_t>( header.offsetX ) << ", " << static_cast<int16_t>( header.offsetY )
+                      << "]" << std::endl;
+
+            fheroes2::Save( image, dstFileName, 23 );
+        }
+    }
 
     return EXIT_SUCCESS;
 }
