b44295a8104520533fd98c87eb088d833b32eba0
Oleg Derevenetz <oleg-derevenetz@yandex.ru>
2023-02-10 00:52:18 +0300

    Apply IWYU suggestions

--- src/tools/extractor.cpp
+++ src/tools/extractor.cpp
@@ -27,7 +27,10 @@
 #include <filesystem>
 #include <iostream>
 #include <map>
+#include <sstream>
 #include <string>
+#include <system_error>
+#include <type_traits>
 #include <utility>
 #include <vector>
 
