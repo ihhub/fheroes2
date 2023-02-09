fc398546e329d1edf67e8a6aef3f79738b98fa52
Oleg Derevenetz <oleg-derevenetz@yandex.ru>
2023-02-10 02:11:10 +0300

    Extractor: support multiple AGG files

--- src/tools/extractor.cpp
+++ src/tools/extractor.cpp
@@ -70,85 +70,101 @@ namespace
 
 int main( int argc, char ** argv )
 {
-    if ( argc != 3 ) {
+    if ( argc < 3 ) {
         std::string baseName = System::GetBasename( argv[0] );
 
-        std::cerr << baseName << " extracts the contents of the specified AGG file." << std::endl << "Syntax: " << baseName << " input_file.agg dst_dir" << std::endl;
+        std::cerr << baseName << " extracts the contents of the specified AGG file(s)." << std::endl
+                  << "Syntax: " << baseName << " dst_dir input_file.agg ..." << std::endl;
         return EXIT_FAILURE;
     }
 
-    StreamFile inputStream;
-    if ( !inputStream.open( argv[1], "rb" ) ) {
-        std::cerr << "Cannot open file " << argv[1] << std::endl;
-        return EXIT_FAILURE;
+    const char * dstDir = argv[1];
+
+    std::vector<std::string> inputFileNames;
+    for ( int i = 2; i < argc; ++i ) {
+        if ( System::isShellLevelGlobbingSupported() ) {
+            inputFileNames.emplace_back( argv[i] );
+        }
+        else {
+            System::globFiles( argv[i], inputFileNames );
+        }
     }
 
     std::error_code ec;
 
     // Using the non-throwing overloads
-    if ( !std::filesystem::exists( argv[2], ec ) && !std::filesystem::create_directories( argv[2], ec ) ) {
-        std::cerr << "Cannot create directory " << argv[2] << std::endl;
+    if ( !std::filesystem::exists( dstDir, ec ) && !std::filesystem::create_directories( dstDir, ec ) ) {
+        std::cerr << "Cannot create directory " << dstDir << std::endl;
         return EXIT_FAILURE;
     }
 
-    const size_t inputStreamSize = inputStream.size();
-    const uint16_t itemsCount = inputStream.getLE16();
+    uint32_t itemsExtracted = 0;
+    uint32_t itemsFailed = 0;
 
-    StreamBuf itemsStream = inputStream.toStreamBuf( static_cast<size_t>( itemsCount ) * 4 * 3 /* hash, offset, size */ );
-    inputStream.seek( inputStreamSize - AGGItemNameLen * itemsCount );
-    StreamBuf namesStream = inputStream.toStreamBuf( AGGItemNameLen * itemsCount );
+    for ( const std::string & inputFileName : inputFileNames ) {
+        std::cout << "Processing " << inputFileName << "..." << std::endl;
 
-    std::map<std::string, AGGItemInfo> aggItemsMap;
-
-    for ( uint16_t i = 0; i < itemsCount; ++i ) {
-        AGGItemInfo & info = aggItemsMap[StringLower( namesStream.toString( AGGItemNameLen ) )];
-
-        info.hash = itemsStream.getLE32();
-        info.offset = itemsStream.getLE32();
-        info.size = itemsStream.getLE32();
-    }
-
-    uint16_t itemsExtracted = 0;
-    uint16_t itemsFailed = 0;
-
-    for ( const auto & item : aggItemsMap ) {
-        const auto & [name, info] = item;
-
-        std::cout << "Processing " << name << "..." << std::endl;
-
-        const uint32_t hash = calculateHash( name );
-        if ( hash != info.hash ) {
-            ++itemsFailed;
-
-            std::cerr << "Invalid hash for item " << name << ": expected " << GetHexString( info.hash ) << ", got " << GetHexString( hash ) << std::endl;
-            continue;
+        StreamFile inputStream;
+        if ( !inputStream.open( inputFileName, "rb" ) ) {
+            std::cerr << "Cannot open file " << inputFileName << std::endl;
+            return EXIT_FAILURE;
         }
 
-        inputStream.seek( info.offset );
+        const size_t inputStreamSize = inputStream.size();
+        const uint16_t itemsCount = inputStream.getLE16();
 
-        const std::vector<uint8_t> buf = inputStream.getRaw( info.size );
-        if ( buf.empty() ) {
-            ++itemsFailed;
+        StreamBuf itemsStream = inputStream.toStreamBuf( static_cast<size_t>( itemsCount ) * 4 * 3 /* hash, offset, size */ );
+        inputStream.seek( inputStreamSize - AGGItemNameLen * itemsCount );
+        StreamBuf namesStream = inputStream.toStreamBuf( AGGItemNameLen * itemsCount );
 
-            std::cerr << "Empty item " << name << std::endl;
-            continue;
+        std::map<std::string, AGGItemInfo> aggItemsMap;
+
+        for ( uint16_t i = 0; i < itemsCount; ++i ) {
+            AGGItemInfo & info = aggItemsMap[StringLower( namesStream.toString( AGGItemNameLen ) )];
+
+            info.hash = itemsStream.getLE32();
+            info.offset = itemsStream.getLE32();
+            info.size = itemsStream.getLE32();
         }
 
-        const std::filesystem::path outputFileName = std::filesystem::path( argv[2] ) / std::filesystem::path( name );
+        for ( const auto & item : aggItemsMap ) {
+            const auto & [name, info] = item;
 
-        StreamFile outputStream;
-        if ( !outputStream.open( outputFileName.string(), "wb" ) ) {
-            ++itemsFailed;
+            const uint32_t hash = calculateHash( name );
+            if ( hash != info.hash ) {
+                ++itemsFailed;
 
-            std::cerr << "Cannot open file " << outputFileName << std::endl;
-            continue;
+                std::cerr << "Invalid hash for item " << name << ": expected " << GetHexString( info.hash ) << ", got " << GetHexString( hash ) << std::endl;
+                continue;
+            }
+
+            inputStream.seek( info.offset );
+
+            const std::vector<uint8_t> buf = inputStream.getRaw( info.size );
+            if ( buf.empty() ) {
+                ++itemsFailed;
+
+                std::cerr << "Empty item " << name << std::endl;
+                continue;
+            }
+
+            const std::filesystem::path outputFileName = std::filesystem::path( dstDir ) / std::filesystem::path( name );
+
+            StreamFile outputStream;
+            if ( !outputStream.open( outputFileName.string(), "wb" ) ) {
+                ++itemsFailed;
+
+                std::cerr << "Cannot open file " << outputFileName << std::endl;
+                continue;
+            }
+
+            outputStream.putRaw( reinterpret_cast<const char *>( buf.data() ), buf.size() );
+
+            ++itemsExtracted;
         }
-
-        outputStream.putRaw( reinterpret_cast<const char *>( buf.data() ), buf.size() );
-
-        ++itemsExtracted;
     }
 
     std::cout << "Total extracted: " << itemsExtracted << ", failed: " << itemsFailed << std::endl;
-    return EXIT_SUCCESS;
+
+    return itemsFailed == 0 ? EXIT_SUCCESS : EXIT_FAILURE;
 }
