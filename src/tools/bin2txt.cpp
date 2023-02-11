43b60dbb9bf6921e01d575927f0c4a92df8a2d0f
Oleg Derevenetz <oleg-derevenetz@yandex.ru>
2023-02-11 13:57:33 +0300

    Fix copyright header, tune tools' dependencies for MSVC

--- src/tools/bin2txt.cpp
+++ src/tools/bin2txt.cpp
@@ -2,9 +2,6 @@
  *   fheroes2: https://github.com/ihhub/fheroes2                           *
  *   Copyright (C) 2022 - 2023                                             *
  *                                                                         *
- *   Free Heroes2 Engine: http://sourceforge.net/projects/fheroes2         *
- *   Copyright (C) 2009 by Andrey Afletdinov <fheroes2@gmail.com>          *
- *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
  *   the Free Software Foundation; either version 2 of the License, or     *
