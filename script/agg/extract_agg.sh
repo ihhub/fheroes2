3d4f3df3c917d22f654e54f47d7585951f8c08c0
Oleg Derevenetz <oleg-derevenetz@yandex.ru>
2023-02-14 19:08:17 +0300

    Add -o pipefail in bash scripts

--- script/agg/extract_agg.sh
+++ script/agg/extract_agg.sh
@@ -20,7 +20,7 @@
 #   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             #
 ###########################################################################
 
-set -e
+set -e -o pipefail
 
 PATH="$(dirname "$0"):$PATH"
 
--- script/homm2/extract_homm2_resources.sh
+++ script/homm2/extract_homm2_resources.sh
@@ -20,7 +20,7 @@
 #   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             #
 ###########################################################################
 
-set -e
+set -e -o pipefail
 
 function echo_red {
     echo -e "\033[0;31m$*\033[0m"
