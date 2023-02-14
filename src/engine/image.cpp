8de67dd42f12e8a0ec751ea7855c46d8e1f7edb6
Osmodium <schubertchr@gmail.com>
2023-02-14 20:33:32 +0100

    [Amend]

--- src/engine/image.cpp
+++ src/engine/image.cpp
@@ -18,7 +18,6 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 
-
 #include <algorithm>
 #include <cassert>
 #include <cstdlib>
