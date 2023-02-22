b4adf3d9d131249224b870b3a464f9a166c3bc46
Oleg Derevenetz <oleg-derevenetz@yandex.ru>
2023-02-23 01:15:27 +0300

    Use a temporary variable when inserting values into a vector from the same vector

--- src/fheroes2/agg/agg_image.cpp
+++ src/fheroes2/agg/agg_image.cpp
@@ -1872,7 +1872,8 @@ namespace fheroes2
                     // Engine expects that letter indexes correspond to charcode - 0x20.
                     // In case CP1251 font.icn contains sprites for chars 0x20-0x7F, 0xC0-0xDF, 0xA8, 0xE0-0xFF, 0xB8 (in that order).
                     // We rearrange sprites array for corresponding sprite indexes to charcode - 0x20.
-                    imageArray.insert( imageArray.begin() + 96, 64, imageArray[0] );
+                    const Sprite firstSprite{ imageArray[0] };
+                    imageArray.insert( imageArray.begin() + 96, 64, firstSprite );
                     std::swap( imageArray[136], imageArray[192] ); // Move sprites for chars 0xA8
                     std::swap( imageArray[152], imageArray[225] ); // and 0xB8 to it's places.
                     imageArray.pop_back();
@@ -1880,7 +1881,8 @@ namespace fheroes2
                 }
                 // German version uses CP1252
                 if ( crc32 == 0x04745D1D || crc32 == 0xD0F0D852 ) {
-                    imageArray.insert( imageArray.begin() + 96, 124, imageArray[0] );
+                    const Sprite firstSprite{ imageArray[0] };
+                    imageArray.insert( imageArray.begin() + 96, 124, firstSprite );
                     std::swap( imageArray[164], imageArray[224] );
                     std::swap( imageArray[182], imageArray[225] );
                     std::swap( imageArray[188], imageArray[226] );
@@ -1892,7 +1894,8 @@ namespace fheroes2
                 }
                 // French version has its own special encoding but should conform to CP1252 too
                 if ( crc32 == 0xD9556567 || crc32 == 0x406967B9 ) {
-                    imageArray.insert( imageArray.begin() + 96, 160 - 32, imageArray[0] );
+                    const Sprite firstSprite{ imageArray[0] };
+                    imageArray.insert( imageArray.begin() + 96, 160 - 32, firstSprite );
                     imageArray[192 - 32] = imageArray[33];
                     imageArray[199 - 32] = imageArray[35];
                     imageArray[201 - 32] = imageArray[37];
@@ -1915,7 +1918,8 @@ namespace fheroes2
                 }
                 // Italian version uses CP1252
                 if ( crc32 == 0x219B3124 || crc32 == 0x1F3C3C74 ) {
-                    imageArray.insert( imageArray.begin() + 101, 155 - 32, imageArray[0] );
+                    const Sprite firstSprite{ imageArray[0] };
+                    imageArray.insert( imageArray.begin() + 101, 155 - 32, firstSprite );
                     imageArray[192 - 32] = imageArray[33];
                     imageArray[200 - 32] = imageArray[37];
                     imageArray[201 - 32] = imageArray[37];
