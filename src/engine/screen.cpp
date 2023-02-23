cab451a7a1c7a10ab111ff15b9f27a8535994374
Districh <districh.ru@gmail.com>
2023-02-23 09:54:43 +0300

    'screen.cpp' - cache before 'emplace_back' to the same vector

--- src/engine/screen.cpp
+++ src/engine/screen.cpp
@@ -147,7 +147,11 @@ namespace
 
                 if ( ( resolutions[biggerId].width % resolutions[currentId].width ) == 0 && ( resolutions[biggerId].height % resolutions[currentId].height ) == 0
                      && ( resolutions[biggerId].width / resolutions[currentId].width ) == ( resolutions[biggerId].height / resolutions[currentId].height ) ) {
-                    resolutions.emplace_back( resolutions[currentId].width, resolutions[currentId].height, resolutions[biggerId].width / resolutions[currentId].width );
+                    // IMPORTANT: we MUST do a copy of a vector element if we want to emplace it to the same vector.
+                    const fheroes2::ResolutionInfo currentResolution = resolutions[currentId];
+                    const int32_t biggerResolutionWidth = resolutions[biggerId].width;
+
+                    resolutions.emplace_back( currentResolution.width, currentResolution.height, biggerResolutionWidth / currentResolution.width );
                 }
             }
         }
