2ec0e7ae8330201fd19a47ff0d18432827d87c90
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2023-03-11 15:01:38 +0800

    Add missing part of Captain's Quarter in Sorceress Town (#6775)

--- src/fheroes2/agg/agg_image.cpp
+++ src/fheroes2/agg/agg_image.cpp
@@ -2614,6 +2614,10 @@ namespace fheroes2
                 _icnVsSprite[id].resize( 1 );
                 h2d::readImage( "barbarian_castle_captain_quarter_left_side.image", _icnVsSprite[id][0] );
                 return true;
+            case ICN::SORCERESS_CASTLE_CAPTAIN_QUARTERS_LEFT_SIDE:
+                _icnVsSprite[id].resize( 1 );
+                h2d::readImage( "sorceress_castle_captain_quarter_left_side.image", _icnVsSprite[id][0] );
+                return true;
             case ICN::NECROMANCER_CASTLE_STANDALONE_CAPTAIN_QUARTERS: {
                 _icnVsSprite[id].resize( 1 );
                 Sprite & output = _icnVsSprite[id][0];
