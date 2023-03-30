e69ee990483258105912bb8c6b88a6ca6482b6c3
Mr-Bajs <93934125+Mr-Bajs@users.noreply.github.com>
2023-03-30 11:35:48 +0000

    Add gold missing in statue building description (#6908)

--- src/fheroes2/castle/castle_building_info.cpp
+++ src/fheroes2/castle/castle_building_info.cpp
@@ -1,6 +1,6 @@
 /***************************************************************************
  *   fheroes2: https://github.com/ihhub/fheroes2                           *
- *   Copyright (C) 2021 - 2022                                             *
+ *   Copyright (C) 2021 - 2023                                             *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
@@ -864,7 +864,7 @@ namespace fheroes2
         case BUILD_WELL:
             return _( "The Well increases the growth rate of all dwellings by %{count} creatures per week." );
         case BUILD_STATUE:
-            return _( "The Statue increases your town's income by %{count} per day." );
+            return _( "The Statue increases your town's income by %{count} gold per day." );
         case BUILD_LEFTTURRET:
             return _( "The Left Turret provides extra firepower during castle combat." );
         case BUILD_RIGHTTURRET:
