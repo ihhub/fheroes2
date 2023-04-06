53eabce847a9c51957b8aed274517bd7a17d59b1
Oleg Derevenetz <oleg-derevenetz@yandex.ru>
2023-04-06 06:18:56 +0000

    Show approximate information about the mine guardians to a non-owner player (#6949)

--- src/fheroes2/dialog/dialog_quickinfo.cpp
+++ src/fheroes2/dialog/dialog_quickinfo.cpp
@@ -168,12 +168,9 @@ namespace
             str = MP2::StringObject( objectType );
         }
 
-        const bool isHauntedMine = ( objectType == MP2::OBJ_MINES && Maps::getSpellIdFromTile( tile ) == Spell::HAUNT );
-        const bool isAbandonedMine = ( objectType == MP2::OBJ_ABANDONED_MINE );
-
         const Troop & troop = tile.QuantityTroop();
 
-        if ( troop.isValid() && ( isOwned || isHauntedMine || isAbandonedMine ) ) {
+        if ( troop.isValid() ) {
             str.append( "\n \n" );
 
             if ( isOwned ) {
