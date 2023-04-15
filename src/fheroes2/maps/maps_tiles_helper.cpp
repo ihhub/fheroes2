e264a36a9c5d80d213daaab9a853df0cb7578409
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2023-04-15 12:40:40 +0800

    Update copyright

--- src/fheroes2/maps/maps_tiles_helper.cpp
+++ src/fheroes2/maps/maps_tiles_helper.cpp
@@ -1,9 +1,6 @@
 /***************************************************************************
  *   fheroes2: https://github.com/ihhub/fheroes2                           *
- *   Copyright (C) 2019 - 2023                                             *
- *                                                                         *
- *   Free Heroes2 Engine: http://sourceforge.net/projects/fheroes2         *
- *   Copyright (C) 2011 by Andrey Afletdinov <fheroes2@gmail.com>          *
+ *   Copyright (C) 2023                                                    *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
