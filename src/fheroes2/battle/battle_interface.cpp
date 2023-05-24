e21aa840b3d6a61983ae136598ef225c1de95e88
Oleg Derevenetz <oleg-derevenetz@yandex.ru>
2023-05-25 00:37:39 +0300

    Fix the rendering of the number of units during the castle siege

--- src/fheroes2/battle/battle_interface.cpp
+++ src/fheroes2/battle/battle_interface.cpp
@@ -1556,7 +1556,7 @@ void Battle::Interface::RedrawArmies()
             }
 
             for ( const Unit * unit : troopCounterAfterWall ) {
-                RedrawTroopSprite( *unit );
+                RedrawTroopCount( *unit );
             }
 
             for ( const Unit * unit : movingTroopAfterWall ) {
