6702d51be4d3eace7d9989743dcfbcc40dd4453a
Oleg Derevenetz <oleg-derevenetz@yandex.ru>
2023-06-03 09:30:24 +0300

    Do not check whether a hero can cast a spell if spellcasting is prohibited in principle (#7258)

--- src/fheroes2/army/army.cpp
+++ src/fheroes2/army/army.cpp
@@ -915,11 +915,17 @@ const Troops & Army::getTroops() const
 
 void Army::setFromTile( const Maps::Tiles & tile )
 {
-    Reset();
+    assert( commander == nullptr );
+
+    Troops::Clean();
 
     const bool isCaptureObject = MP2::isCaptureObject( tile.GetObject( false ) );
-    if ( isCaptureObject )
+    if ( isCaptureObject ) {
         color = getColorFromTile( tile );
+    }
+    else {
+        color = Color::NONE;
+    }
 
     switch ( tile.GetObject( false ) ) {
     case MP2::OBJ_PYRAMID:
@@ -1044,8 +1050,7 @@ void Army::setFromTile( const Maps::Tiles & tile )
 
     default:
         if ( isCaptureObject ) {
-            CapturedObject & capturedObject = world.GetCapturedObject( tile.GetIndex() );
-            const Troop & troop = capturedObject.GetTroop();
+            const Troop & troop = world.GetCapturedObject( tile.GetIndex() ).GetTroop();
 
             if ( troop.isValid() ) {
                 ArrangeForBattle( troop.GetMonster(), troop.GetCount(), tile.GetIndex(), false );
