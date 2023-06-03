9045a216b46f072f453c1e8a888e2dc21a974ac0
Sergei Ivanov (Districh) <districh@ya.ru>
2023-06-03 18:31:33 +0300

    Fix comment

--- src/fheroes2/gui/ui_font.cpp
+++ src/fheroes2/gui/ui_font.cpp
@@ -5817,8 +5817,8 @@ namespace fheroes2
         icnVsSprite[75].setPosition( icnVsSprite[75].x(), icnVsSprite[75].y() );
         updateNormalFontLetterShadow( icnVsSprite[75] );
 
-        // System call 'DELETE' (0x7F) is never used as character.
-        // To make blinking text cursor we have make a transparent character with the width of the cursor '_'.
+        // System call 'DELETE' (0x7F) is never used as a text character in phrases.
+        // To make the blinking text cursor we have to make a transparent character with the width of the cursor '_'.
         icnVsSprite[127 - 32].resize( icnVsSprite[95 - 32].width(), 1 );
         icnVsSprite[127 - 32].reset();
     }
