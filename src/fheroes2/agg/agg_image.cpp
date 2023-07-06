1c5c1dcbc13b99ef0f6eee23970c7ae7f97c1899
Zense <zenseii@users.noreply.github.com>
2023-07-06 20:17:27 +0200

    Call gettext_noop

--- src/fheroes2/agg/agg_image.cpp
+++ src/fheroes2/agg/agg_image.cpp
@@ -721,7 +721,7 @@ namespace fheroes2
                 const char * text = "OKAY";
                 if ( id == ICN::BUTTON_SMALLER_OKAY_EVIL || id == ICN::BUTTON_SMALLER_OKAY_GOOD ) {
                     textWidth = 70;
-                    text = "smallerButton|OKAY";
+                    text = gettext_noop( "smallerButton|OKAY" );
                 }
 
                 createNormalButton( _icnVsSprite[id][0], _icnVsSprite[id][1], textWidth, gettext_noop( text ), isEvilInterface );
