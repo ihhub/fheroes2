d356efdeeb1c49891e295e1572dbda6320465fda
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2023-07-23 22:38:58 +0800

    Address coments

--- src/fheroes2/game/game_startgame.cpp
+++ src/fheroes2/game/game_startgame.cpp
@@ -708,6 +708,12 @@ fheroes2::GameMode Interface::AdventureMap::StartGame()
             Kingdom & kingdom = world.GetKingdom( playerColor );
 
             if ( skipTurns && !player->isColor( conf.CurrentColor() ) ) {
+                if ( kingdom.isPlay() && kingdom.GetControl() == CONTROL_AI ) {
+                    // Only the first AI player can trigger AI bonuses.
+                    // Since this player is the one the bonuses have been applied.
+                    isFirstAIPlayer = false;
+                }
+
                 continue;
             }
 
@@ -767,7 +773,7 @@ fheroes2::GameMode Interface::AdventureMap::StartGame()
                     // TODO: remove this temporary assertion
                     assert( res == fheroes2::GameMode::END_TURN );
 
-                    if ( isFirstAIPlayer && !loadedFromSave ) {
+                    if ( isFirstAIPlayer ) {
                         isFirstAIPlayer = false;
                         // All bonuses for AI must be applied on the first AI player turn, not the first player in general.
                         // This prevents human players to abuse AI bonuses.
