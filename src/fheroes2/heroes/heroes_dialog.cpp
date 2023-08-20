8b55df91c9d4602257d196091e4c099e14be5a9c
zenseii <zenseii@users.noreply.github.com>
2023-08-21 00:55:13 +0200

    Code style

--- src/fheroes2/heroes/heroes_dialog.cpp
+++ src/fheroes2/heroes/heroes_dialog.cpp
@@ -225,8 +225,8 @@ int Heroes::OpenDialog( const bool readonly, const bool fade, const bool disable
     // button dismiss
     dst_pt.x = cur_pt.x + 4;
     dst_pt.y = cur_pt.y + 318;
-    fheroes2::ButtonSprite buttonDismiss( dst_pt.x, dst_pt.y, fheroes2::AGG::GetICN( ICN::BUTTON_VERTICAL_DISMISS, 0 ), fheroes2::AGG::GetICN( ICN::BUTTON_VERTICAL_DISMISS, 1 ),
-                                          fheroes2::AGG::GetICN( ICN::DISMISS_HERO_DISABLED_BUTTON, 0 ) );
+    fheroes2::ButtonSprite buttonDismiss( dst_pt.x, dst_pt.y, fheroes2::AGG::GetICN( ICN::BUTTON_VERTICAL_DISMISS, 0 ),
+                                          fheroes2::AGG::GetICN( ICN::BUTTON_VERTICAL_DISMISS, 1 ), fheroes2::AGG::GetICN( ICN::DISMISS_HERO_DISABLED_BUTTON, 0 ) );
 
     // button exit
     dst_pt.x = cur_pt.x + 603;
