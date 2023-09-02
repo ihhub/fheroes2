280e9be0da54c0ed98626bf079f0efd00793dc04
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2023-09-03 00:14:38 +0800

    Fix some SonarQube smells (#7681)

--- src/fheroes2/editor/editor_interface.cpp
+++ src/fheroes2/editor/editor_interface.cpp
@@ -101,7 +101,7 @@ namespace Interface
                                      - fheroes2::Point( prevRoi.x + prevRoi.width / 2, prevRoi.y + prevRoi.height / 2 ) );
     }
 
-    void Editor::redraw( const uint32_t force /* = 0 */ )
+    void Editor::redraw( const uint32_t force )
     {
         fheroes2::Display & display = fheroes2::Display::instance();
 
@@ -377,7 +377,7 @@ namespace Interface
                 }
 
                 if ( needRedraw() ) {
-                    redraw();
+                    redraw( 0 );
 
                     // If this assertion blows up it means that we are holding a RedrawLocker lock for rendering which should not happen.
                     assert( getRedrawMask() == 0 );
