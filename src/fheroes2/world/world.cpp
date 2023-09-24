f3da1e4c2590cd7d62d70801f0687c85192da970
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2023-09-24 10:05:45 +0800

    Clean up the code for Maps::Tiles (#7796)

--- src/fheroes2/world/world.cpp
+++ src/fheroes2/world/world.cpp
@@ -400,11 +400,11 @@ void World::NewMaps( int32_t sw, int32_t sh )
 
         mp2tile.terrainImageIndex = static_cast<uint16_t>( Rand::Get( 16, 19 ) ); // index sprite ground, see ground32.til
         mp2tile.objectName1 = 0; // object sprite level 1
-        mp2tile.level1IcnImageIndex = 0xff; // index sprite level 1
+        mp2tile.bottomIcnImageIndex = 0xff; // index sprite level 1
         mp2tile.quantity1 = 0;
         mp2tile.quantity2 = 0;
         mp2tile.objectName2 = 0; // object sprite level 2
-        mp2tile.level2IcnImageIndex = 0xff; // index sprite level 2
+        mp2tile.topIcnImageIndex = 0xff; // index sprite level 2
         mp2tile.terrainFlags = static_cast<uint8_t>( Rand::Get( 0, 3 ) ); // shape reflect % 4, 0 none, 1 vertical, 2 horizontal, 3 any
         mp2tile.mapObjectType = MP2::OBJ_NONE;
         mp2tile.nextAddonIndex = 0;
@@ -930,7 +930,7 @@ bool World::DiggingForUltimateArtifact( const fheroes2::Point & center )
         return false;
     }
 
-    tile.pushBottomLayerAddon( Maps::TilesAddon( Maps::BACKGROUND_LAYER, GetUniq(), objectIcnType, imageIndex, false, false ) );
+    tile.pushBottomLayerAddon( Maps::TilesAddon( Maps::BACKGROUND_LAYER, GetUniq(), objectIcnType, imageIndex ) );
 
     if ( ultimate_artifact.isPosition( tile.GetIndex() ) && !ultimate_artifact.isFound() ) {
         ultimate_artifact.markAsFound();
