73a0268c652e2575dd30b456077548eb3c74cc79
Sergei Ivanov (Districh) <113276641+Districh-ru@users.noreply.github.com>
2023-10-10 02:48:31 +0300

    Rework hot-keys dialog (#7909)

--- src/fheroes2/dialog/dialog_hotkeys.cpp
+++ src/fheroes2/dialog/dialog_hotkeys.cpp
@@ -18,49 +18,37 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 
+#include "dialog_hotkeys.h"
+
+#include <algorithm>
 #include <cassert>
 #include <cstdint>
+#include <memory>
 #include <vector>
 
 #include "agg_image.h"
 #include "cursor.h"
 #include "dialog.h"
-#include "dialog_hotkeys.h"
 #include "game_hotkeys.h"
-#include "gamedefs.h"
 #include "icn.h"
 #include "image.h"
 #include "interface_list.h"
 #include "localevent.h"
 #include "math_base.h"
 #include "screen.h"
+#include "settings.h"
 #include "tools.h"
 #include "translations.h"
 #include "ui_button.h"
 #include "ui_dialog.h"
 #include "ui_scrollbar.h"
 #include "ui_text.h"
+#include "ui_window.h"
 
 namespace
 {
-    const int editBoxLength = 266;
-    const int hotKeyLength = 68;
-    const int windowExtensionHeight = 34;
-
-    fheroes2::Sprite generateMainWindow()
-    {
-        // Since the original window contains two buttons: Cancel and Okay as well as a small frame just above the button we have to rebuild the window.
-        const fheroes2::Sprite & originalWindow = fheroes2::AGG::GetICN( ICN::REQBKG, 0 );
-        fheroes2::Sprite window = originalWindow;
-
-        // Copy the bottom of a dialog with Okay button from another window.
-        const fheroes2::Sprite & mapSelectionWindow = fheroes2::AGG::GetICN( ICN::REQSBKG, 0 );
-        fheroes2::Copy( mapSelectionWindow, 0, 409, window, 0, 315, window.width(), window.height() - 314 );
-
-        fheroes2::Copy( originalWindow, 41, 203, window, 41, 203 + windowExtensionHeight, 301, 69 );
-
-        return window;
-    }
+    const int32_t keyDescriptionLength = 300;
+    const int32_t hotKeyLength = 120;
 
     class HotKeyElement : public fheroes2::DialogElement
     {
@@ -68,10 +56,9 @@ namespace
         HotKeyElement( const fheroes2::Key key, fheroes2::Image & output )
             : _restorer( output, 0, 0, 0, 0 )
             , _key( key )
-            , _keyChanged( false )
         {
             // Text always occupies the whole width of the dialog.
-            _area = { BOXAREA_WIDTH, fheroes2::Text( StringUpper( KeySymGetName( _key ) ), fheroes2::FontType::normalYellow() ).height( BOXAREA_WIDTH ) };
+            _area = { BOXAREA_WIDTH, fheroes2::getFontHeight( fheroes2::FontSize::NORMAL ) };
         }
 
         ~HotKeyElement() override = default;
@@ -125,7 +112,7 @@ namespace
     private:
         mutable fheroes2::ImageRestorer _restorer;
         mutable fheroes2::Key _key;
-        mutable bool _keyChanged;
+        mutable bool _keyChanged{ false };
     };
 
     class HotKeyList : public Interface::ListBox<Game::HotKeyEvent>
@@ -143,19 +130,20 @@ namespace
 
             const fheroes2::FontType fontType = current ? fheroes2::FontType::normalYellow() : fheroes2::FontType::normalWhite();
 
+            offsetY += 2;
+
             fheroes2::Text name( _( Game::getHotKeyEventNameByEventId( hotKeyEvent ) ), fontType );
-            name.fitToOneRow( editBoxLength - hotKeyLength );
-            name.draw( offsetX + 5, offsetY, display );
+            name.fitToOneRow( keyDescriptionLength );
+            name.draw( offsetX + 4, offsetY, display );
 
             fheroes2::Text hotkey( Game::getHotKeyNameByEventId( hotKeyEvent ), fontType );
             hotkey.fitToOneRow( hotKeyLength );
-            hotkey.draw( offsetX + editBoxLength - hotKeyLength, offsetY, hotKeyLength, display );
+            hotkey.draw( offsetX + keyDescriptionLength + 9, offsetY, hotKeyLength, display );
         }
 
-        void RedrawBackground( const fheroes2::Point & dst ) override
+        void RedrawBackground( const fheroes2::Point & /* unused */ ) override
         {
-            const fheroes2::Sprite & panel = generateMainWindow();
-            fheroes2::Blit( panel, fheroes2::Display::instance(), dst.x, dst.y );
+            _listBackground->restore();
         }
 
         void ActionCurrentUp() override
@@ -213,84 +201,134 @@ namespace
             Game::setHotKeyForEvent( hotKeyEvent, hotKeyUI.getKey() );
             Game::HotKeySave();
         }
-    };
 
-    void redrawWindowHeader( const fheroes2::Point & dst )
-    {
-        fheroes2::Text text( _( "Hot Keys:" ), fheroes2::FontType::normalYellow() );
-        text.draw( dst.x + ( 377 - text.width() ) / 2, dst.y + 30, fheroes2::Display::instance() );
-    }
+        void initListBackgroundRestorer( fheroes2::Rect roi )
+        {
+            _listBackground = std::make_unique<fheroes2::ImageRestorer>( fheroes2::Display::instance(), roi.x, roi.y, roi.width, roi.height );
+        }
+
+        void updateScrollBarImage()
+        {
+            const int32_t scrollBarWidth = _scrollbar.width();
+
+            setScrollBarImage( fheroes2::generateScrollbarSlider( _scrollbar, false, _scrollbar.getArea().height, VisibleItemCount(), _size(),
+                                                                  { 0, 0, scrollBarWidth, 8 }, { 0, 7, scrollBarWidth, 8 } ) );
+            _scrollbar.moveToIndex( _topId );
+        }
+
+    private:
+        std::unique_ptr<fheroes2::ImageRestorer> _listBackground;
+    };
 }
 
 namespace fheroes2
 {
     void openHotkeysDialog()
     {
-        std::vector<Game::HotKeyEvent> events = Game::getAllHotKeyEvents();
+        // Setup cursor.
+        const CursorRestorer cursorRestorer( true, ::Cursor::POINTER );
 
         fheroes2::Display & display = fheroes2::Display::instance();
 
-        // setup cursor
-        const CursorRestorer cursorRestorer( true, ::Cursor::POINTER );
+        // Dialog height is capped with the current screen height minus 100 pixels.
+        fheroes2::StandardWindow background( keyDescriptionLength + hotKeyLength + 8 + 75, std::min( display.height() - 100, 520 ), true, display );
 
-        const fheroes2::Sprite & sprite = generateMainWindow();
-        const fheroes2::Sprite & spriteShadow = fheroes2::AGG::GetICN( ICN::REQBKG, 1 );
+        const fheroes2::Rect roi( background.activeArea() );
+        const fheroes2::Rect listRoi( roi.x + 24, roi.y + 37, keyDescriptionLength + hotKeyLength + 8, roi.height - 75 );
 
-        const fheroes2::Point dialogOffset( ( display.width() - sprite.width() ) / 2, ( display.height() - sprite.height() ) / 2 );
-        const fheroes2::Point shadowOffset( dialogOffset.x - BORDERWIDTH, dialogOffset.y );
+        const fheroes2::Text title( _( "Hot Keys:" ), fheroes2::FontType::normalYellow() );
+        title.draw( roi.x + ( roi.width - title.width() ) / 2, roi.y + 16, display );
 
-        fheroes2::ImageRestorer restorer( display, shadowOffset.x, shadowOffset.y, sprite.width() + BORDERWIDTH, sprite.height() + BORDERWIDTH );
-        const fheroes2::Rect roi( dialogOffset.x, dialogOffset.y, sprite.width(), sprite.height() );
+        // We divide the list: action description and binded hot-key.
+        background.applyTextBackgroundShading( { listRoi.x, listRoi.y, keyDescriptionLength + 8, listRoi.height } );
+        background.applyTextBackgroundShading( { listRoi.x + keyDescriptionLength + 8, listRoi.y, hotKeyLength, listRoi.height } );
 
-        fheroes2::Blit( spriteShadow, display, roi.x - BORDERWIDTH, roi.y + BORDERWIDTH );
+        const bool isEvilInterface = Settings::Get().isEvilInterfaceEnabled();
 
-        fheroes2::Button buttonOk( roi.x + 140, roi.y + 315, ICN::BUTTON_SMALL_OKAY_GOOD, 0, 1 );
-
-        HotKeyList resList( roi.getPosition() );
-
-        resList.RedrawBackground( roi.getPosition() );
-        resList.SetScrollButtonUp( ICN::REQUESTS, 5, 6, { roi.x + 327, roi.y + 55 } );
-        resList.SetScrollButtonDn( ICN::REQUESTS, 7, 8, { roi.x + 327, roi.y + 257 + windowExtensionHeight } );
-
-        const fheroes2::Sprite & originalSlider = fheroes2::AGG::GetICN( ICN::ESCROLL, 3 );
-        const fheroes2::Image scrollbarSlider
-            = fheroes2::generateScrollbarSlider( originalSlider, false, 180 + windowExtensionHeight, 11, static_cast<int32_t>( events.size() ),
-                                                 { 0, 0, originalSlider.width(), 8 }, { 0, 7, originalSlider.width(), 8 } );
-        resList.setScrollBarArea( { roi.x + 328, roi.y + 73, 12, 180 + windowExtensionHeight } );
-        resList.setScrollBarImage( scrollbarSlider );
-        resList.SetAreaMaxItems( 13 );
-        resList.SetAreaItems( { roi.x + 41, roi.y + 55 + 3, editBoxLength, 215 + windowExtensionHeight } );
-
-        resList.SetListContent( events );
-
-        resList.Redraw();
+        // Prepare OKAY button and render its shadow.
+        const int buttonOkIcn = isEvilInterface ? ICN::BUTTON_SMALL_OKAY_EVIL : ICN::BUTTON_SMALL_OKAY_GOOD;
+        const fheroes2::Sprite & buttonOkSprite = fheroes2::AGG::GetICN( buttonOkIcn, 0 );
+        fheroes2::Button buttonOk( roi.x + ( roi.width - buttonOkSprite.width() ) / 2, roi.y + roi.height - 32, buttonOkIcn, 0, 1 );
+        fheroes2::addGradientShadow( buttonOkSprite, display, buttonOk.area().getPosition(), { -5, 5 } );
         buttonOk.draw();
-        redrawWindowHeader( roi.getPosition() );
-        display.render();
+
+        HotKeyList listbox( roi.getPosition() );
+        listbox.initListBackgroundRestorer( listRoi );
+        listbox.SetAreaItems( { listRoi.x, listRoi.y + 3, listRoi.width - 3, listRoi.height - 4 } );
+
+        // Render the scrollbar.
+        const fheroes2::Sprite & scrollBar = fheroes2::AGG::GetICN( isEvilInterface ? ICN::ADVBORDE : ICN::ADVBORD, 0 );
+        int32_t scrollbarOffsetX = roi.x + roi.width - 35;
+
+        // Top part of scrollbar background.
+        const int32_t topPartHeight = 19;
+        const int32_t scrollBarWidth = 16;
+        fheroes2::Copy( scrollBar, 536, 176, display, scrollbarOffsetX, listRoi.y, scrollBarWidth, topPartHeight );
+
+        // Middle part of scrollbar background.
+        int32_t offsetY = topPartHeight;
+        const int32_t middlePartHeight = 88;
+        const int32_t middlePartCount = ( listRoi.height - 2 * topPartHeight + middlePartHeight - 1 ) / middlePartHeight;
+
+        for ( int32_t i = 0; i < middlePartCount; ++i ) {
+            fheroes2::Copy( scrollBar, 536, 196, display, scrollbarOffsetX, listRoi.y + offsetY, scrollBarWidth,
+                            std::min( middlePartHeight, listRoi.height - offsetY - topPartHeight ) );
+            offsetY += middlePartHeight;
+        }
+
+        // Bottom part of scrollbar background.
+        fheroes2::Copy( scrollBar, 536, 285, display, scrollbarOffsetX, listRoi.y + listRoi.height - topPartHeight, scrollBarWidth, topPartHeight );
+
+        const int listIcnId = isEvilInterface ? ICN::SCROLLE : ICN::SCROLL;
+
+        ++scrollbarOffsetX;
+
+        // Make scrollbar shadow.
+        for ( uint8_t i = 0; i < 4; ++i ) {
+            const uint8_t transformId = i + 2;
+            const int32_t sizeCorrection = i + 1;
+            fheroes2::ApplyTransform( display, scrollbarOffsetX - transformId, listRoi.y + sizeCorrection, 1, listRoi.height - sizeCorrection, transformId );
+            fheroes2::ApplyTransform( display, scrollbarOffsetX - transformId, listRoi.y + listRoi.height + i, scrollBarWidth, 1, transformId );
+        }
+
+        listbox.SetScrollButtonUp( listIcnId, 0, 1, { scrollbarOffsetX, listRoi.y + 1 } );
+        listbox.SetScrollButtonDn( listIcnId, 2, 3, { scrollbarOffsetX, listRoi.y + listRoi.height - 15 } );
+        listbox.setScrollBarArea( { scrollbarOffsetX + 2, listRoi.y + topPartHeight, 10, listRoi.height - 2 * topPartHeight } );
+        listbox.setScrollBarImage( fheroes2::AGG::GetICN( listIcnId, 4 ) );
+        listbox.SetAreaMaxItems( ( listRoi.height - 7 ) / fheroes2::getFontHeight( fheroes2::FontSize::NORMAL ) );
+
+        std::vector<Game::HotKeyEvent> hoKeyEvents = Game::getAllHotKeyEvents();
+
+        listbox.SetListContent( hoKeyEvents );
+        listbox.updateScrollBarImage();
+        listbox.Redraw();
+
+        display.render( background.totalArea() );
 
         LocalEvent & le = LocalEvent::Get();
         while ( le.HandleEvents() ) {
             le.MousePressLeft( buttonOk.area() ) ? buttonOk.drawOnPress() : buttonOk.drawOnRelease();
 
-            resList.QueueEventProcessing();
+            listbox.QueueEventProcessing();
 
-            if ( ( buttonOk.isEnabled() && le.MouseClickLeft( buttonOk.area() ) ) || Game::HotKeyCloseWindow() ) {
+            if ( le.MouseClickLeft( buttonOk.area() ) || Game::HotKeyCloseWindow() ) {
                 return;
             }
-            if ( le.MousePressRight( buttonOk.area() ) ) {
-                fheroes2::Text header( _( "Okay" ), fheroes2::FontType::normalYellow() );
-                fheroes2::Text body( _( "Exit this menu without doing anything." ), fheroes2::FontType::normalWhite() );
-                fheroes2::showMessage( header, body, 0 );
-            }
 
-            if ( !resList.IsNeedRedraw() ) {
+            if ( le.MousePressRight( buttonOk.area() ) ) {
+                const fheroes2::Text header( _( "Okay" ), fheroes2::FontType::normalYellow() );
+                const fheroes2::Text body( _( "Exit this menu." ), fheroes2::FontType::normalWhite() );
+                fheroes2::showMessage( header, body, 0 );
+
                 continue;
             }
 
-            resList.Redraw();
-            buttonOk.draw();
-            redrawWindowHeader( roi.getPosition() );
-            display.render();
+            if ( !listbox.IsNeedRedraw() ) {
+                continue;
+            }
+
+            listbox.Redraw();
+            display.render( roi );
         }
     }
 }
