59c3d6813f0e37d659c24e61ab594f20d6d008ab
Ilya Pomaskin <IlyaPomaskin@users.noreply.github.com>
2023-11-04 18:29:19 +0500

    slider styles

--- h2lwp-settings/src/Settings/Brightness.tsx
+++ h2lwp-settings/src/Settings/Brightness.tsx
@@ -1,4 +1,4 @@
-import { Grid, ListItem, Slider, Typography } from "@mui/material";
+import { Box, Grid, ListItem, Slider, Typography } from "@mui/material";
 import { useState } from "react";
 
 type Props = {
@@ -13,19 +13,21 @@ export const Brightness: React.FC<Props> = ({ value: initialValue }) => {
       <Grid direction="column" width="100%">
         <Typography gutterBottom>Brightness</Typography>
 
-        <Slider
-          aria-label="Brightness"
-          value={value}
-          min={0}
-          max={1}
-          step={0.01}
-          valueLabelDisplay="auto"
-          valueLabelFormat={(v) => (Math.round(Number(v) * 100)) + "%"}
-          onChange={(_, value) => setValue(Number(value))}
-          sx={{
-            p: 0,
-          }}
-        />
+        <Box marginX={2}>
+          <Slider
+            aria-label="Brightness"
+            value={value}
+            min={0}
+            max={1}
+            step={0.01}
+            valueLabelDisplay="auto"
+            valueLabelFormat={(v) => Math.round(Number(v) * 100) + "%"}
+            onChange={(_, value) => setValue(Number(value))}
+            sx={{
+              p: 0,
+            }}
+          />
+        </Box>
       </Grid>
     </ListItem>
   );
