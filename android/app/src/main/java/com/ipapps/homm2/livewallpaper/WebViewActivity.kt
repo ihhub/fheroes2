3db278cb5f1a82d4d1756f2aa642878187b2ff7b
Ilya Pomaskin <IlyaPomaskin@users.noreply.github.com>
2023-11-05 10:39:01 +0500

    copy h2d file

--- android/app/src/main/java/com/ipapps/homm2/livewallpaper/WebViewActivity.kt
+++ android/app/src/main/java/com/ipapps/homm2/livewallpaper/WebViewActivity.kt
@@ -1,11 +1,13 @@
 package com.ipapps.homm2.livewallpaper
 
+import android.app.Application
 import android.app.WallpaperManager
 import android.content.ComponentName
 import android.content.Intent
 import android.os.Bundle
 import android.webkit.WebView
 import androidx.appcompat.app.AppCompatActivity
+import com.ipapps.homm2.livewallpaper.data.ParsingViewModel
 import com.ipapps.homm2.livewallpaper.data.SettingsViewModel
 import com.ipapps.homm2.livewallpaper.data.WallpaperPreferencesRepository
 import de.andycandy.android.bridge.Bridge
@@ -26,6 +28,15 @@ class WebViewActivity : AppCompatActivity() {
     private fun openIconAuthorUrl() {
     }
 
+    private fun copyResurrectionH2d() {
+        val input = assets.open("files/data/resurrection.h2d")
+        val output = getExternalFilesDir("files/data")?.resolve("resurrection.h2d");
+
+        if (output != null && !output.exists() && output.createNewFile()) {
+            input.copyTo(output.outputStream())
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_web_view)
@@ -37,6 +48,8 @@ class WebViewActivity : AppCompatActivity() {
         if (config == null || !config.exists()) {
             config?.createNewFile()
         }
+
+        copyResurrectionH2d()
 
         val prefsRepository = WallpaperPreferencesRepository(config)
         val settingsViewModel =
