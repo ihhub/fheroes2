8f9cfa9d881524e7591f030a00a18b335566d57a
Ilya Pomaskin <IlyaPomaskin@users.noreply.github.com>
2023-11-06 23:19:13 +0500

    check for PoL map

--- android/app/src/main/java/com/ipapps/homm2/livewallpaper/data/MapReader.kt
+++ android/app/src/main/java/com/ipapps/homm2/livewallpaper/data/MapReader.kt
@@ -1,5 +1,6 @@
 package com.ipapps.homm2.livewallpaper.data
 
+import java.io.ByteArrayInputStream
 import java.io.InputStream
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
@@ -45,35 +46,78 @@ class MapReader(val input: InputStream?) {
             input?.skip(offset.toLong())
             val tilesBuffer = ByteArray(width * height * tileSize)
             input?.read(tilesBuffer)
-            val isPoL = isPriceOfLoyalty(tilesBuffer.asUByteArray())
+
+            val uTilesBuffer = tilesBuffer.asUByteArray()
+            println("First bytes: ${uTilesBuffer[0].toString(16)} ${uTilesBuffer[1].toString(16)}")
+            val firstInt = ByteArray(2)
+            firstInt[0] = uTilesBuffer[0].toByte()
+            firstInt[1] = uTilesBuffer[1].toByte()
+            val fInt = ByteBuffer.wrap(firstInt.reversedArray()).short
+            println("First int $fInt")
+
+            val isPoL = isPriceOfLoyalty(tilesBuffer)
+
 
             return MapHeader(readMapName(headerBuffer), width, height, isPoL)
         }
 
         private val polMapObjectIndexes = listOf(
-            247.toUByte(), // MP2::OBJ_BARRIER
-            249.toUByte(), // MP2::OBJ_EXPANSION_DWELLING
-            250.toUByte(), // MP2::OBJ_EXPANSION_OBJECT
-            251.toUByte(), // MP2::OBJ_JAIL
-            248.toUByte(), // MP2::OBJ_TRAVELLER_TENT
+            "247", // MP2::OBJ_BARRIER
+            "249", // MP2::OBJ_EXPANSION_DWELLING
+            "250", // MP2::OBJ_EXPANSION_OBJECT
+            "251", // MP2::OBJ_JAIL
+            "248", // MP2::OBJ_TRAVELLER_TENT
         )
 
-        private fun isPoLTile(tilesBuffer: UByteArray, tileIndex: Int): Boolean {
-            val offset = (tileIndex * tileSize) + 10
-            val mapObjectType: UByte = tilesBuffer[offset]
-
-            return polMapObjectIndexes.contains(mapObjectType)
+        class MapTile {
+            var terrainImageIndex = ByteArray(2) // stream.getLE16()
+            var objectName1 = ByteArray(1) // stream.get()
+            var bottomIcnImageIndex = ByteArray(1) // stream.get()
+            var quantity1 = ByteArray(1) // stream.get()
+            var quantity2 = ByteArray(1) // stream.get()
+            var objectName2 = ByteArray(1) // stream.get()
+            var topIcnImageIndex = ByteArray(1) // stream.get()
+            var terrainFlags = ByteArray(1) // stream.get()
+            var mapObjectType = ByteArray(1) // stream.get()
+            var nextAddonIndex = ByteArray(2) // stream.getLE16()
+            var level1ObjectUID = ByteArray(4) // stream.getLE32()
+            var level2ObjectUID = ByteArray(4) // stream.getLE32()
         }
 
-        private fun isPriceOfLoyalty(tilesBuffer: UByteArray): Boolean {
+        private fun isPoLTile(input: InputStream, tileIndex: Int): Boolean {
+//            val offset = (tileIndex * tileSize) + 10
+//            val mapObjectType: UByte = tilesBuffer[offset]
+
+            val tile = MapTile()
+            input.read(tile.terrainImageIndex);
+            input.read(tile.objectName1);
+            input.read(tile.bottomIcnImageIndex);
+            input.read(tile.quantity1);
+            input.read(tile.quantity2);
+            input.read(tile.objectName2);
+            input.read(tile.topIcnImageIndex);
+            input.read(tile.terrainFlags);
+            input.read(tile.mapObjectType);
+            input.read(tile.nextAddonIndex);
+            input.read(tile.level1ObjectUID);
+            input.read(tile.level2ObjectUID);
+
+            val mapObjectType = ByteBuffer.wrap(tile.mapObjectType.reversedArray())[0].toUByte()
+
+            return polMapObjectIndexes.contains(mapObjectType.toString())
+        }
+
+        private fun isPriceOfLoyalty(tilesBuffer: ByteArray): Boolean {
             var tileIndex = 0;
 
+            val input = ByteArrayInputStream(tilesBuffer)
+
             while (tileIndex < tilesBuffer.size / tileSize) {
-                if (isPoLTile(tilesBuffer, tileIndex)) {
+                if (isPoLTile(input, tileIndex)) {
                     return true
                 }
 
-                tileIndex += tileSize
+                tileIndex += 1
             }
 
             return false
