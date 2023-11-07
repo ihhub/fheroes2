51a8b43170ab8c1f6b919cd3dffb6826b7bff03f
Ilya Pomaskin <IlyaPomaskin@users.noreply.github.com>
2023-11-07 18:49:38 +0500

    add sdl into git

---
+++ android/app/jni/SDL2/Android.mk
@@ -0,0 +1,9 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := SDL2
+LOCAL_SRC_FILES := lib/$(TARGET_ARCH_ABI)/libSDL2.so
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
+
+include $(PREBUILT_SHARED_LIBRARY)
