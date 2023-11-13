7869a5f9bc10abcf44b0b7cba96e8cb97916c50b
Lior Orenbach <lior1504@gmail.com>
2023-11-13 22:57:51 +0200

    code format

--- src/fheroes2/dialog/dialog_selectcount.cpp
+++ src/fheroes2/dialog/dialog_selectcount.cpp
@@ -409,7 +409,7 @@ int Dialog::ArmySplitTroop( uint32_t freeSlots, const uint32_t redistributeMax, 
 
     fheroes2::Text text2( _( "Fast separation into slots:" ), fheroes2::FontType::normalWhite() );
     const int32_t text2Height = text2.rows( BOXAREA_WIDTH ) * text2.height();
-    
+
     const int defaultYPosition = 160;
     const int boxHeight = freeSlots > 1 ? 56 + spacer + textHeight + text2Height : 45;
     const int boxYPosition = defaultYPosition + ( ( display.height() - display.DEFAULT_HEIGHT ) / 2 ) - boxHeight;
