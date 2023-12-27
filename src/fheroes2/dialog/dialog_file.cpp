eb0561978529a3d999135cd5bb8adf0db01fba52
Zenseii <zenseii@users.noreply.github.com>
2023-12-27 13:35:13 +0100

    Change file dialog to use StandardWindow

--- src/fheroes2/dialog/dialog_file.cpp
+++ src/fheroes2/dialog/dialog_file.cpp
@@ -21,7 +21,6 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 
-#include "agg_image.h"
 #include "cursor.h"
 #include "dialog.h"
 #include "dir.h"
@@ -40,43 +39,51 @@
 #include "ui_button.h"
 #include "ui_dialog.h"
 #include "ui_text.h"
+#include "ui_window.h"
 
 fheroes2::GameMode Dialog::FileOptions()
 {
-    // preload
-    const bool isEvilInterface = Settings::Get().isEvilInterfaceEnabled();
-    const int cpanbkg = isEvilInterface ? ICN::CPANBKGE : ICN::CPANBKG;
-
     // setup cursor
     const CursorRestorer cursorRestorer( true, Cursor::POINTER );
 
-    // Image sprite.
-    const fheroes2::Sprite & background = fheroes2::AGG::GetICN( cpanbkg, 0 );
-
     fheroes2::Display & display = fheroes2::Display::instance();
 
-    // Since the original image contains shadow it is important to remove it from calculation of window's position.
-    const fheroes2::Point rb( ( display.width() - background.width() - BORDERWIDTH ) / 2, ( display.height() - background.height() + BORDERWIDTH ) / 2 );
-    fheroes2::ImageRestorer back( display, rb.x, rb.y, background.width(), background.height() );
-    fheroes2::Blit( background, display, rb.x, rb.y );
+    const int32_t totalDialogWidth = 337;
+    const int32_t totalDialogHeight = 252;
 
-    LocalEvent & le = LocalEvent::Get();
+    // Prepare restorer of the adventure map when save feedback dialog is shown.
+    fheroes2::ImageRestorer back( display, ( display.width() - totalDialogWidth - BORDERWIDTH ) / 2, ( display.height() - totalDialogHeight + BORDERWIDTH ) / 2,
+                                  totalDialogWidth, totalDialogHeight );
 
-    fheroes2::Button buttonNew( rb.x + 62, rb.y + 31, isEvilInterface ? ICN::BUTTON_NEW_GAME_EVIL : ICN::BUTTON_NEW_GAME_GOOD, 0, 1 );
-    fheroes2::Button buttonLoad( rb.x + 195, rb.y + 31, isEvilInterface ? ICN::BUTTON_LOAD_GAME_EVIL : ICN::BUTTON_LOAD_GAME_GOOD, 0, 1 );
-    fheroes2::Button buttonSave( rb.x + 62, rb.y + 107, isEvilInterface ? ICN::BUTTON_SAVE_GAME_EVIL : ICN::BUTTON_SAVE_GAME_GOOD, 0, 1 );
-    fheroes2::Button buttonQuit( rb.x + 195, rb.y + 107, isEvilInterface ? ICN::BUTTON_QUIT_EVIL : ICN::BUTTON_QUIT_GOOD, 0, 1 );
-    fheroes2::Button buttonCancel( rb.x + 128, rb.y + 184, isEvilInterface ? ICN::BUTTON_SMALL_CANCEL_EVIL : ICN::BUTTON_SMALL_CANCEL_GOOD, 0, 1 );
+    fheroes2::StandardWindow background( 289, 204, true, display );
 
-    buttonNew.draw();
-    buttonLoad.draw();
-    buttonSave.draw();
-    buttonQuit.draw();
-    buttonCancel.draw();
+    const bool isEvilInterface = Settings::Get().isEvilInterfaceEnabled();
 
-    display.render();
+    fheroes2::Button buttonCancel;
+    fheroes2::Button buttonNew;
+    fheroes2::Button buttonLoad;
+    fheroes2::Button buttonSave;
+    fheroes2::Button buttonQuit;
+
+    const int32_t largeButtonsXOffset = 30;
+    const int32_t largeButtonsYOffset = 15;
+
+    background.renderButton( buttonCancel, isEvilInterface ? ICN::BUTTON_SMALL_CANCEL_EVIL : ICN::BUTTON_SMALL_CANCEL_GOOD, 0, 1, { 0, 11 },
+                             fheroes2::StandardWindow::Padding::BOTTOM_CENTER );
+    background.renderButton( buttonNew, isEvilInterface ? ICN::BUTTON_NEW_GAME_EVIL : ICN::BUTTON_NEW_GAME_GOOD, 0, 1, { largeButtonsXOffset, largeButtonsYOffset },
+                             fheroes2::StandardWindow::Padding::TOP_LEFT );
+    background.renderButton( buttonLoad, isEvilInterface ? ICN::BUTTON_LOAD_GAME_EVIL : ICN::BUTTON_LOAD_GAME_GOOD, 0, 1, { largeButtonsXOffset, largeButtonsYOffset },
+                             fheroes2::StandardWindow::Padding::TOP_RIGHT );
+    background.renderButton( buttonSave, isEvilInterface ? ICN::BUTTON_SAVE_GAME_EVIL : ICN::BUTTON_SAVE_GAME_GOOD, 0, 1,
+                             { largeButtonsXOffset, largeButtonsYOffset + 2 }, fheroes2::StandardWindow::Padding::CENTER_LEFT );
+    background.renderButton( buttonQuit, isEvilInterface ? ICN::BUTTON_QUIT_EVIL : ICN::BUTTON_QUIT_GOOD, 0, 1, { largeButtonsXOffset, largeButtonsYOffset + 2 },
+                             fheroes2::StandardWindow::Padding::CENTER_RIGHT );
+
+    display.render( background.totalArea() );
 
     fheroes2::GameMode result = fheroes2::GameMode::QUIT_GAME;
+
+    LocalEvent & le = LocalEvent::Get();
 
     // dialog menu loop
     while ( le.HandleEvents() ) {
