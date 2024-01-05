bab0078bfa320b32823bd4a6e12a3cb715311ecd
Oleg Derevenetz <oleg-derevenetz@yandex.ru>
2024-01-05 12:46:54 +0300

    Style nits

--- src/fheroes2/battle/battle_main.cpp
+++ src/fheroes2/battle/battle_main.cpp
@@ -490,7 +490,7 @@ Battle::Result Battle::Loader( Army & army1, Army & army2, int32_t mapsindex )
     army1.resetInvalidMonsters();
     army2.resetInvalidMonsters();
 
-    // reset the hero's army to the minimum army if the hero retreated or was defeated
+    // Reset the hero's army to the minimum army if the hero retreated or was defeated
     if ( commander1 && commander1->isHeroes() && ( !army1.isValid() || ( result.army1 & RESULT_RETREAT ) ) ) {
         army1.Reset( false );
     }
