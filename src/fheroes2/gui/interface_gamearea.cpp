2c0cea3f9684ec25e325e062a1e035eee807ec9a
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2024-03-18 03:00:56 +1100

    Add Editor related options (#8469)

--- src/fheroes2/gui/interface_gamearea.cpp
+++ src/fheroes2/gui/interface_gamearea.cpp
@@ -737,47 +737,45 @@ void Interface::GameArea::Redraw( fheroes2::Image & dst, int flag, bool isPuzzle
         }
     }
 
-    const bool drawTowns = ( flag & LEVEL_TOWNS );
+    bool drawPassabilities = ( flag & LEVEL_PASSABILITIES );
 
 #ifdef WITH_DEBUG
-    if ( IS_DEVEL() ) {
-        // redraw grid
-        if ( flag & LEVEL_ALL ) {
-            const int32_t friendColors = Players::FriendColors();
+    if ( IS_DEVEL() && ( flag & LEVEL_ALL ) ) {
+        drawPassabilities = true;
+    }
+#endif
 
-            for ( int32_t y = minY; y < maxY; ++y ) {
-                for ( int32_t x = minX; x < maxX; ++x ) {
-                    redrawPassable( world.GetTiles( x, y ), dst, friendColors, *this );
-                }
+    if ( drawPassabilities ) {
+        const int32_t friendColors = Players::FriendColors();
+
+        for ( int32_t y = minY; y < maxY; ++y ) {
+            for ( int32_t x = minX; x < maxX; ++x ) {
+                redrawPassable( world.GetTiles( x, y ), dst, friendColors, *this );
             }
         }
     }
-    else {
-#endif
-        // redraw fog
-        if ( renderFog ) {
-            for ( int32_t y = minY; y < maxY; ++y ) {
-                for ( int32_t x = minX; x < maxX; ++x ) {
-                    const Maps::Tiles & tile = world.GetTiles( x, y );
+    else if ( renderFog ) {
+        const bool drawTowns = ( flag & LEVEL_TOWNS );
 
-                    if ( tile.getFogDirection() != Direction::UNKNOWN ) {
-                        drawFog( tile, dst, *this );
+        for ( int32_t y = minY; y < maxY; ++y ) {
+            for ( int32_t x = minX; x < maxX; ++x ) {
+                const Maps::Tiles & tile = world.GetTiles( x, y );
 
-                        if ( drawTowns ) {
-                            drawByObjectIcnType( tile, dst, *this, MP2::OBJ_ICN_TYPE_OBJNTWBA );
+                if ( tile.getFogDirection() != Direction::UNKNOWN ) {
+                    drawFog( tile, dst, *this );
 
-                            const MP2::MapObjectType objectType = tile.GetObject( false );
-                            if ( objectType == MP2::OBJ_CASTLE || objectType == MP2::OBJ_NON_ACTION_CASTLE ) {
-                                drawByObjectIcnType( tile, dst, *this, MP2::OBJ_ICN_TYPE_OBJNTOWN );
-                            }
+                    if ( drawTowns ) {
+                        drawByObjectIcnType( tile, dst, *this, MP2::OBJ_ICN_TYPE_OBJNTWBA );
+
+                        const MP2::MapObjectType objectType = tile.GetObject( false );
+                        if ( objectType == MP2::OBJ_CASTLE || objectType == MP2::OBJ_NON_ACTION_CASTLE ) {
+                            drawByObjectIcnType( tile, dst, *this, MP2::OBJ_ICN_TYPE_OBJNTOWN );
                         }
                     }
                 }
             }
         }
-#ifdef WITH_DEBUG
     }
-#endif
 
     updateObjectAnimationInfo();
 }
