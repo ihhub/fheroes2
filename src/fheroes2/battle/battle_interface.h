07c1d5af176f142a635fd50ab017e42d5fad1e84
Zenseii <zenseii@users.noreply.github.com>
2024-03-19 14:14:30 +0100

    Fix missing declaration name change

--- src/fheroes2/battle/battle_interface.h
+++ src/fheroes2/battle/battle_interface.h
@@ -394,7 +394,7 @@ namespace Battle
         void _openBattleSettingsDialog();
         void EventStartAutoBattle( const Unit & unit, Actions & actions );
         void EventAutoFinish( Actions & actions );
-        void ShowAutoModeDialog( const Unit & unit, Actions & actions );
+        void OpenAutoModeDialog( const Unit & unit, Actions & actions );
         void EventShowOptions();
         void ButtonAutoAction( const Unit & unit, Actions & actions );
         void ButtonSettingsAction();
