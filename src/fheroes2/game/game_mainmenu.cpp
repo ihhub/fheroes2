a5cdcd5454f73f418716c26387a0327d3d5b073d
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2024-06-25 17:41:04 +0800

    Address comment

--- src/fheroes2/game/game_mainmenu.cpp
+++ src/fheroes2/game/game_mainmenu.cpp
@@ -172,9 +172,10 @@ void Game::mainGameLoop( bool isFirstGameRun, bool isProbablyDemoVersion )
         case fheroes2::GameMode::SELECT_SCENARIO_FOUR_HUMAN_PLAYERS:
         case fheroes2::GameMode::SELECT_SCENARIO_FIVE_HUMAN_PLAYERS:
         case fheroes2::GameMode::SELECT_SCENARIO_SIX_HUMAN_PLAYERS: {
-            const uint8_t humanPlayCount = static_cast<uint8_t>( static_cast<int>( result ) - static_cast<int>( fheroes2::GameMode::SELECT_SCENARIO_ONE_HUMAN_PLAYER ) );
+            const uint8_t humanPlayerCount =
+                static_cast<uint8_t>( static_cast<int>( result ) - static_cast<int>( fheroes2::GameMode::SELECT_SCENARIO_ONE_HUMAN_PLAYER ) );
             // Add +1 since we don't have zero human player option.
-            result = Game::SelectScenario( humanPlayCount + 1 );
+            result = Game::SelectScenario( humanPlayerCount + 1 );
             break;
         }
         case fheroes2::GameMode::START_GAME:
