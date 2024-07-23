448537f96403c3cc8f3bede48f606baad561c0a1
Oleg Derevenetz <oleg-derevenetz@yandex.ru>
2024-07-23 07:26:36 +0300

    Editor: take into account the arbitrary case of the map directory name (#8981)

--- src/fheroes2/editor/editor_interface.cpp
+++ src/fheroes2/editor/editor_interface.cpp
@@ -1731,11 +1731,23 @@ namespace Interface
             return;
         }
 
-        const std::string mapDirectory = System::concatPath( dataPath, "maps" );
+        std::string mapDirectory = System::concatPath( dataPath, "maps" );
 
         if ( !System::IsDirectory( mapDirectory ) && !System::MakeDirectory( mapDirectory ) ) {
             fheroes2::showStandardTextMessage( _( "Warning!" ), _( "Unable to create a directory to save the map." ), Dialog::OK );
             return;
+        }
+
+        // Since the name of the map directory can be in arbitrary case, we need to get its real case-sensitive name first
+        {
+            std::string correctedMapDirectory;
+
+            if ( !System::GetCaseInsensitivePath( mapDirectory, correctedMapDirectory ) ) {
+                fheroes2::showStandardTextMessage( _( "Warning!" ), _( "Unable to locate a directory to save the map." ), Dialog::OK );
+                return;
+            }
+
+            mapDirectory = std::move( correctedMapDirectory );
         }
 
         std::string fileName = _loadedFileName;
