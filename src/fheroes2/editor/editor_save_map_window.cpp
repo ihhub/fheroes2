1b7e1fc4f9f4558e16f2a37a69d25569c6e8eba9
Oleg Derevenetz <oleg-derevenetz@yandex.ru>
2024-08-17 14:32:33 +0300

    editor_save_map_window.cpp

--- src/fheroes2/editor/editor_save_map_window.cpp
+++ src/fheroes2/editor/editor_save_map_window.cpp
@@ -18,6 +18,8 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 
+#include "editor_save_map_window.h"
+
 #include <algorithm>
 #include <cstdint>
 #include <ctime>
