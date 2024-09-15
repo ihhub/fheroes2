da8815d87acb7c7ea7f1b548143aafc45f05bf3d
filipni <filip.nilsson+github@protonmail.com>
2024-09-15 12:16:44 +0200

    Prevent redistribution of hero's last unit

--- src/fheroes2/army/army_bar.cpp
+++ src/fheroes2/army/army_bar.cpp
@@ -61,6 +61,10 @@ namespace
 
         assert( overallCount > 0 );
 
+        if ( saveLastTroop && troopFrom.GetCount() <= 1 ) {
+            return;
+        }
+
         if ( overallCount == 1 ) {
             // prevent cross-army split if we lose the last unit in the hero army
             if ( saveLastTroop ) {
