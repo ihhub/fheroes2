aafb3158baa78b191aae57e46808d4befbacfcf1
Oleg Derevenetz <oleg-derevenetz@yandex.ru>
2024-09-27 14:36:12 +0300

    Apply the IWYU suggestions

--- src/fheroes2/world/world.h
+++ src/fheroes2/world/world.h
@@ -27,6 +27,7 @@
 #include <cstdint>
 #include <list>
 #include <map>
+#include <memory>
 #include <string>
 #include <vector>
 
@@ -48,7 +49,6 @@
 class IStreamBase;
 class OStreamBase;
 
-struct MapEvent;
 struct Week;
 
 namespace MP2
