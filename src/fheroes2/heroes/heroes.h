6190c0e131adc22aefe7a6885ee1cb224a5b7747
Oleg Derevenetz <oleg-derevenetz@yandex.ru>
2024-10-09 15:57:46 +0300

    Specify the pointer part of the type outside of auto (#9170)

--- src/fheroes2/heroes/heroes.h
+++ src/fheroes2/heroes/heroes.h
@@ -830,7 +830,7 @@ public:
             : BaseIterator( std::move( other ) )
         {}
 
-        auto operator*() const noexcept
+        auto * operator*() const noexcept
         {
             return BaseIterator::operator*().get();
         }
