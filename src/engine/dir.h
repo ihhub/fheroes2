25d8607c71ab2824bbcb1146190ed41488af1e19
Oleg Derevenetz <oleg-derevenetz@yandex.ru>
2024-12-03 03:09:09 +0300

    Speed up getting a list of files on PS Vita (#9310)

--- src/engine/dir.h
+++ src/engine/dir.h
@@ -32,13 +32,13 @@ struct ListFiles : public std::list<std::string>
     void Append( ListFiles && files );
 
     // Adds files from the 'path' directory ending in 'filter' to the list, case-insensitive.
-    void ReadDir( const std::string_view path, const std::string & filter );
+    void ReadDir( const std::string & path, const std::string & filter );
 
-    // Adds files from the 'path' directory with names matching 'fileName' to the list, case-insensitive.
-    void FindFileInDir( const std::string_view path, const std::string & fileName );
+    // Adds the first found file from the 'path' directory with a name matching 'fileName' to the list, case-insensitive.
+    void FindFileInDir( const std::string_view path, const std::string_view fileName );
 
     // Returns true if there are no files in the 'path' directory with names ending in 'filter', case-insensitive, otherwise returns false.
-    static bool IsEmpty( const std::string_view path, const std::string & filter );
+    static bool IsEmpty( const std::string & path, const std::string & filter );
 };
 
 #endif
