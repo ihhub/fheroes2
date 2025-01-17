a0c22a8a9a038ac5dd019357bcd24f6f46b882b1
Oleg Derevenetz <oleg-derevenetz@yandex.ru>
2025-01-17 14:59:32 +0300

    Revert the wording

--- src/fheroes2/dialog/dialog_system_options.cpp
+++ src/fheroes2/dialog/dialog_system_options.cpp
@@ -184,7 +184,7 @@ namespace
         const Settings & conf = Settings::Get();
         if ( conf.BattleAutoResolve() ) {
             const bool spellcast = conf.BattleAutoSpellcast();
-            std::string value = spellcast ? _( "Quick Combat" ) : _( "Quick, No Spells" );
+            std::string value = spellcast ? _( "Auto Resolve" ) : _( "Auto, No Spells" );
 
             const fheroes2::Sprite & autoBattleIcon = fheroes2::AGG::GetICN( ICN::CSPANEL, spellcast ? 7 : 6 );
             fheroes2::drawOption( optionRoi, autoBattleIcon, _( "Battles" ), std::move( value ), fheroes2::UiOptionTextWidth::THREE_ELEMENTS_ROW );
