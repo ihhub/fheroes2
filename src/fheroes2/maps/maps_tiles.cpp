a774648cce326b9e402f76e510602b3ef5d08a5c
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2025-02-16 09:45:19 +0800

    Remove assertion for passability checks
    This is due some maps being hacked or the OG editor's bugs.

--- src/fheroes2/maps/maps_tiles.cpp
+++ src/fheroes2/maps/maps_tiles.cpp
@@ -828,8 +828,10 @@ void Maps::Tile::updatePassability()
 
         if ( isShortObject( bottomTileObjectType )
              || ( !bottomTile.containsAnyObjectIcnType( getValidObjectIcnTypes() ) && ( isCombinedObject( objectType ) || isCombinedObject( bottomTileObjectType ) ) ) ) {
-            // If this assertion blows up then the above checks are invalid!
-            assert( ( bottomTile.getTileIndependentPassability() & Direction::TOP ) == 0 );
+            // Checking the main object type of a tile is not entirely correct idea
+            // as some tiles can be mark as valid objects but nothing was set of them.
+            // As an example, Necroking map contains a tile with rock shadow which is marked as object.
+            // Most likely these maps are hacked but since we are reading old map we cannot assume things and assert them.
         }
         else {
             _tilePassabilityDirections = 0;
