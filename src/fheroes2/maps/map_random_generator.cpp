b3892e6d09c207ef942707a0f805c6f7ff6831cf
Ivan Shibanov <idshibanov@users.noreply.github.com>
2025-02-18 01:58:19 -0500

    Rename the source files

--- src/fheroes2/maps/map_random_generator.cpp
+++ src/fheroes2/maps/map_random_generator.cpp
@@ -18,7 +18,7 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 
-#include "map_generator.h"
+#include "map_random_generator.h"
 
 #include <algorithm>
 #include <cassert>
