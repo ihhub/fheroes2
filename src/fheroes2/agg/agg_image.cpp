25564a94167c5ee6c14d657fd07b99672e8e4493
Pavel <53114202+shprotru@users.noreply.github.com>
2025-03-10 16:58:02 +0300

    Fixing Golden bow sprite glowing pixels (#8542)

--- src/fheroes2/agg/agg_image.cpp
+++ src/fheroes2/agg/agg_image.cpp
@@ -4069,10 +4069,17 @@ namespace
                 const fheroes2::Sprite & originalImage = fheroes2::AGG::GetICN( ICN::ARTIFACT, 83 );
                 SubpixelResize( originalImage, _icnVsSprite[id][82] );
             }
+            if ( _icnVsSprite[id].size() > 63 ) {
+                // This fixes "Golden Bow" (#63) small artifact icon glowing yellow pixel
+                Copy( _icnVsSprite[id][63], 12, 17, _icnVsSprite[id][63], 16, 12, 1, 1 );
+            }
             return true;
         case ICN::ARTIFACT:
             LoadOriginalICN( id );
             if ( _icnVsSprite[id].size() > 99 ) {
+                // This fixes "Golden Bow" (#64) large artifact icon glowing yellow pixel
+                Copy( _icnVsSprite[id][64], 35, 24, _icnVsSprite[id][64], 56, 12, 1, 1 );
+
                 // This fixes "Arm of the Martyr" (#88) and " Sphere of Negation" (#99) artifacts rendering which initially has some incorrect transparent pixels.
                 for ( const int32_t index : { 88, 99 } ) {
                     fheroes2::Sprite & originalImage = _icnVsSprite[id][index];
