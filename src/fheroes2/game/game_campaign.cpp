722e64eacc75b5313350d759086da80fbf057b06
Oleg Derevenetz <oleg-derevenetz@yandex.ru>
2025-03-18 22:42:15 +0300

    Campaign: allow to raise the game difficulty during the campaign

--- src/fheroes2/game/game_campaign.cpp
+++ src/fheroes2/game/game_campaign.cpp
@@ -907,7 +907,7 @@ namespace
         }
     }
 
-    int32_t setCampaignDifficulty( int32_t currentDifficulty, const int32_t maximumAllowedDifficulty )
+    int32_t setCampaignDifficulty( int32_t currentDifficulty )
     {
         // It's better to have frame border width value divisible to 2 and 3 w/o remainder.
         const fheroes2::StandardWindow frameborder( 300, 284, true );
@@ -998,17 +998,6 @@ namespace
             break;
         }
 
-        const std::array<bool, 3> allowedSelection{ ( maximumAllowedDifficulty >= Campaign::CampaignDifficulty::Easy ),
-                                                    ( maximumAllowedDifficulty >= Campaign::CampaignDifficulty::Normal ),
-                                                    ( maximumAllowedDifficulty >= Campaign::CampaignDifficulty::Hard ) };
-
-        for ( size_t i = 0; i < allowedSelection.size(); ++i ) {
-            if ( !allowedSelection[i] ) {
-                fheroes2::ApplyPalette( display, iconArea[i].x, iconArea[i].y, display, iconArea[i].x, iconArea[i].y, iconArea[i].width, iconArea[i].height,
-                                        PAL::GetPalette( PAL::PaletteType::GRAY ) );
-            }
-        }
-
         const int32_t textWidth = windowRoi.width - 16;
         const fheroes2::Point textOffset{ windowRoi.x + 8, windowRoi.y + 140 };
 
@@ -1054,19 +1043,19 @@ namespace
                 updateInfo = true;
             }
 
-            if ( allowedSelection[0] && le.MouseClickLeft( difficultyArea[0] ) ) {
+            if ( le.MouseClickLeft( difficultyArea[0] ) ) {
                 currentDescription = easyDescription;
                 selection.setPosition( difficultyArea[0].x, difficultyArea[0].y );
                 currentDifficulty = Campaign::CampaignDifficulty::Easy;
                 updateInfo = true;
             }
-            else if ( allowedSelection[1] && le.MouseClickLeft( difficultyArea[1] ) ) {
+            else if ( le.MouseClickLeft( difficultyArea[1] ) ) {
                 currentDescription = normalDescription;
                 selection.setPosition( difficultyArea[1].x, difficultyArea[1].y );
                 currentDifficulty = Campaign::CampaignDifficulty::Normal;
                 updateInfo = true;
             }
-            else if ( allowedSelection[2] && le.MouseClickLeft( difficultyArea[2] ) ) {
+            else if ( le.MouseClickLeft( difficultyArea[2] ) ) {
                 currentDescription = hardDescription;
                 selection.setPosition( difficultyArea[2].x, difficultyArea[2].y );
                 currentDifficulty = Campaign::CampaignDifficulty::Hard;
@@ -1263,7 +1252,7 @@ fheroes2::GameMode Game::CompleteCampaignScenario( const bool isLoadingSaveFile 
         ratingText.add( { textBody, fheroes2::FontType::normalWhite() } );
 
         textBody = _( "\nDifficulty: %{difficulty}\n\n" );
-        StringReplace( textBody, "%{difficulty}", getCampaignDifficultyText( campaignSaveData.getDifficulty() ) );
+        StringReplace( textBody, "%{difficulty}", getCampaignDifficultyText( campaignSaveData.getMinDifficulty() ) );
         ratingText.add( { textBody, fheroes2::FontType::smallWhite() } );
 
         textBody = _( "Score: %{score}\n\nRating:\n%{rating}" );
@@ -1575,8 +1564,8 @@ fheroes2::GameMode Game::SelectCampaignScenario( const fheroes2::GameMode prevMo
         else if ( ( buttonOk.isEnabled() && ( le.MouseClickLeft( buttonOk.area() ) || HotKeyPressEvent( HotKeyEvent::DEFAULT_OKAY ) ) ) || restartButtonClicked ) {
             if ( ( !campaignSaveData.isStarting() || allowToRestart ) && currentDifficulty != campaignSaveData.getDifficulty()
                  && fheroes2::showStandardTextMessage( _( "Difficulty" ),
-                                                       _( "You have changed to a lower difficulty for the campaign. You will not be able to revert this after this "
-                                                          "point. The high score will be calculated based solely on the new difficulty. Do you want to proceed?" ),
+                                                       _( "You have changed the difficulty of the campaign. The high score will be calculated solely based on the "
+                                                          "lowest difficulty level of those selected for the campaign missions. Do you want to proceed?" ),
                                                        Dialog::YES | Dialog::NO )
                         == Dialog::NO ) {
                 continue;
@@ -1619,7 +1608,7 @@ fheroes2::GameMode Game::SelectCampaignScenario( const fheroes2::GameMode prevMo
             }
 
             // Scenario difficulty must be set before loading the map, because it is used during the map loading process.
-            campaignSaveData.setDifficulty( currentDifficulty );
+            campaignSaveData.setDifficulty( currentDifficulty, campaignSaveData.isStarting() && !allowToRestart );
 
             Players & players = conf.GetPlayers();
             players.SetStartGame();
@@ -1671,13 +1660,7 @@ fheroes2::GameMode Game::SelectCampaignScenario( const fheroes2::GameMode prevMo
             updateDisplay = true;
         }
         else if ( le.MouseClickLeft( buttonDifficulty.area() ) || HotKeyPressEvent( HotKeyEvent::CAMPAIGN_SELECT_DIFFICULTY ) ) {
-            if ( campaignSaveData.isStarting() && !allowToRestart ) {
-                currentDifficulty = setCampaignDifficulty( currentDifficulty, Campaign::CampaignDifficulty::Hard );
-            }
-            else {
-                currentDifficulty = setCampaignDifficulty( currentDifficulty, campaignSaveData.getDifficulty() );
-            }
-
+            currentDifficulty = setCampaignDifficulty( currentDifficulty );
             updateDisplay = true;
         }
 
