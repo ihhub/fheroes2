2f3351cc8738a86925c12ff43bcc4e08ab8a7778
Oleg Derevenetz <oleg-derevenetz@yandex.ru>
2025-03-18 22:46:48 +0300

    Paraphrase the text

--- src/fheroes2/game/game_campaign.cpp
+++ src/fheroes2/game/game_campaign.cpp
@@ -1565,7 +1565,7 @@ fheroes2::GameMode Game::SelectCampaignScenario( const fheroes2::GameMode prevMo
             if ( ( !campaignSaveData.isStarting() || allowToRestart ) && currentDifficulty != campaignSaveData.getDifficulty()
                  && fheroes2::showStandardTextMessage( _( "Difficulty" ),
                                                        _( "You have changed the difficulty of the campaign. The high score will be calculated solely based on the "
-                                                          "lowest difficulty level of those selected for the campaign missions. Do you want to proceed?" ),
+                                                          "lowest difficulty level of those selected for individual campaign missions. Do you want to proceed?" ),
                                                        Dialog::YES | Dialog::NO )
                         == Dialog::NO ) {
                 continue;
