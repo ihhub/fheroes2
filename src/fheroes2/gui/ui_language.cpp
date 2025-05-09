9368d3552878dc0758f47913152e20fa6d424aec
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2025-05-10 00:34:43 +0800

    Optimize getSupportedLanguages() function

--- src/fheroes2/gui/ui_language.cpp
+++ src/fheroes2/gui/ui_language.cpp
@@ -103,11 +103,12 @@ namespace fheroes2
 
     std::vector<SupportedLanguage> getSupportedLanguages()
     {
-        std::vector<SupportedLanguage> languages;
+        // We need to group languages by code pages to avoid recreating font related resources while switching languages.
+        std::map<CodePage, std::vector<SupportedLanguage>> supportedLanguges;
 
         const SupportedLanguage resourceLanguage = getResourceLanguage();
         if ( resourceLanguage != SupportedLanguage::English ) {
-            languages.emplace_back( resourceLanguage );
+            supportedLanguges[getCodePage(resourceLanguage)].emplace_back( resourceLanguage );
         }
 
         const std::set<SupportedLanguage> possibleLanguages{ SupportedLanguage::French,     SupportedLanguage::Polish,    SupportedLanguage::German,
@@ -120,7 +121,7 @@ namespace fheroes2
 
         for ( const SupportedLanguage language : possibleLanguages ) {
             if ( language != resourceLanguage && isAlphabetSupported( language ) ) {
-                languages.emplace_back( language );
+                supportedLanguges[getCodePage(language)].emplace_back( language );
             }
         }
 
@@ -130,11 +131,13 @@ namespace fheroes2
 
         std::vector<fheroes2::SupportedLanguage> validSupportedLanguages{ fheroes2::SupportedLanguage::English };
 
-        for ( fheroes2::SupportedLanguage language : languages ) {
+        for ( const auto & [codePage, languages] : supportedLanguges ) {
             // TODO: we shouldn't load all language resources just for the sake of verifying whether their translations exist.
             //       Find another way to avoid this heavy operation.
-            if ( conf.setGameLanguage( fheroes2::getLanguageAbbreviation( language ) ) ) {
-                validSupportedLanguages.emplace_back( language );
+            for ( const auto language : languages ) {
+                if ( conf.setGameLanguage( fheroes2::getLanguageAbbreviation( language ) ) ) {
+                    validSupportedLanguages.emplace_back( language );
+                }
             }
         }
 
