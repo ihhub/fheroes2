334432789ad2d21f89119df2a302dfc5dcfc0cc4
Zenseii <zenseii@users.noreply.github.com>
2025-05-19 07:07:17 +0200

    Add missing locale
    Remove source charset from PO file

--- src/engine/translations.cpp
+++ src/engine/translations.cpp
@@ -127,6 +127,7 @@ namespace
         LOCALE_FI,
         LOCALE_FR,
         LOCALE_GL,
+        LOCALE_GR,
         LOCALE_HE,
         LOCALE_HR,
         LOCALE_HU,
@@ -198,6 +199,9 @@ namespace
                                                                                   // Galician
                                                                                   { "gl", LocaleType::LOCALE_GL },
                                                                                   { "galician", LocaleType::LOCALE_GL },
+                                                                                  // Greek
+                                                                                  { "gr", LocaleType::LOCALE_GR },
+                                                                                  { "greek", LocaleType::LOCALE_GR },
                                                                                   // Hebrew
                                                                                   { "he", LocaleType::LOCALE_HE },
                                                                                   { "hebrew", LocaleType::LOCALE_HE },
@@ -620,6 +624,7 @@ const char * Translation::ngettext( const char * str, const char * plural, size_
         case LocaleType::LOCALE_EU:
         case LocaleType::LOCALE_FI:
         case LocaleType::LOCALE_GL:
+        case LocaleType::LOCALE_GR:
         case LocaleType::LOCALE_HE:
         case LocaleType::LOCALE_HU:
         case LocaleType::LOCALE_ID:
