ihhub/fheroes2

commit 9094e5ac4359423f629e471cd3885a715aeb2ca2

Fix map center positioning when AI interacts with action object (#9778)

src/fheroes2/ai/ai_hero_action.cpp
@@ -1919,6 +1919,12 @@ void AI::HeroesAction( Heroes & hero, const int32_t dst_index )
         hero.SetModes( Heroes::ACTION );
     }
 
+        if ( AIIsShowAnimationForHero( hero, AIGetAllianceColors() ) ) {
+            // Most likely there will be some action, center the map on the hero to avoid subsequent minor screen movements.
+            Interface::AdventureMap::Get().getGameArea().SetCenter( hero.GetCenter() );
+        }
+    }
+
     switch ( objectType ) {
     case MP2::OBJ_BOAT:
         AIToBoat( hero, dst_index );
@@ -2256,14 +2262,17 @@ fheroes2::GameMode AI::HeroesMove( Heroes & hero )
 
                 gameArea.ShiftCenter( { heroAnimationOffset.x * heroMovementSkipValue, heroAnimationOffset.y * heroMovementSkipValue } );
                 gameArea.SetRedraw();
+
                 heroAnimationFrameCount -= heroMovementSkipValue;
                 if ( ( heroAnimationFrameCount & 0x3 ) == 0 ) { // % 4
                     hero.SetSpriteIndex( heroAnimationSpriteId );
 
-                    if ( heroAnimationFrameCount == 0 )
+                    if ( heroAnimationFrameCount == 0 ) {
                         resetHeroSprite = true;
-                    else
+                    }
+                    else {
                         ++heroAnimationSpriteId;
+                    }
                 }
                 const int offsetStep = ( ( 4 - ( heroAnimationFrameCount & 0x3 ) ) & 0x3 ); // % 4
                 hero.SetOffset( { heroAnimationOffset.x * offsetStep, heroAnimationOffset.y * offsetStep } );
@@ -2286,6 +2295,7 @@ fheroes2::GameMode AI::HeroesMove( Heroes & hero )
 
                         heroAnimationOffset = movement;
                         gameArea.ShiftCenter( movement );
+
                         heroAnimationFrameCount = 32 - heroMovementSkipValue;
                         heroAnimationSpriteId = hero.GetSpriteIndex();
                         if ( heroMovementSkipValue < 4 ) {

commit 44181c0fc75ac19410f5c26f8697a8d8096bd395

Compilation fix
regression from #9778

src/fheroes2/ai/ai_hero_action.cpp
@@ -1919,10 +1919,9 @@ void AI::HeroesAction( Heroes & hero, const int32_t dst_index )
         hero.SetModes( Heroes::ACTION );
     }
 
-        if ( AIIsShowAnimationForHero( hero, AIGetAllianceColors() ) ) {
-            // Most likely there will be some action, center the map on the hero to avoid subsequent minor screen movements.
-            Interface::AdventureMap::Get().getGameArea().SetCenter( hero.GetCenter() );
-        }
+    if ( AIIsShowAnimationForHero( hero, AIGetAllianceColors() ) ) {
+        // Most likely there will be some action, center the map on the hero to avoid subsequent minor screen movements.
+        Interface::AdventureMap::Get().getGameArea().SetCenter( hero.GetCenter() );
     }
 
     switch ( objectType ) {