f14e32d5d2d70ae3bbbabb3f6458d82c16741b44
Sergei Ivanov (Districh) <113276641+Districh-ru@users.noreply.github.com>
2025-06-21 19:04:26 +0300

    Add hero casting animation to Teleport, Summon Elemental, Mirror Image and Earthquake spells animation (#9895)

--- src/fheroes2/battle/battle_arena.h
+++ src/fheroes2/battle/battle_arena.h
@@ -299,11 +299,11 @@ namespace Battle
         void ApplyActionToggleAutoCombat( Command & cmd );
         void ApplyActionQuickCombat( const Command & cmd );
 
-        void ApplyActionSpellSummonElemental( const Command & cmd, const Spell & spell );
-        void ApplyActionSpellMirrorImage( Command & cmd );
-        void ApplyActionSpellTeleport( Command & cmd );
-        void ApplyActionSpellEarthquake( const Command & cmd );
-        void ApplyActionSpellDefaults( Command & cmd, const Spell & spell );
+        void _applyActionSpellSummonElemental( const Spell & spell );
+        void _applyActionSpellMirrorImage( Command & cmd );
+        void _applyActionSpellTeleport( Command & cmd );
+        void _applyActionSpellEarthquake();
+        void _applyActionSpellDefaults( Command & cmd, const Spell & spell );
 
         // Moves the given unit to a position where the index of the head cell is equal to 'dst'. If 'dst' is -1,
         // then this method does nothing. Otherwise, it's the caller's responsibility to make sure that this position
