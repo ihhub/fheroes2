c9627500598109d3b0b1bd6eac684c1664edea2e
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2025-10-04 22:37:15 +0800

    Make AI heroes to predict weekly object states
    This allows AI heroes to plan actions in advance.
    
    close #7381

--- src/fheroes2/ai/ai_planner.h
+++ src/fheroes2/ai/ai_planner.h
@@ -182,6 +182,9 @@ namespace AI
         double getObjectValue( const Heroes & hero, const int32_t index, const MP2::MapObjectType objectType, const double valueToIgnore,
                                const uint32_t distanceToObject ) const;
 
+        double getFutureObjectValue( const Heroes & hero, const int32_t index, const MP2::MapObjectType objectType, const double valueToIgnore,
+                                     const uint32_t distanceToObject ) const;
+
         // Returns the strength of the army guarding the given tile. Note that the army is obtained by calling
         // Army::setFromTile(), so this method is not suitable for hero armies or castle garrisons.
         double getTileArmyStrength( const Maps::Tile & tile );
@@ -215,6 +218,7 @@ namespace AI
         double getFighterObjectValue( const Heroes & hero, const int32_t index, const double valueToIgnore, const uint32_t distanceToObject ) const;
         double getCourierObjectValue( const Heroes & hero, const int32_t index, const double valueToIgnore, const uint32_t distanceToObject ) const;
         double getScoutObjectValue( const Heroes & hero, const int32_t index, const double valueToIgnore, const uint32_t distanceToObject ) const;
+        double getFutureGeneralObjectValue( const Heroes & hero, const int32_t index, const double valueToIgnore, const uint32_t distanceToObject ) const;
 
         int getCourierMainTarget( const Heroes & hero, const double lowestPossibleValue );
 
