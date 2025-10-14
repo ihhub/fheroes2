60f00f337abe3ca7b2517fd81a88d2b6a1d97927
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2025-10-14 09:08:17 +0800

    Reduce code duplication

--- src/fheroes2/castle/castle_dialog.cpp
+++ src/fheroes2/castle/castle_dialog.cpp
@@ -293,14 +293,8 @@ Castle::CastleDialogReturnValue Castle::OpenDialog( const bool openConstructionW
         return CastleDialogReturnValue::DoNothing;
     };
 
-    if ( openConstructionWindow && isBuild( BUILD_CASTLE ) ) {
-        const CastleDialogReturnValue constructionResult = constructionDialogHandler();
-        if ( constructionResult != CastleDialogReturnValue::DoNothing ) {
-            return constructionResult;
-        }
-    }
-    else if ( openMageGuildWindow && isBuild( BUILD_MAGEGUILD ) ) {
-        const auto result = _openMageGuild( hero );
+    auto mageGuildDialogHandler = [this](Heroes * visitingHero) {
+        const auto result = _openMageGuild( visitingHero );
         switch ( result ) {
         case MageGuildDialogResult::NextMageGuildWindow:
             return CastleDialogReturnValue::NextMageGuildWindow;
@@ -309,6 +303,21 @@ Castle::CastleDialogReturnValue Castle::OpenDialog( const bool openConstructionW
         default:
             assert( result == MageGuildDialogResult::DoNothing );
             break;
+        }
+
+        return CastleDialogReturnValue::DoNothing;
+    };
+
+    if ( openConstructionWindow && isBuild( BUILD_CASTLE ) ) {
+        const CastleDialogReturnValue constructionResult = constructionDialogHandler();
+        if ( constructionResult != CastleDialogReturnValue::DoNothing ) {
+            return constructionResult;
+        }
+    }
+    else if ( openMageGuildWindow && isBuild( BUILD_MAGEGUILD ) ) {
+        const auto result = mageGuildDialogHandler( hero );
+        if ( result != CastleDialogReturnValue::DoNothing ) {
+            return result;
         }
     }
 
@@ -568,15 +577,9 @@ Castle::CastleDialogReturnValue Castle::OpenDialog( const bool openConstructionW
                             needRedraw = true;
                         }
 
-                        const auto mageGuildResult = _openMageGuild( hero );
-                        switch ( mageGuildResult ) {
-                        case MageGuildDialogResult::NextMageGuildWindow:
-                            return CastleDialogReturnValue::NextMageGuildWindow;
-                        case MageGuildDialogResult::PrevMageGuildWindow:
-                            return CastleDialogReturnValue::PreviousMageGuildWindow;
-                        default:
-                            assert( mageGuildResult == MageGuildDialogResult::DoNothing );
-                            break;
+                        const auto result = mageGuildDialogHandler( hero );
+                        if ( result != CastleDialogReturnValue::DoNothing ) {
+                            return result;
                         }
                     }
                     else if ( isMonsterDwelling ) {
