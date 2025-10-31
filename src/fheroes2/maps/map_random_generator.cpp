79c558cc06628605dc6e664454cd9a01d7800f32
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2025-10-31 12:56:32 +0800

    Fix code style

--- src/fheroes2/maps/map_random_generator.cpp
+++ src/fheroes2/maps/map_random_generator.cpp
@@ -456,7 +456,8 @@ namespace Maps::RandomGenerator
                 continue;
             }
 
-            DEBUG_LOG( DBG_ENGINE, DBG_TRACE, "Region #" << region._id << " of size " << region._nodes.size() << " tiles has " << region._neighbours.size() << " neighbours" )
+            DEBUG_LOG( DBG_ENGINE, DBG_TRACE,
+                       "Region #" << region._id << " of size " << region._nodes.size() << " tiles has " << region._neighbours.size() << " neighbours" )
 
             int xMin = 0;
             int xMax = width;
