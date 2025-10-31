47184fc064959bebd4056f2751b2f377aeeef320
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2025-10-31 12:36:02 +0800

    Add const

--- src/fheroes2/maps/map_random_generator.cpp
+++ src/fheroes2/maps/map_random_generator.cpp
@@ -357,7 +357,7 @@ namespace
 
 namespace Maps::Generator
 {
-    bool generateMap( Map_Format::MapFormat & mapFormat, const Configuration & config, int32_t width, int32_t height )
+    bool generateMap( Map_Format::MapFormat & mapFormat, const Configuration & config, const int32_t width, const int32_t height )
     {
         if ( config.playerCount < 2 || config.playerCount > 6 ) {
             assert( config.playerCount <= 6 );
