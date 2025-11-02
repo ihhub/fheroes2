678cca0c5d06ae2a665334a7551cd5f4f28e4ffc
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2025-11-02 18:02:22 +0800

    Do not cast Curse and Mass Curse spells on monsters with no damage range (#10334)

--- src/fheroes2/ai/ai_battle_spell.cpp
+++ src/fheroes2/ai/ai_battle_spell.cpp
@@ -418,6 +418,10 @@ double AI::BattlePlanner::spellEffectValue( const Spell & spell, const Battle::U
     }
     case Spell::CURSE:
     case Spell::MASSCURSE:
+        if ( target.GetDamageMax() == target.GetDamageMin() ) {
+            // It is useless to apply Curse spell as the monster already has minimal damage.
+            return 0;
+        }
         ratio = 0.15;
         break;
     case Spell::BERSERKER:
@@ -443,8 +447,10 @@ double AI::BattlePlanner::spellEffectValue( const Spell & spell, const Battle::U
         break;
     case Spell::BLESS:
     case Spell::MASSBLESS: {
-        if ( target.GetDamageMax() == target.GetDamageMin() )
+        if ( target.GetDamageMax() == target.GetDamageMin() ) {
+            // It is useless to apply Bless spell as the monster already has maximum damage.
             return 0.0;
+        }
         ratio = 0.15;
         break;
     }
