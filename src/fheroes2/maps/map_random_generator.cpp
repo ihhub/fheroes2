589922f1b3d6c8d1d5f42fd26d7400b69fda3f1f
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2025-11-09 00:16:21 +0800

    Use prefix decrement

--- src/fheroes2/maps/map_random_generator.cpp
+++ src/fheroes2/maps/map_random_generator.cpp
@@ -1034,7 +1034,7 @@ namespace Maps::Random_Generator
 
             for ( size_t idx = 0; idx < mapEconomy.secondaryMines.size(); ++idx ) {
                 const int resource = mapEconomy.pickNextMineResource( false );
-                for ( size_t ringIndex = tileRings.size() - 3; ringIndex > 0; ringIndex-- ) {
+                for ( size_t ringIndex = tileRings.size() - 3; ringIndex > 0; --ringIndex ) {
                     const auto & ring = tileRings[ringIndex];
                     const int mineIndex = minePlacementLambda( ring, resource );
                     if ( mineIndex != -1 ) {
