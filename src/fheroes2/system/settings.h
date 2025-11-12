6432300ab98f3421a57b3f1d5d83f630cdbb6933
Ruben Montes <ruben.montes.mendoza@gmail.com>
2025-11-12 12:38:24 +0100

    fix code issue

--- src/fheroes2/system/settings.h
+++ src/fheroes2/system/settings.h
@@ -30,11 +30,15 @@
 
 #include "bitmodes.h"
 #include "dir.h"
-#include "game_mode.h"
 #include "maps_fileinfo.h"
 #include "math_base.h"
 #include "players.h"
 #include "screen.h"
+
+namespace fheroes2
+{
+    enum class GameMode : int;
+}
 
 class IStreamBase;
 class OStreamBase;
