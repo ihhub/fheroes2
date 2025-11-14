ihhub/fheroes2

commit e4676f634e8a211d93af6506d3241b525562a8eb

Update FPS counter and color cycling synchronization logic (#10298)

src/engine/render_processor.cpp
@@ -24,13 +24,6 @@
 
 #include "pal.h"
 
-namespace
-{
-    // The maximum FPS the engine can handle is 125.
-    // This means that a frame should be generated only every 8 ms.
-    const uint64_t frameHalfInterval{ 4 };
-}
-
 namespace fheroes2
 {
     RenderProcessor & RenderProcessor::instance()
@@ -54,10 +47,13 @@ namespace fheroes2
             _preRenderer();
         }
 
-        if ( _cyclingTimer.getMs() < ( _cyclingInterval - frameHalfInterval ) ) {
+        const uint64_t cylingTime = _previousCyclingInterval + _cyclingTimer.getMs();
+        if ( cylingTime < ( _cyclingInterval * 2 - _frameHalfInterval ) ) {
             // If the current timer is less than cycling internal minus half of the frame generation then nothing is needed.
             return false;
         }
+
+        _previousCyclingInterval = cylingTime - _cyclingInterval;
 
         // TODO: here we need to deduct possible time difference from the current time to have consistent FPS.
         _cyclingTimer.reset();

src/engine/render_processor.h
@@ -84,7 +84,7 @@ namespace fheroes2
 
         bool isCyclingUpdateRequired() const
         {
-            return _enableCycling && _lastRenderCall.getMs() >= _cyclingInterval;
+            return _enableCycling && _cyclingTimer.getMs() + _previousCyclingInterval >= 2 * _cyclingInterval && _lastRenderCall.getMs() > _frameHalfInterval;
         }
 
     private:
@@ -96,11 +96,18 @@ namespace fheroes2
         fheroes2::Time _cyclingTimer;
         fheroes2::Time _lastRenderCall;
 
+        // This variable represents the inaccuracy of the previous color cycling rendering to be taken into account in the next one.
+        uint64_t _previousCyclingInterval{ _cyclingInterval };
+
         uint32_t _cyclingCounter{ 0 };
 
         bool _enableRenderers{ false };
         bool _enableCycling{ false };
 
         static const uint64_t _cyclingInterval{ 220 };
+
+        // Lets assume that the desynchronization of one frame is not noticeable on 60 FPS.
+        // To reduce the render calls we may do color cycling update 8 ms earlier or later.
+        static constexpr uint64_t _frameHalfInterval{ 8 };
     };
 }

src/engine/pal.cpp
@@ -170,23 +170,27 @@ namespace
 
 std::vector<uint8_t> PAL::GetCyclingPalette( const uint32_t stepId )
 {
-    std::vector<uint8_t> palette = PAL::GetPalette( PaletteType::STANDARD );
+    std::vector<uint8_t> palette = GetPalette( PaletteType::STANDARD );
 
     static const std::array<CyclingColorSet, 4> cycleSet{ CyclingColorSet{ 214, 4, false }, CyclingColorSet{ 218, 4, false }, CyclingColorSet{ 231, 5, true },
                                                           CyclingColorSet{ 238, 4, false } };
 
     for ( const CyclingColorSet & colorSet : cycleSet ) {
-        for ( uint32_t id = 0; id < colorSet.length; ++id ) {
-            uint32_t newColorID;
-            if ( colorSet.forward ) {
-                newColorID = colorSet.start + ( ( id + stepId ) % colorSet.length );
+        if ( colorSet.forward ) {
+            for ( uint32_t id = 0; id < colorSet.length; ++id ) {
+                const uint32_t newColorID = colorSet.start + ( ( id + stepId ) % colorSet.length );
+                palette[colorSet.start + id] = static_cast<uint8_t>( newColorID );
             }
-            else {
-                const uint32_t lastColorID = colorSet.length - 1;
-                newColorID = colorSet.start + lastColorID - ( ( lastColorID + stepId - id ) % colorSet.length );
-            }
+        }
+        else {
+            const uint32_t lastColorID = colorSet.length - 1;
+            const uint32_t lastColor = colorSet.start + lastColorID;
+            const uint32_t colorOffset = stepId + lastColorID;
 
-            palette[colorSet.start + id] = static_cast<uint8_t>( newColorID );
+            for ( uint32_t id = 0; id < colorSet.length; ++id ) {
+                const uint32_t newColorID = lastColor - ( ( colorOffset - id ) % colorSet.length );
+                palette[colorSet.start + id] = static_cast<uint8_t>( newColorID );
+            }
         }
     }
 

src/fheroes2/battle/battle_interface.cpp
@@ -6860,6 +6860,7 @@ void Battle::Interface::CheckGlobalEvents( LocalEvent & le )
     // Animation of the currently active unit's contour
     if ( Game::validateAnimationDelay( Game::BATTLE_SELECTED_UNIT_DELAY ) ) {
         UpdateContourColor();
+        humanturn_redraw = true;
     }
 
     // Animation of flags and heroes idle.

src/fheroes2/gui/ui_tool.cpp
@@ -277,27 +277,27 @@ namespace fheroes2
         const std::chrono::duration<double> time = endTime - _startTime;
         _startTime = endTime;
 
-        const double totalTime = time.count() * 1000.0;
-        const double fps = totalTime < 1 ? 0 : 1000 / totalTime;
+        const double totalTime = time.count();
 
-        _fps.push_front( fps );
-        while ( _fps.size() > 10 ) {
-            _fps.pop_back();
+        _delays.push_front( totalTime );
+
+        double allTime = 0;
+        for ( size_t i = 0; i < _delays.size(); ++i ) {
+            allTime += _delays[i];
+            if ( allTime > 1.0 ) {
+                // Remove all delays that exceed to one second time period.
+                _delays.resize( i + 1 );
+            }
         }
 
-        double averageFps = 0;
-        for ( const double value : _fps ) {
-            averageFps += value;
-        }
-
-        averageFps /= static_cast<double>( _fps.size() );
-        const int32_t currentFps = static_cast<int32_t>( averageFps );
+        const double averageFps = static_cast<double>( _delays.size() ) / allTime + 0.05;
+        const int32_t integerFps = static_cast<int32_t>( averageFps );
 
         info += _( ", FPS: " );
-        info += std::to_string( currentFps );
-        if ( averageFps < 10 ) {
+        info += std::to_string( integerFps );
+        if ( integerFps < 10 ) {
             info += '.';
-            info += std::to_string( static_cast<int32_t>( ( averageFps - currentFps ) * 10 ) );
+            info += std::to_string( static_cast<int32_t>( ( averageFps - integerFps ) * 10 ) );
         }
 
         _text.update( std::make_unique<fheroes2::Text>( std::move( info ), fheroes2::FontType::normalWhite() ) );

src/fheroes2/gui/ui_tool.h
@@ -212,7 +212,7 @@ namespace fheroes2
     private:
         std::chrono::time_point<std::chrono::steady_clock> _startTime;
         fheroes2::MovableText _text;
-        std::deque<double> _fps;
+        std::deque<double> _delays;
     };
 
     class TimedEventValidator : public ActionObject

commit f500ccb0ad33801bf7306b94217f606aaada1fe8

If an application becomes busy then cycling animation goes wild
regression from #10298

src/engine/render_processor.cpp
@@ -24,6 +24,13 @@
 
 #include "pal.h"
 
+namespace
+{
+    // The maximum FPS the engine can handle is 125.
+    // This means that a frame should be generated only every 8 ms.
+    const uint64_t frameHalfInterval{ 4 };
+}
+
 namespace fheroes2
 {
     RenderProcessor & RenderProcessor::instance()
@@ -47,13 +54,10 @@ namespace fheroes2
             _preRenderer();
         }
 
-        const uint64_t cylingTime = _previousCyclingInterval + _cyclingTimer.getMs();
-        if ( cylingTime < ( _cyclingInterval * 2 - _frameHalfInterval ) ) {
+        if ( _cyclingTimer.getMs() < ( _cyclingInterval - frameHalfInterval ) ) {
             // If the current timer is less than cycling internal minus half of the frame generation then nothing is needed.
             return false;
         }
-
-        _previousCyclingInterval = cylingTime - _cyclingInterval;
 
         // TODO: here we need to deduct possible time difference from the current time to have consistent FPS.
         _cyclingTimer.reset();

src/engine/render_processor.h
@@ -84,7 +84,7 @@ namespace fheroes2
 
         bool isCyclingUpdateRequired() const
         {
-            return _enableCycling && _cyclingTimer.getMs() + _previousCyclingInterval >= 2 * _cyclingInterval && _lastRenderCall.getMs() > _frameHalfInterval;
+            return _enableCycling && _lastRenderCall.getMs() >= _cyclingInterval;
         }
 
     private:
@@ -96,18 +96,11 @@ namespace fheroes2
         fheroes2::Time _cyclingTimer;
         fheroes2::Time _lastRenderCall;
 
-        // This variable represents the inaccuracy of the previous color cycling rendering to be taken into account in the next one.
-        uint64_t _previousCyclingInterval{ _cyclingInterval };
-
         uint32_t _cyclingCounter{ 0 };
 
         bool _enableRenderers{ false };
         bool _enableCycling{ false };
 
         static const uint64_t _cyclingInterval{ 220 };
-
-        // Lets assume that the desynchronization of one frame is not noticeable on 60 FPS.
-        // To reduce the render calls we may do color cycling update 8 ms earlier or later.
-        static constexpr uint64_t _frameHalfInterval{ 8 };
     };
 }