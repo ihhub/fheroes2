5fa0f0e32432f02be405167367cd19633ec41290
Sergei Ivanov (Districh) <113276641+Districh-ru@users.noreply.github.com>
2025-11-17 08:57:20 +0300

    Inline some methods in UI-related code (#10376)

--- src/fheroes2/gui/interface_border.cpp
+++ src/fheroes2/gui/interface_border.cpp
@@ -1,6 +1,6 @@
 /***************************************************************************
  *   fheroes2: https://github.com/ihhub/fheroes2                           *
- *   Copyright (C) 2019 - 2024                                             *
+ *   Copyright (C) 2019 - 2025                                             *
  *                                                                         *
  *   Free Heroes2 Engine: http://sourceforge.net/projects/fheroes2         *
  *   Copyright (C) 2009 by Andrey Afletdinov <fheroes2@gmail.com>          *
@@ -416,10 +416,6 @@ void Interface::GameBorderRedraw( const bool viewWorldMode )
         fheroes2::Blit( icnadv, srcrt.x, srcrt.y, display, dstpt.x, dstpt.y, srcrt.width, srcrt.height );
     }
 }
-
-Interface::BorderWindow::BorderWindow( const fheroes2::Rect & rt )
-    : area( rt )
-{}
 
 const fheroes2::Rect & Interface::BorderWindow::GetRect() const
 {
