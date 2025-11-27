e7df3e53d4f34870f790ee1fa657ec89fc079d64
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2025-11-27 10:23:40 +0800

    Update dialog_random_map.cpp

--- src/fheroes2/dialog/dialog_random_map.cpp
+++ src/fheroes2/dialog/dialog_random_map.cpp
@@ -46,7 +46,7 @@ namespace
     {
     public:
         HorizontalSlider() = default;
-        ~HorizontalSlider() override = default;
+        ~HorizontalSlider() = default;
         HorizontalSlider( const HorizontalSlider & ) = delete;
         HorizontalSlider & operator=( const HorizontalSlider & ) = delete;
 
