77bbdfb14a00fdfc65fb51497fbbdebd718782e5
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2025-12-04 17:03:41 +0800

    Update Android and Windows hashes (#10422)

--- script/windows/install_packages.bat
+++ script/windows/install_packages.bat
@@ -23,7 +23,7 @@
 set DST_DIR=%~dp0\..\..\VisualStudio\packages
 
 set PKG_FILE=windows.zip
-set PKG_FILE_SHA256=617A9A31B7496227112318DC57D4A4F8A757F5B2401D781DE396303288AA89A0
+set PKG_FILE_SHA256=A52CA78EA56C8DA415CDAE5311B45B96967C968D944EB85835262237C476380B
 set PKG_URL=https://github.com/fheroes2/fheroes2-prebuilt-deps/releases/download/windows-deps/%PKG_FILE%
 set PKG_TLS=[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
