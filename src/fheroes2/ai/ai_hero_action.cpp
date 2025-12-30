5ddc8cd67d908463c7869ccaf46cdf59945384d2
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2025-12-30 11:10:24 +0800

    Add an extra casting

--- src/fheroes2/ai/ai_hero_action.cpp
+++ src/fheroes2/ai/ai_hero_action.cpp
@@ -287,7 +287,7 @@ namespace
         }
 
         for ( const Spell spell : spells ) {
-            if ( hero.CanCastSpell( spell ) && hero.GetSpellPoints() >= spellMultiplier * spell.spellPoints( &hero ) ) {
+            if ( hero.CanCastSpell( spell ) && hero.GetSpellPoints() >= static_cast<uint32_t>( spellMultiplier * spell.spellPoints( &hero ) + 0.5 ) ) {
                 // Looks like this hero knows the spell and casting it won't take too many spell points.
                 // So, let's do it!
                 if ( hero.ActionSpellCast( spell ) ) {
