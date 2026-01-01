48dbb3c39d4cb7b5f8385354dcf66ee93392f8c0
Martin <pum.m@gmx.at>
2026-01-01 18:55:16 +0100

    fix: crash when line breaks after hyphen

--- src/fheroes2/gui/ui_text.cpp
+++ src/fheroes2/gui/ui_text.cpp
@@ -639,11 +639,14 @@ namespace fheroes2
                             // The '-' symbol has been found. In this case we consider everything after it as a separate word.
                             const int32_t postHyphenCharCount = static_cast<int32_t>( data - hyphenPos ) - 1;
 
-                            lineCharCount -= postHyphenCharCount;
-                            lineWidth -= getLineWidth( data - postHyphenCharCount, postHyphenCharCount, charHandler, true );
+                            // Only split at the hyphen if there are characters after it to move to the next line.
+                            if ( postHyphenCharCount > 0 ) {
+                                lineCharCount -= postHyphenCharCount;
+                                lineWidth -= getLineWidth( data - postHyphenCharCount, postHyphenCharCount, charHandler, true );
 
-                            data = hyphenPos;
-                            ++data;
+                                data = hyphenPos;
+                                ++data;
+                            }
                         }
                         else if ( firstLineOffsetX > 0 && ( textLineInfos.empty() || textLineInfos.back().offsetY == offsetY ) ) {
                             // This word was not the first in the line so we can move it to the next line.
@@ -1179,7 +1182,7 @@ namespace fheroes2
             else {
                 area.height = std::max( area.height, textArea.y - area.y + textArea.height );
             }
-
+            
             area.width += textArea.x + textArea.width;
         }
 
