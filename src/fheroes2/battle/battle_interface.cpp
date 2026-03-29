26e6a3d31449a361bbf7c87bf4048ac5526be1e5
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2026-03-29 17:54:26 +0800

    Add a TODO comment

--- src/fheroes2/battle/battle_interface.cpp
+++ src/fheroes2/battle/battle_interface.cpp
@@ -6037,6 +6037,10 @@ void Battle::Interface::_redrawActionStoneSpell( const Unit & target )
 void Battle::Interface::_redrawActionResurrectSpell( Unit & target, const Spell & spell )
 {
     if ( !target.isValid() ) {
+        // TODO: the below logic doesn't work properly for cases when a 2-hex unit is being resurrected in relation to another 2-hex unit.
+        //       This will require us to redo rendering sequence as we render from left to right and the right hex always has higher priority
+        //       over left hex.
+
         // Move the current monster to the top of a stack in graveyard if it exists there.
         Battle::Arena::GetGraveyard()->moveToLastIfPresent( &target );
 
