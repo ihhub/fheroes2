8a2e0eefe56c505c96f175fe96b25693cf50214e
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2026-03-29 09:28:00 +0800

    Update CP-1253 fonts for Greek language (#10598)

--- src/fheroes2/gui/ui_keyboard.cpp
+++ src/fheroes2/gui/ui_keyboard.cpp
@@ -387,6 +387,7 @@ namespace
         case fheroes2::SupportedLanguage::English:
         case fheroes2::SupportedLanguage::French:
         case fheroes2::SupportedLanguage::German:
+        case fheroes2::SupportedLanguage::Greek:
         case fheroes2::SupportedLanguage::Polish:
         case fheroes2::SupportedLanguage::Russian:
         case fheroes2::SupportedLanguage::Slovak:
@@ -421,6 +422,8 @@ namespace
             return { "\xC9\xC8\xC7\xC0\xC2\xCA\xCB\xCD\xCE\xCF\xDB\xDC", "AZERTYUIOP\xD4", "QSDFGHJKLM\xD9", "WXCVBN" };
         case fheroes2::SupportedLanguage::German:
             return { "QWERTZUIOP\xDC", "ASDFGHJKL\xD6\xC4", "YXCVBNM\xDF" };
+        case fheroes2::SupportedLanguage::Greek:
+            return { "\xF2\xC5\xC1\xD4\xD5\xC8\xC9\xCF\xD0", "\xC1\xD3\xC4\xD6\xC3\xC7\xCE\xCA\xCB", "\xC6\xD7\xD8\xD9\xC2\xCD\xCC" };
         case fheroes2::SupportedLanguage::Polish:
             return { "\x8C\x8F\xA3\xA5\xAF\xC6\xCA\xD1\xD3", "QWERTYUIOP", "ASDFGHJKL", "ZXCVBNM" };
         case fheroes2::SupportedLanguage::Russian:
@@ -453,6 +456,8 @@ namespace
             return { "\xE9\xE8\xE7\xE0\xE2\xEA\xEB\xED\xEE\xEF\xFB\xFC", "azertyuiop\xF4", "qsdfghjklm\xF9", "wxcvbn" };
         case fheroes2::SupportedLanguage::German:
             return { "qwertzuiop\xFC", "asdfghjkl\xF6\xE4", "yxcvbnm\xDF" };
+        case fheroes2::SupportedLanguage::Greek:
+            return { "\xF2\xE5\xF1\xF4\xF5\xE8\xE9\xEF\xF0", "\xE1\xF3\xE4\xF6\xE3\xE7\xEE\xEA\xEB", "\xE6\xF7\xF8\xF9\xE2\xED\xEC" };
         case fheroes2::SupportedLanguage::Polish:
             return { "\x9C\x9F\xB3\xB9\xBF\xE6\xEA\xF1\xF3", "qwertyuiop", "asdfghjkl", "zxcvbnm" };
         case fheroes2::SupportedLanguage::Russian:
@@ -510,6 +515,7 @@ namespace
         case fheroes2::SupportedLanguage::Belarusian:
         case fheroes2::SupportedLanguage::Danish:
         case fheroes2::SupportedLanguage::German:
+        case fheroes2::SupportedLanguage::Greek:
         case fheroes2::SupportedLanguage::Russian:
         case fheroes2::SupportedLanguage::French:
         case fheroes2::SupportedLanguage::Slovak:
@@ -722,6 +728,7 @@ namespace
         case fheroes2::SupportedLanguage::English:
         case fheroes2::SupportedLanguage::French:
         case fheroes2::SupportedLanguage::German:
+        case fheroes2::SupportedLanguage::Greek:
         case fheroes2::SupportedLanguage::Polish:
         case fheroes2::SupportedLanguage::Russian:
         case fheroes2::SupportedLanguage::Slovak:
@@ -729,6 +736,7 @@ namespace
             addExtraStandardButtons( buttons, layoutType, isEvilInterface, isExtraLanguageSupported );
             break;
         default:
+            // Did you add a new supported language? Add the value above!
             assert( 0 );
             break;
         }
