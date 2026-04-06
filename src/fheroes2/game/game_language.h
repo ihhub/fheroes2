50498ada31db90df9f8235cd4fc57cace78dcce6
Kaffeino <kaffeino@posteo.co.uk>
2026-04-06 20:55:49 +0200

    Add Esperanto translation

--- src/fheroes2/game/game_language.h
+++ src/fheroes2/game/game_language.h
@@ -40,6 +40,7 @@ namespace fheroes2
         Bulgarian,
         Danish,
         Dutch,
+        Esperanto,
         Greek,
         Hungarian,
         Norwegian,
@@ -63,5 +64,6 @@ namespace fheroes2
         CP1254,
         CP1258,
         ISO8859_16,
+	    ISO8859_3
     };
 }
