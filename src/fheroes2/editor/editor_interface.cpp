b4ee8225055c9194aa317fcf231fe8c8bfecba62
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2026-04-25 14:59:50 +0800

    Sort entries

--- src/fheroes2/editor/editor_interface.cpp
+++ src/fheroes2/editor/editor_interface.cpp
@@ -873,13 +873,13 @@ namespace
         case Maps::ObjectGroup::ADVENTURE_DWELLINGS:
         case Maps::ObjectGroup::ADVENTURE_MINES:
         case Maps::ObjectGroup::ADVENTURE_MISCELLANEOUS:
-        case Maps::ObjectGroup::LANDSCAPE_MOUNTAINS:
         case Maps::ObjectGroup::ADVENTURE_POWER_UPS:
         case Maps::ObjectGroup::ADVENTURE_TREASURES:
         case Maps::ObjectGroup::ADVENTURE_WATER:
         case Maps::ObjectGroup::KINGDOM_HEROES:
         case Maps::ObjectGroup::KINGDOM_TOWNS:
         case Maps::ObjectGroup::LANDSCAPE_MISCELLANEOUS:
+        case Maps::ObjectGroup::LANDSCAPE_MOUNTAINS:
         case Maps::ObjectGroup::LANDSCAPE_ROCKS:
         case Maps::ObjectGroup::LANDSCAPE_TREES:
         case Maps::ObjectGroup::LANDSCAPE_WATER:
