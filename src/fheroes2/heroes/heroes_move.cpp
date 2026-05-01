6357aaf8d753eb32a9b1425ee610ba47dfb927fc
Kacper <22472584+Solaire@users.noreply.github.com>
2026-05-01 04:27:17 +0100

    Make hero turning animation depend on the animation speed (#10674)

--- src/fheroes2/heroes/heroes_move.cpp
+++ src/fheroes2/heroes/heroes_move.cpp
@@ -1,6 +1,6 @@
 /***************************************************************************
  *   fheroes2: https://github.com/ihhub/fheroes2                           *
- *   Copyright (C) 2019 - 2025                                             *
+ *   Copyright (C) 2019 - 2026                                             *
  *                                                                         *
  *   Free Heroes2 Engine: http://sourceforge.net/projects/fheroes2         *
  *   Copyright (C) 2009 by Andrey Afletdinov <fheroes2@gmail.com>          *
@@ -294,6 +294,23 @@ bool Heroes::_moveStep( const bool jumpToNextTile )
 
 void Heroes::_angleStep( const int targetDirection )
 {
+    // Hero movement animation depends on the animation speed.
+    // To make the speed of the hero turning animation the same
+    // we need to skip certain frames.
+    const int speedMultiplier = isControlHuman() ? Game::HumanHeroAnimSpeedMultiplier() : Game::AIHeroAnimSpeedMultiplier();
+    assert( speedMultiplier > 0 );
+    const int32_t framesToSkip = std::clamp( 4 / speedMultiplier, 1, 3 );
+    assert( _skippedFramesForAngleStep >= 0 );
+
+    ++_skippedFramesForAngleStep;
+    if ( _skippedFramesForAngleStep < framesToSkip ) {
+        // Skip the animation change for this frame.
+        return;
+    }
+
+    // Reset turning animation delay.
+    _skippedFramesForAngleStep = 0;
+
     const bool clockwise = Direction::ShortDistanceClockWise( _direction, targetDirection );
 
     // start index
