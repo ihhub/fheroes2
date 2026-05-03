53dae1b24cf4b5e21b8b9fdcbbb9fc4ca6f19020
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2026-05-03 16:52:22 +0800

    Update the comment

--- src/engine/image.cpp
+++ src/engine/image.cpp
@@ -34,7 +34,7 @@ namespace
     // 0 in shadow part means no shadow, 1 means skip any drawings so to don't waste extra CPU cycles for ( tableId - 2 ) command we just add extra fake tables
     // Mirror palette was modified as it was containing 238, 238, 239, 240 values instead of 238, 239, 240, 241
     // !!! WARNING !!!
-    // If you modify this part of the table make proper modifications inside nonCyclingUniqueColorPos variable.
+    // If you modify "No cycle" part of the table make sure to update nonCyclingUniqueColorPos variable below (see GetPALColorId() function).
     const uint8_t transformTable[256 * 16] = {
         0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
         0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
