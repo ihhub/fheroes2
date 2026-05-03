9e9ec8339369267848d7d273559210729b337bc1
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2026-05-03 16:46:11 +0800

    Reduce application startup time
    Very noticeable on low-spec devices.

--- src/engine/image.cpp
+++ src/engine/image.cpp
@@ -21,6 +21,7 @@
 #include "image.h"
 
 #include <algorithm>
+#include <array>
 #include <cmath>
 #include <cstddef>
 #include <cstdlib>
@@ -185,6 +186,8 @@ namespace
         199, 200, 201, 202, 203, 204, 205, 206, 207, 208, 209, 214, 215, 216, 217, 218, 219, 220, 221, 222, 222, 223, 224, 225, 226, 227, 228, 229, 231,
         232, 233, 234, 235, 236, 237, 238, 239, 240, 241, 242, 242, 243, 244, 246, 247, 248, 249, 250, 251, 252, 253, 254, 255, // Mirror
 
+        // !!! WARNING !!!
+        // If you modify this part of the table make proper modifications inside nonCyclingUniqueColorPos variable.
         0,   1,   2,   3,   4,   5,   6,   7,   8,   9,   10,  11,  12,  13,  14,  15,  16,  17,  18,  19,  20,  21,  22,  23,  24,  25,  26,  27,  28,
         29,  30,  31,  32,  33,  34,  35,  36,  37,  38,  39,  40,  41,  42,  43,  44,  45,  46,  47,  48,  49,  50,  51,  52,  53,  54,  55,  56,  57,
         58,  59,  60,  61,  62,  63,  64,  65,  66,  67,  68,  69,  70,  71,  72,  73,  74,  75,  76,  77,  78,  79,  80,  81,  82,  83,  84,  85,  86,
@@ -352,8 +355,17 @@ namespace
 
             const uint8_t * gamePalette = fheroes2::getGamePalette();
 
-            constexpr uint32_t startOffset = 10;
-            const uint8_t * correctorY = transformTable + 256 * 15 + startOffset;
+            constexpr uint32_t colorCount{ 219 };
+            const std::array<uint8_t, colorCount> nonCyclingUniqueColorPos{ 10,  11,  12,  13,  14,  15,  16,  17,  18,  19,  20,  21,  22,  23,  24,  25,  26,  27,  28,
+                29,  30,  31,  32,  33,  34,  35,  36,  37,  38,  39,  40,  41,  42,  43,  44,  45,  46,  47,  48,  49,  50,  51,  52,  53,  54,  55,  56,  57,
+                58,  59,  60,  61,  62,  63,  64,  65,  66,  67,  68,  69,  70,  71,  72,  73,  74,  75,  76,  77,  78,  79,  80,  81,  82,  83,  84,  85,  86,
+                87,  88,  89,  90,  91,  92,  93,  94,  95,  96,  97,  98,  99,  100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115,
+                116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144,
+                145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173,
+                174, 175, 176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191, 192, 193, 194, 195, 196, 197, 198, 199, 200, 201, 202,
+                203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 213, 222, 223, 224, 225, 226, 227, 228, 229, 230, 236, 237, 242, 243, 244, 245 };
+
+            const uint8_t * correctorY = nonCyclingUniqueColorPos.data();
 
             for ( uint32_t id = 0; id < size; ++id ) {
                 const int32_t r = static_cast<int32_t>( id & 63 );
@@ -366,7 +378,7 @@ namespace
                 // The first 10 and the last 10 colors are undefined in the original palette. We skip them to avoid usage of these colors.
                 const uint8_t * correctorX = correctorY;
 
-                for ( uint32_t i = startOffset; i < 246; ++i, ++correctorX ) {
+                for ( uint32_t i = 0; i < colorCount; ++i, ++correctorX ) {
                     const uint8_t * palette = gamePalette + static_cast<ptrdiff_t>( *correctorX ) * 3;
 
                     const int32_t sumRed = static_cast<int32_t>( *palette ) + r;
