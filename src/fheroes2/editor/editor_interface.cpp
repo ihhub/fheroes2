ihhub/fheroes2

commit 43d2ab2bdf4f5bea6e19f466044f9311b409cd64

Add drag-to-draw ability to Roads and Streams (#10680)

src/fheroes2/editor/editor_interface.cpp
@@ -1418,6 +1418,43 @@ namespace Interface
                             _brushTiles.clear();
                         }
                     }
+                    else if ( _editorPanel.isRoadDraw() ) {
+                        if ( le.isMouseLeftButtonPressed() ) {
+                            if ( _brushTiles.count( _tileUnderCursor ) == 0 && !world.getTile( _tileUnderCursor ).isWater() ) {
+                                _brushTiles.emplace( _tileUnderCursor );
+
+                                fheroes2::ActionCreator action( _historyManager, _mapFormat );
+
+                                if ( Maps::setRoadOnTile( _mapFormat, _tileUnderCursor ) ) {
+                                    _redraw |= mapUpdateFlags;
+
+                                    action.commit();
+                                }
+                            }
+                        }
+                        else {
+                            _brushTiles.clear();
+                        }
+                    }
+
+                    else if ( _editorPanel.isStreamDraw() ) {
+                        if ( le.isMouseLeftButtonPressed() ) {
+                            if ( _brushTiles.count( _tileUnderCursor ) == 0 ) {
+                                _brushTiles.emplace( _tileUnderCursor );
+
+                                fheroes2::ActionCreator action( _historyManager, _mapFormat );
+
+                                if ( Maps::addStream( _mapFormat, tileIndex ) ) {
+                                    _redraw |= mapUpdateFlags;
+
+                                    action.commit();
+                                }
+                            }
+                        }
+                        else {
+                            _brushTiles.clear();
+                        }
+                    }
                 }
                 else if ( _areaSelectionStartTileId != -1 ) {
                     assert( _editorPanel.showAreaSelectRect() && isBrushEmpty );
@@ -2088,6 +2125,8 @@ namespace Interface
 
                 action.commit();
             }
+
+            _brushTiles.clear();
         }
         else if ( _editorPanel.isStreamDraw() ) {
             if ( tile.isWater() ) {
@@ -2104,6 +2143,8 @@ namespace Interface
 
                 action.commit();
             }
+
+            _brushTiles.clear();
         }
         else if ( _editorPanel.isEraseMode() ) {
             const fheroes2::Rect brushSize = _editorPanel.getBrushArea();

src/fheroes2/editor/editor_interface.h
@@ -1,6 +1,6 @@
 /***************************************************************************
  *   fheroes2: https://github.com/ihhub/fheroes2                           *
- *   Copyright (C) 2023 - 2025                                             *
+ *   Copyright (C) 2023 - 2026                                             *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *

src/fheroes2/editor/editor_interface_panel.h
@@ -1,6 +1,6 @@
 /***************************************************************************
  *   fheroes2: https://github.com/ihhub/fheroes2                           *
- *   Copyright (C) 2023 - 2025                                             *
+ *   Copyright (C) 2023 - 2026                                             *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
@@ -96,7 +96,8 @@ namespace Interface
 
         bool useMouseDragMovement() const
         {
-            return ( _selectedInstrument != Instrument::TERRAIN && _selectedInstrument != Instrument::ERASE );
+            return ( _selectedInstrument != Instrument::TERRAIN && _selectedInstrument != Instrument::ERASE && _selectedInstrument != Instrument::ROAD
+                     && _selectedInstrument != Instrument::STREAM );
         }
 
         // Set Editor panel positions on screen.

commit a9252563cfd31e6dc88562b751e70cebd18c37f6

Fix an assertion hit while using move / copy tool in the Editor
regression from #10680

src/fheroes2/editor/editor_interface.cpp
@@ -1731,7 +1731,10 @@ namespace Interface
                             }
                         }
                         else {
-                            if ( le.isMouseLeftButtonReleased() ) {
+                            // We need to check that the movable info is valid.
+                            // It could happen that a map maker presses left mouse button, then while holding presses right button,
+                            // and then releases the left button.
+                            if ( _movableObjectInfo.tileIndex >= 0 && le.isMouseLeftButtonReleased() ) {
                                 if ( _editorPanel.isObjectMovingMode() ) {
                                     _tryToMoveObject( _movableObjectInfo, _tileUnderCursor );
                                 }