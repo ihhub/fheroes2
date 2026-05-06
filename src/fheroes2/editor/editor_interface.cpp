ihhub/fheroes2

commit 7cd6940dfa279daee7982eeaa92e1fd90fc7fb85

Add ability to move an object within the Editor (#10684)

src/fheroes2/editor/editor_interface.h
@@ -22,6 +22,7 @@
 
 #include <cstdint>
 #include <functional>
+#include <memory>
 #include <set>
 #include <string>
 #include <utility>
@@ -31,6 +32,7 @@
 #include "history_manager.h"
 #include "interface_base.h"
 #include "map_format_info.h"
+#include "map_object_info.h"
 #include "map_random_generator.h"
 #include "timing.h"
 
@@ -39,8 +41,6 @@ enum class PlayerColor : uint8_t;
 namespace Maps
 {
     class Tile;
-
-    enum class ObjectGroup : uint8_t;
 }
 
 namespace Interface
@@ -147,6 +147,14 @@ namespace Interface
             fheroes2::Time _timer;
         };
 
+        struct MovableObjectInfo final
+        {
+            int32_t tileIndex{ -1 };
+            int32_t objectType{ -1 };
+            uint32_t objectUID{ 0 };
+            Maps::ObjectGroup groupType{ Maps::ObjectGroup::NONE };
+        };
+
         EditorInterface()
             : BaseInterface( true )
             , _editorPanel( *this )
@@ -159,14 +167,19 @@ namespace Interface
 
         bool _setObjectOnTile( Maps::Tile & tile, const Maps::ObjectGroup groupType, const int32_t objectIndex );
 
-        bool _setObjectOnTileAsAction( Maps::Tile & tile, const Maps::ObjectGroup groupType, const int32_t objectIndex );
-
         void _handleObjectMouseLeftClick( Maps::Tile & tile );
+
+        // Returns true if the placement was successful.
+        // 'action' input argument will be populated in case of success.
+        bool _tryToPlaceObject( Maps::Tile & tile, const int32_t objectType, const Maps::ObjectGroup groupType, const bool isNewObject,
+                                std::unique_ptr<fheroes2::ActionCreator> & action );
+
+        void _tryToMoveObject( const MovableObjectInfo & movableObjectInfo, const int32_t destinationTile );
 
         void _validateObjectsOnTerrainUpdate();
 
-        // Returns true if an existing object was moved.
-        bool _moveExistingObject( const int32_t tileIndex, const Maps::ObjectGroup groupType, int32_t objectIndex );
+        // Returns true if an existing object was moved on top.
+        bool _tryToMoveObjectOnTop( const int32_t tileIndex, const Maps::ObjectGroup groupType, int32_t objectIndex );
 
         void _updateObjectMetadata( const Maps::Map_Format::TileObjectInfo & object, const uint32_t newObjectUID );
 
@@ -174,12 +187,16 @@ namespace Interface
 
         bool _placeCastle( const int32_t posX, const int32_t posY, const PlayerColor color, const int32_t type );
 
+        void _resetMovableObjectInfo();
+
         EditorPanel _editorPanel;
 
         int32_t _areaSelectionStartTileId{ -1 };
         int32_t _tileUnderCursor{ -1 };
 
         std::set<int32_t> _brushTiles;
+
+        MovableObjectInfo _movableObjectInfo;
 
         Maps::Random_Generator::Configuration _randomMapConfig;
 

src/fheroes2/editor/editor_interface_panel.cpp
@@ -553,7 +553,7 @@ namespace Interface
             }
         }
         else if ( _selectedInstrument == Instrument::DETAIL ) {
-            const fheroes2::Text instrumentName( _( "Cell\nDetails" ), fheroes2::FontType::normalWhite() );
+            const fheroes2::Text instrumentName( _( "Click to edit\n\nor\n\nclick and hold to move" ), fheroes2::FontType::normalWhite() );
             instrumentName.draw( _rectInstrumentPanel.x + 5, _rectInstrumentPanel.y + 8, _rectInstrumentPanel.width - 10, display );
         }
         else if ( _selectedInstrument == Instrument::ROAD ) {
@@ -620,16 +620,25 @@ namespace Interface
             if ( _selectedLandscapeObject < 0 ) {
                 return -1;
             }
+
+            assert( static_cast<size_t>( _selectedLandscapeObject ) < _selectedLandscapeObjectType.size() );
+
             return _selectedLandscapeObjectType[_selectedLandscapeObject];
         case Instrument::ADVENTURE_OBJECTS:
             if ( _selectedAdventureObject < 0 ) {
                 return -1;
             }
+
+            assert( static_cast<size_t>( _selectedAdventureObject ) < _selectedAdventureObjectType.size() );
+
             return _selectedAdventureObjectType[_selectedAdventureObject];
         case Instrument::KINGDOM_OBJECTS:
             if ( _selectedKingdomObject < 0 ) {
                 return -1;
             }
+
+            assert( static_cast<size_t>( _selectedKingdomObject ) < _selectedKingdomObjectType.size() );
+
             return _selectedKingdomObjectType[_selectedKingdomObject];
         default:
             // Why are you trying to get type for the non-object instrument. Check your logic!
@@ -797,80 +806,88 @@ namespace Interface
     {
         switch ( _selectedInstrument ) {
         case Instrument::MONSTERS:
-            _interface.setCursorUpdater(
-                [type = getSelectedObjectType(), group = getSelectedObjectGroup()]( const int32_t /*tileIndex*/ ) { setCustomCursor( group, type ); } );
+            setObjectBasedCursor( getSelectedObjectType(), getSelectedObjectGroup() );
             return;
         case Instrument::LANDSCAPE_OBJECTS:
-            switch ( _selectedLandscapeObject ) {
-            case LandscapeObjectBrush::LANDSCAPE_MISC:
-            case LandscapeObjectBrush::MOUNTAINS:
-            case LandscapeObjectBrush::ROCKS:
-            case LandscapeObjectBrush::TREES:
-            case LandscapeObjectBrush::WATER_OBJECTS:
-                _interface.setCursorUpdater(
-                    [type = getSelectedObjectType(), group = getSelectedObjectGroup()]( const int32_t /*tileIndex*/ ) { setCustomCursor( group, type ); } );
+            if ( _selectedLandscapeObject > -1 ) {
+                setObjectBasedCursor( getSelectedObjectType(), getSelectedObjectGroup() );
                 return;
-            default:
-                break;
             }
             break;
         case Instrument::ADVENTURE_OBJECTS:
-            switch ( _selectedAdventureObject ) {
-            case AdventureObjectBrush::ADVENTURE_MISC:
-            case AdventureObjectBrush::ARTIFACTS:
-            case AdventureObjectBrush::DWELLINGS:
-            case AdventureObjectBrush::MINES:
-            case AdventureObjectBrush::POWER_UPS:
-            case AdventureObjectBrush::TREASURES:
-            case AdventureObjectBrush::WATER_ADVENTURE:
-                _interface.setCursorUpdater(
-                    [type = getSelectedObjectType(), group = getSelectedObjectGroup()]( const int32_t /*tileIndex*/ ) { setCustomCursor( group, type ); } );
+            if ( _selectedAdventureObject > -1 ) {
+                setObjectBasedCursor( getSelectedObjectType(), getSelectedObjectGroup() );
                 return;
-            default:
-                break;
             }
             break;
         case Instrument::KINGDOM_OBJECTS:
-            switch ( _selectedKingdomObject ) {
-            case KingdomObjectBrush::HEROES:
-                _interface.setCursorUpdater( [type = getSelectedObjectType()]( const int32_t /*tileIndex*/ ) {
-                    if ( type == -1 ) {
-                        // The object type is not set. We show the POINTER cursor for this case.
-                        Cursor::Get().SetThemes( Cursor::POINTER );
-                        return;
-                    }
-
-                    // TODO: render ICN::MINIHERO from the existing hero images.
-                    const fheroes2::Sprite & image = fheroes2::AGG::GetICN( ICN::MINIHERO, type );
-
-                    // Mini-hero images contain a pole with a flag.
-                    // This causes a situation that a selected tile does not properly correspond to current position of the cursor.
-                    // We need to add a hardcoded correction.
-                    const int32_t heroCorrectionY{ 12 };
-                    Cursor::Get().setCustomImage( image, { -image.width() / 2, -image.height() / 2 - heroCorrectionY } );
-                } );
+            if ( _selectedKingdomObject > -1 ) {
+                setObjectBasedCursor( getSelectedObjectType(), getSelectedObjectGroup() );
                 return;
-            case KingdomObjectBrush::TOWNS:
-                _interface.setCursorUpdater( [this]( const int32_t tileIndex ) {
-                    int32_t type = -1;
-                    int32_t color = -1;
-                    getTownObjectProperties( type, color );
-
-                    if ( type == -1 || color == -1 ) {
-                        // The object type is not set. We show the POINTER cursor for this case.
-                        Cursor::Get().SetThemes( Cursor::POINTER );
-                        return;
-                    }
-
-                    const fheroes2::Sprite & image = fheroes2::generateTownObjectImage( type, color, world.getTile( tileIndex ).GetGround() );
-
-                    Cursor::Get().setCustomImage( image, { image.x(), image.y() } );
-                } );
-                return;
-            default:
-                break;
             }
             break;
+        default:
+            break;
+        }
+
+        _interface.setCursorUpdater( {} );
+    }
+
+    void EditorPanel::setObjectBasedCursor( const int32_t type, const Maps::ObjectGroup group )
+    {
+        if ( type < 0 ) {
+            // The object type is not set. We show the POINTER cursor for this case.
+            Cursor::Get().SetThemes( Cursor::POINTER );
+            _interface.setCursorUpdater( {} );
+            return;
+        }
+
+        switch ( group ) {
+        case Maps::ObjectGroup::ADVENTURE_ARTIFACTS:
+        case Maps::ObjectGroup::ADVENTURE_DWELLINGS:
+        case Maps::ObjectGroup::ADVENTURE_MINES:
+        case Maps::ObjectGroup::ADVENTURE_MISCELLANEOUS:
+        case Maps::ObjectGroup::ADVENTURE_POWER_UPS:
+        case Maps::ObjectGroup::ADVENTURE_TREASURES:
+        case Maps::ObjectGroup::ADVENTURE_WATER:
+        case Maps::ObjectGroup::LANDSCAPE_MISCELLANEOUS:
+        case Maps::ObjectGroup::LANDSCAPE_MOUNTAINS:
+        case Maps::ObjectGroup::LANDSCAPE_ROCKS:
+        case Maps::ObjectGroup::LANDSCAPE_TREES:
+        case Maps::ObjectGroup::LANDSCAPE_WATER:
+        case Maps::ObjectGroup::MONSTERS:
+            _interface.setCursorUpdater( [group, type]( const int32_t /*tileIndex*/ ) { setCustomCursor( group, type ); } );
+            return;
+        case Maps::ObjectGroup::KINGDOM_HEROES:
+            _interface.setCursorUpdater( [type]( const int32_t /*tileIndex*/ ) {
+                // TODO: render ICN::MINIHERO from the existing hero images.
+                const fheroes2::Sprite & image = fheroes2::AGG::GetICN( ICN::MINIHERO, type );
+
+                // Mini-hero images contain a pole with a flag.
+                // This causes a situation that a selected tile does not properly correspond to current position of the cursor.
+                // We need to add a hardcoded correction.
+                constexpr int32_t heroCorrectionY{ 12 };
+                Cursor::Get().setCustomImage( image, { -image.width() / 2, -image.height() / 2 - heroCorrectionY } );
+            } );
+            return;
+        case Maps::ObjectGroup::KINGDOM_TOWNS: {
+            int32_t townType = -1;
+            int32_t color = -1;
+            getTownObjectProperties( type, townType, color );
+
+            _interface.setCursorUpdater( [townType, color]( const int32_t tileIndex ) {
+                if ( townType == -1 || color == -1 ) {
+                    // The object type is not set. We show the POINTER cursor for this case.
+                    Cursor::Get().SetThemes( Cursor::POINTER );
+                    return;
+                }
+
+                const fheroes2::Sprite & image = fheroes2::generateTownObjectImage( townType, color, world.getTile( tileIndex ).GetGround() );
+
+                Cursor::Get().setCustomImage( image, { image.x(), image.y() } );
+            } );
+            return;
+        }
         default:
             break;
         }
@@ -1140,10 +1157,10 @@ namespace Interface
                     int32_t type = -1;
                     int32_t color = -1;
 
-                    getTownObjectProperties( type, color );
+                    getTownObjectProperties( getSelectedObjectType(), type, color );
                     Dialog::selectTownType( type, color );
 
-                    return _generateTownObjectProperties( type, color );
+                    return generateTownObjectProperties( type, color );
                 } );
                 return res;
             }
@@ -1213,7 +1230,7 @@ namespace Interface
                                                Dialog::ZERO );
         }
         else if ( le.isMouseRightButtonPressedInArea( _instrumentButtonsRect[Instrument::DETAIL] ) ) {
-            fheroes2::showStandardTextMessage( _( "Detail Mode" ), _( "Used for special editing of action objects." ), Dialog::ZERO );
+            fheroes2::showStandardTextMessage( _( "Detail Mode" ), _( "Used for moving or customizing objects." ), Dialog::ZERO );
         }
         else if ( le.isMouseRightButtonPressedInArea( _instrumentButtonsRect[Instrument::ADVENTURE_OBJECTS] ) ) {
             fheroes2::showStandardTextMessage( _( "Adventure Objects Mode" ),
@@ -1285,7 +1302,7 @@ namespace Interface
         _interface.updateCursor( _interface.getGameArea().GetValidTileIdFromPoint( LocalEvent::Get().getMouseCursorPos() ) );
     }
 
-    void EditorPanel::getTownObjectProperties( int32_t & type, int32_t & color ) const
+    void EditorPanel::getTownObjectProperties( const int32_t packedType, int32_t & type, int32_t & color )
     {
         const auto & townObjects = Maps::getObjectsByGroup( Maps::ObjectGroup::KINGDOM_TOWNS );
         if ( townObjects.empty() ) {
@@ -1296,11 +1313,11 @@ namespace Interface
             return;
         }
 
-        type = _selectedKingdomObjectType[KingdomObjectBrush::TOWNS] % static_cast<int32_t>( townObjects.size() );
-        color = _selectedKingdomObjectType[KingdomObjectBrush::TOWNS] / static_cast<int32_t>( townObjects.size() );
+        type = packedType % static_cast<int32_t>( townObjects.size() );
+        color = packedType / static_cast<int32_t>( townObjects.size() );
     }
 
-    int32_t EditorPanel::_generateTownObjectProperties( const int32_t type, const int32_t color )
+    int32_t EditorPanel::generateTownObjectProperties( const int32_t type, const int32_t color )
     {
         const auto & townObjects = Maps::getObjectsByGroup( Maps::ObjectGroup::KINGDOM_TOWNS );
         if ( townObjects.empty() ) {

src/fheroes2/editor/editor_interface_panel.h
@@ -96,8 +96,17 @@ namespace Interface
 
         bool useMouseDragMovement() const
         {
-            return ( _selectedInstrument != Instrument::TERRAIN && _selectedInstrument != Instrument::ERASE && _selectedInstrument != Instrument::ROAD
-                     && _selectedInstrument != Instrument::STREAM );
+            switch ( _selectedInstrument ) {
+            case Instrument::ADVENTURE_OBJECTS:
+            case Instrument::KINGDOM_OBJECTS:
+            case Instrument::LANDSCAPE_OBJECTS:
+            case Instrument::MONSTERS:
+                return true;
+            default:
+                break;
+            }
+
+            return false;
         }
 
         // Set Editor panel positions on screen.
@@ -116,11 +125,15 @@ namespace Interface
 
         Maps::ObjectGroup getSelectedObjectGroup() const;
 
-        void getTownObjectProperties( int32_t & type, int32_t & color ) const;
-
         static const char * getObjectGroupName( const Maps::ObjectGroup groupName );
 
         void updateUndoRedoButtonsStates( const bool isUndoAvailable, const bool isRedoAvailable );
+
+        void setObjectBasedCursor( const int32_t type, const Maps::ObjectGroup group );
+
+        static void getTownObjectProperties( const int32_t packedType, int32_t & type, int32_t & color );
+
+        static int32_t generateTownObjectProperties( const int32_t type, const int32_t color );
 
     private:
         static int _getGroundId( const uint8_t brushId );
@@ -134,8 +147,6 @@ namespace Interface
         static const char * _getAdventureObjectTypeName( const uint8_t brushId );
         static const char * _getKingdomObjectTypeName( const uint8_t brushId );
         static const char * _getEraseObjectTypeName( const uint32_t eraseObjectType );
-
-        static int32_t _generateTownObjectProperties( const int32_t type, const int32_t color );
 
         void _setCursor();
 

src/fheroes2/heroes/heroes.cpp
@@ -1300,7 +1300,7 @@ void Heroes::SetVisited( const int32_t tileIndex, const Visit::Type type /* = Vi
 
     // An object could be bigger than 1 tile so we need to check all its tiles.
     constexpr int32_t searchDist = []() constexpr {
-        constexpr int32_t max = std::max( Maps::maxActionObjectDimensions.width, Maps::maxActionObjectDimensions.height );
+        constexpr int32_t max = std::max( Maps::maxActionGroundObjectDimensions.width, Maps::maxActionGroundObjectDimensions.height );
         static_assert( max > 0 );
 
         return max - 1;
@@ -1342,7 +1342,7 @@ void Heroes::setVisitedForAllies( const int32_t tileIndex ) const
 
     // An object could be bigger than 1 tile so we need to check all its tiles.
     constexpr int32_t searchDist = []() constexpr {
-        constexpr int32_t max = std::max( Maps::maxActionObjectDimensions.width, Maps::maxActionObjectDimensions.height );
+        constexpr int32_t max = std::max( Maps::maxActionGroundObjectDimensions.width, Maps::maxActionGroundObjectDimensions.height );
         static_assert( max > 0 );
 
         return max - 1;

src/fheroes2/maps/map_object_info.cpp
@@ -6156,24 +6156,45 @@ namespace
         }
 
         // Calculate the maximum possible dimensions of the area occupied by the parts of multi-tile action objects and verify them.
+        fheroes2::Size maxActionObjDim{ 1, 1 };
         fheroes2::Size maxObjDim{ 1, 1 };
 
         for ( const auto & objects : objectData ) {
             for ( const auto & objectInfo : objects ) {
-                if ( !MP2::isOffGameActionObject( objectInfo.objectType ) ) {
-                    continue;
+                if ( MP2::isOffGameActionObject( objectInfo.objectType ) ) {
+                    fheroes2::Point minOffset;
+                    fheroes2::Point maxOffset;
+
+                    for ( const auto & info : objectInfo.groundLevelParts ) {
+                        if ( info.objectType == objectInfo.objectType ) {
+                            minOffset.x = std::min( minOffset.x, info.tileOffset.x );
+                            minOffset.y = std::min( minOffset.y, info.tileOffset.y );
+                            maxOffset.x = std::max( maxOffset.x, info.tileOffset.x );
+                            maxOffset.y = std::max( maxOffset.y, info.tileOffset.y );
+                        }
+                    }
+
+                    const fheroes2::Size objDim{ maxOffset.x - minOffset.x + 1, maxOffset.y - minOffset.y + 1 };
+
+                    maxActionObjDim.width = std::max( maxActionObjDim.width, objDim.width );
+                    maxActionObjDim.height = std::max( maxActionObjDim.height, objDim.height );
                 }
 
                 fheroes2::Point minOffset;
                 fheroes2::Point maxOffset;
 
                 for ( const auto & info : objectInfo.groundLevelParts ) {
-                    if ( info.objectType == objectInfo.objectType ) {
-                        minOffset.x = std::min( minOffset.x, info.tileOffset.x );
-                        minOffset.y = std::min( minOffset.y, info.tileOffset.y );
-                        maxOffset.x = std::max( maxOffset.x, info.tileOffset.x );
-                        maxOffset.y = std::max( maxOffset.y, info.tileOffset.y );
-                    }
+                    minOffset.x = std::min( minOffset.x, info.tileOffset.x );
+                    minOffset.y = std::min( minOffset.y, info.tileOffset.y );
+                    maxOffset.x = std::max( maxOffset.x, info.tileOffset.x );
+                    maxOffset.y = std::max( maxOffset.y, info.tileOffset.y );
+                }
+
+                for ( const auto & info : objectInfo.topLevelParts ) {
+                    minOffset.x = std::min( minOffset.x, info.tileOffset.x );
+                    minOffset.y = std::min( minOffset.y, info.tileOffset.y );
+                    maxOffset.x = std::max( maxOffset.x, info.tileOffset.x );
+                    maxOffset.y = std::max( maxOffset.y, info.tileOffset.y );
                 }
 
                 const fheroes2::Size objDim{ maxOffset.x - minOffset.x + 1, maxOffset.y - minOffset.y + 1 };
@@ -6185,7 +6206,8 @@ namespace
 
         // If this assertion blows up then some in-game logic might break. If a new object of a non-standard
         // size has been added, consider updating the maximum action object dimensions.
-        assert( maxObjDim == Maps::maxActionObjectDimensions );
+        assert( maxActionObjDim == Maps::maxActionGroundObjectDimensions );
+        assert( maxObjDim == Maps::maxObjectDimensions );
 #endif
 
         isPopulated = true;

src/fheroes2/maps/map_object_info.h
@@ -1,6 +1,6 @@
 /***************************************************************************
  *   fheroes2: https://github.com/ihhub/fheroes2                           *
- *   Copyright (C) 2023 - 2025                                             *
+ *   Copyright (C) 2023 - 2026                                             *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
@@ -30,8 +30,11 @@
 
 namespace Maps
 {
-    // The maximum possible dimensions (width and height) for an action object on the map.
-    constexpr fheroes2::Size maxActionObjectDimensions{ 4, 2 };
+    // The maximum possible dimensions (width and height) for an action ground object on the map.
+    constexpr fheroes2::Size maxActionGroundObjectDimensions{ 4, 2 };
+
+    // The maximum object dimensions possible, including top and bottom parts.
+    constexpr fheroes2::Size maxObjectDimensions{ 8, 5 };
 
     // An object usually contains of multiple parts / tiles. Each part has its own features like object layer type or image index.
     // An object always contains a main object part.

commit 3703255c704362141f83543ca05f44f89876f7af

Fix any issues with object moving mode while map makers try to do other actions
regression from #10684

src/fheroes2/editor/editor_interface.cpp
@@ -1279,6 +1279,11 @@ namespace Interface
 
         const fheroes2::Rect newRoi = _gameArea.GetROI();
 
+        _areaSelectionStartTileId = -1;
+        _tileUnderCursor = -1;
+        _brushTiles.clear();
+        _movableObjectInfo = {};
+
         if ( prevRoi == fheroes2::Rect{} ) {
             // This is the first initialization of the game area for the Editor.
             // Make the top-left corner of the first tile to be at the top-left corner of the shown game area.
@@ -1627,6 +1632,17 @@ namespace Interface
                 const bool isBrushEmpty = ( _editorPanel.getBrushArea() == fheroes2::Rect() );
 
                 if ( isValidTile ) {
+                    if ( !_editorPanel.isObjectMovingMode() && !_editorPanel.isObjectCopyingMode() ) {
+                        // There could be the case when a map maker does very weird stuff:
+                        // - start moving an object
+                        // - move the cursor to Interface Panel
+                        // - switch to another brush
+                        // - switch back to move or copy brush
+                        // - clicks on the map
+                        // The state of the map might be different.
+                        _resetMovableObjectInfo();
+                    }
+
                     const int32_t tileIndex = tilePos.y * world.w() + tilePos.x;
                     if ( _tileUnderCursor != tileIndex ) {
                         _tileUnderCursor = tileIndex;

src/fheroes2/editor/editor_interface.h
@@ -78,6 +78,9 @@ namespace Interface
 
         void undoAction()
         {
+            // Movable object state must be reset if a user tries to do another action at the same time.
+            _resetMovableObjectInfo();
+
             if ( _historyManager.undo() ) {
                 _redraw |= ( REDRAW_GAMEAREA | REDRAW_RADAR );
             }
@@ -85,6 +88,9 @@ namespace Interface
 
         void redoAction()
         {
+            // Movable object state must be reset if a user tries to do another action at the same time.
+            _resetMovableObjectInfo();
+
             if ( _historyManager.redo() ) {
                 _redraw |= ( REDRAW_GAMEAREA | REDRAW_RADAR );
             }