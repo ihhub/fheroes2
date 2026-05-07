bc2f3287ff25f674cafe07f1173b73bb9545754e
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2026-05-07 23:12:19 +0800

    Fix misleading code which might not work for some platforms
    Just to make sure to have a safe code everywhere.

--- src/fheroes2/castle/castle.cpp
+++ src/fheroes2/castle/castle.cpp
@@ -2294,8 +2294,6 @@ void AllCastles::AddCastle( std::unique_ptr<Castle> && castle )
 {
     assert( castle );
 
-    const fheroes2::Point & center = castle->GetCenter();
-
     _castles.emplace_back( std::move( castle ) );
 
     /* Register position of all castle elements on the map
@@ -2315,6 +2313,8 @@ void AllCastles::AddCastle( std::unique_ptr<Castle> && castle )
 
     // Castles are added from top to bottom, from left to right.
     // Tiles containing castle ID cannot be overwritten.
+
+    const fheroes2::Point & center = _castles.back()->GetCenter();
 
     for ( int32_t y = -2; y <= 1; ++y ) {
         for ( int32_t x = -2; x <= 2; ++x ) {
