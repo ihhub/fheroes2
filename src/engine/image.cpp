ae1e23eb25e0309571278416347504de4825b802
Sergei Ivanov (Districh) <districh@ya.ru>
2026-05-07 20:13:04 +0300

    Use PAL::PaletteType::STANDARD for game palette color indices in ApplyAlpha()

--- src/engine/image.cpp
+++ src/engine/image.cpp
@@ -28,6 +28,7 @@
 #include <cstring>
 
 #include "image_palette.h"
+#include "pal.h"
 
 namespace
 {
@@ -1131,31 +1132,15 @@ namespace fheroes2
     {
         std::vector<uint8_t> palette( 256 );
 
-        // Use the special palette that includes all original palette colors including cycling colors
-        // and extra linked non-cycling virtual copies of some unique cycling colors:
-        // - water cycling colors: link 231, 232, 233 and 235 colors to 246 - 249 positions (color 234 has already non-cycling copy at 236),
-        // - lava cycling colors: link 214 - 217 colors to 250 - 253 position.
-        // The first 10 colors are undefined in the original palette. Colors 254 and 255 are also unused.
-        constexpr uint32_t colorCount{ 244 };
-        const std::array<uint8_t, colorCount> originalPaletteColorPos{ 10,  11,  12,  13,  14,  15,  16,  17,  18,  19,  20,  21,  22,  23,  24,  25,  26,  27,  28,
-                                                                       29,  30,  31,  32,  33,  34,  35,  36,  37,  38,  39,  40,  41,  42,  43,  44,  45,  46,  47,
-                                                                       48,  49,  50,  51,  52,  53,  54,  55,  56,  57,  58,  59,  60,  61,  62,  63,  64,  65,  66,
-                                                                       67,  68,  69,  70,  71,  72,  73,  74,  75,  76,  77,  78,  79,  80,  81,  82,  83,  84,  85,
-                                                                       86,  87,  88,  89,  90,  91,  92,  93,  94,  95,  96,  97,  98,  99,  100, 101, 102, 103, 104,
-                                                                       105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123,
-                                                                       124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142,
-                                                                       143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161,
-                                                                       162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180,
-                                                                       181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191, 192, 193, 194, 195, 196, 197, 198, 199,
-                                                                       200, 201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218,
-                                                                       219, 220, 221, 222, 223, 224, 225, 226, 227, 228, 229, 230, 231, 232, 233, 234, 235, 236, 237,
-                                                                       238, 239, 240, 241, 242, 243, 244, 245, 231, 232, 233, 235, 214, 215, 216, 217 };
-
         const uint8_t * gamePalette = getGamePalette();
-        const uint8_t * corrector = originalPaletteColorPos.data();
 
-        for ( uint32_t i = 0; i < colorCount; ++i ) {
-            const uint8_t * value = gamePalette + static_cast<ptrdiff_t>( corrector[i] ) * 3;
+        // Use `standardPalette` that includes all original palette colors including cycling colors
+        // and extra linked non-cycling virtual copies of some unique cycling colors.
+        const std::vector<uint8_t> & standardPalette = PAL::GetPalette( PAL::PaletteType::STANDARD );
+
+        // The first 10 colors are undefined in the original palette. Colors 254 and 255 are also unused in `standardPalette`.
+        for ( uint32_t i = 10; i < 254; ++i ) {
+            const uint8_t * value = gamePalette + static_cast<ptrdiff_t>( standardPalette[i] ) * 3;
 
             const uint32_t red = static_cast<uint32_t>( *value ) * alpha / 255;
             ++value;
@@ -1163,7 +1148,7 @@ namespace fheroes2
             ++value;
             const uint32_t blue = static_cast<uint32_t>( *value ) * alpha / 255;
             ++value;
-            palette[i + 10] = GetPALColorId( static_cast<uint8_t>( red ), static_cast<uint8_t>( green ), static_cast<uint8_t>( blue ) );
+            palette[i] = GetPALColorId( static_cast<uint8_t>( red ), static_cast<uint8_t>( green ), static_cast<uint8_t>( blue ) );
         }
 
         ApplyPalette( in, inX, inY, out, outX, outY, width, height, palette );
