ihhub/fheroes2

commit 36e416bcf4a1d424d5b82ad29ba2dd8d6c856103

Speed up object evaluation for AI on the Adventure Map (#10797)

src/fheroes2/ai/ai_common.cpp
@@ -1,6 +1,6 @@
 /***************************************************************************
  *   fheroes2: https://github.com/ihhub/fheroes2                           *
- *   Copyright (C) 2020 - 2025                                             *
+ *   Copyright (C) 2020 - 2026                                             *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
@@ -39,22 +39,20 @@
 #include "color.h"
 #include "difficulty.h"
 #include "game.h"
+#include "game_static.h"
 #include "heroes.h"
 #include "kingdom.h"
 #include "logging.h"
 #include "maps.h"
 #include "maps_tiles.h"
+#include "maps_tiles_helper.h"
+#include "mp2.h"
 #include "payment.h"
 #include "players.h"
 #include "puzzle.h"
 #include "resource.h"
 #include "resource_trading.h"
 #include "world.h"
-
-namespace MP2
-{
-    enum MapObjectType : uint16_t;
-}
 
 bool AI::BuildIfPossible( Castle & castle, const BuildingType building )
 {
@@ -435,4 +433,94 @@ bool AI::isUltimateArtifactAvailableToHero( const UltimateArtifact & art, const 
     const int32_t idx = art.getPosition();
 
     return Maps::isValidAbsIndex( idx ) && world.getTile( idx ).isSuitableForUltimateArtifact();
+}
+
+bool AI::isValuableAdventureMapObject( const Kingdom & kingdom, const MP2::MapObjectType objectType, const int32_t tileIndex )
+{
+    if ( !MP2::isInGameActionObject( objectType ) ) {
+        // Only action objects have any value.
+        return false;
+    }
+
+    switch ( objectType ) {
+    case MP2::OBJ_CASTLE: {
+        const Castle * castle = world.getCastleEntrance( Maps::GetPoint( tileIndex ) );
+        if ( castle == nullptr ) {
+            // How is it possible that a castle does not exist?
+            assert( 0 );
+            return false;
+        }
+
+        if ( kingdom.GetColor() == castle->GetColor() ) {
+            return true;
+        }
+
+        // We can interact only with enemy castles.
+        return !Players::isFriends( kingdom.GetColor(), static_cast<PlayerColorsSet>( castle->GetColor() ) );
+    }
+    case MP2::OBJ_HERO: {
+        const Heroes * otherHero = world.getTile( tileIndex ).getHero();
+        if ( otherHero == nullptr ) {
+            // How is it possible that a hero does not exist?
+            assert( 0 );
+            return false;
+        }
+
+        if ( kingdom.GetColor() == otherHero->GetColor() ) {
+            return true;
+        }
+
+        // We can interact only with enemy heroes.
+        return !Players::isFriends( kingdom.GetColor(), static_cast<PlayerColorsSet>( otherHero->GetColor() ) );
+    }
+    case MP2::OBJ_MAGELLANS_MAPS:
+        // Magellan's Maps object should be visited only once.
+        return !kingdom.isVisited( objectType );
+    case MP2::OBJ_ALCHEMIST_LAB:
+    case MP2::OBJ_LIGHTHOUSE:
+    case MP2::OBJ_MINE:
+    case MP2::OBJ_SAWMILL:
+        // Heroes cannot re-capture their own or ally's objects.
+        return !Players::isFriends( kingdom.GetColor(), static_cast<PlayerColorsSet>( Maps::getColorFromTile( world.getTile( tileIndex ) ) ) );
+    case MP2::OBJ_DAEMON_CAVE:
+    case MP2::OBJ_LEAN_TO:
+    case MP2::OBJ_SKELETON:
+    case MP2::OBJ_WAGON:
+        // Single visit objects with no resources have no value.
+        return Maps::doesTileContainValuableItems( world.getTile( tileIndex ) );
+    case MP2::OBJ_OBSERVATION_TOWER: {
+        // If Observation Tower uncovers nothing, then we skip it.
+        const int32_t fogCountToUncoverByTower
+            = Maps::getFogTileCountToBeRevealed( tileIndex, GameStatic::getFogDiscoveryDistance( GameStatic::FogDiscoveryType::OBSERVATION_TOWER ), kingdom.GetColor() );
+
+        return fogCountToUncoverByTower > 0;
+    }
+    case MP2::OBJ_OBELISK:
+        // An obelisk should be visited only once.
+        return !kingdom.isVisited( tileIndex, objectType );
+    case MP2::OBJ_TRAVELLER_TENT:
+        // Traveller's Tent should be visited only once.
+        return !kingdom.isTravellerTentVisited( Maps::getBarrierColorFromTile( world.getTile( tileIndex ) ) );
+    case MP2::OBJ_DERELICT_SHIP:
+    case MP2::OBJ_GRAVEYARD:
+    case MP2::OBJ_PYRAMID:
+    case MP2::OBJ_SHIPWRECK:
+        // Single visit objects with no resources have no value.
+        return !kingdom.isVisited( tileIndex, objectType ) && Maps::doesTileContainValuableItems( world.getTile( tileIndex ) );
+    case MP2::OBJ_HUT_OF_MAGI:
+        // A Hut of Magi should uncover all areas at once so visiting any other similar object makes no sense.
+        return !kingdom.isVisited( objectType ) && Maps::doesObjectExistOnMap( MP2::OBJ_EYE_OF_MAGI );
+    case MP2::OBJ_BOAT:
+    case MP2::OBJ_EYE_OF_MAGI:
+    case MP2::OBJ_ORACLE:
+    case MP2::OBJ_SIGN:
+    case MP2::OBJ_STONE_LITHS:
+    case MP2::OBJ_WHIRLPOOL:
+        // These objects aren't considered valuable.
+        return false;
+    default:
+        break;
+    }
+
+    return true;
 }

src/fheroes2/ai/ai_common.h
@@ -1,6 +1,6 @@
 /***************************************************************************
  *   fheroes2: https://github.com/ihhub/fheroes2                           *
- *   Copyright (C) 2024 - 2025                                             *
+ *   Copyright (C) 2024 - 2026                                             *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
@@ -32,6 +32,11 @@ class UltimateArtifact;
 struct Funds;
 
 enum BuildingType : uint32_t;
+
+namespace MP2
+{
+    enum MapObjectType : uint16_t;
+}
 
 namespace AI
 {
@@ -72,4 +77,7 @@ namespace AI
     // the Ultimate Artifact is considered available to the given hero if this hero knows its exact location and there
     // is a free slot in the hero's artifact bag. See the implementation for details.
     bool isUltimateArtifactAvailableToHero( const UltimateArtifact & art, const Heroes & hero );
+
+    // Returns true if the AI has some benefit of visiting this object under any circumstances.
+    bool isValuableAdventureMapObject( const Kingdom & kingdom, const MP2::MapObjectType objectType, const int32_t tileIndex );
 }

src/fheroes2/ai/ai_hero_action.cpp
@@ -1715,7 +1715,7 @@ namespace
         Maps::Tile & tile = world.getTile( dst_index );
         const Kingdom & kingdom = hero.GetKingdom();
 
-        if ( kingdom.IsVisitTravelersTent( getBarrierColorFromTile( tile ) ) ) {
+        if ( kingdom.isTravellerTentVisited( getBarrierColorFromTile( tile ) ) ) {
             removeMainObjectFromTile( tile );
             resetObjectMetadata( tile );
         }
@@ -1728,7 +1728,7 @@ namespace
         const Maps::Tile & tile = world.getTile( dst_index );
         Kingdom & kingdom = hero.GetKingdom();
 
-        kingdom.SetVisitTravelersTent( getBarrierColorFromTile( tile ) );
+        kingdom.markTravellerTentVisited( getBarrierColorFromTile( tile ) );
     }
 
     void AIToShipwreckSurvivor( Heroes & hero, const MP2::MapObjectType objectType, int32_t dst_index )

src/fheroes2/ai/ai_planner.cpp
@@ -27,6 +27,7 @@
 #include <utility>
 #include <vector>
 
+#include "ai_common.h"
 #include "army.h"
 #include "heroes.h"
 #include "kingdom.h"
@@ -50,7 +51,7 @@ void AI::Planner::revealFog( const Maps::Tile & tile, const Kingdom & kingdom )
         return;
     }
 
-    updateMapActionObjectCache( tile.GetIndex() );
+    updateMapActionObjectCache( kingdom, tile.GetIndex() );
     updatePriorityAttackTarget( kingdom, tile );
 
     // If this is an action object and one of AI heroes is moving,
@@ -171,11 +172,11 @@ double AI::Planner::getFundsValueBasedOnPriority( const Funds & funds ) const
     return value;
 }
 
-void AI::Planner::updateMapActionObjectCache( const int mapIndex )
+void AI::Planner::updateMapActionObjectCache( const Kingdom & kingdom, const int mapIndex )
 {
     const MP2::MapObjectType objectType = world.getTile( mapIndex ).getMainObjectType();
 
-    if ( !MP2::isInGameActionObject( objectType ) ) {
+    if ( !isValuableAdventureMapObject( kingdom, objectType, mapIndex ) ) {
         _mapActionObjects.erase( mapIndex );
 
         return;

src/fheroes2/ai/ai_planner.h
@@ -233,7 +233,7 @@ namespace AI
         bool purchaseNewHeroes( const std::vector<AICastle> & sortedCastleList, const std::set<int> & castlesInDanger, const int32_t availableHeroCount,
                                 const bool moreTasksForHeroes );
 
-        void updateMapActionObjectCache( const int mapIndex );
+        void updateMapActionObjectCache( const Kingdom & kingdom, const int mapIndex );
 
         std::set<int> findCastlesInDanger( const Kingdom & kingdom );
 

src/fheroes2/ai/ai_planner_hero.cpp
@@ -350,7 +350,7 @@ namespace
             return !hero.isObjectTypeVisited( objectType ) && hero.GetMorale() < Morale::BLOOD && !army.AllTroopsAreUndead();
 
         case MP2::OBJ_MAGELLANS_MAPS:
-            return !hero.isObjectTypeVisited( objectType, Visit::GLOBAL ) && kingdom.AllowPayment( PaymentConditions::getMagellansMapsPurchasePrice() );
+            return !kingdom.isVisited( objectType ) && kingdom.AllowPayment( PaymentConditions::getMagellansMapsPurchasePrice() );
 
         case MP2::OBJ_ALCHEMIST_LAB:
         case MP2::OBJ_LIGHTHOUSE:
@@ -415,13 +415,13 @@ namespace
         }
 
         case MP2::OBJ_OBELISK:
-            return !kingdom.isVisited( tile );
+            return !kingdom.isVisited( index, objectType );
 
         case MP2::OBJ_BARRIER:
-            return kingdom.IsVisitTravelersTent( getBarrierColorFromTile( tile ) );
+            return kingdom.isTravellerTentVisited( getBarrierColorFromTile( tile ) );
 
         case MP2::OBJ_TRAVELLER_TENT:
-            return !kingdom.IsVisitTravelersTent( getBarrierColorFromTile( tile ) );
+            return !kingdom.isTravellerTentVisited( getBarrierColorFromTile( tile ) );
 
         case MP2::OBJ_SHRINE_FIRST_CIRCLE:
         case MP2::OBJ_SHRINE_SECOND_CIRCLE:
@@ -442,7 +442,7 @@ namespace
                 return false;
             }
 
-            if ( !hero.isVisited( tile, Visit::GLOBAL ) ) {
+            if ( !kingdom.isVisited( index, objectType ) ) {
                 // This shrine has not been visited by any hero. It's worth to do it.
                 return true;
             }
@@ -480,7 +480,7 @@ namespace
                 return false;
             }
 
-            if ( !hero.isVisited( tile, Visit::GLOBAL ) ) {
+            if ( !kingdom.isVisited( index, objectType ) ) {
                 // The AI heroes should not have prior knowledge what skill this object has.
                 return true;
             }
@@ -668,14 +668,14 @@ namespace
         case MP2::OBJ_DERELICT_SHIP:
         case MP2::OBJ_GRAVEYARD:
         case MP2::OBJ_SHIPWRECK:
-            if ( !hero.isVisited( tile, Visit::GLOBAL ) && doesTileContainValuableItems( tile ) ) {
+            if ( !kingdom.isVisited( index, objectType ) && doesTileContainValuableItems( tile ) ) {
                 Army enemy( tile );
                 return enemy.isValid() && isHeroStrongerThan( tile, ai, heroArmyStrength, 2 );
             }
             break;
 
         case MP2::OBJ_PYRAMID:
-            if ( !hero.isVisited( tile, Visit::GLOBAL ) && doesTileContainValuableItems( tile ) ) {
+            if ( !kingdom.isVisited( index, objectType ) && doesTileContainValuableItems( tile ) ) {
                 Army enemy( tile );
                 return enemy.isValid() && Skill::Level::EXPERT == hero.GetLevelSkill( Skill::Secondary::WISDOM )
                        && isHeroStrongerThan( tile, ai, heroArmyStrength, AI::ARMY_ADVANTAGE_LARGE );
@@ -728,7 +728,7 @@ namespace
         case MP2::OBJ_JAIL:
             return kingdom.GetHeroes().size() < Kingdom::GetMaxHeroes();
         case MP2::OBJ_HUT_OF_MAGI:
-            return !hero.isObjectTypeVisited( objectType, Visit::GLOBAL ) && Maps::doesObjectExistOnMap( MP2::OBJ_EYE_OF_MAGI );
+            return !kingdom.isVisited( objectType ) && Maps::doesObjectExistOnMap( MP2::OBJ_EYE_OF_MAGI );
 
         case MP2::OBJ_ALCHEMIST_TOWER: {
             const BagArtifacts & bag = hero.GetBagArtifacts();
@@ -741,24 +741,28 @@ namespace
             return kingdom.AllowPayment( payment );
         }
 
+        // TODO: AI has no brains to handle Sirens object.
+        case MP2::OBJ_SIRENS:
+        // TODO: AI doesn't know how it use Sphinx object properly.
+        case MP2::OBJ_SPHINX:
+        // TODO: AI doesn't know how it use Trading Post object properly.
+        case MP2::OBJ_TRADING_POST:
+            return false;
+
+        // The following objects must never be analyzed.
         // AI should never consider a boat as a destination point. It uses them only to make a path.
         case MP2::OBJ_BOAT:
         // Eye of Magi is not an action object at all.
         case MP2::OBJ_EYE_OF_MAGI:
         // No use of these object for AI.
         case MP2::OBJ_ORACLE:
-        // AI has no brains to do anything from sign messages.
+        // AI doesn't read messages.
         case MP2::OBJ_SIGN:
-        // AI has no brains to handle Sirens object.
-        case MP2::OBJ_SIRENS:
-        // TODO: AI doesn't know how it use Sphinx object properly.
-        case MP2::OBJ_SPHINX:
         // AI should never consider a stone lith as a destination point. It uses them only to make a path.
         case MP2::OBJ_STONE_LITHS:
-        // TODO: AI doesn't know how it use Trading Post object properly.
-        case MP2::OBJ_TRADING_POST:
         // AI should never consider a whirlpool as a destination point. It uses them only to make a path.
         case MP2::OBJ_WHIRLPOOL:
+            assert( 0 );
             return false;
 
         default:
@@ -2901,8 +2905,9 @@ void AI::Planner::HeroesBeginMovement( Heroes & hero )
 
     const int32_t formerBoatIdx = HeroesCastSummonBoat( hero, nextTileIdx );
 
-    updateMapActionObjectCache( formerBoatIdx );
-    updateMapActionObjectCache( nextTileIdx );
+    const auto & kingdom = hero.GetKingdom();
+    updateMapActionObjectCache( kingdom, formerBoatIdx );
+    updateMapActionObjectCache( kingdom, nextTileIdx );
 }
 
 void AI::Planner::HeroesActionComplete( Heroes & hero, const int32_t tileIndex, const MP2::MapObjectType objectType )
@@ -2928,7 +2933,7 @@ void AI::Planner::HeroesActionComplete( Heroes & hero, const int32_t tileIndex, 
 
     updatePriorityTargets( hero, tileIndex, objectType );
 
-    updateMapActionObjectCache( tileIndex );
+    updateMapActionObjectCache( hero.GetKingdom(), tileIndex );
 }
 
 void AI::Planner::HeroesActionNewPosition( Heroes & hero )
@@ -3003,8 +3008,9 @@ void AI::Planner::HeroesActionNewPosition( Heroes & hero )
 
     const int32_t formerBoatIdx = HeroesCastSummonBoat( hero, nextTileIdx );
 
-    updateMapActionObjectCache( formerBoatIdx );
-    updateMapActionObjectCache( nextTileIdx );
+    const auto & kingdom = hero.GetKingdom();
+    updateMapActionObjectCache( kingdom, formerBoatIdx );
+    updateMapActionObjectCache( kingdom, nextTileIdx );
 }
 
 bool AI::Planner::isValidHeroObject( const Heroes & hero, const int32_t index, const bool underHero )
@@ -3163,8 +3169,9 @@ fheroes2::GameMode AI::Planner::HeroesTurn( VecHeroes & heroes, uint32_t & curre
                     // and this results in inserting a new hero position into the action object cache. Perform the necessary updates.
                     assert( bestHero->isActive() && bestHero->GetIndex() != prevHeroPosition );
 
-                    updateMapActionObjectCache( prevHeroPosition );
-                    updateMapActionObjectCache( bestHero->GetIndex() );
+                    const auto & kingdom = bestHero->GetKingdom();
+                    updateMapActionObjectCache( kingdom, prevHeroPosition );
+                    updateMapActionObjectCache( kingdom, bestHero->GetIndex() );
 
                     prevHeroPosition = bestHero->GetIndex();
                 }
@@ -3192,12 +3199,13 @@ fheroes2::GameMode AI::Planner::HeroesTurn( VecHeroes & heroes, uint32_t & curre
         }
 
         if ( !bestHero->isActive() || bestHero->GetIndex() != prevHeroPosition ) {
+            const auto & kingdom = bestHero->GetKingdom();
             // The hero died or moved to another position. We have to update the action object cache.
-            updateMapActionObjectCache( prevHeroPosition );
+            updateMapActionObjectCache( kingdom, prevHeroPosition );
 
             if ( bestHero->isActive() ) {
                 // Hero moved to another position and is still alive.
-                updateMapActionObjectCache( bestHero->GetIndex() );
+                updateMapActionObjectCache( kingdom, bestHero->GetIndex() );
             }
         }
 

src/fheroes2/ai/ai_planner_kingdom.cpp
@@ -717,7 +717,6 @@ fheroes2::GameMode AI::Planner::KingdomTurn( Kingdom & kingdom )
 
     for ( int idx = 0; idx < mapSize; ++idx ) {
         const Maps::Tile & tile = world.getTile( idx );
-        MP2::MapObjectType objectType = tile.getMainObjectType();
 
         const uint32_t regionID = tile.GetRegion();
         if ( regionID >= _regions.size() ) {
@@ -725,12 +724,14 @@ fheroes2::GameMode AI::Planner::KingdomTurn( Kingdom & kingdom )
             continue;
         }
 
-        RegionStats & stats = _regions[regionID];
         if ( !isUnderViewSpell && tile.isFog( myColor ) ) {
             continue;
         }
 
-        if ( !MP2::isInGameActionObject( objectType ) ) {
+        MP2::MapObjectType objectType = tile.getMainObjectType();
+        // Remove useless objects for AI heroes as they bring no value.
+        // It is good to exclude them here to avoid unnecessary calculations.
+        if ( !isValuableAdventureMapObject( kingdom, objectType, idx ) ) {
             continue;
         }
 
@@ -738,6 +739,7 @@ fheroes2::GameMode AI::Planner::KingdomTurn( Kingdom & kingdom )
             assert( 0 );
         }
 
+        RegionStats & stats = _regions[regionID];
         if ( objectType == MP2::OBJ_HERO ) {
             const Heroes * hero = tile.getHero();
             assert( hero != nullptr );
@@ -860,7 +862,7 @@ fheroes2::GameMode AI::Planner::KingdomTurn( Kingdom & kingdom )
         if ( purchaseNewHeroes( sortedCastleList, castlesInDanger, availableHeroCount, moreTaskForHeroes ) ) {
             assert( !heroes.empty() && heroes.back() != nullptr );
 
-            updateMapActionObjectCache( heroes.back()->GetIndex() );
+            updateMapActionObjectCache( heroes.back()->GetKingdom(), heroes.back()->GetIndex() );
 
             ++availableHeroCount;
 

src/fheroes2/dialog/dialog_quickinfo.cpp
@@ -1,6 +1,6 @@
 /***************************************************************************
  *   fheroes2: https://github.com/ihhub/fheroes2                           *
- *   Copyright (C) 2019 - 2025                                             *
+ *   Copyright (C) 2019 - 2026                                             *
  *                                                                         *
  *   Free Heroes2 Engine: http://sourceforge.net/projects/fheroes2         *
  *   Copyright (C) 2009 by Andrey Afletdinov <fheroes2@gmail.com>          *
@@ -374,7 +374,7 @@ namespace
         const int32_t barrierColor = getBarrierColorFromTile( tile );
         StringReplace( str, "%{color}", fheroes2::getBarrierColorName( barrierColor ) );
 
-        if ( kingdom.IsVisitTravelersTent( barrierColor ) ) {
+        if ( kingdom.isTravellerTentVisited( barrierColor ) ) {
             str.append( "\n\n" );
             str.append( _( "(tent visited)" ) );
         }
@@ -388,7 +388,7 @@ namespace
         const int32_t tentColor = getBarrierColorFromTile( tile );
         StringReplace( str, "%{color}", fheroes2::getTentColorName( tentColor ) );
 
-        if ( kingdom.IsVisitTravelersTent( tentColor ) ) {
+        if ( kingdom.isTravellerTentVisited( tentColor ) ) {
             str.append( "\n\n" );
             str.append( _( "(already visited)" ) );
         }

commit 0a71fc901541532b4accb3f895813437e8050be6

Remove assertion as it leads to a crash while loading a map
regression from #10797

src/fheroes2/ai/ai_common.cpp
@@ -461,8 +461,7 @@ bool AI::isValuableAdventureMapObject( const Kingdom & kingdom, const MP2::MapOb
     case MP2::OBJ_HERO: {
         const Heroes * otherHero = world.getTile( tileIndex ).getHero();
         if ( otherHero == nullptr ) {
-            // How is it possible that a hero does not exist?
-            assert( 0 );
+            // It could happen when adding new heroes at the start of game.
             return false;
         }
 