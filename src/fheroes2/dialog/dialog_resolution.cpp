2b67e5b0d15deaa192a725ac042cca9b8217813b
AchFilip <60099220+AchFilip@users.noreply.github.com>
2026-06-10 17:50:47 +0300

    Fix the title alignment in the resolution window (#10803)

--- src/fheroes2/dialog/dialog_resolution.cpp
+++ src/fheroes2/dialog/dialog_resolution.cpp
@@ -256,8 +256,15 @@ namespace
 
         RedrawInfo( selectedResRoi.getPosition(), selectedResolution, display );
 
-        const fheroes2::Text title( _( "Select Game Resolution:" ), fheroes2::FontType::normalYellow() );
-        title.draw( roi.x + ( roi.width - title.width() ) / 2, roi.y + 16, display );
+        fheroes2::Text title( _( "Select Game Resolution:" ), fheroes2::FontType::normalYellow() );
+        if ( title.width() > listRoi.width ) {
+            // Make sure that the title isn't going beyond the dialog's borders.
+            title.fitToOneRow( roi.width - 10 );
+            title.draw( roi.x, roi.y + 16, roi.width, display );
+        }
+        else {
+            title.draw( listRoi.x, roi.y + 16, listRoi.width, display );
+        }
 
         display.render( background.totalArea() );
 
