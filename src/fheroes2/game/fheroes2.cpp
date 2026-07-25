de33838b05d80efb495c0ca646425057d6f9e719
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2026-07-26 00:39:52 +0800

    Code style fix

--- src/fheroes2/game/fheroes2.cpp
+++ src/fheroes2/game/fheroes2.cpp
@@ -87,7 +87,7 @@ int main( int argc, char ** argv )
 
     try {
         auto hardwareComponent = Game::createHardwareComponent();
-        
+
         Game::initLogging();
         Game::initDataDir();
         Game::initConfigDir( argv[0] );
