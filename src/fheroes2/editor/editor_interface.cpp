03604aee5bc8062e70f054c963ca71c7e14a1a8a
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2026-08-30 21:36:24 +0800

    Explicitly state the action that the moved object got moved in the Editor (#10972)

--- src/fheroes2/editor/editor_interface.cpp
+++ src/fheroes2/editor/editor_interface.cpp
@@ -3408,6 +3408,8 @@ namespace Interface
 
         action.commit();
 
+        _warningMessage.reset( _( "The selected object has been moved to the top layer." ) );
+
         // TODO: so far this is the only way to update objects for rendering.
         return Maps::readMapInEditor( _mapFormat );
     }
