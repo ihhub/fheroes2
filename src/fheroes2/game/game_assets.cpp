8543ea69e4811e312dc9a0f7fcfef8a8016e75e1
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2026-08-31 17:20:09 +0800

    Revert the button changes leading to longer buttons in the Main Menu (#10979)

--- src/fheroes2/game/game_assets.cpp
+++ src/fheroes2/game/game_assets.cpp
@@ -1715,7 +1715,6 @@ namespace
             if ( isPoLPresent ) {
                 texts.emplace_back( fheroes2::getSupportedText( gettext_noop( "ORIGINAL\nCAMPAIGN" ), buttonFontType ) );
                 texts.emplace_back( fheroes2::getSupportedText( gettext_noop( "EXPANSION\nCAMPAIGN" ), buttonFontType ) );
-                texts.emplace_back( fheroes2::getSupportedText( gettext_noop( "RESURRECTION\nCAMPAIGN" ), buttonFontType ) );
             }
 
             fheroes2::makeSymmetricBackgroundSprites( _icnVsSprite[id], texts, false, 117 );
