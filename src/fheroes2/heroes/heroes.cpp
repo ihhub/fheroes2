aa29cc5e69c53e1bbf419a7d961832ad975ed4e6
Zenseii <zenseii@users.noreply.github.com>
2026-09-08 13:00:20 +0200

    Fix transparent pixels in empty movement and mana bars (#10995)

--- src/fheroes2/heroes/heroes.cpp
+++ src/fheroes2/heroes/heroes.cpp
@@ -2111,8 +2111,6 @@ void Heroes::PortraitRedraw( const int32_t px, const int32_t py, const PortraitT
         }
         else if ( PORT_SMALL == type ) {
             const fheroes2::Sprite & background = Assets::getImage( ICN::PORTXTRA, 0 );
-            const fheroes2::Sprite & mobility = Assets::getImage( ICN::MOBILITY, GetMobilityIndexSprite() );
-            const fheroes2::Sprite & mana = Assets::getImage( ICN::MANA, getManaIndexSprite() );
 
             const int barw = 7;
 
@@ -2120,13 +2118,23 @@ void Heroes::PortraitRedraw( const int32_t px, const int32_t py, const PortraitT
             fheroes2::Blit( background, dstsf, px, py );
 
             // Draw mobility.
-            fheroes2::Copy( mobility, 0, 0, dstsf, px, py + mobility.y(), mobility.width(), mobility.height() );
+            const uint32_t mobilitySpriteIndex = GetMobilityIndexSprite();
+            // Don't draw this sprite if there are no movement points left.
+            if ( mobilitySpriteIndex > 0 ) {
+                const fheroes2::Sprite & mobility = Assets::getImage( ICN::MOBILITY, mobilitySpriteIndex );
+                fheroes2::Copy( mobility, 0, 0, dstsf, px, py + mobility.y(), mobility.width(), mobility.height() );
+            }
 
             // Draw hero's portrait.
             fheroes2::Copy( port, 0, 0, dstsf, px + barw + 1, py, port.width(), port.height() );
 
             // Draw mana.
-            fheroes2::Copy( mana, 0, 0, dstsf, px + barw + port.width() + 2, py + mana.y(), mana.width(), mana.height() );
+            const uint32_t manaSpriteIndex = getManaIndexSprite();
+            // Don't draw this sprite if there are no spell points left.
+            if ( manaSpriteIndex > 0 ) {
+                const fheroes2::Sprite & mana = Assets::getImage( ICN::MANA, manaSpriteIndex );
+                fheroes2::Copy( mana, 0, 0, dstsf, px + barw + port.width() + 2, py + mana.y(), mana.width(), mana.height() );
+            }
 
             mp.x = 35;
         }
